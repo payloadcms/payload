4798525dbe9b8823a4b2e9e2dbe7376ce77207d2
Elliot DeNolf <denolfe@gmail.com>
2018-04-14 20:39:18 -0600

    Add comments on how Payload is initialized

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 
+// Get Payload class
 const Payload = require('./payload');
+// Initialize class
 const payload = new Payload(app);
 
+// Must add payload views here
 app.set('views', [`${__dirname}/views`, payload.views]);
 
 app.get('/', (req, res) => res.render('index',
