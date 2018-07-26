4eff00d4cf87f021fc0ef9b1db93d9a7618c5cdd
James <james@keen-studio.com>
2018-07-26 17:00:13 -0400

    god knows

--- config/webpack.demo.config.js
+++ config/webpack.demo.config.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseConfig = require('./webpack.base.config');
 const optConfig = require('./webpack.opt.config');
+const path = require('path');
 
 const demoConfig = {
   entry: './demo/client/index.js',
@@ -10,12 +11,17 @@ const demoConfig = {
       template: './demo/client/index.html',
       filename: './index.html'
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      Payload: path.resolve(__dirname, '../src')
+    }
+  }
 };
 
 // Instead of simply merging the base and demo configs,
-// we need to replace the plugisn array entirely
-// as local development will feature a different index.html entry point
-module.exports = merge.strategy({ 
+// we need to replace the plugins array entirely
+// as module demo / development will feature a different index.html entry point
+module.exports = merge.strategy({
   plugins: 'replace'
 })(baseConfig, optConfig, demoConfig);
--- demo/client/index.js
+++ demo/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Sidebar from '../../src/client/components/Sidebar';
+import Sidebar from 'Payload/client/components/Sidebar';
 
 import './testStyles.css';
 
