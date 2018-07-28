3ec2c33718a98c3bf3c35acfeb51cd7f02e83616
James <james@keen-studio.com>
2018-07-28 11:58:07 -0400

    scaffolds folder structure, writes out scss framework

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, withRouter, Route } from 'react-router-dom';
+import Sidebar from 'payload/client/components/layout/Sidebar';
 
 import '../scss/app.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Sidebar />
         <h1>Here's the main h1</h1>
       </div>
     )
--- src/client/components/layout/Sidebar/index.js
+++ src/client/components/layout/Sidebar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Emblem from 'payload/client/components/graphics/Emblem';
 
 import './index.css';
 
 export default () => {
 	return (
 		<aside className="sidebar">
+      <Emblem />
 			Testing 123
 		</aside>
 	)
