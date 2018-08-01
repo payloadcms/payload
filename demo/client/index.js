6e4250138b15f89b3182da4102185f2d7673f1b2
James <james@keen-studio.com>
2018-08-01 12:22:03 -0400

    keeps code that will be commonly edited within demo, moves all else to src

--- demo/client/index.js
+++ demo/client/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Route, Switch, Link } from 'react-router-dom';
+
+import Sidebar from 'payload/client/components/layout/Sidebar';
+import Dashboard from 'payload/client/components/views/Dashboard';
+import Login from 'payload/client/components/views/Login';
+import AdminTemplate from 'payload/client/components/layout/AdminTemplate';
 import App from 'payload/client/components/App';
+
+import collections from '../collections';
+import store from './store';
 
 const Index = () => {
   return (
-    <App />
+    <App store={store}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/forgot" component={ () => { return <h1>Forgot Password</h1>; } } />
+        <Route path="/" render={() => {
+          return (
+            <div className="default-view">
+              <Sidebar />
+              <Route path="/" exact component={Dashboard} />
+              {collections.map((collection) => {
+                const collectionsPath = 'collections';
+
+                return (
+                  <React.Fragment key={collection.slug}>
+                    <Route path={`/${collectionsPath}/${collection.slug}`} exact component={collection.archive} />
+                    <Route path={`/${collectionsPath}/${collection.slug}/:id`} component={collection.edit} />
+                  </React.Fragment>
+                );
+              })}
+            </div>
+          );
+        }} />
+      </Switch>
+      <Link to="/">Dashboard</Link>
+    </App>
   );
 };
 
--- src/client/components/App.js
+++ src/client/components/App.js
@@ -3,21 +3,18 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import MeasureWindow from 'payload/client/components/utilities/MeasureWindow';
 import MeasureScroll from 'payload/client/components/utilities/MeasureScroll';
-import Routes from './Routes';
-import store from 'payload/client/store';
 
 import 'payload/client/scss/app.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+      <Provider store={this.props.store}>
         <Router>
           <React.Fragment>
             <MeasureScroll />
             <MeasureWindow />
-            <Routes />
-            <Link to="/">Dashboard</Link>
+            {this.props.children}
           </React.Fragment>
         </Router>
       </Provider>
