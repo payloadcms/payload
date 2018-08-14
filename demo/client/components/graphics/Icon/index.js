96b43f8b55bc6611f4fe6f57bb3711ffa978e1c6
James <james@keen-studio.com>
2018-08-14 09:50:13 -0400

    adds missing icon component, adjusts gitignore to not hit icon files

---
+++ demo/client/components/graphics/Icon/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { PayloadIcon } from 'payload/components';
+
+export default () => {
+  return (
+    <div className="icon-wrap">
+      <PayloadIcon />
+    </div>
+  );
+};
