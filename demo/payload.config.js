12b640695b1c72e0ab843bdc13a53986db0cf1d9
Dan Ribbens <dan.ribbens@gmail.com>
2018-08-17 17:05:01 -0400

    removes fake config file

--- demo/payload.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  port: 1337,
-  adminURL: '/payload-login',
-  modelsDir: './models',
-  apiDir: './api',
-  adminDir: './client',
-  foo: 'bar',
-  mongoURL: 'mongodb://localhost/payload'
-};
