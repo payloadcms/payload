511f42fe87b3e4908ccecef612f7426ee8e812b8
James <james@keen-studio.com>
2018-09-17 18:47:03 -0400

    negligence caused me to forget to flatten Orders input

--- demo/collections/Orders.js
+++ demo/collections/Orders.js
@@ -1,43 +1,39 @@
 export default {
-  orders: {
-    attrs: {
-      label: 'Orders',
-      singular: 'Order',
-      plural: 'Orders',
+  slug: 'orders',
+  label: 'Orders',
+  singular: 'Order',
+  plural: 'Orders',
+  fields: {
+    metaInfo: {
+      type: 'group',
+      fields: {
+        title: {
+          type: 'string',
+          maxLength: 100
+        },
+        description: { type: 'textarea',
+          wysiwyg: false,
+          height: 100
+        },
+        keywords: { type: 'text' }
+      }
     },
-
-    fields: {
-      metaInfo: {
-        type: 'group',
-        fields: {
-          title: {
-            type: 'string',
-            maxLength: 100
-          },
-          description: { type: 'textarea',
-            wysiwyg: false,
-            height: 100
-          },
-          keywords: { type: 'text' }
-        }
-      },
-      content: {
-        type: 'group',
-        fields: {
-          exampleField1: {
-            type: 'textarea',
-            wysiwyg: true,
-            height: 400
-          },
-          flexibleContentExample: {
-            type: 'flex',
-            availableLayouts: [
-              'layout1',
-              'layout5'
-            ]
-          }
+    content: {
+      type: 'group',
+      fields: {
+        exampleField1: {
+          type: 'textarea',
+          wysiwyg: true,
+          height: 400
+        },
+        flexibleContentExample: {
+          type: 'flex',
+          availableLayouts: [
+            'layout1',
+            'layout5'
+          ]
         }
       }
     }
-  },
+  }
 };
