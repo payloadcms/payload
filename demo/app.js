65f4afd4b687867bff221ed65e68d3962403c6a5
James <james@keen-studio.com>
2018-11-07 19:59:57 -0500

    restores elliot's init

--- demo/app.js
+++ demo/app.js
@@ -1,33 +1,15 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const payload = require('../src');
-const passport = require('passport');
 const User = require('./User/User.model');
 const payloadConfig = require('./payload.config');
 const router = express.Router({}); // eslint-disable-line new-cap
 const app = module.exports = express();
 
-mongoose.connect(payloadConfig.mongoURL, { useNewUrlParser: true }, (err) => {
-  if (err) {
-    console.log('Unable to connect to the Mongo server. Please start the server. Error:', err);
-  } else {
-    console.log('Connected to Mongo server successfully!');
-  }
-});
-
-// configure passport for Auth
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Headers',
-    'Origin X-Requested-With, Content-Type, Accept');
-  next();
+payload.init({
+  config: payloadConfig,
+  app: app,
+  user: User,
+  router: router
 });
 
 const authRoutes = require('./Auth/Auth.routes');
@@ -42,10 +24,6 @@ router.use('/pages', pageRoutes);
 // Not scaffolded, but this is how it works
 // const orderRoutes = require('./Order/Order.routes');
 // router.use('/orders', orderRoutes);
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(router);
 
 app.listen(payloadConfig.port, () => {
   console.log(`listening on ${payloadConfig.port}...`);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = app => {
+const mongoose = require('mongoose');
+const passport = require('passport');
+const express = require('express');
 
-}
+module.exports = {
+  init: options => {
+    mongoose.connect(options.config.mongoURL, { useNewUrlParser: true }, (err) => {
+      if (err) {
+        console.log('Unable to connect to the Mongo server. Please start the server. Error:', err);
+      } else {
+        console.log('Connected to Mongo server successfully!');
+      }
+    });
+
+    // configure passport for Auth
+    options.app.use(passport.initialize());
+    options.app.use(passport.session());
+
+    passport.use(options.user.createStrategy());
+    passport.serializeUser(options.user.serializeUser());
+    passport.deserializeUser(options.user.deserializeUser());
+
+    options.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+      res.header('Access-Control-Allow-Headers',
+        'Origin X-Requested-With, Content-Type, Accept');
+      next();
+    });
+
+    options.app.use(express.json());
+    options.app.use(express.urlencoded({extended: true}));
+    options.app.use(options.router);
+  }
+};
