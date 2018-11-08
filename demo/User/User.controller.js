792733441e344b78bc25c5456434bf9106b4a143
James <james@keen-studio.com>
2018-11-07 20:05:41 -0500

    moves post controller to user package

--- demo/User/User.controller.js
+++ demo/User/User.controller.js
@@ -1,28 +1,8 @@
 const User = require('./User.model');
-const httpStatus = require('http-status');
-const passport = require('passport');
-const APIError = require('../../src/lib/helpers/APIError');
+const user = require('../../src/user')(User);
 
 const userController = {
-  /**
-   * Returns User when succesfully registered
-   * @param req
-   * @param res
-   * @param next
-   * @returns {*}
-   */
-  post: (req, res, next) => {
-    User.register(new User({ email: req.body.email }), req.body.password, (err, user) => {
-      if (err) {
-        const error = new APIError('Authentication error', httpStatus.UNAUTHORIZED);
-        return next(error);
-      }
-      passport.authenticate('local')(req, res, () => {
-        res.json({ user });
-      });
-    });
-  },
+  post: user.post,
 }
 
 module.exports = userController;
-
