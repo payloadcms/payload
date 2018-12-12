46dc80d46dfe18c24b20df7c8d4bfa0e5e3d950e
James <james@keen-studio.com>
2018-12-11 22:49:21 -0500

    simplifies jwt auth

--- src/auth/jwt.js
+++ src/auth/jwt.js
@@ -7,15 +7,13 @@ const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'SECRET_KEY'; //normally store this in process.env.secret
 
-export default User => {
-  return new JwtStrategy(opts, (jwtPayload, done) => {
+export default User => new JwtStrategy(opts, (jwtPayload, done) => {
 
-    // Access to User model
-    console.log(User);
+  // Access to User model
+  console.log(User);
 
-    if (jwtPayload.email === 'james@jamestest.com') {
-        return done(null, true)
-    }
-    return done(null, false)
-  })
-}
+  if (jwtPayload.email === 'james@jamestest.com') {
+      return done(null, true)
+  }
+  return done(null, false)
+})
