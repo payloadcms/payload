90a416f1fa134215a0cb7597d5b24141fd426744
Elliot DeNolf <denolfe@gmail.com>
2019-01-22 20:44:10 -0500

    Fix indentation

--- src/controllers/uploads.controller.js
+++ src/controllers/uploads.controller.js
@@ -3,26 +3,26 @@ import { resize } from '../../src/utils/imageResizer';
 
 
 function upload(req, res, next, config) {
-    if (Object.keys(req.files).length === 0) {
-      return res.status(400).send('No files were uploaded.');
+  if (Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  mkdirp(config.staticDir, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Upload failed.');
     }
+  });
 
-    mkdirp(config.staticDir, (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Upload failed.');
-      }
-    });
+  let outputFilepath = `${config.staticDir}/${req.files.file.name}`;
+  req.files.file.mv(outputFilepath, (err) => {
+    if (err) return res.status(500).send(err);
 
-    let outputFilepath = `${config.staticDir}/${req.files.file.name}`;
-    req.files.file.mv(outputFilepath, (err) => {
-      if (err) return res.status(500).send(err);
-
-      if (req.files.file.mimetype.split('/')[0] === 'image') {
-        resize(config, req.files.file);
-      }
-      res.send('File uploaded.');
-    })
+    if (req.files.file.mimetype.split('/')[0] === 'image') {
+      resize(config, req.files.file);
+    }
+    res.send('File uploaded.');
+  })
 }
 
 export default { upload };
