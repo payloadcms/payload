4f1fdc2820451d4fb150e2bb8e6d9a71e3a821a9
Dan Ribbens <dan.ribbens@gmail.com>
2019-09-23 09:32:51 -0400

    adds enum support for model config

--- src/helpers/mongoose/fieldToSchemaMap.js
+++ src/helpers/mongoose/fieldToSchemaMap.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 
-const formatBaseSchema = ({ localized, unique }) => {
+const formatBaseSchema = field => {
   const schema = {};
 
-  if (localized === true) schema.intl = true;
-  if (unique === true) schema.unique = true;
+  if (field.localized === true) schema.intl = true;
+  if (field.unique === true) schema.unique = true;
+  if (field.default) schema.default = field.default;
 
   return schema;
-}
+};
 
 const fieldToSchemaMap = {
   input: field => {
@@ -16,21 +17,28 @@ const fieldToSchemaMap = {
     return schema;
   },
   textarea: field => {
-    const schema = formatBaseSchema(field);
-    schema.type = String;
-
-    return schema;
+    return {
+      ...formatBaseSchema(field),
+      type: String,
+    };
   },
   relationship: field => {
-    const schema = formatBaseSchema(field);
-    schema.type = mongoose.Schema.Types.ObjectId;
-    schema.autopopulate = true;
-    schema.ref = field.relationTo;
-    return [schema];
+    return [{
+      ...formatBaseSchema(field),
+      type: mongoose.Schema.Types.ObjectId,
+      autopopulate: true,
+      ref: field.relationTo,
+    }];
   },
   repeater: field => {
     return field.fields.map(subField => fieldToSchemaMap[subField.type](subField));
+  },
+  enum: field => {
+    return {...formatBaseSchema(field),
+      type: String,
+      enum: field.enum,
+    };
   }
-}
+};
 
 export default fieldToSchemaMap;
