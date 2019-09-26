41c1d663b0d7ca6f87566460160da15773826345
Dan Ribbens <dan.ribbens@gmail.com>
2019-09-26 16:13:43 -0400

    cleanup user config

--- demo/config/User.js
+++ demo/config/User.js
@@ -1,8 +1,6 @@
 import passportLocalMongoose from 'passport-local-mongoose';
 import payloadConfig from '../payload.config';
-import APIError from '../../src/lib/helpers/APIError';
 import userValidate from '../User/User.validate';
-const httpStatus = require('http-status');
 
 export default {
   slug: 'users',
@@ -31,42 +29,7 @@ export default {
     registration: true,
     registrationValidation: userValidate.post,
   },
-  // TODO: Is having a configurable mongoose plugins array a good idea?
-  // Should the user have insight as to how that will I cannot decide if this is a good pattern or not
   plugins: [{plugin: passportLocalMongoose, options: {usernameField: 'email'}}],
-  statics: {
-  // TODO: I grabbed these from the demo/Users/User.model.js, but I don't see anywhere that these are being used
-    /**
-     * Get user
-     * @param {ObjectId} id - The objectId of user.
-     * @returns {Promise<User, APIError>}
-     */
-    get(id) {
-      return this.findById(id)
-        .exec()
-        .then(user => {
-          if (user) {
-            return user;
-          }
-          const err = new APIError('No such user exists!', httpStatus.NOT_FOUND);
-          return Promise.reject(err);
-        });
-    },
-
-    /**
-     * List users in descending order of 'createdAt' timestamp.
-     * @param {number} skip - Number of users to be skipped.
-     * @param {number} limit - Limit number of users to be returned.
-     * @returns {Promise<User[]>}
-     */
-    list({skip = 0, limit = 50} = {}) {
-      return this.find()
-        .sort({createdAt: -1})
-        .skip(skip)
-        .limit(limit)
-        .exec();
-    },
-  },
   fields: [
     {
       name: 'email',
