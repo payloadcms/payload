50da2f0b90048b7836eb6c551eb8b8838701c919
Dan Ribbens <dan.ribbens@gmail.com>
2019-10-07 10:58:29 -0400

    remove copied fields from page in header config

--- demo/config/Header.js
+++ demo/config/Header.js
@@ -19,82 +19,18 @@ module.exports = {
   },
   fields: [
     {
-      name: 'title',
-      label: 'Page Title',
+      name: 'siteTitle',
+      label: 'Site Title',
       type: 'input',
-      unique: true,
       localized: true,
       maxLength: 100,
       required: true,
     },
     {
-      name: 'content',
-      label: 'Content',
-      type: 'textarea',
-      localized: true,
-      height: 100,
-      required: true
-    },
-    {
-      name: 'categories',
-      label: 'Categories',
-      type: 'relationship',
-      relationType: 'reference',
-      relationTo: 'Category',
-    },
-    {
-      name: 'image',
-      label: 'Image',
+      name: 'logo',
+      label: 'Logo',
       type: 'media',
       required: true
     },
-    {
-      name: 'author',
-      label: 'Written by:',
-      type: 'reference',
-      reference: {
-        to: 'Author',
-        nested: true,
-        relationship: 'oneToMany'
-      }
-    },
-    {
-      name: 'slides',
-      label: 'Slides',
-      type: 'repeater',
-      fields: [
-        {
-          name: 'content',
-          type: 'textarea',
-          label: 'Content'
-        }
-      ]
-    },
-    {
-      label: 'Meta Information',
-      type: 'group',
-      fields: [
-        {
-          name: 'metaTitle',
-          type: 'input',
-          maxLength: 100,
-          label: 'Meta Title',
-          width: 50
-        },
-        {
-          name: 'metaKeywords',
-          type: 'input',
-          maxLength: 100,
-          label: 'Meta Keywords',
-          width: 50
-        },
-        {
-          name: 'metaDesc',
-          type: 'textarea',
-          label: 'Meta Description',
-          height: 100
-        }
-      ]
-    }
   ],
 };
