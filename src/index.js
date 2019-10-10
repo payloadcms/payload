ad6a9437046099844ea4faa77ea6a02cb7836b0b
Dan Ribbens <dan.ribbens@gmail.com>
2019-10-10 12:41:23 -0400

    correctly adds localization to the global schema

--- src/index.js
+++ src/index.js
@@ -181,8 +181,6 @@ class Payload {
         if (fieldSchema) globalFields[config.slug][field.name] = fieldSchema(field);
       });
       globalSchemaGroups[config.slug] = new mongoose.Schema(globalFields[config.slug], { _id : false });
-      globalSchemaGroups[config.slug].plugin(localizationPlugin, options.config.localization);
-      globalSchemaGroups[config.slug].plugin(autopopulate);
   });
 
     if (options.config.globals) {
