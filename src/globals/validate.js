8ce80be2eea4fd3e29e2d953d528bc0a9988dd87
Jarrod Flesch <jarrodmflesch@gmail.com>
2020-02-25 21:44:00 -0500

    adds check for globalConfigs before attempting to loop

--- src/globals/validate.js
+++ src/globals/validate.js
@@ -1,12 +1,14 @@
 const { MissingGlobalLabel, DuplicateGlobalLabel } = require('../errors');
 
 module.exports = function validateGlobal(globalConfigs, Globals) {
-  Object.values(globalConfigs).forEach((config) => {
-    if (!config.label) {
-      throw new MissingGlobalLabel(config);
-    }
-    if (Globals && Globals[config.label]) {
-      throw new DuplicateGlobalLabel(config);
-    }
-  });
+  if (globalConfigs) {
+    Object.values(globalConfigs).forEach((config) => {
+      if (!config.label) {
+        throw new MissingGlobalLabel(config);
+      }
+      if (Globals && Globals[config.label]) {
+        throw new DuplicateGlobalLabel(config);
+      }
+    });
+  }
 };
