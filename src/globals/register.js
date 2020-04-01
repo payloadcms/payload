77d4b2f9244569d2702ce6ee20d50e08af5440b2
James <james@trbl.design>
2020-04-01 16:06:20 -0400

    fixes bug with how globals were registered

--- src/globals/register.js
+++ src/globals/register.js
@@ -2,11 +2,10 @@ const validate = require('./validate');
 const buildSchema = require('./buildSchema');
 const routes = require('./routes');
 
-
 function registerGlobals() {
   validate(this.config.globals);
   this.globals = buildSchema(this.config);
-  this.router.use(routes(this.config.globals, this.globals));
+  this.router.use(routes(this.config.globals, this.globals.model));
 }
 
 module.exports = registerGlobals;
