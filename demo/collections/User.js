7477c637145f5d00b8ae264d07bcf93a1a713f74
James <james@trbl.design>
2020-04-12 18:32:22 -0400

    revises the base fields for User

--- demo/collections/User.js
+++ demo/collections/User.js
@@ -2,42 +2,42 @@ const roles = require('../policies/roles');
 const checkRole = require('../policies/checkRole');
 
 module.exports = {
-	slug: 'users',
-	labels: {
-		singular: 'User',
-		plural: 'Users',
-	},
-	useAsTitle: 'email',
-	policies: {
-		create: user => checkRole(['admin', 'user'], user),
-		read: null,
-		update: user => checkRole(['admin', 'user'], user),
-		destroy: user => checkRole(['admin', 'user'], user),
-	},
-	auth: {
-		passwordIndex: 1,
-		useAsUsername: 'email',
-		tokenExpiration: 7200,
-		secretKey: 'SECRET_KEY',
-	},
-	fields: [
-		{
-			name: 'email',
-			label: 'Email Address',
-			type: 'input',
-			unique: true,
-			maxLength: 100,
-			required: true,
-		},
-		{
-			name: 'role',
-			label: 'Role',
-			type: 'select',
-			options: roles,
-			defaultValue: 'user',
-			required: true,
-			saveToJWT: true,
-		},
-	],
-	timestamps: true,
+  slug: 'users',
+  labels: {
+    singular: 'User',
+    plural: 'Users',
+  },
+  useAsTitle: 'email',
+  policies: {
+    create: user => checkRole(['admin', 'user'], user),
+    read: null,
+    update: user => checkRole(['admin', 'user'], user),
+    destroy: user => checkRole(['admin', 'user'], user),
+  },
+  auth: {
+    passwordIndex: 1,
+    useAsUsername: 'email',
+    tokenExpiration: 7200,
+    secretKey: 'SECRET_KEY',
+  },
+  fields: [
+    {
+      name: 'email',
+      label: 'Email Address',
+      type: 'text',
+      unique: true,
+      maxLength: 100,
+      required: true,
+    },
+    {
+      name: 'role',
+      label: 'Role',
+      type: 'select',
+      options: roles,
+      defaultValue: 'user',
+      required: true,
+      saveToJWT: true,
+    },
+  ],
+  timestamps: true,
 };
