d64909b3eb3fa02b2d886780f66650cc9a6019f5
James <james@trbl.design>
2020-04-14 15:13:58 -0400

    removes settings json and ignores it for future

--- demo/globals/Header.js
+++ demo/globals/Header.js
@@ -3,35 +3,35 @@ const Quote = require('../content-blocks/Quote');
 const CallToAction = require('../content-blocks/CallToAction');
 
 module.exports = {
-	slug: 'header',
-	label: 'Header',
-	policies: {
-		create: user => checkRole(['admin', 'user'], user),
-		read: () => true,
-		update: user => checkRole(['admin', 'user'], user),
-		destroy: user => checkRole(['admin', 'user'], user),
-	},
-	fields: [
-		{
-			name: 'title',
-			label: 'Site Title',
-			type: 'text',
-			localized: true,
-			maxLength: 100,
-			required: true,
-		},
-		{
-			name: 'logo',
-			label: 'Logo',
-			type: 'upload',
-			required: false,
-		},
-		{
-			name: 'flexibleGlobal',
-			label: 'Global Flexible Block',
-			type: 'flexible',
-			blocks: [Quote, CallToAction],
-			localized: true,
-		},
-	],
+  slug: 'header',
+  label: 'Header',
+  policies: {
+    create: user => checkRole(['admin', 'user'], user),
+    read: () => true,
+    update: user => checkRole(['admin', 'user'], user),
+    destroy: user => checkRole(['admin', 'user'], user),
+  },
+  fields: [
+    {
+      name: 'title',
+      label: 'Site Title',
+      type: 'text',
+      localized: true,
+      maxLength: 100,
+      required: true,
+    },
+    {
+      name: 'logo',
+      label: 'Logo',
+      type: 'upload',
+      required: false,
+    },
+    {
+      name: 'flexibleGlobal',
+      label: 'Global Flexible Block',
+      type: 'flexible',
+      blocks: [Quote, CallToAction],
+      localized: true,
+    },
+  ],
 };
