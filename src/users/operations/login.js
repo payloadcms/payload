cd8f396fb34c0f61ef66fc0ffde8fe0432e1238d
James <james@trbl.design>
2020-04-16 12:05:50 -0400

    breaks login into request handler and operation

---
+++ src/users/operations/login.js
@@ -0,0 +1,85 @@
+const jwt = require('jsonwebtoken');
+const { Forbidden } = require('../../errors');
+
+const login = async (args) => {
+  try {
+    // Await validation here
+
+    let options = {
+      Model: args.Model,
+      config: args.config,
+      api: args.api,
+      data: args.data,
+    };
+
+    // /////////////////////////////////////
+    // 1. Execute before login hook
+    // /////////////////////////////////////
+
+    const beforeLoginHook = args.config && args.config.hooks && args.config.hooks.beforeLogin;
+
+    if (typeof beforeLoginHook === 'function') {
+      options = await beforeLoginHook(options);
+    }
+
+    // /////////////////////////////////////
+    // 2. Perform login
+    // /////////////////////////////////////
+
+    const {
+      Model,
+      config,
+      data,
+    } = options;
+
+    const usernameField = config.auth.useAsUsername;
+    const username = data[usernameField];
+    const { password } = data;
+
+    const user = await Model.findByUsername(username);
+
+    if (!user) throw new Forbidden();
+
+    await user.authenticate(password);
+
+    const fieldsToSign = config.fields.reduce((signedFields, field) => {
+      if (field.saveToJWT) {
+        return {
+          ...signedFields,
+          [field.name]: user[field.name],
+        };
+      }
+      return signedFields;
+    }, {
+      [usernameField]: username,
+    });
+
+    const token = jwt.sign(
+      fieldsToSign,
+      config.auth.secretKey,
+      {
+        expiresIn: config.auth.tokenExpiration,
+      },
+    );
+
+    // /////////////////////////////////////
+    // 3. Execute after login hook
+    // /////////////////////////////////////
+
+    const afterLoginHook = args.config && args.config.hooks && args.config.hooks.afterLogin;
+
+    if (typeof afterLoginHook === 'function') {
+      await afterLoginHook(options, token);
+    }
+
+    // /////////////////////////////////////
+    // 4. Return token
+    // /////////////////////////////////////
+
+    return token;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = login;
--- src/users/requestHandlers/login.js
+++ src/users/requestHandlers/login.js
@@ -1,7 +1,6 @@
-const jwt = require('jsonwebtoken');
 const httpStatus = require('http-status');
-const APIError = require('../../errors/APIError');
 const formatErrorResponse = require('../../express/responses/formatError');
+const { login } = require('../operations');
 
 /**
    * Returns passport login response (JWT) when valid username and password is provided
@@ -9,39 +8,23 @@ const formatErrorResponse = require('../../express/responses/formatError');
    * @param res
    * @returns {*}
    */
-const login = (User, config) => (req, res) => {
-  const usernameField = config.user.auth.useAsUsername;
-  const username = req.body[usernameField];
-  const { password } = req.body;
-
-  User.findByUsername(username, (err, user) => {
-    if (err || !user) {
-      return res.status(httpStatus.UNAUTHORIZED).json(formatErrorResponse(err));
-    }
-
-    return user.authenticate(password, (authErr, model, passwordError) => {
-      if (authErr || passwordError) return new APIError('Authentication Failed', httpStatus.UNAUTHORIZED);
-
-      const opts = {};
-      opts.expiresIn = config.user.auth.tokenExpiration;
-      const secret = config.user.auth.secretKey;
-
-      const fieldsToSign = config.user.fields.reduce((acc, field) => {
-        if (field.saveToJWT) acc[field.name] = user[field.name];
-        return acc;
-      }, {
-        [usernameField]: username,
-      });
-
-      const token = jwt.sign(fieldsToSign, secret, opts);
-
-      return res.status(200)
-        .json({
-          message: 'Auth Passed',
-          token,
-        });
+const loginHandler = (User, config) => async (req, res) => {
+  try {
+    const token = await login({
+      Model: User,
+      config: config.user,
+      data: req.body,
+      api: 'REST',
     });
-  });
+
+    return res.status(200)
+      .json({
+        message: 'Auth Passed',
+        token,
+      });
+  } catch (error) {
+    return res.status(error.status || httpStatus.INTERNAL_SERVER_ERROR).json(formatErrorResponse(error));
+  }
 };
 
-module.exports = login;
+module.exports = loginHandler;
