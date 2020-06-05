6c1b77dd48f6daf15a8efb0b4313e2daef9d65b5
James <james@trbl.design>
2020-06-05 17:52:18 -0400

    allows Select field to locate its own full options

--- src/client/components/elements/WhereBuilder/Condition/index.js
+++ src/client/components/elements/WhereBuilder/Condition/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import RenderCustomComponent from '../../../utilities/RenderCustomComponent';
 import ReactSelect from '../../ReactSelect';
