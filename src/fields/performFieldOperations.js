198e62cce0f75ad4d1ae07114f406b878469b8f1
James <james@trbl.design>
2020-06-28 11:28:42 -0400

    adds WIP field sanitization comment

--- src/fields/performFieldOperations.js
+++ src/fields/performFieldOperations.js
@@ -56,6 +56,8 @@ module.exports = async (config, operation) => {
         if (data[field.name] === '') dataCopy[field.name] = null;
       }
 
+      // TODO: sanitize additional field types as necessary i.e. relationships
+
       if (field.type === 'checkbox') {
         if (data[field.name] === 'true') dataCopy[field.name] = true;
         if (data[field.name] === 'false') dataCopy[field.name] = false;
