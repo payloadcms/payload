16462204fa171bffee593b6db52ec6d1b6381b25
James <james@trbl.design>
2020-07-06 16:38:06 -0400

    swaps in UploadGallery in place of Table for collections that have uploads present

--- src/client/components/views/collections/List/Default.js
+++ src/client/components/views/collections/List/Default.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import queryString from 'qs';
 import PropTypes from 'prop-types';
 import config from 'payload/config';
 import usePayloadAPI from '../../../../hooks/usePayloadAPI';
+import UploadGallery from '../../../elements/UploadGallery';
 import Eyebrow from '../../../elements/Eyebrow';
 import Paginator from '../../../elements/Paginator';
 import ListControls from '../../../elements/ListControls';
@@ -23,6 +24,7 @@ const DefaultList = (props) => {
   const {
     collection,
     collection: {
+      upload,
       fields,
       slug,
       labels: {
@@ -32,6 +34,7 @@ const DefaultList = (props) => {
     },
   } = props;
 
+  const history = useHistory();
   const location = useLocation();
   const [listControls, setListControls] = useState({});
   const [sort, setSort] = useState(null);
@@ -66,46 +69,58 @@ const DefaultList = (props) => {
         <ListControls
           handleChange={setListControls}
           collection={collection}
+          disableColumns={Boolean(upload)}
         />
         {(data.docs && data.docs.length > 0) && (
-          <Table
-            data={data.docs}
-            columns={listControls.columns.reduce((cols, col, colIndex) => {
-              const field = fields.find(fieldToCheck => fieldToCheck.name === col);
+          <>
+            {!upload && (
+              <Table
+                data={data.docs}
+                columns={listControls.columns.reduce((cols, col, colIndex) => {
+                  const field = fields.find(fieldToCheck => fieldToCheck.name === col);
 
-              if (field) {
-                return [
-                  ...cols,
-                  {
-                    accessor: field.name,
-                    components: {
-                      Heading: (
-                        <SortColumn
-                          label={field.label}
-                          name={field.name}
-                          handleChange={setSort}
-                          disable={field.disableSort || undefined}
-                        />
-                      ),
-                      renderCell: (rowData, cellData) => {
-                        return (
-                          <Cell
-                            field={field}
-                            colIndex={colIndex}
-                            collection={collection}
-                            rowData={rowData}
-                            cellData={cellData}
-                          />
-                        );
+                  if (field) {
+                    return [
+                      ...cols,
+                      {
+                        accessor: field.name,
+                        components: {
+                          Heading: (
+                            <SortColumn
+                              label={field.label}
+                              name={field.name}
+                              handleChange={setSort}
+                              disable={field.disableSort || undefined}
+                            />
+                          ),
+                          renderCell: (rowData, cellData) => {
+                            return (
+                              <Cell
+                                field={field}
+                                colIndex={colIndex}
+                                collection={collection}
+                                rowData={rowData}
+                                cellData={cellData}
+                              />
+                            );
+                          },
+                        },
                       },
-                    },
-                  },
-                ];
-              }
+                    ];
+                  }
 
-              return cols;
-            }, [])}
-          />
+                  return cols;
+                }, [])}
+              />
+            )}
+            {upload && (
+              <UploadGallery
+                docs={data.docs}
+                collection={collection}
+                onCardClick={doc => history.push(`${admin}/collections/${slug}/${doc.id}`)}
+              />
+            )}
+          </>
         )}
         {data.docs && data.docs.length === 0 && (
           <div className={`${baseClass}__no-results`}>
@@ -160,6 +175,7 @@ const DefaultList = (props) => {
 
 DefaultList.propTypes = {
   collection: PropTypes.shape({
+    upload: PropTypes.shape({}),
     labels: PropTypes.shape({
       singular: PropTypes.string,
       plural: PropTypes.string,
