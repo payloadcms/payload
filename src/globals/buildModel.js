ae35bd7266b3613492eba5e7ac56c020bf497797
James <james@trbl.design>
2020-07-06 23:10:50 -0400

    fixes broken global virtuals

--- src/globals/buildModel.js
+++ src/globals/buildModel.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 const autopopulate = require('mongoose-autopopulate');
-const mongooseHidden = require('mongoose-hidden')({
-  hidden: {
-    _id: true, __v: true,
-  },
-  applyRecursively: true,
-});
+const mongooseHidden = require('mongoose-hidden');
 const buildSchema = require('../mongoose/buildSchema');
 const localizationPlugin = require('../localization/plugin');
 
@@ -14,12 +9,18 @@ const buildModel = (config) => {
     const globalsSchema = new mongoose.Schema({}, { discriminatorKey: 'globalType', timestamps: false })
       .plugin(localizationPlugin, config.localization)
       .plugin(autopopulate)
-      .plugin(mongooseHidden);
+      .plugin(mongooseHidden());
 
     const Globals = mongoose.model('globals', globalsSchema);
 
     Object.values(config.globals).forEach((globalConfig) => {
       const globalSchema = buildSchema(globalConfig.fields);
+
+      globalSchema
+        .plugin(localizationPlugin, config.localization)
+        .plugin(autopopulate)
+        .plugin(mongooseHidden());
+
       Globals.discriminator(globalConfig.slug, globalSchema);
     });
 
