b6a90587e6e6bb98035b7200d0c890916847cad9
James <james@trbl.design>
2020-07-18 13:37:41 -0400

    exposes errors, fixes bug with globals and localization

--- src/globals/buildModel.js
+++ src/globals/buildModel.js
@@ -6,20 +6,26 @@ const localizationPlugin = require('../localization/plugin');
 
 const buildModel = (config) => {
   if (config.globals && config.globals.length > 0) {
-    const globalsSchema = new mongoose.Schema({}, { discriminatorKey: 'globalType', timestamps: true })
-      .plugin(localizationPlugin, config.localization)
-      .plugin(autopopulate)
-      .plugin(mongooseHidden);
+    const globalsSchema = new mongoose.Schema({}, { discriminatorKey: 'globalType', timestamps: true });
+
+    if (config.localization) {
+      globalsSchema.plugin(localizationPlugin, config.localization);
+    }
+
+    globalsSchema.plugin(autopopulate);
+    globalsSchema.plugin(mongooseHidden);
 
     const Globals = mongoose.model('globals', globalsSchema);
 
     Object.values(config.globals).forEach((globalConfig) => {
       const globalSchema = buildSchema(globalConfig.fields);
 
-      globalSchema
-        .plugin(localizationPlugin, config.localization)
-        .plugin(autopopulate)
-        .plugin(mongooseHidden);
+      if (config.localization) {
+        globalSchema.plugin(localizationPlugin, config.localization);
+      }
+
+      globalSchema.plugin(autopopulate);
+      globalSchema.plugin(mongooseHidden);
 
       Globals.discriminator(globalConfig.slug, globalSchema);
     });
