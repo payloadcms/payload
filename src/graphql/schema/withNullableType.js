dd5acae76febbe2f3be7440dff569cf24491b59b
James <james@trbl.design>
2020-09-11 14:11:23 -0400

    only sets nullable gql type if no admin condition

--- src/graphql/schema/withNullableType.js
+++ src/graphql/schema/withNullableType.js
@@ -2,8 +2,9 @@ const { GraphQLNonNull } = require('graphql');
 
 const withNullableType = (field, type) => {
   const hasReadAccessControl = field.access && field.access.read;
+  const condition = field.admin && field.admin.condition;
 
-  if (field.required && !field.localized && !hasReadAccessControl) {
+  if (field.required && !field.localized && !condition && !hasReadAccessControl) {
     return new GraphQLNonNull(type);
   }
 
