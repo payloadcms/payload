b011163d90f4addd3a0c5905315c1c2639ea213d
Elliot DeNolf <denolfe@gmail.com>
2020-09-28 00:46:54 -0400

    use singular label from block items

--- src/client/components/views/collections/List/Cell/types/Blocks/index.js
+++ src/client/components/views/collections/List/Cell/types/Blocks/index.js
@@ -3,17 +3,21 @@ import PropTypes from 'prop-types';
 
 const BlocksCell = ({ data, field }) => {
   const selectedBlocks = data ? data.map(({ blockType }) => blockType) : [];
+  const blockLabels = field.blocks.map((s) => ({ slug: s.slug, label: s.labels.singular }));
 
   let label = `0 ${field.label}.`;
 
-  const formatBlocks = (blocks) => blocks.map((b) => b.charAt(0).toUpperCase() + b.slice(1)).join(', ');
+  const formatBlockList = (blocks) => blocks.map((b) => {
+    const filtered = blockLabels.filter((f) => f.slug === b)?.[0];
+    return filtered?.label;
+  }).join(', ');
 
   const itemsToShow = 5;
   if (selectedBlocks.length > itemsToShow) {
     const more = selectedBlocks.length - itemsToShow;
-    label = `${selectedBlocks.length} ${field.label} - ${formatBlocks(selectedBlocks.slice(0, itemsToShow))} and ${more} more`;
+    label = `${selectedBlocks.length} ${field.label} - ${formatBlockList(selectedBlocks.slice(0, itemsToShow))} and ${more} more`;
   } else if (selectedBlocks.length > 0) {
-    label = `${selectedBlocks.length} ${field.label} - ${formatBlocks(selectedBlocks)}`;
+    label = `${selectedBlocks.length} ${field.label} - ${formatBlockList(selectedBlocks)}`;
   }
 
   return (
@@ -32,6 +36,9 @@ BlocksCell.propTypes = {
   field: PropTypes.shape({
     singularLabel: PropTypes.string,
     label: PropTypes.string,
+    blocks: PropTypes.arrayOf(
+      PropTypes.shape({}),
+    ),
   }).isRequired,
 };
 
