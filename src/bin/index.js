79284941db61868e7a25ae079c0d3bb2cecc452a
James <james@trbl.design>
2020-10-04 20:42:36 -0400

    fixes a grave mistake

--- src/bin/index.js
+++ src/bin/index.js
@@ -4,7 +4,7 @@ const args = require('minimist')(process.argv.slice(2));
 const build = require('./build');
 
 const scriptIndex = args._.findIndex(
-  (x) => x === 'build' || x === 'test',
+  (x) => x === 'build',
 );
 
 const script = scriptIndex === -1 ? args._[0] : args._[scriptIndex];
@@ -15,10 +15,6 @@ switch (script) {
     break;
   }
 
-  case 'test': {
-    console.log('testing');
-    break;
-  }
   default:
     console.log(`Unknown script "${script}".`);
     break;
--- src/fields/richText/defaultValue.js
+++ src/fields/richText/defaultValue.js
@@ -1,3 +1,3 @@
-export default [{
+module.exports = [{
   children: [{ text: '' }],
 }];
