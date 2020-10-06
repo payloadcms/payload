fb73e772af674e3c4e0205414b1a8f07a1fbc15c
James <james@trbl.design>
2020-10-06 09:11:09 -0400

    implements a named populate function to memoize

--- src/fields/performFieldOperations.js
+++ src/fields/performFieldOperations.js
@@ -50,7 +50,7 @@ async function performFieldOperations(entityConfig, args) {
   // Note - this function is recreated each time a request comes in
   // ////////////////////////////////////////////////////////////////
 
-  const populate = memoize(async (
+  const nonMemoizedPopulate = async (
     data,
     dataReference,
     field,
@@ -99,7 +99,9 @@ async function performFieldOperations(entityConfig, args) {
         }
       }
     }
-  }, {
+  };
+
+  const populate = memoize(nonMemoizedPopulate, {
     serializer: (populateArgs) => JSON.stringify(populateArgs[0]),
   });
 
