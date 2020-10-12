02279a769da99ab8c0ab6b0a111c2c8b4b3d6583
Elliot DeNolf <denolfe@gmail.com>
2020-10-12 16:56:49 -0400

    test: upload with same name

--- src/collections/requestHandlers/collections.spec.js
+++ src/collections/requestHandlers/collections.spec.js
@@ -593,54 +593,120 @@ describe('Collections - REST', () => {
       });
     });
 
-    it('create', async () => {
-      const formData = new FormData();
-      formData.append('file', fs.createReadStream(path.join(__dirname, '../../..', 'tests/api/assets/image.png')));
-      formData.append('alt', 'test media');
-      formData.append('locale', 'en');
+    describe('create', () => {
+      it('creates', async () => {
+        const formData = new FormData();
+        formData.append('file', fs.createReadStream(path.join(__dirname, '../../..', 'tests/api/assets/image.png')));
+        formData.append('alt', 'test media');
+        formData.append('locale', 'en');
 
-      const response = await fetch(`${url}/api/media`, {
-        body: formData,
-        headers: {
-          Authorization: `JWT ${token}`,
-        },
-        method: 'post',
-      });
+        const response = await fetch(`${url}/api/media`, {
+          body: formData,
+          headers: {
+            Authorization: `JWT ${token}`,
+          },
+          method: 'post',
+        });
 
-      const data = await response.json();
+        const data = await response.json();
 
-      expect(response.status).toBe(201);
+        expect(response.status).toBe(201);
 
-      // Check for files
-      expect(await fileExists(path.join(mediaDir, 'image.png'))).toBe(true);
-      expect(await fileExists(path.join(mediaDir, 'image-16x16.png'))).toBe(true);
-      expect(await fileExists(path.join(mediaDir, 'image-320x240.png'))).toBe(true);
-      expect(await fileExists(path.join(mediaDir, 'image-640x480.png'))).toBe(true);
+        // Check for files
+        expect(await fileExists(path.join(mediaDir, 'image.png'))).toBe(true);
+        expect(await fileExists(path.join(mediaDir, 'image-16x16.png'))).toBe(true);
+        expect(await fileExists(path.join(mediaDir, 'image-320x240.png'))).toBe(true);
+        expect(await fileExists(path.join(mediaDir, 'image-640x480.png'))).toBe(true);
 
-      // Check api response
-      expect(data).toMatchObject({
-        doc: {
-          alt: 'test media',
-          filename: 'image.png',
-          mimeType: 'image/png',
-          sizes: {
-            icon: {
-              filename: 'image-16x16.png',
-              width: 16,
-              height: 16,
-            },
-            mobile: {
-              filename: 'image-320x240.png',
-              width: 320,
-              height: 240,
-            },
-            tablet: {
-              filename: 'image-640x480.png',
-              width: 640,
-              height: 480,
+        // Check api response
+        expect(data).toMatchObject({
+          doc: {
+            alt: 'test media',
+            filename: 'image.png',
+            mimeType: 'image/png',
+            sizes: {
+              icon: {
+                filename: 'image-16x16.png',
+                width: 16,
+                height: 16,
+              },
+              mobile: {
+                filename: 'image-320x240.png',
+                width: 320,
+                height: 240,
+              },
+              tablet: {
+                filename: 'image-640x480.png',
+                width: 640,
+                height: 480,
+              },
             },
           },
-        },
+        });
+      });
+
+      it('creates with same name', async () => {
+        const formData = new FormData();
+        formData.append('file', fs.createReadStream(path.join(__dirname, '../../..', 'tests/api/assets/samename.png')));
+        formData.append('alt', 'test media');
+        formData.append('locale', 'en');
+
+        const firstResponse = await fetch(`${url}/api/media`, {
+          body: formData,
+          headers: {
+            Authorization: `JWT ${token}`,
+          },
+          method: 'post',
+        });
+
+        expect(firstResponse.status).toBe(201);
+
+        const sameForm = new FormData();
+        sameForm.append('file', fs.createReadStream(path.join(__dirname, '../../..', 'tests/api/assets/samename.png')));
+        sameForm.append('alt', 'test media');
+        sameForm.append('locale', 'en');
+
+        const response = await fetch(`${url}/api/media`, {
+          body: sameForm,
+          headers: {
+            Authorization: `JWT ${token}`,
+          },
+          method: 'post',
+        });
+
+        expect(response.status).toBe(201);
+        const data = await response.json();
+
+        // Check for files
+        expect(await fileExists(path.join(mediaDir, 'samename-1.png'))).toBe(true);
+        expect(await fileExists(path.join(mediaDir, 'samename-1-16x16.png'))).toBe(true);
+        expect(await fileExists(path.join(mediaDir, 'samename-1-320x240.png'))).toBe(true);
+        expect(await fileExists(path.join(mediaDir, 'samename-1-640x480.png'))).toBe(true);
+
+        expect(data).toMatchObject({
+          doc: {
+            alt: 'test media',
+            filename: 'samename-1.png',
+            mimeType: 'image/png',
+            sizes: {
+              icon: {
+                filename: 'samename-1-16x16.png',
+                width: 16,
+                height: 16,
+              },
+              mobile: {
+                filename: 'samename-1-320x240.png',
+                width: 320,
+                height: 240,
+              },
+              tablet: {
+                filename: 'samename-1-640x480.png',
+                width: 640,
+                height: 480,
+              },
+            },
+          },
+        });
       });
     });
 
