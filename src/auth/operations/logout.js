354d1527e1aebf05f9a7c21fc9149573143d187a
Elliot DeNolf <denolfe@gmail.com>
2020-10-26 20:41:23 -0400

    fix: only allow /logout on current user's collection

--- src/auth/operations/logout.js
+++ src/auth/operations/logout.js
@@ -1,5 +1,12 @@
+const httpStatus = require('http-status');
+const { APIError } = require('../../errors');
+
 async function logout(args) {
   const { config } = this;
+
+  const requestedSlug = args.req.route.path.split('/').filter((r) => r !== '')[0];
+  if (!args.req.user) throw new APIError('No User', httpStatus.BAD_REQUEST);
+  if (args.req.user.collection !== requestedSlug) throw new APIError('Incorrect collection', httpStatus.FORBIDDEN);
 
   const {
     res,
