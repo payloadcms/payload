894a400b86f368b066cba9d49f1adc4024f7ff17
James <james@trbl.design>
2020-11-21 10:05:10 -0500

    client tests pass

--- jest.react.config.js
+++ jest.react.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testRegex: '(/src/admin/.*\\.(test|spec))\\.[jt]sx?$',
   setupFilesAfterEnv: ['<rootDir>/tests/client/globalSetup.js'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/mocks/fileMock.ts',
-    '\\.(css|scss)$': '<rootDir>/src/mocks/emptyModule.ts',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/mocks/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/tests/mocks/emptyModule.js',
   },
 };
