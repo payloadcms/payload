55b44b41bf4781081102e6e7dc4dad141ec8a5b8
Elliot DeNolf <denolfe@gmail.com>
2020-12-27 21:25:14 -0500

    feat: add help message and README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Create Payload App
+
+CLI for easily starting new Payload project
+
+## Usage
+
+```text
+
+  USAGE
+
+      $ yarn create payload-app --name my-payload-app
+      $ npx create-payload-app --name my-payload-app
+
+  OPTIONS
+
+      --template template-name           Choose specific template
+      --help                             Shows this help message
+```
--- cli.js
+++ cli.js
@@ -1,16 +1,35 @@
 const prompts = require('prompts');
 const arg = require('arg');
+const chalk = require('chalk');
+const { getArgs } = require('./utils/getArgs');
 const { getTemplate } = require('./utils/getTemplate');
 const { getProjectName } = require('./utils/getProjectName');
 const { createProject } = require('./utils/createProject');
 
 (async () => {
-  PROJECT_NAME = await getProjectName();
-  TEMPLATE = await getTemplate();
+  const args = getArgs();
+  if (args['--help'] || args.count === 0) {
+    const helpMessage = chalk`
+  {bold USAGE}
+
+      {dim $} {bold yarn create payload-app} --name {underline my-payload-app}
+      {dim $} {bold npx create-payload-app} --name {underline my-payload-app}
+
+  {bold OPTIONS}
+
+      --template {underline template-name}           Choose specific template
+      --help                             Shows this help message
+`;
+    console.log(helpMessage);
+    return 0;
+  }
+  if (!args['--name']) throw new Error('--name is a required argument');
+
+  console.log('args :>> ', args);
+  await getProjectName();
+  await getTemplate();
   await createProject();
 
-  // console.log('values :>> ', {
-  //   projectName: PROJECT_NAME,
-  //   template: TEMPLATE,
-  // });
+  // TODO: Print some sort of success message with links to docs
+
 })();
