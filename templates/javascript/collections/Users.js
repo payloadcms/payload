8066ce6f49dec047d413f601a7e525627e346214
Elliot DeNolf <denolfe@gmail.com>
2021-01-16 10:16:56 -0500

    feat: add auth and adjust access on Users

--- templates/javascript/collections/Users.js
+++ templates/javascript/collections/Users.js
@@ -1,7 +1,14 @@
 const Users = {
   slug: 'users',
+  auth: true,
   admin: {
     useAsTitle: 'email',
+  },
+  access: {
+    create: () => true,
+    // read: () => true,  // Only admin panel access until modified
+    // update: () => true, // Only admin panel access until modified
+    // delete: () => true, // Only admin panel access until modified
   },
   fields: [
     // Email added by default
--- templates/typescript/src/collections/Users.ts
+++ templates/typescript/src/collections/Users.ts
@@ -2,8 +2,16 @@ import { CollectionConfig } from 'payload/types';
 
 const Users: CollectionConfig = {
   slug: 'users',
+  auth: true,
   admin: {
     useAsTitle: 'email',
+  },
+  access: {
+    create: () => true,
+    // read: () => true,  // Only admin panel access until modified
+    // update: () => true, // Only admin panel access until modified
+    // delete: () => true, // Only admin panel access until modified
+
   },
   fields: [
     // Email added by default
