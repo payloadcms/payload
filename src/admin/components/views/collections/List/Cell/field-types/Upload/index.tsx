776b9c9c30b6d9d795c509a558fd1eee666b2652
Elliot DeNolf <denolfe@gmail.com>
2021-04-13 13:02:55 -0400

    feat: remove mimetype from upload cell type

--- src/admin/components/views/collections/List/Cell/field-types/Upload/index.tsx
+++ src/admin/components/views/collections/List/Cell/field-types/Upload/index.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import './index.scss';
-
-const baseClass = 'upload-cell';
-
 const UploadCell = ({ data }) => (
   <React.Fragment>
     <span>
       {data?.filename}
-    </span>
-    <span className={`${baseClass}--mime-type`}>
-      {' '}
-      {data?.mimeType}
     </span>
   </React.Fragment>
 );
