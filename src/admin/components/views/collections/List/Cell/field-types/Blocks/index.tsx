81aa4105d1546fe707655d8bdd6b4b3e41d3ddad
Elliot DeNolf <denolfe@gmail.com>
2021-04-16 20:33:02 -0400

    feat: handle block cell type labeling pluralization

--- src/admin/components/views/collections/List/Cell/field-types/Blocks/index.tsx
+++ src/admin/components/views/collections/List/Cell/field-types/Blocks/index.tsx
@@ -4,7 +4,7 @@ const BlocksCell = ({ data, field }) => {
   const selectedBlocks = data ? data.map(({ blockType }) => blockType) : [];
   const blockLabels = field.blocks.map((s) => ({ slug: s.slug, label: s.labels.singular }));
 
-  let label = `0 ${field.label}`;
+  let label = `0 ${field.labels.plural}`;
 
   const formatBlockList = (blocks) => blocks.map((b) => {
     const filtered = blockLabels.filter((f) => f.slug === b)?.[0];
@@ -14,9 +14,9 @@ const BlocksCell = ({ data, field }) => {
   const itemsToShow = 5;
   if (selectedBlocks.length > itemsToShow) {
     const more = selectedBlocks.length - itemsToShow;
-    label = `${selectedBlocks.length} ${field.label} - ${formatBlockList(selectedBlocks.slice(0, itemsToShow))} and ${more} more`;
+    label = `${selectedBlocks.length} ${field.labels.plural} - ${formatBlockList(selectedBlocks.slice(0, itemsToShow))} and ${more} more`;
   } else if (selectedBlocks.length > 0) {
-    label = `${selectedBlocks.length} ${field.label} - ${formatBlockList(selectedBlocks)}`;
+    label = `${selectedBlocks.length} ${selectedBlocks.length === 1 ? field.labels.singular : field.labels.plural} - ${formatBlockList(selectedBlocks)}`;
   }
 
   return (
