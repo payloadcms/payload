99704707dda25f8617d26552942915aa6e9d7a57
James <james@trbl.design>
2021-04-19 10:23:07 -0400

    feat: sets enableRichTextRelationship to true by default

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -17,6 +17,7 @@ export const defaults = {
     components: {},
     css: path.resolve(__dirname, '../admin/scss/custom.css'),
     scss: path.resolve(__dirname, '../admin/scss/overrides.scss'),
+    enableRichTextRelationship: true,
   },
   upload: {},
   graphQL: {
