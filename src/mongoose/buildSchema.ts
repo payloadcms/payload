11874134caeba894a90100580f00e351b91493fb
James <james@trbl.design>
2021-06-14 21:59:12 -0400

    fix: ensures IDs are not added to array fields until they are necessary

--- src/mongoose/buildSchema.ts
+++ src/mongoose/buildSchema.ts
@@ -360,7 +360,7 @@ const fieldToSchemaMap = {
   array: (field: ArrayField, fields: SchemaDefinition, config: Config) => {
     const baseSchema = {
       ...formatBaseSchema(field),
-      type: [buildSchema(config, field.fields)],
+      type: [buildSchema(config, field.fields, { _id: false })],
     };
 
     let schemaToReturn;
