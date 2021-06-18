8e136a5c13ee77de118ca07ac72f17c1f26e95a0
Dan Ribbens <dan.ribbens@gmail.com>
2021-06-18 17:05:27 -0400

    fix: add id to arrays

--- src/graphql/schema/buildBlockType.ts
+++ src/graphql/schema/buildBlockType.ts
@@ -16,7 +16,7 @@ function buildBlockType(block: Block): void {
       [
         ...block.fields,
         {
-          name: '_id',
+          name: 'id',
           type: 'text',
         },
         {
