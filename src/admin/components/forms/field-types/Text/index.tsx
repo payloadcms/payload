c649362b95f1ddaeb47cb121b814ca30712dea86
James <james@trbl.design>
2021-07-27 18:29:46 -0400

    fix: ensures text component is always controlled

--- src/admin/components/forms/field-types/Text/index.tsx
+++ src/admin/components/forms/field-types/Text/index.tsx
@@ -65,7 +65,7 @@ const Text: React.FC<Props> = (props) => {
         required={required}
       />
       <input
-        value={value}
+        value={value || ''}
         onChange={setValue}
         disabled={readOnly}
         placeholder={placeholder}
