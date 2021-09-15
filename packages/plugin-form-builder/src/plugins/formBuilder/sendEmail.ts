3fdf23a1a0287673a190cfd3c7bc55d64cff37ba
James <james@trbl.design>
2021-09-15 17:47:12 -0400

    fix: misc bugs with data safety

--- packages/plugin-form-builder/src/plugins/formBuilder/sendEmail.ts
+++ packages/plugin-form-builder/src/plugins/formBuilder/sendEmail.ts
@@ -2,10 +2,14 @@ import { CollectionBeforeChangeHook } from 'payload/types';
 import { SanitizedOptions } from './types';
 
 const sendEmail = (options: SanitizedOptions): CollectionBeforeChangeHook => async ({ data, req: { payload } }) => {
-  const form = await payload.findByID({
-    id: data.id,
-    collection: 'formSubmissions',
-  });
+  try {
+    const form = await payload.findByID({
+      id: data.id,
+      collection: options?.formsOverrides?.slug || 'formSubmissions',
+    });
+  } catch (err) {
+    return data;
+  }
 
   return data;
 };
