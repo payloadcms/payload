40ca3dae61f8ddf05363b6cad426deba4cde1e30
Jacob Fletcher <jacobsfletch@gmail.com>
2021-11-24 12:32:44 -0500

    feat: migrates admin preview to async

--- src/config/types.ts
+++ src/config/types.ts
@@ -25,7 +25,7 @@ type GeneratePreviewURLOptions = {
   token: string
 }
 
-export type GeneratePreviewURL = (doc: Record<string, unknown>, options: GeneratePreviewURLOptions) => string
+export type GeneratePreviewURL = (doc: Record<string, unknown>, options: GeneratePreviewURLOptions) => Promise<string>
 
 export type EmailTransport = Email & {
   transport: Transporter;
