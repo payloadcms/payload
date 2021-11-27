974fdd0bfd78af4de3cb8603625fe4611489e13c
James <james@trbl.design>
2021-11-27 12:24:56 -0500

    chore: tests revisions REST API

--- src/revisions/tests/spec.ts
+++ src/revisions/tests/spec.ts
@@ -8,6 +8,7 @@ const { serverURL: url } = getConfig();
 let token = null;
 let headers = null;
 let postID;
+let revisionID;
 
 describe('Revisions - REST', () => {
   beforeAll(async (done) => {
@@ -59,6 +60,63 @@ describe('Revisions - REST', () => {
       }).then((res) => res.json());
 
       expect(updatedPost.doc.title).toBe(title2);
+
+      const revisions = await fetch(`${url}/api/localized-posts/revisions`, {
+        headers,
+      }).then((res) => res.json());
+
+      expect(revisions.docs).toHaveLength(1);
+
+      revisionID = revisions.docs[0].id;
+    });
+
+    it('should allow a revision to be retrieved by ID', async () => {
+      const revision = await fetch(`${url}/api/localized-posts/revisions/${revisionID}`, {
+        headers,
+      }).then((res) => res.json());
+
+      expect(revision.id).toStrictEqual(revisionID);
+    });
+
+    it('should allow a revision to save locales properly', async () => {
+      const englishTitle = 'Title in ES';
+      const spanishTitle = 'Title in ES';
+
+      await fetch(`${url}/api/localized-posts/${postID}`, {
+        body: JSON.stringify({
+          title: englishTitle,
+        }),
+        headers,
+        method: 'put',
+      }).then((res) => res.json());
+
+      const updatedPostES = await fetch(`${url}/api/localized-posts/${postID}?locale=es`, {
+        body: JSON.stringify({
+          title: spanishTitle,
+        }),
+        headers,
+        method: 'put',
+      }).then((res) => res.json());
+
+      expect(updatedPostES.doc.title).toBe(spanishTitle);
+
+      const newEnglishTitle = 'New title in EN';
+
+      await fetch(`${url}/api/localized-posts/${postID}`, {
+        body: JSON.stringify({
+          title: newEnglishTitle,
+        }),
+        headers,
+        method: 'put',
+      }).then((res) => res.json());
+
+      const revisions = await fetch(`${url}/api/localized-posts/revisions?locale=all`, {
+        headers,
+      }).then((res) => res.json());
+
+      expect(revisions.docs).toHaveLength(4);
+      expect(revisions.docs[0].revision.title.en).toStrictEqual(englishTitle);
+      expect(revisions.docs[0].revision.title.es).toStrictEqual(spanishTitle);
     });
   });
 });
