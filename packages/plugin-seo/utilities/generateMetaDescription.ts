0acb49422d6c23222fd8f958f9ea242a8e26dddb
Jacob Fletcher <jacobsfletch@gmail.com>
2021-12-10 14:57:50 -0500

    fix: adds error handling to meta title and prioritizes exerpt in meta description

--- packages/plugin-seo/utilities/generateMetaDescription.ts
+++ packages/plugin-seo/utilities/generateMetaDescription.ts
@@ -11,48 +11,50 @@ export const generateMetaDescription = (fields: Fields): string => {
 
   let description = excerpt as string || '';
 
-  const firstBlock = getDataByPath(fields, 'layout.0');
+  if (!excerpt) {
+    const firstBlock = getDataByPath(fields, 'layout.0');
 
-  if (firstBlock) {
-    // instead of writing a custom block for every type, we'll just iterate threw some generic keys and write a block each of those
-    const commonKeys = [
-      'introContent',
-      'richText',
-      'columns',
-    ];
+    if (firstBlock) {
+      // instead of writing a custom block for every type, we'll just iterate threw some generic keys and write a block each of those
+      const commonKeys = [
+        'introContent',
+        'richText',
+        'columns',
+      ];
 
-    const blockKeys = Object.keys(firstBlock);
-    const matchingKeys = commonKeys.filter((commonKey) => blockKeys.includes(commonKey));
+      const blockKeys = Object.keys(firstBlock);
+      const matchingKeys = commonKeys.filter((commonKey) => blockKeys.includes(commonKey));
 
-    if (matchingKeys.length > -1) {
-      const keyToUse = matchingKeys[0];
-      const field = firstBlock?.[keyToUse];
+      if (matchingKeys.length > -1) {
+        const keyToUse = matchingKeys[0];
+        const field = firstBlock?.[keyToUse];
 
-      if (field) {
-        if (keyToUse === 'introContent') {
-          const introContent = stringifyRichText(field);
-          if (!description && introContent) description = introContent;
-        }
+        if (field) {
+          if (keyToUse === 'introContent') {
+            const introContent = stringifyRichText(field);
+            if (!description && introContent) description = introContent;
+          }
 
-        if (keyToUse === 'richText') {
-          const richText = stringifyRichText(field);
-          if (!description && richText) description = richText;
-        }
+          if (keyToUse === 'richText') {
+            const richText = stringifyRichText(field);
+            if (!description && richText) description = richText;
+          }
 
-        if (keyToUse === 'columns') {
-          const firstColumn = field[0];
-          if (firstColumn) {
-            const {
-              richText,
-            } = firstColumn;
+          if (keyToUse === 'columns') {
+            const firstColumn = field[0];
+            if (firstColumn) {
+              const {
+                richText,
+              } = firstColumn;
 
-            let richTextToUse = richText;
-            if (typeof richText === 'string') {
-              richTextToUse = JSON.parse(richText);
+              let richTextToUse = richText;
+              if (typeof richText === 'string') {
+                richTextToUse = JSON.parse(richText);
+              }
+
+              const columnText = stringifyRichText(richTextToUse);
+              if (!description && columnText) description = columnText;
             }
-
-            const columnText = stringifyRichText(richTextToUse);
-            if (!description && columnText) description = columnText;
           }
         }
       }
--- packages/plugin-seo/utilities/generateMetaTitle.ts
+++ packages/plugin-seo/utilities/generateMetaTitle.ts
@@ -15,14 +15,21 @@ export const generateMetaTitle = async (fields: Fields): Promise<string> => {
       value: subsiteID
     } = subsite;
 
-    const req = await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/subsites/${subsiteID}`);
-    const doc = await req.json();
-    if (doc) {
-      const {
-        title: subsiteTitle
-      } = doc;
+    if (typeof subsiteID === 'string') {
+      try {
+        const req = await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/subsites/${subsiteID}`);
+        const doc = await req.json();
 
-      return `${base} | ${subsiteTitle} - ${docTitle}`;
+        if (req.status === 200) {
+          const {
+            title: subsiteTitle
+          } = doc;
+
+          return `${base} | ${subsiteTitle} - ${docTitle}`;
+        }
+      } catch (e) {
+        console.error(`error while generating meta title, cannot find subsite with id: ${subsiteID}`);
+      }
     }
   }
 
