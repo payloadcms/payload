b57267e60aa118e255be86e90e55da9852e97fd8
Elliot DeNolf <denolfe@gmail.com>
2021-12-13 21:43:21 -0500

    feat: add prepublish script, upgrade jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   verbose: true,
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test|it-test).[tj]s?(x)'],
+  testMatch: ['**/src/**/?(*.)+(spec|test|it-test).[tj]s?(x)'],
   testTimeout: 10000,
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
