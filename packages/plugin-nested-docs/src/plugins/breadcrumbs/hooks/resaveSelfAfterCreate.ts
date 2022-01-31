238db1750c55541637d91520a86d21d5d2cba091
James <james@trbl.design>
2022-01-31 16:18:46 -0500

    fix: potential bug in breadcrumbs plugin

--- packages/plugin-nested-docs/src/plugins/breadcrumbs/hooks/resaveSelfAfterCreate.ts
+++ packages/plugin-nested-docs/src/plugins/breadcrumbs/hooks/resaveSelfAfterCreate.ts
@@ -1,13 +1,19 @@
 import { CollectionConfig, CollectionAfterChangeHook } from 'payload/types';
 
-const resaveSelfAfterCreate = (collection: CollectionConfig): CollectionAfterChangeHook => ({ req: { payload }, req, doc, operation }) => {
+const resaveSelfAfterCreate = (collection: CollectionConfig): CollectionAfterChangeHook => async ({ req: { payload }, req, doc, operation }) => {
   if (operation === 'create') {
+    const originalDocWithDepth0 = await payload.findByID({
+      collection: collection.slug,
+      depth: 0,
+      id: doc.id,
+    });
+
     payload.update({
       collection: collection.slug,
       id: doc.id,
       depth: 0,
       data: {
-        ...doc,
+        ...originalDocWithDepth0,
         breadcrumbs: doc.breadcrumbs.map((crumb, i) => ({
           ...crumb,
           doc: doc?.breadcrumbs?.length === i + 1 ? doc.id : crumb.doc,
