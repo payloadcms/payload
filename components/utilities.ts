bef02062e769d1b0279c51af748f06d41c924c8a
James <james@trbl.design>
2022-02-01 09:33:23 -0500

    feat: exposes useLocale for reuse

--- components/utilities.ts
+++ components/utilities.ts
@@ -1,1 +1,2 @@
-export { default as Meta } from '../src/admin/components/utilities/Meta';
+export { default as Meta } from '../dist/admin/components/utilities/Meta';
+export { useLocale } from '../dist/admin/components/utilities/Locale';
