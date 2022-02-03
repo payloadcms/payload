6400095f1faf6e026c6568053d2abae621b01f86
Elliot DeNolf <denolfe@gmail.com>
2022-02-03 11:00:22 -0500

    feat: redirect root to admin panel

--- src/templates/js-todo/server.js
+++ src/templates/js-todo/server.js
@@ -4,6 +4,11 @@ const payload = require('payload');
 require('dotenv').config();
 const app = express();
 
+// Redirect root to Admin panel
+app.get('/', (_, res) => {
+  res.redirect('/admin');
+});
+
 // Initialize Payload
 payload.init({
   secret: process.env.PAYLOAD_SECRET,
--- src/templates/ts-todo/src/server.ts
+++ src/templates/ts-todo/src/server.ts
@@ -4,6 +4,11 @@ import payload from 'payload';
 require('dotenv').config();
 const app = express();
 
+// Redirect root to Admin panel
+app.get('/', (_, res) => {
+  res.redirect('/admin');
+});
+
 // Initialize Payload
 payload.init({
   secret: process.env.PAYLOAD_SECRET,
