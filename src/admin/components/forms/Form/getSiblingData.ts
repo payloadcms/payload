845b0b37090dea9839266ca4fb3a301af4c2992c
Dan Ribbens <dan.ribbens@gmail.com>
2022-04-01 11:34:13 -0400

    chore: top level getSiblingData

--- src/admin/components/forms/Form/getSiblingData.ts
+++ src/admin/components/forms/Form/getSiblingData.ts
@@ -1,19 +1,21 @@
 import { unflatten } from 'flatley';
 import { Fields, Data } from './types';
+import reduceFieldsToValues from './reduceFieldsToValues';
 
 const getSiblingData = (fields: Fields, path: string): Data => {
+  if (path.indexOf('.') === -1) {
+    return reduceFieldsToValues(fields, true);
+  }
   const siblingFields = {};
 
   // If this field is nested
   // We can provide a list of sibling fields
-  if (path.indexOf('.') > 0) {
-    const parentFieldPath = path.substring(0, path.lastIndexOf('.') + 1);
-    Object.keys(fields).forEach((fieldKey) => {
-      if (!fields[fieldKey].disableFormData && fieldKey.indexOf(parentFieldPath) === 0) {
-        siblingFields[fieldKey.replace(parentFieldPath, '')] = fields[fieldKey].value;
-      }
-    });
-  }
+  const parentFieldPath = path.substring(0, path.lastIndexOf('.') + 1);
+  Object.keys(fields).forEach((fieldKey) => {
+    if (!fields[fieldKey].disableFormData && fieldKey.indexOf(parentFieldPath) === 0) {
+      siblingFields[fieldKey.replace(parentFieldPath, '')] = fields[fieldKey].value;
+    }
+  });
 
   return unflatten(siblingFields, { safe: true });
 };
