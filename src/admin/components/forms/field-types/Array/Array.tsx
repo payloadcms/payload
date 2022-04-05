1749df6a4435cecfad6b3b3d94ce9f1c2b00f041
George <gb5510@gmail.com>
2022-04-05 12:33:36 +0100

    feat: support className config for row, block and array fields

--- src/admin/components/forms/field-types/Array/Array.tsx
+++ src/admin/components/forms/field-types/Array/Array.tsx
@@ -33,6 +33,7 @@ const ArrayFieldType: React.FC<Props> = (props) => {
       readOnly,
       description,
       condition,
+      className,
     },
   } = props;
 
@@ -115,10 +116,15 @@ const ArrayFieldType: React.FC<Props> = (props) => {
 
   const hasMaxRows = maxRows && rows.length >= maxRows;
 
+  const classes = [
+    baseClass,
+    className,
+  ].filter(Boolean).join(' ');
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div
-        className={baseClass}
+        className={classes}
       >
         <div className={`${baseClass}__error-wrap`}>
           <Error
