55b4dfb309f420faede9296c2af8fc0b39d85974
Dan Ribbens <dan.ribbens@gmail.com>
2022-04-29 15:24:56 -0400

    chore: test defaultValue with localization

--- src/collections/tests/defaultValue.spec.ts
+++ src/collections/tests/defaultValue.spec.ts
@@ -55,5 +55,55 @@ describe('DefaultValue - REST', () => {
       expect(document.group.nestedText2).toStrictEqual('nested default text 2');
       expect(document.group.nestedText3).toStrictEqual('neat');
     });
+
+    it('should not overwrite other locales when updating', async () => {
+      const slug = 'updated';
+      const esSlug = 'spanish';
+      const createResult = await fetch(`${url}/api/default-values`, {
+        body: JSON.stringify({
+          text: 'unique',
+          slug: 'unique',
+        }),
+        headers,
+        method: 'post',
+      });
+
+      const createData = await createResult.json();
+
+      const { id } = createData.doc;
+
+      const enResult = await fetch(`${url}/api/default-values/${id}?locale=en`, {
+        body: JSON.stringify({
+          slug,
+        }),
+        headers,
+        method: 'put',
+      });
+
+      const enData = await enResult.json();
+
+      const esResult = await fetch(`${url}/api/default-values/${id}?locale=es`, {
+        body: JSON.stringify({
+          slug: esSlug,
+        }),
+        headers,
+        method: 'put',
+      });
+
+      const esData = await esResult.json();
+
+      const allResult = await fetch(`${url}/api/default-values/${id}?locale=all`, {
+        headers,
+        method: 'get',
+      });
+
+      const allData = await allResult.json();
+
+      expect(createData.doc.slug).toStrictEqual('unique');
+      expect(enData.doc.slug).toStrictEqual(slug);
+      expect(esData.doc.slug).toStrictEqual(esSlug);
+      expect(allData.slug.en).toStrictEqual(slug);
+      expect(allData.slug.es).toStrictEqual(esSlug);
+    });
   });
 });
