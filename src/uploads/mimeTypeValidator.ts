bf48fdf18961a2e57bcc5aae73de4c569e97e42b
Dan Ribbens <DanRibbens@users.noreply.github.com>
2022-04-29 10:36:58 -0400

    fix: file upload safely handles missing mimeTypes (#540)
    * fix: file upload safely handles missing mimeTypes
    
    * feat: uploaded files that do not have mimeTypes are given one based on file extension

--- src/uploads/mimeTypeValidator.ts
+++ src/uploads/mimeTypeValidator.ts
@@ -3,6 +3,10 @@ import { Validate } from '../fields/config/types';
 export const mimeTypeValidator = (mimeTypes: string[]): Validate => (val: string) => {
   const cleanedMimeTypes = mimeTypes.map((v) => v.replace('*', ''));
 
+  if (!val) {
+    return 'Invalid file type';
+  }
+
   return !cleanedMimeTypes.some((v) => val.startsWith(v))
     ? `Invalid file type: '${val}'`
     : true;
