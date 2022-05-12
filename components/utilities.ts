9aca8e1ad7b35216a2c176c4a080b62a76613744
Dan Ribbens <dan.ribbens@gmail.com>
2022-05-11 21:28:08 -0400

    chore: export useDocumentInfo

--- components/utilities.ts
+++ components/utilities.ts
@@ -1,2 +1,3 @@
 export { default as Meta } from '../dist/admin/components/utilities/Meta';
 export { useLocale } from '../dist/admin/components/utilities/Locale';
+export { useDocumentInfo } from '../dist/admin/components/utilities/DocumentInfo';
