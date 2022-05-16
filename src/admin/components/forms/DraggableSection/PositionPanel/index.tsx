16b7edbc9782dcfb3bef77f1ff312e041d66922c
Dan Ribbens <DanRibbens@users.noreply.github.com>
2022-05-16 19:00:08 -0400

    fix: prevent changing order of readOnly arrays (#563)

--- src/admin/components/forms/DraggableSection/PositionPanel/index.tsx
+++ src/admin/components/forms/DraggableSection/PositionPanel/index.tsx
@@ -7,34 +7,45 @@ import './index.scss';
 const baseClass = 'position-panel';
 
 const PositionPanel: React.FC<Props> = (props) => {
-  const { moveRow, positionIndex, rowCount } = props;
+  const { moveRow, positionIndex, rowCount, readOnly } = props;
 
   const adjustedIndex = positionIndex + 1;
 
   const classes = [
     baseClass,
+    `${baseClass}__${readOnly ? 'read-only' : ''}`,
   ].filter(Boolean).join(' ');
 
   return (
     <div className={classes}>
-      <Button
-        className={`${baseClass}__move-backward ${positionIndex === 0 ? 'first-row' : ''}`}
-        buttonStyle="none"
-        icon="chevron"
-        round
-        onClick={() => moveRow(positionIndex, positionIndex - 1)}
-      />
+      {!readOnly && (
+        <Button
+          className={`${baseClass}__move-backward ${positionIndex === 0 ? 'first-row' : ''}`}
+          buttonStyle="none"
+          icon="chevron"
+          round
+          onClick={() => moveRow(positionIndex, positionIndex - 1)}
+        />
+      )}
 
       {(adjustedIndex && typeof positionIndex === 'number')
-        && <div className={`${baseClass}__current-position`}>{adjustedIndex >= 10 ? adjustedIndex : `0${adjustedIndex}`}</div>}
+        && (
+        <div
+          className={`${baseClass}__current-position`}
+        >
+          {adjustedIndex >= 10 ? adjustedIndex : `0${adjustedIndex}`}
+        </div>
+        )}
 
-      <Button
-        className={`${baseClass}__move-forward ${(positionIndex === rowCount - 1) ? 'last-row' : ''}`}
-        buttonStyle="none"
-        icon="chevron"
-        round
-        onClick={() => moveRow(positionIndex, positionIndex + 1)}
-      />
+      {!readOnly && (
+        <Button
+          className={`${baseClass}__move-forward ${(positionIndex === rowCount - 1) ? 'last-row' : ''}`}
+          buttonStyle="none"
+          icon="chevron"
+          round
+          onClick={() => moveRow(positionIndex, positionIndex + 1)}
+        />
+      )}
     </div>
   );
 };
