f2bf2399fa34c49b3b68be55257908b0f8733962
Dan Ribbens <DanRibbens@users.noreply.github.com>
2022-06-08 11:37:31 -0400

    fix: custom fields values resetting in ui (#626)

--- src/admin/components/utilities/RenderCustomComponent/index.tsx
+++ src/admin/components/utilities/RenderCustomComponent/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Props } from './types';
-import withCondition from '../../forms/withCondition';
 
 const RenderCustomComponent: React.FC<Props> = (props) => {
   const { CustomComponent, DefaultComponent, componentProps } = props;
 
   if (CustomComponent) {
-    const ConditionalCustomComponent = withCondition(CustomComponent);
     return (
-      <ConditionalCustomComponent {...componentProps} />
+      <CustomComponent {...componentProps} />
     );
   }
 
