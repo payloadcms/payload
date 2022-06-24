6325eb19dbeabb5d9266d0049d1ba2b22052351b
Steven Roh <hello@stevenroh.ch>
2022-06-24 10:07:33 +0200

    Avoid app crashing when code (data) is undefined

--- src/admin/components/views/collections/List/Cell/field-types/Code/index.tsx
+++ src/admin/components/views/collections/List/Cell/field-types/Code/index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import './index.scss';
 
 const CodeCell = ({ data }) => {
-  const textToShow = data.length > 100 ? `${data.substr(0, 100)}\u2026` : data;
+  let textToShow = '';
+
+  if (data) textToShow = data.length > 100 ? `${data.substr(0, 100)}\u2026` : data;
+
   return (
     <code className="code-cell">
       <span>{textToShow}</span>
