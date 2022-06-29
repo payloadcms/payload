9644366776b254586fea12588f77e66541b0a512
Dan Ribbens <dan.ribbens@gmail.com>
2022-06-29 13:19:06 -0400

    fix: empty cell data renders in list

--- src/admin/components/views/collections/List/Cell/field-types/Code/index.tsx
+++ src/admin/components/views/collections/List/Cell/field-types/Code/index.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 import './index.scss';
 
 const CodeCell = ({ data }) => {
-  let textToShow = '';
-
-  if (data) textToShow = data.length > 100 ? `${data.substr(0, 100)}\u2026` : data;
+  const textToShow = data.length > 100 ? `${data.substring(0, 100)}\u2026` : data;
 
   return (
     <code className="code-cell">
