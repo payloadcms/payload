601e69ab0d5ec228b2650b7e45cc9dc3cdfee4fc
Akansh Sirohi <34837239+akanshSirohi@users.noreply.github.com>
2022-07-03 15:55:03 +0530

    Fixes payloadcms/payload#650 (#706)
    * fix: min/max number validation of decimal values

--- src/fields/validations.ts
+++ src/fields/validations.ts
@@ -24,9 +24,9 @@ import canUseDOM from '../utilities/canUseDOM';
 const defaultMessage = 'This field is required.';
 
 export const number: Validate<unknown, unknown, NumberField> = (value: string, { required, min, max }) => {
-  const parsedValue = parseInt(value, 10);
+  const parsedValue = parseFloat(value);
 
-  if ((value && typeof parsedValue !== 'number') || (required && Number.isNaN(parsedValue))) {
+  if ((value && typeof parsedValue !== 'number') || (required && Number.isNaN(parsedValue)) || (value && Number.isNaN(parsedValue))) {
     return 'Please enter a valid number.';
   }
 
