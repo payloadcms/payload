aec8dcd289137aadb400d986d851814bb2032681
Elliot DeNolf <denolfe@gmail.com>
2022-07-07 12:01:09 -0700

    test: unique playwright workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,5 +4,6 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   // Look for test files in the "tests" directory, relative to this configuration file
   testDir: 'test/e2e',
+  workers: 999,
 };
 export default config;
