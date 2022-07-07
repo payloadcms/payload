b69c46bdadc5271678444e1305156b9eb8240233
Dan Ribbens <dan.ribbens@gmail.com>
2022-07-07 19:04:18 -0400

    test: collection create

--- test/e2e/collections/index.spec.ts
+++ test/e2e/collections/index.spec.ts
@@ -23,10 +23,25 @@ test.describe('collections', () => {
     await firstRegister({ page, serverURL });
   });
 
-  test('should nav to create', async () => {
+  test('should nav to list', async () => {
     await page.goto(`${serverURL}/admin`);
-    const collectionLink = await page.locator('nav >> text=Posts');
+    const collectionLink = page.locator('css=nav >> text=Posts');
     await collectionLink.click();
-    await expect(page).toHaveURL(`${serverURL}/admin/collections/posts`);
+    await expect(page.url()).toContain(`${serverURL}/admin/collections/posts`);
+  });
+
+  test('should create', async () => {
+    const title = 'post title';
+    const description = 'description';
+    await page.goto(`${serverURL}/admin/collections/posts/create`);
+    const titleField = page.locator('#title');
+    const descriptionField = page.locator('#description');
+    await titleField.fill(title);
+    await descriptionField.fill(description);
+    await page.click('text=Save');
+    await expect(page.locator('text=Post successfully created')).toBeVisible();
+    await expect(page.url()).not.toContain('create');
+    await expect(titleField).toHaveValue(title);
+    await expect(descriptionField).toHaveValue(description);
   });
 });
