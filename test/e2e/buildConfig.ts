851aa839f8c96e69e2bccaa1e4e881ace41e5bfa
Dan Ribbens <dan.ribbens@gmail.com>
2022-07-08 09:27:14 -0400

    test: e2e cache type changed to memory

--- test/e2e/buildConfig.ts
+++ test/e2e/buildConfig.ts
@@ -4,5 +4,15 @@ import { Config, SanitizedConfig } from '../../src/config/types';
 
 export function buildConfig(overrides?: Partial<Config>): SanitizedConfig {
   const baseConfig: Config = {};
+  if (process.env.NODE_ENV === 'test') {
+    baseConfig.admin = {
+      webpack: (config) => ({
+        ...config,
+        cache: {
+          type: 'memory',
+        },
+      }),
+    };
+  }
   return buildPayloadConfig(merge(baseConfig, overrides || {}));
 }
