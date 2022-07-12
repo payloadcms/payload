2c9e9f2e183dce707331972b0e50ad53fbab7e27
James <james@trbl.design>
2022-07-11 20:53:42 -0700

    chore: getting ready for dark mode

--- src/admin/components/graphics/Icon/index.tsx
+++ src/admin/components/graphics/Icon/index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { useConfig } from '../../utilities/Config';
 import RenderCustomComponent from '../../utilities/RenderCustomComponent';
 
+const css = `
+  path {
+    fill: #333333;
+  }
+
+  html[data-theme=dark] path {
+    fill: white;
+  }
+`;
+
 const PayloadIcon: React.FC = () => (
   <svg
     width="25"
@@ -9,13 +19,14 @@ const PayloadIcon: React.FC = () => (
     viewBox="0 0 25 25"
     xmlns="http://www.w3.org/2000/svg"
   >
+    <style>
+      {css}
+    </style>
     <path
       d="M11.5293 0L23 6.90096V19.9978L14.3608 25V11.9032L2.88452 5.00777L11.5293 0Z"
-      fill="#333333"
     />
     <path
       d="M10.6559 24.2727V14.0518L2 19.0651L10.6559 24.2727Z"
-      fill="#333333"
     />
   </svg>
 );
