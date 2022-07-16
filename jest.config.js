34d0b893767c9bd81886518adadc385f3b85c941
James <james@trbl.design>
2022-07-16 11:01:09 -0700

    chore: increases jest timeout

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '**/test/**/*int.spec.ts',
   ],
   globalSetup: './test/jest.setup.ts',
-  testTimeout: 15000,
+  testTimeout: 90000,
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/webpack/mocks/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/src/webpack/mocks/emptyModule.js',
