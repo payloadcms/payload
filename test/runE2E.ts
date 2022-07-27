0cd5b4fb70347c2e515e6bbd575dd128f4e57641
Elliot DeNolf <denolfe@gmail.com>
2022-07-27 17:18:23 -0400

    test: finish e2e script

--- test/runE2E.ts
+++ test/runE2E.ts
@@ -6,48 +6,18 @@ import glob from 'glob';
 
 const playwrightBin = path.resolve(__dirname, '../node_modules/.bin/playwright');
 
-const clearWebpackCache = () => {
-  const webpackCachePath = path.resolve(__dirname, '../node_modules/.cache/webpack');
-  console.log('Clearing webpack cache.');
-  shelljs.rm('-rf', webpackCachePath);
-};
-
 const testRunCodes: { suiteName: string; code: number }[] = [];
-
-const executePlaywright = (suitePath: string, bail = false) => {
-  console.log(`Executing ${suitePath}...`);
-  const playwrightCfg = path.resolve(
-    __dirname,
-    '..',
-    `${bail ? 'playwright.bail.config.ts' : 'playwright.config.ts'}`,
-  );
-
-  const cmd = `DISABLE_LOGGING=true ${playwrightBin} test ${suitePath} -c ${playwrightCfg}`;
-  console.log('command: ', cmd);
-  const { stdout, code } = shelljs.exec(cmd);
-  if (code) {
-    if (bail) {
-      console.error(`TEST FAILURE DURING ${suiteName} suite.`);
-      process.exit(1);
-    } else {
-      testRunCodes.push({ suiteName, code });
-    }
-  }
-  return stdout;
-};
-
 const args = process.argv.slice(2);
 const suiteName = args[0];
 
+// Run all
 if (!suiteName || args[0].startsWith('-')) {
-  // Run all
-  console.log('Executing all E2E tests...');
   const bail = args.includes('--bail');
-  glob(`${path.resolve(__dirname)}/**/*e2e.spec.ts`, (err, files) => {
-    files.forEach((file) => {
-      clearWebpackCache();
-      executePlaywright(file, bail);
-    });
+  const files = glob.sync(`${path.resolve(__dirname)}/**/*e2e.spec.ts`);
+  console.log(`\n\nExecuting all ${files.length} E2E tests...`);
+  files.forEach((file) => {
+    clearWebpackCache();
+    executePlaywright(file, bail);
   });
 } else {
   // Run specific suite
@@ -56,8 +26,38 @@ if (!suiteName || args[0].startsWith('-')) {
   executePlaywright(suitePath);
 }
 
+console.log('\nRESULTS:');
 testRunCodes.forEach((tr) => {
-  console.log(`Suite: ${tr.suiteName}, Success: ${tr.code === 0}`);
+  console.log(`\tSuite: ${tr.suiteName}, Success: ${tr.code === 0}`);
 });
+console.log('\n');
 
 if (testRunCodes.some((tr) => tr.code > 0)) process.exit(1);
+
+function executePlaywright(suitePath: string, bail = false) {
+  console.log(`Executing ${suitePath}...`);
+  const playwrightCfg = path.resolve(
+    __dirname,
+    '..',
+    `${bail ? 'playwright.bail.config.ts' : 'playwright.config.ts'}`,
+  );
+
+  const cmd = `DISABLE_LOGGING=true ${playwrightBin} test ${suitePath} -c ${playwrightCfg}`;
+  console.log('\n', cmd);
+  const { stdout, code } = shelljs.exec(cmd);
+  const suite = path.basename(path.dirname(suitePath));
+  const results = { suiteName: suite, code };
+  if (code) {
+    if (bail) {
+      console.error(`TEST FAILURE DURING ${suite} suite.`);
+      process.exit(1);
+    }
+  }
+  testRunCodes.push(results);
+  return stdout;
+}
+
+function clearWebpackCache() {
+  const webpackCachePath = path.resolve(__dirname, '../node_modules/.cache/webpack');
+  shelljs.rm('-rf', webpackCachePath);
+}
