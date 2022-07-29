a5973842f77b3fd637fdbc8e441a3989a8264eeb
Alex <alex.bechmann@outlook.com>
2022-07-29 14:01:19 +0200

    feat: refactor resize handler to prioritize read from disk

--- src/collections/requestHandlers/resize.ts
+++ src/collections/requestHandlers/resize.ts
@@ -5,7 +5,9 @@ import { FileData } from '../../uploads/types';
 import { SanitizedCollectionConfig } from '../config/types';
 import fetch from 'node-fetch';
 import { TypeWithID } from '../../globals/config/types';
-import sharp from 'sharp';
+import sharp, { ResizeOptions } from 'sharp';
+import fs from 'fs';
+import path from 'path';
 
 export default (collection: SanitizedCollectionConfig) =>
   async function resizeHandler(
@@ -20,41 +22,44 @@ export default (collection: SanitizedCollectionConfig) =>
           id: req.params.id,
         });
 
-      if (!image.url) {
-        const errorMessage = 'No url property found';
-        payload.logger.error(errorMessage);
-        res.status(500).send(errorMessage);
-      }
+      let imageBuffer: Buffer;
 
-      let imageUrl = image.url;
-      const relativeUrl = imageUrl?.indexOf('http') === -1;
-
-      if (relativeUrl) {
-        imageUrl = [req.protocol, '://', req.get('host'), image.url].join('');
+      if (collection.upload.disableLocalStorage) {
+        if (!image.url) {
+          const errorMessage = 'No url property found';
+          payload.logger.error(errorMessage);
+          res.status(500).send(errorMessage);
+        }
+        payload.logger.info(`Fetching image to resize from ${image.url}`);
+        const response = await fetch(image.url);
+        const arrayBuffer = await response.arrayBuffer();
+        imageBuffer = Buffer.from(arrayBuffer);
+      } else {
+        let imagePath = path.resolve(
+          payload.config.paths.configDir,
+          collection.upload.staticDir,
+          image.filename
+        );
+        imageBuffer = fs.readFileSync(imagePath);
       }
 
       const width = parseInt(req.query.width as string);
       const height = parseInt(req.query.height as string);
       const position = (req.query.position as string) || 'centre';
 
+      const options: ResizeOptions = {
+        position,
+      };
+
       payload.logger.info(
-        `Resizing image from source: ${imageUrl} with parameters: ${JSON.stringify(
-          {
-            width,
-            height,
-            position,
-          }
-        )}`
+        `Resizing image ${image.id} with parameters: ${JSON.stringify({
+          width,
+          height,
+          options,
+        })}`
       );
 
-      const response = await fetch(imageUrl);
-      const arrayBuffer = await response.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
-
-      const resized = sharp(buffer).resize(width, height, {
-        position,
-      });
-
+      const resized = sharp(imageBuffer).resize(width, height, options);
       const resizedBuffer = await resized.toBuffer({
         resolveWithObject: true,
       });
