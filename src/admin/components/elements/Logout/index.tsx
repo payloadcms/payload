9448dd048b95a4f71d7c66c740dcea4c9ce67f05
Alberto Maghini (MSC Technology Italia) <alberto.maghini@msc.com>
2022-10-24 14:28:49 +0200

    extracted getSanitizedLogoutRoutes method

--- src/admin/components/elements/Logout/index.tsx
+++ src/admin/components/elements/Logout/index.tsx
@@ -3,51 +3,56 @@ import { Link } from 'react-router-dom';
 import { useConfig } from '../../utilities/Config';
 import RenderCustomComponent from '../../utilities/RenderCustomComponent';
 import LogOut from '../../icons/LogOut';
+import { SanitizedConfig } from 'payload/config';
 
 const baseClass = 'nav';
 
 export const logoutDefaultRoute = '/logout';
 export const logoutDefaultInactivityRoute = '/logout-inactivity';
 
-const DefaultLogout = ()=> {
-    const {
-        routes: {
-          admin,
-        },
-        admin: {
-          components: {
-            logout
-          },
-        },
-      } = useConfig();
-    const { route: logoutRoute = logoutDefaultRoute } = logout;
-    return <Link 
-        to={`${admin}${logoutRoute}`}
-        className={`${baseClass}__log-out`}
-        >
-            <LogOut />
-    </Link>;
+export interface SanitizedLogoutRoutes {
+  logoutRoute: string;
+  logoutInactivityRoute: string;
 }
 
+export function getSanitizedLogoutRoutes(config: SanitizedConfig): SanitizedLogoutRoutes {
+  const {
+    inactivityRoute: logoutInactivityRoute = logoutDefaultInactivityRoute,
+    route: logoutRoute = logoutDefaultRoute,
+  } = config.admin?.components?.logout || {};
+  return { logoutRoute, logoutInactivityRoute };
+}
+
+const DefaultLogout = () => {
+  const config = useConfig();
+  const {
+    routes: { admin },
+  } = config;
+  const { logoutRoute } = getSanitizedLogoutRoutes(config);
+  return (
+    <Link to={`${admin}${logoutRoute}`} className={`${baseClass}__log-out`}>
+      <LogOut />
+    </Link>
+  );
+};
+
 const Logout: React.FC = () => {
-    const {
-      admin: {
-        components: {
-          logout: {
-            Component: CustomLogout,
-          } = {
-            Component: undefined
-          }
-        } = {},
+  const {
+    admin: {
+      components: {
+        logout: { Component: CustomLogout } = {
+          Component: undefined,
+        },
       } = {},
-    } = useConfig();
-  
-    return (
-      <RenderCustomComponent
-        CustomComponent={CustomLogout}
-        DefaultComponent={DefaultLogout}
-      />
-    );
-  };
-  
-  export default Logout;
+    } = {},
+  } = useConfig();
+
+  return (
+    <RenderCustomComponent
+      CustomComponent={CustomLogout}
+      DefaultComponent={DefaultLogout}
+    />
+  );
+};
+
+export default Logout;
