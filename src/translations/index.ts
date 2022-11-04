47f45fae150779ec319bbb35b91469baaa308688
Hung Vu <hunghvu2017@gmail.com>
2022-11-04 13:00:11 -0700

    feat: add Vietnamese translation for the admin panel (#1346)

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -2,10 +2,12 @@ import en from './en.json';
 import es from './es.json';
 import de from './de.json';
 import pl from './pl.json';
+import vn from './vn.json'
 
 export default {
   de,
   en,
   es,
   pl,
+  vn
 };
