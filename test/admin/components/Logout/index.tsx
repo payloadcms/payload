c800ae9b7a5d41aaf608c7707ce0575834da324d
Alberto Maghini <alberto@newesis.com>
2022-11-11 13:40:50 +0100

    updated test ref

--- test/admin/components/Logout/index.tsx
+++ test/admin/components/Logout/index.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { useConfig } from '../../../../src/admin/components/utilities/Config';
 import LogOut from '../../../../src/admin/components/icons/LogOut';
-import { getSanitizedLogoutRoutes } from '../../../../src/admin/components/elements/Logout';
 
 
 const Logout: React.FC = () => {
   const config = useConfig();
   const {
-      routes: {
-        admin,
-      },
-      admin: {
-        components: {
-          logout
-        },
-      },
-    } = config;
-    const { Route: logoutRoute } = logout;
+    routes: {
+      admin,
+    },
+    admin: {
+      logoutRoute
+    },
+  } = config;
   return (
     <a href={`${admin}${logoutRoute}#custom`}>
       <LogOut />
