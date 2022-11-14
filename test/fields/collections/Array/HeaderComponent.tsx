7162f31ca69b101c0fdae8364b3f99d2e06d7bc6
bigmistqke <vandijckv@gmail.com>
2022-11-14 17:42:50 +0100

    add index to tests

--- test/fields/collections/Array/HeaderComponent.tsx
+++ test/fields/collections/Array/HeaderComponent.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { RowHeaderComponent } from '../../../../src/admin/components/forms/RowHeader/types';
 
 const ArrayRowHeader: RowHeaderComponent = (props) => {
-  const { value } = props;
-  return <React.Fragment>{value.title || 'enter title'}</React.Fragment>;
+  const { value, index } = props;
+  return <React.Fragment>{value.title || `array_${index}`}</React.Fragment>;
 };
 
 export default ArrayRowHeader;
