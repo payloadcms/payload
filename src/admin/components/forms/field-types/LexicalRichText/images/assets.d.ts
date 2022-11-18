59be7d8e1b900f241999c266d953d90319179a70
Alessio Gravili <alessio@gravili.de>
2022-11-18 02:34:15 +0100

    Fix gif imports and thus build

---
+++ src/admin/components/forms/field-types/LexicalRichText/images/assets.d.ts
@@ -0,0 +1,27 @@
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.json' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
