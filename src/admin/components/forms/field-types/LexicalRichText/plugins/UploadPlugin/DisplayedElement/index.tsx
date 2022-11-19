b0b9e814e74e7062a853587c19e19f1ac2f42b0c
Alessio Gravili <alessio@gravili.de>
2022-11-19 19:40:44 +0100

    Work on image inserting & simplify!

---
+++ src/admin/components/forms/field-types/LexicalRichText/plugins/UploadPlugin/DisplayedElement/index.tsx
@@ -0,0 +1,150 @@
+import { useModal } from '@faceless-ui/modal';
+import { useTranslation } from 'react-i18next';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useConfig } from '../../../../../../utilities/Config';
+import { SanitizedCollectionConfig } from '../../../../../../../../collections/config/types';
+import usePayloadAPI from '../../../../../../../hooks/usePayloadAPI';
+import useThumbnail from '../../../../../../../hooks/useThumbnail';
+import FileGraphic from '../../../../../../graphics/File';
+import { getTranslation } from '../../../../../../../../utilities/getTranslation';
+import Button from '../../../../../../elements/Button';
+import { SwapUploadModal } from '../../../../RichText/elements/upload/Element/SwapUploadModal';
+import { EditModal } from '../../../../RichText/elements/upload/Element/EditModal';
+
+const baseClass = 'rich-text-upload';
+
+const initialParams = {
+  depth: 0,
+};
+
+const DisplayedElement = ({ attributes, children, element, path, fieldProps, activeEditor }) => {
+  const { relationTo, value } = element;
+  const { toggleModal } = useModal();
+  const { collections, serverURL, routes: { api } } = useConfig();
+  const [modalToRender, setModalToRender] = useState(undefined);
+  const [relatedCollection, setRelatedCollection] = useState<SanitizedCollectionConfig>(() => collections.find((coll) => coll.slug === relationTo));
+  const { t, i18n } = useTranslation('fields');
+
+  const modalSlug = `${path}-edit-upload-${modalToRender}`;
+
+  // Get the referenced document
+  const [{ data: upload }] = usePayloadAPI(
+    `${serverURL}${api}/${relatedCollection.slug}/${value?.id}`,
+    { initialParams },
+  );
+
+  const thumbnailSRC = useThumbnail(relatedCollection, upload);
+
+  //Remove upload element from editor
+  const removeUpload = useCallback(() =>
+    //TODO
+  , [activeEditor, element]);
+
+  const closeModal = useCallback(() => {
+    toggleModal(modalSlug);
+    setModalToRender(null);
+  }, [toggleModal, modalSlug]);
+
+  useEffect(() => {
+    if (modalToRender) {
+      toggleModal(modalSlug);
+    }
+  }, [modalToRender, toggleModal, modalSlug]);
+
+  const fieldSchema = fieldProps?.admin?.upload?.collections?.[relatedCollection.slug]?.fields;
+
+  return (
+    <div
+      className={[
+        baseClass,
+        (/*selected && focused*/true) && `${baseClass}--selected`,
+      ].filter(Boolean).join(' ')}
+      contentEditable={false}
+      {...attributes}
+    >
+      <div className={`${baseClass}__card`}>
+        <div className={`${baseClass}__topRow`}>
+          <div className={`${baseClass}__thumbnail`}>
+            {thumbnailSRC ? (
+              <img
+                src={thumbnailSRC}
+                alt={upload?.filename}
+              />
+            ) : (
+              <FileGraphic />
+            )}
+          </div>
+          <div className={`${baseClass}__topRowRightPanel`}>
+            <div className={`${baseClass}__collectionLabel`}>
+              {getTranslation(relatedCollection.labels.singular, i18n)}
+            </div>
+            <div className={`${baseClass}__actions`}>
+              {fieldSchema && (
+                <Button
+                  icon="edit"
+                  round
+                  buttonStyle="icon-label"
+                  className={`${baseClass}__actionButton`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setModalToRender('edit');
+                  }}
+                  tooltip={t('general:edit')}
+                />
+              )}
+              <Button
+                icon="swap"
+                round
+                buttonStyle="icon-label"
+                className={`${baseClass}__actionButton`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setModalToRender('swap');
+                }}
+                tooltip={t('swapUpload')}
+              />
+              <Button
+                icon="x"
+                round
+                buttonStyle="icon-label"
+                className={`${baseClass}__actionButton`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  removeUpload();
+                }}
+                tooltip={t('removeUpload')}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className={`${baseClass}__bottomRow`}>
+          <strong>{upload?.filename}</strong>
+        </div>
+      </div>
+      {children}
+
+      {modalToRender === 'swap' && (
+        <SwapUploadModal
+          slug={modalSlug}
+          element={element}
+          closeModal={closeModal}
+          setRelatedCollectionConfig={setRelatedCollection}
+          relatedCollectionConfig={relatedCollection}
+        />
+      )}
+
+      {(modalToRender === 'edit' && fieldSchema) && (
+        <EditModal
+          slug={modalSlug}
+          closeModal={closeModal}
+          relatedCollectionConfig={relatedCollection}
+          fieldSchema={fieldSchema}
+          element={element}
+        />
+      )}
+    </div>
+  );
+};
+
+export default DisplayedElement;
--- src/admin/components/forms/field-types/LexicalRichText/plugins/UploadPlugin/index.tsx
+++ src/admin/components/forms/field-types/LexicalRichText/plugins/UploadPlugin/index.tsx
@@ -29,179 +29,19 @@ import { useEffect, useRef, useState } from 'react';
 import * as React from 'react';
 import getSelection from '../../shared/getDOMSelection';
 
-import landscapeImage from '../../images/landscape.jpg';
-import yellowFlowerImage from '../../images/yellow-flower.jpg';
+
 import {
   $createImageNode,
   $isImageNode,
   ImageNode,
   ImagePayload,
 } from '../../nodes/ImageNode';
-import Button from '../../ui/Button';
-import { DialogActions, DialogButtonsList } from '../../ui/Dialog';
-import FileInput from '../../ui/FileInput';
-import TextInput from '../../ui/TextInput';
+
 
 export type InsertImagePayload = Readonly<ImagePayload>;
 
 export const INSERT_IMAGE_COMMAND: LexicalCommand<InsertImagePayload> = createCommand('INSERT_IMAGE_COMMAND');
 
-export function InsertImageUriDialogBody({
-  onClick,
-}: {
-  onClick: (payload: InsertImagePayload) => void;
-}) {
-  const [src, setSrc] = useState('');
-  const [altText, setAltText] = useState('');
-
-  const isDisabled = src === '';
-
-  return (
-    <React.Fragment>
-      <TextInput
-        label="Image URL"
-        placeholder="i.e. https://source.unsplash.com/random"
-        onChange={setSrc}
-        value={src}
-        data-test-id="image-modal-url-input"
-      />
-      <TextInput
-        label="Alt Text"
-        placeholder="Random unsplash image"
-        onChange={setAltText}
-        value={altText}
-        data-test-id="image-modal-alt-text-input"
-      />
-      <DialogActions>
-        <Button
-          data-test-id="image-modal-confirm-btn"
-          disabled={isDisabled}
-          onClick={() => onClick({ altText, src })}
-        >
-          Confirm
-        </Button>
-      </DialogActions>
-    </React.Fragment>
-  );
-}
-
-export function InsertImageUploadedDialogBody({
-  onClick,
-}: {
-  onClick: (payload: InsertImagePayload) => void;
-}) {
-  const [src, setSrc] = useState('');
-  const [altText, setAltText] = useState('');
-
-  const isDisabled = src === '';
-
-  const loadImage = (files: FileList | null) => {
-    const reader = new FileReader();
-    reader.onload = function () {
-      if (typeof reader.result === 'string') {
-        setSrc(reader.result);
-      }
-      return '';
-    };
-    if (files !== null) {
-      reader.readAsDataURL(files[0]);
-    }
-  };
-
-  return (
-    <React.Fragment>
-      <FileInput
-        label="Image Upload"
-        onChange={loadImage}
-        accept="image/*"
-        data-test-id="image-modal-file-upload"
-      />
-      <TextInput
-        label="Alt Text"
-        placeholder="Descriptive alternative text"
-        onChange={setAltText}
-        value={altText}
-        data-test-id="image-modal-alt-text-input"
-      />
-      <DialogActions>
-        <Button
-          data-test-id="image-modal-file-upload-btn"
-          disabled={isDisabled}
-          onClick={() => onClick({ altText, src })}
-        >
-          Confirm
-        </Button>
-      </DialogActions>
-    </React.Fragment>
-  );
-}
-
-export function InsertImageDialog({
-  activeEditor,
-  onClose,
-}: {
-  activeEditor: LexicalEditor;
-  onClose: () => void;
-}): JSX.Element {
-  const [mode, setMode] = useState<null | 'url' | 'file'>(null);
-  const hasModifier = useRef(false);
-
-  useEffect(() => {
-    hasModifier.current = false;
-    const handler = (e: KeyboardEvent) => {
-      hasModifier.current = e.altKey;
-    };
-    document.addEventListener('keydown', handler);
-    return () => {
-      document.removeEventListener('keydown', handler);
-    };
-  }, [activeEditor]);
-
-  const onClick = (payload: InsertImagePayload) => {
-    activeEditor.dispatchCommand(INSERT_IMAGE_COMMAND, payload);
-    onClose();
-  };
-
-  return (
-    <React.Fragment>
-      {!mode && (
-        <DialogButtonsList>
-          <Button
-            data-test-id="image-modal-option-sample"
-            onClick={() => onClick(
-              hasModifier.current
-                ? {
-                  altText:
-                    'Daylight fir trees forest glacier green high ice landscape',
-                  src: landscapeImage,
-                }
-                : {
-                  altText: 'Yellow flower in tilt shift lens',
-                  src: yellowFlowerImage,
-                },
-            )}
-          >
-            Sample
-          </Button>
-          <Button
-            data-test-id="image-modal-option-url"
-            onClick={() => setMode('url')}
-          >
-            URL
-          </Button>
-          <Button
-            data-test-id="image-modal-option-file"
-            onClick={() => setMode('file')}
-          >
-            File
-          </Button>
-        </DialogButtonsList>
-      )}
-      {mode === 'url' && <InsertImageUriDialogBody onClick={onClick} />}
-      {mode === 'file' && <InsertImageUploadedDialogBody onClick={onClick} />}
-    </React.Fragment>
-  );
-}
 
 export default function UploadPlugin({
   captionsEnabled,
