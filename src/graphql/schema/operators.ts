cde1f128e9342790ca7f27597b4e7d25fa3f9f67
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-11-23 14:59:51 -0500

    chore: adds contains to operators.contains

--- src/graphql/schema/operators.ts
+++ src/graphql/schema/operators.ts
@@ -1,6 +1,6 @@
 const operators = {
   equality: ['equals', 'not_equals'],
-  contains: ['in', 'not_in', 'all'],
+  contains: ['contains', 'in', 'not_in', 'all'],
   comparison: ['greater_than_equal', 'greater_than', 'less_than_equal', 'less_than'],
   geo: ['near'],
 };
