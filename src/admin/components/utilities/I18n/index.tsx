07b2ccad61a619478f6613fa65f4f630222639d4
Jessica Boezwinkle <jessica@trbl.design>
2022-12-16 15:04:25 +0000

    feat: wires up i18n with monaco editor

--- src/admin/components/utilities/I18n/index.tsx
+++ src/admin/components/utilities/I18n/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import i18n from 'i18next';
+import { loader } from '@monaco-editor/react';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import deepmerge from 'deepmerge';
@@ -17,7 +18,7 @@ export const I18n: React.FC = () => {
     .use(LanguageDetector)
     .use(initReactI18next)
     .init(deepmerge(defaultOptions, config.i18n || {}));
-
+  loader.config({ 'vs/nls': { availableLanguages: { '*': i18n.language } } });
   return null;
 };
 
