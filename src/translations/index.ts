0796d5394de163706550c1b9474ffc03d61f5b62
Elliot DeNolf <denolfe@gmail.com>
2022-12-19 16:15:11 -0500

    chore: fix single quotes in translations file

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,20 +1,20 @@
-import cs from "./cs.json";
-import de from "./de.json";
-import en from "./en.json";
-import es from "./es.json";
-import fr from "./fr.json";
-import it from "./it.json";
-import ja from "./ja.json";
-import my from "./my.json";
-import nb from "./nb.json";
-import nl from "./nl.json";
-import pl from "./pl.json";
-import pt from "./pt.json";
-import ru from "./ru.json";
-import tr from "./tr.json";
-import vi from "./vi.json";
-import sv from "./sv.json";
-import th from "./th.json";
+import cs from './cs.json';
+import de from './de.json';
+import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
+import it from './it.json';
+import ja from './ja.json';
+import my from './my.json';
+import nb from './nb.json';
+import nl from './nl.json';
+import pl from './pl.json';
+import pt from './pt.json';
+import ru from './ru.json';
+import tr from './tr.json';
+import vi from './vi.json';
+import sv from './sv.json';
+import th from './th.json';
 
 export default {
   cs,
