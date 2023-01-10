972cb470f60b5915ab0870a7880392277a064e5d
Phillip Malboeuf <phil@phils.computer>
2023-01-10 18:50:29 -0500

    draggable columns using dnd

--- src/admin/components/elements/ColumnSelector/index.tsx
+++ src/admin/components/elements/ColumnSelector/index.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useId, useState } from 'react';
+import React, { useCallback, useEffect, useId, useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { useTranslation } from 'react-i18next';
 import flattenTopLevelFields from '../../../../utilities/flattenTopLevelFields';
 import Pill from '../Pill';
 import Plus from '../../icons/Plus';
 import X from '../../icons/X';
+import DragHandle from '../../icons/Drag';
 import { Props } from './types';
 import { getTranslation } from '../../../../utilities/getTranslation';
 import { useEditDepth } from '../../utilities/EditDepth';
@@ -28,35 +30,89 @@ const ColumnSelector: React.FC<Props> = (props) => {
   const uuid = useId();
   const editDepth = useEditDepth();
 
-  return (
-    <div className={baseClass}>
-      {fields && fields.map((field, i) => {
-        const isEnabled = columns.find((column) => column === field.name);
-        return (
-          <Pill
-            onClick={() => {
-              let newState = [...columns];
-              if (isEnabled) {
-                newState = newState.filter((remainingColumn) => remainingColumn !== field.name);
-              } else {
-                newState.unshift(field.name);
-              }
+  const moveColumn = useCallback((moveFromIndex: number, moveToIndex: number) => {
+    console.log(moveFromIndex, moveToIndex);
+    const newState = [...fields];
+    const element = fields[moveFromIndex];
+    newState.splice(moveFromIndex, 1);
+    newState.splice(moveToIndex, 0, element);
+    setFields(newState);
+    setColumns(newState.filter((field) => columns.find((column) => column === field.name)).map((field) => field.name));
+  }, [columns, fields, setColumns, setFields]);
 
-              setColumns(newState);
-            }}
-            alignIcon="left"
-            key={`${field.name || i}${editDepth ? `-${editDepth}-` : ''}${uuid}`}
-            icon={isEnabled ? <X /> : <Plus />}
-            className={[
-              `${baseClass}__column`,
-              isEnabled && `${baseClass}__column--active`,
-            ].filter(Boolean).join(' ')}
+  const onDragEnd = useCallback((result) => {
+    if (!result.destination) return;
+    const sourceIndex = result.source.index;
+    const destinationIndex = result.destination.index;
+    moveColumn(sourceIndex, destinationIndex);
+  }, [moveColumn]);
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable
+        droppableId="columns-drop"
+        isDropDisabled={false}
+        direction="horizontal"
+      >
+        {(provided) => (
+          <div
+            className={baseClass}
+            style={{ display: 'flex' }}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
           >
-            {getTranslation(field.label || field.name, i18n)}
-          </Pill>
-        );
-      })}
-    </div>
+            {fields && fields.map((field, i) => {
+              const isEnabled = columns.find((column) => column === field.name);
+              return (
+                <Draggable
+                  key={field.name}
+                  draggableId={field.name}
+                  index={i}
+                  isDragDisabled={false}
+                >
+                  {(providedDrag) => (
+                    <div
+                      id={`col-${i}`}
+                      ref={providedDrag.innerRef}
+                      {...providedDrag.draggableProps}
+                    >
+                      <span
+                        className={`${baseClass}__drag`}
+                        {...providedDrag.dragHandleProps}
+                      >
+                        <DragHandle />
+                      </span>
+                      <Pill
+                        onClick={() => {
+                          let newState = [...columns];
+                          if (isEnabled) {
+                            newState = newState.filter((remainingColumn) => remainingColumn !== field.name);
+                          } else {
+                            newState.unshift(field.name);
+                          }
+
+                          setColumns(newState);
+                        }}
+                        alignIcon="left"
+                        key={`${field.name || i}${editDepth ? `-${editDepth}-` : ''}${uuid}`}
+                        icon={isEnabled ? <X /> : <Plus />}
+                        className={[
+                          `${baseClass}__column`,
+                          isEnabled && `${baseClass}__column--active`,
+                        ].filter(Boolean).join(' ')}
+                      >
+                        {getTranslation(field.label || field.name, i18n)}
+                      </Pill>
+                    </div>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
