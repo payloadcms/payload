852664ffe96a0be41866409681e9e3f3b4a51560
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2023-01-20 22:24:26 +0100

    wip

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,21 +1,22 @@
-import cs from './cs.json';
-import de from './de.json';
-import en from './en.json';
-import es from './es.json';
-import fr from './fr.json';
-import it from './it.json';
-import ja from './ja.json';
-import my from './my.json';
-import nb from './nb.json';
-import nl from './nl.json';
-import pl from './pl.json';
-import pt from './pt.json';
-import ru from './ru.json';
-import sv from './sv.json';
-import th from './th.json';
-import tr from './tr.json';
-import ua from './ua.json';
-import vi from './vi.json';
+import cs from "./cs.json";
+import de from "./de.json";
+import en from "./en.json";
+import es from "./es.json";
+import fr from "./fr.json";
+import hr from "./hr.json";
+import it from "./it.json";
+import ja from "./ja.json";
+import my from "./my.json";
+import nb from "./nb.json";
+import nl from "./nl.json";
+import pl from "./pl.json";
+import pt from "./pt.json";
+import ru from "./ru.json";
+import sv from "./sv.json";
+import th from "./th.json";
+import tr from "./tr.json";
+import ua from "./ua.json";
+import vi from "./vi.json";
 
 export default {
   cs,
@@ -23,6 +24,7 @@ export default {
   en,
   es,
   fr,
+  hr,
   it,
   ja,
   my,
