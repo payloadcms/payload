865f51300df1dda2808894cbf0c6efa9b91a7ade
PatrikKozak <patrik@trbl.design>
2023-01-24 15:36:07 -0500

    chore: updates seeded data

--- examples/preview/cms/src/seed/home.ts
+++ examples/preview/cms/src/seed/home.ts
@@ -5,7 +5,7 @@ export const home = {
       {
         "children": [
           {
-            "text": "This is the home page"
+            "text": "Published Page"
           }
         ],
         "type": "h1"
@@ -28,6 +28,22 @@ export const home = {
           "children": [
             {
               "text": "Enim sit amet venenatis urna cursus eget nunc scelerisque viverra. Dignissim enim sit amet venenatis. Sed augue lacus viverra vitae congue eu consequat. Vulputate enim nulla aliquet porttitor lacus. Vel pretium lectus quam id leo in. Venenatis cras sed felis eget velit. Hendrerit gravida rutrum quisque non tellus orci. Venenatis lectus magna fringilla urna porttitor. Eu sem integer vitae justo eget. Sed nisi lacus sed viverra tellus in hac. Pellentesque habitant morbi tristique senectus et netus et. Velit sed ullamcorper morbi tincidunt ornare massa. Interdum velit laoreet id donec ultrices tincidunt arcu. Egestas integer eget aliquet nibh praesent tristique magna sit. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Tristique senectus et netus et malesuada fames ac turpis egestas. Pharetra sit amet aliquam id diam maecenas ultricies mi. Lacus vel facilisis volutpat est velit egestas dui. Massa ultricies mi quis hendrerit dolor."
+            }
+          ]
+        },
+        {
+          "type": "p",
+          "children": [
+            {
+              "text": "Sed velit dignissim sodales ut eu sem integer vitae justo. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed. Et ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Dui sapien eget mi proin sed. Integer enim neque volutpat ac tincidunt vitae semper. Diam in arcu cursus euismod quis viverra nibh. Risus viverra adipiscing at in tellus. Sed odio morbi quis commodo odio aenean sed adipiscing diam. Diam phasellus vestibulum lorem sed risus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Volutpat ac tincidunt vitae semper quis. Rhoncus aenean vel elit scelerisque. Quisque id diam vel quam elementum. Netus et malesuada fames ac turpis egestas. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique."
+            }
+          ]
+        },
+        {
+          "type": "p",
+          "children": [
+            {
+              "text": "Hendrerit gravida rutrum quisque non tellus. Et sollicitudin ac orci phasellus. Elit eget gravida cum sociis natoque. Ultrices gravida dictum fusce ut placerat orci nulla pellentesque dignissim. Eros donec ac odio tempor orci dapibus ultrices. Malesuada fames ac turpis egestas integer eget aliquet. Mauris ultrices eros in cursus. Purus in mollis nunc sed id semper risus. Nunc mi ipsum faucibus vitae aliquet. Lorem sed risus ultricies tristique nulla aliquet. Diam ut venenatis tellus in. Tellus integer feugiat scelerisque varius. Fusce id velit ut tortor. Vitae tortor condimentum lacinia quis vel eros donec ac."
             }
           ]
         },
