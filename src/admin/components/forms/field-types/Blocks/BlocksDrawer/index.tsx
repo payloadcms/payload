b7aaa53a8a61d9348dac2acabaec3b987d9b5f0c
Jacob Fletcher <jacobsfletch@gmail.com>
2023-01-31 21:02:20 -0500

    chore: adds default drawer header

--- src/admin/components/forms/field-types/Blocks/BlocksDrawer/index.tsx
+++ src/admin/components/forms/field-types/Blocks/BlocksDrawer/index.tsx
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import BlockSearch from './BlockSearch';
 import { Props } from './types';
 import { Drawer } from '../../../../elements/Drawer';
-import { Gutter } from '../../../../elements/Gutter';
 import { getTranslation } from '../../../../../../utilities/getTranslation';
 import { ThumbnailCard } from '../../../../elements/ThumbnailCard';
 import DefaultBlockImage from '../../../../graphics/DefaultBlockImage';
@@ -37,51 +36,49 @@ export const BlocksDrawer: React.FC<Props> = (props) => {
   }, [searchTerm, blocks]);
 
   return (
-    <Drawer slug={drawerSlug}>
-      <Gutter className={`${baseClass}__wrapper`}>
-        <h2>
-          {t('addLabel', { label: getTranslation(labels.singular, i18n) })}
-        </h2>
-        <BlockSearch setSearchTerm={setSearchTerm} />
-        <div className={`${baseClass}__blocks-wrapper`}>
-          <ul className={`${baseClass}__blocks`}>
-            {filteredBlocks?.map((block, index) => {
-              const {
-                labels: blockLabels,
-                slug,
-                imageURL,
-                imageAltText,
-              } = block;
+    <Drawer
+      slug={drawerSlug}
+      title={t('addLabel', { label: getTranslation(labels.singular, i18n) })}
+    >
+      <BlockSearch setSearchTerm={setSearchTerm} />
+      <div className={`${baseClass}__blocks-wrapper`}>
+        <ul className={`${baseClass}__blocks`}>
+          {filteredBlocks?.map((block, index) => {
+            const {
+              labels: blockLabels,
+              slug,
+              imageURL,
+              imageAltText,
+            } = block;
 
-              return (
-                <li
-                  key={index}
-                  className={`${baseClass}__block`}
-                >
-                  <ThumbnailCard
-                    onClick={() => {
-                      addRow(addRowIndex, slug);
-                      closeModal(drawerSlug);
-                    }}
-                    thumbnail={imageURL ? (
-                      <img
-                        src={imageURL}
-                        alt={imageAltText}
-                      />
-                    ) : (
-                      <div className={`${baseClass}__default-image`}>
-                        <DefaultBlockImage />
-                      </div>
-                    )}
-                    label={getTranslation(blockLabels.singular, i18n)}
-                    alignLabel="center"
-                  />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </Gutter>
+            return (
+              <li
+                key={index}
+                className={`${baseClass}__block`}
+              >
+                <ThumbnailCard
+                  onClick={() => {
+                    addRow(addRowIndex, slug);
+                    closeModal(drawerSlug);
+                  }}
+                  thumbnail={imageURL ? (
+                    <img
+                      src={imageURL}
+                      alt={imageAltText}
+                    />
+                  ) : (
+                    <div className={`${baseClass}__default-image`}>
+                      <DefaultBlockImage />
+                    </div>
+                  )}
+                  label={getTranslation(blockLabels.singular, i18n)}
+                  alignLabel="center"
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </Drawer>
   );
 };
