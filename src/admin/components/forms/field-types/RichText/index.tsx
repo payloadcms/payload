5cf4a66da2df1ac5e40245ac81537a4ece15091d
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-02-06 16:34:02 -0500

    chore: removes RichTextProvider

--- src/admin/components/forms/field-types/RichText/index.tsx
+++ src/admin/components/forms/field-types/RichText/index.tsx
@@ -1,15 +1,12 @@
 import React, { Suspense, lazy } from 'react';
 import { ShimmerEffect } from '../../../elements/ShimmerEffect';
-import { RichTextProvider } from './provider';
 import { Props } from './types';
 
 const RichText = lazy(() => import('./RichText'));
 
 const RichTextField: React.FC<Props> = (props) => (
   <Suspense fallback={<ShimmerEffect height="35vh" />}>
-    <RichTextProvider>
-      <RichText {...props} />
-    </RichTextProvider>
+    <RichText {...props} />
   </Suspense>
 );
 
