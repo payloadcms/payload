ce08148ec33e38ebd67a8de6b7971c73da53de4c
stef gootzen <stefgootzen@gmail.com>
2023-03-21 13:55:16 +0100

    refactor: use "mime" instead of "file-type" package

--- src/uploads/getFileByPath.ts
+++ src/uploads/getFileByPath.ts
@@ -1,19 +1,20 @@
 import fs from 'fs';
-import { fromFile } from 'file-type';
 import path from 'path';
+import mime from 'mime';
 import { File } from './types';
 
 const getFileByPath = async (filePath: string): Promise<File> => {
   if (typeof filePath === 'string') {
     const data = fs.readFileSync(filePath);
-    const mimetype = fromFile(filePath);
+    const mimetype = mime.getType(filePath);
+
     const { size } = fs.statSync(filePath);
 
     const name = path.basename(filePath);
 
     return {
       data,
-      mimetype: (await mimetype).mime,
+      mimetype,
       name,
       size,
     };
