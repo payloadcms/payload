fb2fd3e9b7e302d8069bfcb6f3cb698ac7abf0ca
fiona <53770528+fionues@users.noreply.github.com>
2023-03-22 15:18:37 +0100

    fix: DateField admin type (#2256)

--- src/fields/config/types.ts
+++ src/fields/config/types.ts
@@ -170,7 +170,6 @@ export type DateField = FieldBase & {
   admin?: Admin & {
     placeholder?: Record<string, string> | string
     date?: ConditionalDateProps
-    displayFormat?: string
   }
 }
 
