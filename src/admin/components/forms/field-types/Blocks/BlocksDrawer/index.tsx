34017c71728eb67d111dafc8f5f69720a02abcf7
Martin Choutka <martafiixek@gmail.com>
2023-04-24 23:58:18 +0200

    Add types to getBlockLabel params

--- src/admin/components/forms/field-types/Blocks/BlocksDrawer/index.tsx
+++ src/admin/components/forms/field-types/Blocks/BlocksDrawer/index.tsx
@@ -7,12 +7,14 @@ import { Drawer } from '../../../../elements/Drawer';
 import { getTranslation } from '../../../../../../utilities/getTranslation';
 import { ThumbnailCard } from '../../../../elements/ThumbnailCard';
 import DefaultBlockImage from '../../../../graphics/DefaultBlockImage';
+import { i18n } from 'i18next';
+import { Block } from '../../../../../../fields/config/types';
 
 import './index.scss';
 
 const baseClass = 'blocks-drawer';
 
-const getBlockLabel = (block, i18n) => {
+const getBlockLabel = (block: Block, i18n: i18n) => {
   if (typeof block.labels.singular === 'string') return block.labels.singular.toLowerCase();
   if (typeof block.labels.singular === 'object') {
     return getTranslation(block.labels.singular, i18n).toLowerCase();
