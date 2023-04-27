60cf803e8d8289761e2abf3680c1bb53456645e9
James <james@trbl.design>
2023-04-27 16:28:16 -0400

    fix: #43, not deleting old files

--- src/hooks/beforeChange.ts
+++ src/hooks/beforeChange.ts
@@ -11,15 +11,44 @@ interface Args {
 
 export const getBeforeChangeHook =
   ({ collection, adapter }: Args): CollectionBeforeChangeHook<FileData & TypeWithID> =>
-  async ({ req, data }) => {
+  async ({ req, data, originalDoc }) => {
     try {
       const files = getIncomingFiles({ req, data })
 
-      const promises = files.map(async file => {
-        await adapter.handleUpload({ collection, data, req, file })
-      })
+      if (files.length > 0) {
+        // If there is an original doc,
+        // And we have new files,
+        // We need to delete the old files before uploading new
+        if (originalDoc) {
+          let filesToDelete: string[] = []
 
-      await Promise.all(promises)
+          if (typeof originalDoc?.filename === 'string') {
+            filesToDelete.push(originalDoc.filename)
+          }
+
+          if (typeof originalDoc.sizes === 'object') {
+            filesToDelete = filesToDelete.concat(
+              Object.values(originalDoc?.sizes || []).map(
+                resizedFileData => resizedFileData?.filename,
+              ),
+            )
+          }
+
+          const deletionPromises = filesToDelete.map(async filename => {
+            if (filename) {
+              await adapter.handleDelete({ collection, doc: originalDoc, req, filename })
+            }
+          })
+
+          await Promise.all(deletionPromises)
+        }
+
+        const promises = files.map(async file => {
+          await adapter.handleUpload({ collection, data, req, file })
+        })
+
+        await Promise.all(promises)
+      }
     } catch (err: unknown) {
       req.payload.logger.error(
         `There was an error while uploading files corresponding to the collection ${collection.slug} with filename ${data.filename}:`,
