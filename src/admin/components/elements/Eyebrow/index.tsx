72766dba471a087d7413a36b482a60ac2a3f1c14
Eustachio <eustachio.dipaola@gmail.com>
2023-05-10 23:11:24 +0100

    revert some changes

---
+++ src/admin/components/elements/Eyebrow/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import StepNav from '../StepNav';
+import { Props } from './types';
+import { Gutter } from '../Gutter';
+
+import './index.scss';
+
+const baseClass = 'eyebrow';
+
+const Eyebrow: React.FC<Props> = ({ actions }) => (
+  <div className={baseClass}>
+    <Gutter className={`${baseClass}__wrap`}>
+      <StepNav />
+      {actions}
+    </Gutter>
+  </div>
+);
+
+export default Eyebrow;
