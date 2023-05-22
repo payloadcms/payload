666c2383bad3d6e5b6cbd1ae0da16fd4436b00ff
Dan Ribbens <dan.ribbens@gmail.com>
2023-05-22 13:58:25 -0400

    chore: lint fix

--- src/fields/validations.ts
+++ src/fields/validations.ts
@@ -6,6 +6,7 @@ import {
   CodeField,
   DateField,
   EmailField,
+  fieldAffectsData,
   JSONField,
   NumberField,
   PointField,
@@ -18,7 +19,6 @@ import {
   TextField,
   UploadField,
   Validate,
-  fieldAffectsData,
 } from './config/types';
 import canUseDOM from '../utilities/canUseDOM';
 import { isValidID } from '../utilities/isValidID';
@@ -319,7 +319,7 @@ export const relationship: Validate<unknown, unknown, RelationshipField> = async
         requestedID = val.value;
       }
 
-      if (requestedID === null) return false
+      if (requestedID === null) return false;
 
       const idField = payload.collections[collection]?.config?.fields?.find((field) => fieldAffectsData(field) && field.name === 'id');
       let type;
