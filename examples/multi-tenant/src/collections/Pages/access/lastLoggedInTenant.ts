908d5747a8892c946da87c0a2bb3d19ff116c33b
Jacob Fletcher <jacobsfletch@gmail.com>
2023-05-30 13:10:14 -0400

    chore: allows super-admins to view as tenant in multi-tenant example (#2719)

--- examples/multi-tenant/src/collections/Pages/access/lastLoggedInTenant.ts
+++ examples/multi-tenant/src/collections/Pages/access/lastLoggedInTenant.ts
@@ -1,6 +1,4 @@
 import type { Access } from 'payload/types'
 
-import { checkUserRoles } from '../../utilities/checkUserRoles'
-
 export const lastLoggedInTenant: Access = ({ req: { user }, data }) =>
-  checkUserRoles(['super-admin'], user) || user?.lastLoggedInTenant?.id === data?.id
+  user?.lastLoggedInTenant?.id === data?.id
