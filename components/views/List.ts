dc942fefb5f51ca651a20a7737e722480f2c8235
Alessio Gravili <alessio@gravili.de>
2023-06-06 11:15:21 +0200

    fix: incorrect props exposed

--- components/views/List.ts
+++ components/views/List.ts
@@ -1,2 +1,2 @@
 export { default as List } from '../../dist/admin/components/views/collections/List/Default';
-export type { Props } from '../../dist/admin/components/views/collections/Edit/types';
+export type { Props } from '../../dist/admin/components/views/collections/List/types';
