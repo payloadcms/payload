9d06f023e4c9ae9a7a2cbed15d5b177a15bdd6e9
Alessio Gravili <alessio@gravili.de>
2023-06-08 20:04:09 +0200

    add jsdocs

--- src/express/types.ts
+++ src/express/types.ts
@@ -24,10 +24,14 @@ export declare type PayloadRequest<U = any> = Request & {
    * - Configuration from payload-config.ts
    * - MongoDB model for this collection
    * - GraphQL type metadata
-   * */
+   */
   collection?: Collection;
   /** What triggered this request */
   payloadAPI?: 'REST' | 'local' | 'GraphQL';
+  /** payloadContext allows you to pass your own data to the request object as context
+   * This is useful for, for example, passing data from a beforeChange hook to an afterChange hook.
+   * payoadContext can also be fully typed using declare module (more info in the docs)
+   */
   payloadContext: PayloadRequestContext;
   /** Uploaded files */
   files?: {
