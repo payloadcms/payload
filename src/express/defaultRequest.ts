13bfe732396bf2d393fd2dd378c4a8a844de5c3a
Alessio Gravili <alessio@gravili.de>
2023-06-08 21:13:45 +0200

    fix: populateDefaultRequest not working for undefined payloadRequest

--- src/express/defaultRequest.ts
+++ src/express/defaultRequest.ts
@@ -1,7 +1,12 @@
 import type { PayloadRequest } from './types';
 
-export function populateDefaultRequest(payloadRequest: PayloadRequest) {
-  if (!payloadRequest.payloadContext) {
+export function populateDefaultRequest(payloadRequest: PayloadRequest|undefined) {
+  if (!payloadRequest) {
+    // eslint-disable-next-line no-param-reassign
+    payloadRequest = {
+      payloadContext: {},
+    } as PayloadRequest;
+  } else if (!payloadRequest.payloadContext) {
     // eslint-disable-next-line no-param-reassign
     payloadRequest.payloadContext = {}; // Mutate directly to avoid copying memory. As this is not a primitive, the scope of the mutation is not limited to this function but should also be reflected in the calling function.
   }
