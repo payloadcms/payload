9664e4b96f4bce3e329a350922a79b531c2b62d4
Tylan Davis <89618855+tylandavis@users.noreply.github.com>
2023-06-14 10:21:23 -0400

    chore: version diff styles (#2824)
    Co-authored-by: Tylan Davis <tylan@Tylans-MacBook-Pro.local>

--- src/admin/components/views/Version/RenderFieldsToDiff/fields/styles.ts
+++ src/admin/components/views/Version/RenderFieldsToDiff/fields/styles.ts
@@ -2,6 +2,7 @@ export const diffStyles = {
   variables: {
     light: {
       diffViewerBackground: 'transparent',
+      diffViewerColor: 'var(--theme-text)',
       addedBackground: 'var(--theme-success-100)',
       addedColor: 'var(--theme-success-900)',
       removedBackground: 'var(--theme-error-100)',
@@ -12,6 +13,7 @@ export const diffStyles = {
     },
     dark: {
       diffViewerBackground: 'transparent',
+      diffViewerColor: 'var(--theme-text)',
       addedBackground: 'var(--theme-success-900)',
       addedColor: 'var(--theme-success-100)',
       removedBackground: 'var(--theme-error-900)',
