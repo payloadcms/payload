4dd703a6bff0ab7d06af234baa975553bd62f176
James <james@trbl.design>
2023-06-19 10:13:47 -0400

    fix: slow like queries with lots of records

--- test/collections-rest/int.spec.ts
+++ test/collections-rest/int.spec.ts
@@ -648,6 +648,22 @@ describe('collections-rest', () => {
         expect(result.totalDocs).toEqual(1);
       });
 
+      it('like - cyrillic characters in multiple words', async () => {
+        const post1 = await createPost({ title: 'привет, это тест полезной нагрузки' });
+
+        const { status, result } = await client.find<Post>({
+          query: {
+            title: {
+              like: 'привет нагрузки',
+            },
+          },
+        });
+
+        expect(status).toEqual(200);
+        expect(result.docs).toEqual([post1]);
+        expect(result.totalDocs).toEqual(1);
+      });
+
       it('like - partial word match', async () => {
         const post = await createPost({ title: 'separate words should partially match' });
 
