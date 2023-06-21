b0b30f5aef05590cc40ada9331b9a8e9b83ea163
Dan Ribbens <DanRibbens@users.noreply.github.com>
2023-06-21 16:13:05 -0400

    mongoose adapter create operation (#2882)

--- src/database/types.ts
+++ src/database/types.ts
@@ -188,11 +188,9 @@ export type FindOneArgs = {
 
 type FindOne = (args: FindOneArgs) => Promise<PaginatedDocs>
 
-type CreateArgs = {
+export type CreateArgs = {
   collection: string
   data: Record<string, unknown>
-  draft?: boolean
-  locale?: string
 }
 
 type Create = (args: CreateArgs) => Promise<Document>
