12af9e8bad3d5366c4ce565bb0df8454b3beee6e
Alessio Gravili <alessio@gravili.de>
2023-06-21 21:23:36 +0200

    _id => id

--- src/collections/operations/findByID.ts
+++ src/collections/operations/findByID.ts
@@ -79,7 +79,7 @@ async function findByID<T extends TypeWithID>(
   // Find by ID
   // /////////////////////////////////////
 
-  // if (!findArgs.where?.$and[0].id) throw new NotFound(t); // TODO: doesn't work. Previously, it was if (!query.$and[0]._id) throw new NotFound(t);
+  if (!findArgs.where.and[0].id) throw new NotFound(t);
 
   if (!req.findByID) req.findByID = {};
 
