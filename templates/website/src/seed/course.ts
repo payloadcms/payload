49f117e220318701004b686a28b4b9c8ab9b8516
Jacob Fletcher <jacobsfletch@gmail.com>
2023-06-28 12:48:08 -0400

    chore: adds website template (#2928)

---
+++ templates/website/src/seed/course.ts
@@ -0,0 +1,40 @@
+export const courseImage = {
+  alt: 'Online Course',
+  caption: [
+    {
+      children: [
+        {
+          text: 'Photo by ',
+        },
+        {
+          type: 'link',
+          linkType: 'custom',
+          url: 'https://unsplash.com/@christinhumephoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+          newTab: true,
+          children: [
+            {
+              text: 'Christin Hume',
+            },
+          ],
+        },
+        {
+          text: ' on ',
+        },
+        {
+          type: 'link',
+          linkType: 'custom',
+          url: 'https://unsplash.com/photos/Hcfwew744z4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+          newTab: true,
+          children: [
+            {
+              text: 'Unsplash',
+            },
+          ],
+        },
+        {
+          text: '.',
+        },
+      ],
+    },
+  ],
+}
