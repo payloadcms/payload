5b7756e266b10481dc4717e298fafe73f1944c70
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-07-07 11:34:19 -0400

    chore: fixes auth example protectRoles function

--- examples/auth/cms/src/collections/hooks/protectRoles.ts
+++ examples/auth/cms/src/collections/hooks/protectRoles.ts
@@ -3,7 +3,6 @@ import type { FieldHook } from 'payload/types'
 
 // ensure there is always a `user` role
 // do not let non-admins change roles
-// eslint-disable-next-line consistent-return
 export const protectRoles: FieldHook<User & { id: string }> = async ({ req, data }) => {
   const isAdmin = req.user?.roles.includes('admin') || data.email === 'demo@payloadcms.com' // for the seed script
 
@@ -11,5 +10,8 @@ export const protectRoles: FieldHook<User & { id: string }> = async ({ req, data
     return ['user']
   }
 
-  return [...(data?.roles || []), 'user']
+  const userRoles = new Set(data?.roles || [])
+  userRoles.add('user')
+
+  return [...userRoles]
 }
