ef5174305bd2575b04d6bdd89b779d0661b13aac
Lucas Blancas <lablancas@gmail.com>
2023-07-14 10:22:28 -0500

    refactor: 💡 add GeoJSON enum type for Polygon

--- src/graphql/schema/types.ts
+++ src/graphql/schema/types.ts
@@ -4,18 +4,32 @@ import {
   GraphQLObjectType,
   GraphQLInputObjectType,
   GraphQLString,
+  GraphQLEnumType,
 } from 'graphql';
 
 const Position = new GraphQLList(GraphQLFloat);
 
 const LinearRing = new GraphQLList(Position);
 
+export const enums = {
+  geojson: new GraphQLEnumType({
+    name: 'GeoJSONType',
+    description: 'The `GeoJSONType` enum represents a subset of the Geometry Object types as defined by [rfc7946#section-3.1](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1)',
+    values: ['Polygon'].reduce((acc, next) => {
+      return {
+        ...acc,
+        [next]: { value: next }
+      }
+    }, {}),
+  }),
+}
+
 export const Polygon = new GraphQLObjectType({
   name: 'Polygon',
   description: 'The `Polygon` type represents a GeoJSON polygon as defined by [rfc7946#section-3.1.6](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.6)',
   fields: {
     type: {
-      type: GraphQLString,
+      type: enums.geojson,
     },
     coordinates: {
       type: new GraphQLList(LinearRing),
@@ -28,7 +42,7 @@ export const PolygonInput = new GraphQLInputObjectType({
   description: 'The `PolygonInput` input type represents a GeoJSON polygon as defined by [rfc7946#section-3.1.6](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.6)',
   fields: {
     type: {
-      type: GraphQLString,
+      type: enums.geojson,
     },
     coordinates: {
       type: new GraphQLList(LinearRing),
