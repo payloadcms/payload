6ad0e51fe5ea73d0638df0cf88a209cc3a229aa5
Lucas Blancas <lablancas@gmail.com>
2023-07-14 10:22:03 -0500

    refactor: 💡 add descriptions for Polygon and PolygonInput types

--- src/graphql/schema/types.ts
+++ src/graphql/schema/types.ts
@@ -12,6 +12,7 @@ const LinearRing = new GraphQLList(Position);
 
 export const Polygon = new GraphQLObjectType({
   name: 'Polygon',
+  description: 'The `Polygon` type represents a GeoJSON polygon as defined by [rfc7946#section-3.1.6](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.6)',
   fields: {
     type: {
       type: GraphQLString,
@@ -24,6 +25,7 @@ export const Polygon = new GraphQLObjectType({
 
 export const PolygonInput = new GraphQLInputObjectType({
   name: 'PolygonInput',
+  description: 'The `PolygonInput` input type represents a GeoJSON polygon as defined by [rfc7946#section-3.1.6](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.6)',
   fields: {
     type: {
       type: GraphQLString,
