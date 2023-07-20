2da4b83d0c9ba3287c8dae68948a7741af399f16
Alessio Gravili <alessio@gravili.de>
2023-07-20 03:14:39 +0200

    chore: add jsDocs

--- src/graphql/schema/withOperators.ts
+++ src/graphql/schema/withOperators.ts
@@ -197,11 +197,24 @@ const defaults: DefaultsType = {
 
 const listOperators = ['in', 'not_in', 'all'];
 
+/**
+ * In GraphQL, you can use "where" as an argument to filter a collection. Example:
+ * { Posts(where: { title: { equals: "Hello" } }) { text } }
+ * This function defines the operators for a field's condition in the "where" argument of the collection (it thus gets called for every field).
+ * For example, in the example above, it would control that
+ * - "equals" is a valid operator for the "title" field
+ * - the accepted type of the "equals" argument has to be a string.
+ *
+ * @param field the field for which their valid operators inside a "where" argument is being defined
+ * @param parentName the name of the parent field (if any)
+ * @returns all the operators (including their types) which can be used as a condition for a given field inside a where
+ */
 export const withOperators = (field: FieldAffectingData, parentName: string): GraphQLInputObjectType => {
   if (!defaults?.[field.type]) throw new Error(`Error: ${field.type} has no defaults configured.`);
 
   const name = `${combineParentName(parentName, field.name)}_operator`;
 
+  // Get the default operators for the field type which are hard-coded above
   const fieldOperators = [...defaults[field.type].operators];
 
   if (!('required' in field) || !field.required) {
@@ -215,6 +228,7 @@ export const withOperators = (field: FieldAffectingData, parentName: string): Gr
   return new GraphQLInputObjectType({
     name,
     fields: fieldOperators.reduce((objectTypeFields, operator) => {
+      // Get the type of the operator. It can be either static, or dynamic (=> a function)
       let gqlType: GraphQLType = typeof operator.type === 'function'
         ? operator.type(field, parentName)
         : operator.type;
