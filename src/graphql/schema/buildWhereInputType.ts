6816293b2fc1c2aa240d409675c9d54fd2290f89
Alessio Gravili <alessio@gravili.de>
2023-07-20 03:21:32 +0200

    chore: add more jsDocs

--- src/graphql/schema/buildWhereInputType.ts
+++ src/graphql/schema/buildWhereInputType.ts
@@ -15,13 +15,20 @@ import formatName from '../utilities/formatName';
 import { withOperators } from './withOperators';
 import fieldToSchemaMap from './fieldToWhereInputSchemaMap';
 
-// buildWhereInputType is similar to buildObjectType and operates
-// on a field basis with a few distinct differences.
-//
-// 1. Everything needs to be a GraphQLInputObjectType or scalar / enum
-// 2. Relationships, groups, repeaters and flex content are not
-//    directly searchable. Instead, we need to build a chained pathname
-//    using dot notation so MongoDB can properly search nested paths.
+/** This does as the function name suggests. It builds a where GraphQL input type
+ * for all the fields which are passed to the function.
+ * Each field has different operators which may be valid for a where input type.
+ * For example, a text field may have a "contains" operator, but a number field
+ * may not.
+ *
+ * buildWhereInputType is similar to buildObjectType and operates
+ * on a field basis with a few distinct differences.
+ *
+ * 1. Everything needs to be a GraphQLInputObjectType or scalar / enum
+ * 2. Relationships, groups, repeaters and flex content are not
+ *    directly searchable. Instead, we need to build a chained pathname
+ *    using dot notation so MongoDB can properly search nested paths.
+ */
 const buildWhereInputType = (name: string, fields: Field[], parentName: string): GraphQLInputObjectType => {
   // This is the function that builds nested paths for all
   // field types with nested paths.
