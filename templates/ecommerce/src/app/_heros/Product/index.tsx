db94e3ec493f7721670e9f331be96da3f54f7c0f
Jacob Fletcher <jacobsfletch@gmail.com>
2023-07-22 13:19:11 -0400

    chore: renders warning message on product pages that are not linked to stripe

--- templates/ecommerce/src/app/_heros/Product/index.tsx
+++ templates/ecommerce/src/app/_heros/Product/index.tsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
+import Link from 'next/link'
 
 import { Product } from '../../../payload/payload-types'
 import { AddToCartButton } from '../../_components/AddToCartButton'
 import { Gutter } from '../../_components/Gutter'
 import { Media } from '../../_components/Media'
+import { Message } from '../../_components/Message'
 import { Price } from '../../_components/Price'
 import RichText from '../../_components/RichText'
 
@@ -12,43 +14,69 @@ import classes from './index.module.scss'
 export const ProductHero: React.FC<{
   product: Product
 }> = ({ product }) => {
-  const { title, categories, meta: { image: metaImage, description } = {} } = product
+  const {
+    id,
+    stripeProductID,
+    title,
+    categories,
+    meta: { image: metaImage, description } = {},
+  } = product
 
   return (
-    <Gutter className={classes.productHero}>
-      <div className={classes.content}>
-        <div className={classes.categories}>
-          {categories?.map((category, index) => {
-            const { title: categoryTitle } = category
-
-            const titleToUse = categoryTitle || 'Untitled category'
-
-            const isLast = index === categories.length - 1
-
-            return (
-              <Fragment key={index}>
-                {titleToUse}
-                {!isLast && <Fragment>, &nbsp;</Fragment>}
+    <Fragment>
+      {!stripeProductID && (
+        <Gutter>
+          <Message
+            className={classes.message}
+            warning={
+              <Fragment>
+                {'This product is not yet connected to Stripe. To link this product, '}
+                <Link
+                  href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}
+                >
+                  navigate to the admin dashboard
+                </Link>
+                {'.'}
               </Fragment>
-            )
-          })}
+            }
+          />
+        </Gutter>
+      )}
+      <Gutter className={classes.productHero}>
+        <div className={classes.content}>
+          <div className={classes.categories}>
+            {categories?.map((category, index) => {
+              const { title: categoryTitle } = category
+
+              const titleToUse = categoryTitle || 'Untitled category'
+
+              const isLast = index === categories.length - 1
+
+              return (
+                <Fragment key={index}>
+                  {titleToUse}
+                  {!isLast && <Fragment>, &nbsp;</Fragment>}
+                </Fragment>
+              )
+            })}
+          </div>
+          <h1 className={classes.title}>{title}</h1>
+          {description && <p className={classes.description}>{description}</p>}
+          <Price product={product} button={false} />
+          <AddToCartButton product={product} className={classes.addToCartButton} />
         </div>
-        <h1 className={classes.title}>{title}</h1>
-        {description && <p className={classes.description}>{description}</p>}
-        <Price product={product} button={false} />
-        <AddToCartButton product={product} className={classes.addToCartButton} />
-      </div>
-      <div className={classes.media}>
-        <div className={classes.mediaWrapper}>
-          {!metaImage && <div className={classes.placeholder}>No image</div>}
-          {metaImage && typeof metaImage !== 'string' && (
-            <Media imgClassName={classes.image} resource={metaImage} fill />
+        <div className={classes.media}>
+          <div className={classes.mediaWrapper}>
+            {!metaImage && <div className={classes.placeholder}>No image</div>}
+            {metaImage && typeof metaImage !== 'string' && (
+              <Media imgClassName={classes.image} resource={metaImage} fill />
+            )}
+          </div>
+          {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
+            <RichText content={metaImage.caption} className={classes.caption} />
           )}
         </div>
-        {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
-          <RichText content={metaImage.caption} className={classes.caption} />
-        )}
-      </div>
-    </Gutter>
+      </Gutter>
+    </Fragment>
   )
 }
