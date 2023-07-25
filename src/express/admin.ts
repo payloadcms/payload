e71866856fffefcfb61dd3d29135cccb66939a62
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2023-07-25 10:16:55 -0400

    fix: admin route not mounting on production serve (#3071)

--- src/express/admin.ts
+++ src/express/admin.ts
@@ -3,7 +3,7 @@ import { Payload } from '../payload';
 async function initAdmin(ctx: Payload): Promise<void> {
   if (!ctx.config.admin.disable) {
     if (process.env.NODE_ENV === 'production') {
-      ctx.express.use(await ctx.config.admin.bundler.serve(ctx));
+      ctx.express.use(ctx.config.routes.admin, await ctx.config.admin.bundler.serve(ctx));
     } else {
       ctx.express.use(await ctx.config.admin.bundler.dev(ctx));
     }
