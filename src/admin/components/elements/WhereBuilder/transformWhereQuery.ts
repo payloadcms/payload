9eaa9407ccb31f69095c8be6f6907d93e84c1ded
Alessio Gravili <alessio@gravili.de>
2023-07-26 22:33:19 +0200

    chore: add type to import

--- src/admin/components/elements/WhereBuilder/transformWhereQuery.ts
+++ src/admin/components/elements/WhereBuilder/transformWhereQuery.ts
@@ -1,4 +1,4 @@
-import { Where } from '../../../../types';
+import type { Where } from '../../../../types';
 
 /**
  * Something like [or][0][and][0][text][equals]=example%20post will work and pass through the validateWhereQuery check.
