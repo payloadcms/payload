6355d732be38abb9c55c5be4eeb0281c558e9621
Elliot DeNolf <denolfe@gmail.com>
2023-08-01 11:27:55 -0400

    chore: drop pg database when PAYLOAD_DROP_DATABASE

--- packages/db-postgres/src/connect.ts
+++ packages/db-postgres/src/connect.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import type { Connect } from 'payload/dist/database/types';
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
-import { Client, Pool } from 'pg';
+import { Client, ClientConfig, Pool, PoolConfig } from 'pg';
+import { dropdb } from 'pgtools';
 
 import type { PostgresAdapter } from '.';
 
@@ -12,20 +12,38 @@ export const connect: Connect = async function connect(
   let db: NodePgDatabase<Record<string, never>>;
 
   try {
+    let config: string | ClientConfig | PoolConfig;
     if ('pool' in this && this.pool !== false) {
       const pool = new Pool(this.pool);
       db = drizzle(pool);
+      config = this.pool;
     }
 
     if ('client' in this && this.client !== false) {
       const client = new Client(this.client);
       await client.connect();
       db = drizzle(client);
+      config = this.client;
     }
 
     if (process.env.PAYLOAD_DROP_DATABASE === 'true') {
       this.payload.logger.info('---- DROPPING DATABASE ----');
-      // NEED TO DROP DATABASE HERE
+
+      // Get database name from config
+      let databaseName: string | undefined;
+      if (typeof config === 'string') {
+        databaseName = config.split('/').pop() || '';
+      } else {
+        databaseName = config.database;
+      }
+
+      if (!databaseName) {
+        throw new Error(
+          'Cannot drop database. Database name not found in config.',
+        );
+      }
+
+      await dropdb(config, databaseName);
       this.payload.logger.info('---- DROPPED DATABASE ----');
     }
   } catch (err) {
