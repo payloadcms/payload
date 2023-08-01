79bf9c64f8abf4fef5987586075e6d3f98ca6796
Jessica Boezwinkle <jessica@trbl.design>
2023-08-01 11:46:17 +0100

    feat: opens upload relationship in drawer instead of upload URL

--- src/admin/components/elements/FileDetails/Meta/index.tsx
+++ src/admin/components/elements/FileDetails/Meta/index.tsx
@@ -5,13 +5,24 @@ import formatFilesize from '../../../../../uploads/formatFilesize';
 import { Props } from './types';
 
 import './index.scss';
+import { useDocumentDrawer } from '../../DocumentDrawer';
 
 const baseClass = 'file-meta';
 
 const Meta: React.FC<Props> = (props) => {
   const {
-    filename, filesize, width, height, mimeType, staticURL, url,
+    filename, filesize, width, height, mimeType, staticURL, url, id, collection,
   } = props;
+
+  const openInDrawer = Boolean(id && collection);
+
+  const [
+    DocumentDrawer,
+    DocumentDrawerToggler,
+    { isDrawerOpen },
+  ] = useDocumentDrawer({
+    id, collectionSlug: collection,
+  });
 
   const { serverURL } = useConfig();
 
@@ -20,13 +31,22 @@ const Meta: React.FC<Props> = (props) => {
   return (
     <div className={baseClass}>
       <div className={`${baseClass}__url`}>
-        <a
-          href={fileURL}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {filename}
-        </a>
+        {openInDrawer && <DocumentDrawer />}
+        {openInDrawer && !isDrawerOpen
+          ? (
+            <DocumentDrawerToggler>
+              {filename}
+            </DocumentDrawerToggler>
+          )
+          : (
+            <a
+              href={fileURL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {filename}
+            </a>
+          )}
         <CopyToClipboard
           value={fileURL}
           defaultMessage="Copy URL"
