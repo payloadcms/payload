a0b13a5b01fa0d7f4c4dffd1895bfe507e5c676d
Mark Barton <90132604+MarkAtOmniux@users.noreply.github.com>
2023-08-14 16:08:50 +0100

    feat: text alignment for richtext editor (#2803)
    * Update isActive.tsx
    
    This change allows us to define toggling of custom types in Slate. Specifically, this fixes the ability to toggle Alignment on nodes that use other active elements.
    
    isElementActive(editor, format, TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type');
    
    Type is the default for elements, allowing us to use a custom field lets us greater extend the functionality of Slate in Payload without causing any breaking changes
    
    * Update toggle.tsx
    
    Added to toggleElement public function
    
    * Update isActive.tsx
    
    * Update toggle.tsx
    
    Added Rich Text Alignment, updated toggle function, added tests and doc updates
    
    * added margin to void elements
    
    * fix: list alignment
    
    * removed textAlign from elements and added docs
    
    * chore: fix typo
    
    ---------
    
    Co-authored-by: Alessio Gravili <alessio@gravili.de>

---
+++ src/admin/components/icons/AlignRight/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AlignRightIcon: React.FC = () => (
+  <svg
+    viewBox="0 0 1024 1024"
+    fill="currentColor"
+    height="1em"
+    width="1em"
+  >
+    <path d="M904 158H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 424H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 212H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0-424H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z" />
+  </svg>
+);
+
+export default AlignRightIcon;
