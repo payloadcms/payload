f01e432347057558fddab3aedb8cc861cf747119
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-08-16 12:24:27 -0400

    fix: ensures initial checkboxInput checked prop is not undefined

--- src/admin/components/views/collections/List/SelectRow/index.tsx
+++ src/admin/components/views/collections/List/SelectRow/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useSelection } from '../SelectionProvider';
+import { CheckboxInput } from '../../../../forms/field-types/Checkbox/Input';
 
 import './index.scss';
-import { CheckboxInput } from '../../../../forms/field-types/Checkbox/Input';
 
 const SelectRow: React.FC<{ id: string | number }> = ({ id }) => {
   const { selected, setSelection } = useSelection();
 
   return (
     <CheckboxInput
-      checked={selected[id]}
+      checked={selected[id] ?? false}
       onToggle={() => setSelection(id)}
     />
   );
