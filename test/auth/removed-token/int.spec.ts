590451fd78e6b145a7adf4aacddf0531fd48465d
Alessio Gravili <alessio@gravili.de>
2023-08-22 17:51:59 +0200

    chore: spelling improvements

--- test/auth/removed-token/int.spec.ts
+++ test/auth/removed-token/int.spec.ts
@@ -25,28 +25,28 @@ describe('Remove token from auth responses', () => {
     await payload.mongoMemoryServer.stop();
   });
 
-  it('should not be token in response from /login', async () => {
+  it('should not include token in response from /login', async () => {
     const { status, data } = await client.endpoint(`/api/${collectionSlug}/login`, 'post', devUser);
     expect(status).toBe(200);
     expect(data.token).not.toBeDefined();
     expect(data.user.email).toBeDefined();
   });
 
-  it('should not be token in response from /me', async () => {
+  it('should not include token in response from /me', async () => {
     const { status, data } = await client.endpointWithAuth(`/api/${collectionSlug}/me`);
     expect(status).toBe(200);
     expect(data.token).not.toBeDefined();
     expect(data.user.email).toBeDefined();
   });
 
-  it('should not be token in response from /refresh-token', async () => {
+  it('should not include token in response from /refresh-token', async () => {
     const { status, data } = await client.endpointWithAuth(`/api/${collectionSlug}/refresh-token`, 'post');
     expect(status).toBe(200);
     expect(data.refreshedToken).not.toBeDefined();
     expect(data.user.email).toBeDefined();
   });
 
-  it('should not be token in response from /reset-password', async () => {
+  it('should not include token in response from /reset-password', async () => {
     const token = await payload.forgotPassword({
       collection: collectionSlug,
       data: { email: devUser.email },
