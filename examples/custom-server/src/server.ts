7647905c1cecd5e882f1a2261935b3585ce04e5d
Jacob Fletcher <jacobsfletch@gmail.com>
2023-08-24 01:07:03 -0400

    chore(examples): handles post requests in custom server example

--- examples/custom-server/src/server.ts
+++ examples/custom-server/src/server.ts
@@ -25,7 +25,7 @@ const start = async (): Promise<void> => {
     },
   })
 
-  if (process.env.PAYLOAD_SEED === 'true') {
+  if (process.env.PAYLOAD_PUBLIC_SEED === 'true') {
     payload.logger.info('---- SEEDING DATABASE ----')
     await seed(payload)
   }
@@ -47,7 +47,7 @@ const start = async (): Promise<void> => {
 
   const nextHandler = nextApp.getRequestHandler()
 
-  app.get('*', (req, res) => nextHandler(req, res))
+  app.use((req, res) => nextHandler(req, res))
 
   nextApp.prepare().then(() => {
     payload.logger.info('Next.js started')
