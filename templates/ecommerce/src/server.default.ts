cf4fabbd593d071724685afddc6393c3e54c9e91
Jacob Fletcher <jacobsfletch@gmail.com>
2023-08-25 15:39:07 -0400

    chore(templates): removes payload seed logger from default server

--- templates/ecommerce/src/server.default.ts
+++ templates/ecommerce/src/server.default.ts
@@ -33,7 +33,6 @@ const start = async (): Promise<void> => {
   })
 
   if (process.env.PAYLOAD_SEED === 'true') {
-    payload.logger.info('---- SEEDING DATABASE ----')
     await seed(payload)
     process.exit()
   }
