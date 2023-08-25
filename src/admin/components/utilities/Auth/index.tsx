ec5eaa91e482d1cda3c4d524b5b732058679e88f
Miguel Victor <miguelvictor.remulta@outlook.com>
2023-08-25 18:34:08 +0200

    Updated code to pass e2e tests

--- src/admin/components/utilities/Auth/index.tsx
+++ src/admin/components/utilities/Auth/index.tsx
@@ -152,17 +152,11 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
           if (json?.token) {
             setToken(json.token);
-          } else {
-            setToken(null);
           }
 
           if (json?.user) {
             setUser(json.user);
-          } else {
-            setUser(null);
-          }
-
-          if (!user && autoLogin && autoLogin.prefillOnly !== true) {
+          } else if (!user && autoLogin && autoLogin.prefillOnly !== true) {
             // auto log-in with the provided autoLogin credentials. This is used in dev mode
             // so you don't have to log in over and over again
             const autoLoginResult = await requests.post(`${serverURL}${api}/${userSlug}/login`, {
