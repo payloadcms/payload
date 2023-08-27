2aa76b5dabebbd527838155fde5aa9c905c73c42
Alessio Gravili <alessio@gravili.de>
2023-08-27 20:06:14 +0200

    fix: date-fns import

--- packages/payload/src/admin/utilities/formatDate/index.ts
+++ packages/payload/src/admin/utilities/formatDate/index.ts
@@ -1,9 +1,9 @@
 import { format } from 'date-fns';
-import * as Locale from 'date-fns/locale';
+import * as Locales from "date-fns/locale/index.js";
 import { getSupportedDateLocale } from './getSupportedDateLocale.js';
 
 export const formatDate = (date: Date | number | string | undefined, pattern: string, locale?: string): string => {
   const theDate = new Date(date);
-  const currentLocale = Locale[getSupportedDateLocale(locale)];
+  const currentLocale = Locales[getSupportedDateLocale(locale)];
   return format(theDate, pattern, { locale: currentLocale });
 };
