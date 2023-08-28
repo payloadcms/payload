32e7c56a0d4eadde3aa05f6eda0f5348eeeff163
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-08-28 11:24:24 -0400

    chore(example): adjusts custom server example with pattern to utilize local api

--- examples/custom-server/src/server.ts
+++ examples/custom-server/src/server.ts
@@ -8,27 +8,22 @@ dotenv.config({
 })
 
 import express from 'express'
-import payload from 'payload'
 
-import { seed } from './seed'
+import { getPayloadClient } from './getPayload'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 const start = async (): Promise<void> => {
-  await payload.init({
-    secret: process.env.PAYLOAD_SECRET || '',
-    mongoURL: process.env.MONGODB_URI || '',
-    express: app,
-    onInit: () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async newPayload => {
+        newPayload.logger.info(`Payload Admin URL: ${newPayload.getAdminURL()}`)
+      },
     },
+    seed: process.env.PAYLOAD_SEED === 'true',
   })
-
-  if (process.env.PAYLOAD_SEED === 'true') {
-    payload.logger.info('---- SEEDING DATABASE ----')
-    await seed(payload)
-  }
 
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
