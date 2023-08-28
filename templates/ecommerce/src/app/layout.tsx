2c7a15ceca893b4789aaa3d587293918b67cc163
Jacob Fletcher <jacobsfletch@gmail.com>
2023-08-28 16:11:11 -0400

    chore(templates/ecommerce): uses svg favicon

--- templates/ecommerce/src/app/layout.tsx
+++ templates/ecommerce/src/app/layout.tsx
@@ -15,6 +15,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en" suppressHydrationWarning>
       <head>
         <InitTheme />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body>
         <Providers>
