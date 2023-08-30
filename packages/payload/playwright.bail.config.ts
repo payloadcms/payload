f1d3f24ea76f921e5bf83f3afd80ca97eb8d5136
Alessio Gravili <alessio@gravili.de>
2023-08-30 13:47:14 +0200

    fix: playwright.bail.config.ts import

--- packages/payload/playwright.bail.config.ts
+++ packages/payload/playwright.bail.config.ts
@@ -1,9 +1,9 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import baseConfig from './playwright.config';
+import type { PlaywrightTestConfig } from '@playwright/test'
+import baseConfig from './playwright.config.js'
 
 const config: PlaywrightTestConfig = {
   ...baseConfig,
   maxFailures: 1,
-};
+}
 
-export default config;
+export default config
