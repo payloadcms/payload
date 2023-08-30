966d126054e1456510e4bace894fa9c046d3e2ae
Elliot DeNolf <denolfe@gmail.com>
2023-08-29 22:05:36 -0400

    chore: move overrides into packages/payload

--- packages/eslint-config-payload/eslint-config/index.cjs
+++ packages/eslint-config-payload/eslint-config/index.cjs
@@ -39,30 +39,4 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/consistent-type-imports': 'warn',
   },
-  overrides: [
-    {
-      files: ['test/**/int.spec.ts'],
-      rules: {
-        'jest/prefer-strict-equal': 'off',
-      },
-    },
-    {
-      files: ['test/**/e2e.spec.ts'],
-      extends: ['plugin:playwright/playwright-test'],
-      rules: {
-        'jest/consistent-test-it': 'off',
-        'jest/require-top-level-describe': 'off',
-        'jest/no-test-callback': 'off',
-        'jest/prefer-strict-equal': 'off',
-        'jest/expect-expect': 'off',
-        'jest-dom/prefer-to-have-attribute': 'off',
-      },
-    },
-    {
-      files: ['*.e2e.ts'],
-      rules: {
-        'jest/expect-expect': 'off',
-      },
-    },
-  ],
 };
