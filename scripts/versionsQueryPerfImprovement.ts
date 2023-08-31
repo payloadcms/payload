e79a84d20023d7475b07491787272a270dc580fc
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-08-31 16:56:21 -0400

    chore: migration script

---
+++ scripts/versionsQueryPerfImprovement.ts
@@ -0,0 +1,92 @@
+const payload = require('payload');
+
+require('dotenv').config();
+
+const { PAYLOAD_SECRET, MONGODB_URI } = process.env;
+
+// This function ensures that there is at least one corresponding version for any document
+// within each of your draft-enabled collections.
+
+const boostVersionsQueryPerformance = async () => {
+  // Initialize Payload
+  // IMPORTANT: make sure your ENV variables are filled properly here
+  // as the below variable names are just for reference.
+  await payload.init({
+    secret: PAYLOAD_SECRET,
+    mongoURL: MONGODB_URI,
+    local: true,
+  });
+
+
+  // For each collection
+  await Promise.all(
+    payload.config.collections.map(async ({ slug, versions }) => {
+      // If drafts are enabled
+      if (versions) {
+        const VersionsModel = payload.versions[slug];
+
+        const newestDocs = VersionsModel.aggregate([
+          // Sort so that newest are first
+          { $sort: { updatedAt: -1 } },
+          // Group by parent ID, and take the first of each
+          {
+            $group: {
+              _id: '$parent',
+              _versionID: { $first: '$_id' },
+              version: { $first: '$version' },
+              updatedAt: { $first: '$updatedAt' },
+              createdAt: { $first: '$createdAt' },
+            },
+          },
+        ], {
+          allowDiskUse: true,
+        })
+          .exec();
+
+        const newDocIds = newestDocs.map((doc) => doc._versionID);
+
+        await VersionsModel.updateMany({
+          _id: {
+            $in: newDocIds,
+          },
+        }, {
+          latest: true,
+        });
+
+        const newVersions = await VersionsModel.find({
+          latest: {
+            $eq: true,
+          },
+        });
+
+        console.log(newVersions);
+      }
+    }),
+  );
+
+  // For each collection
+  await Promise.all(
+    payload.config.globals.map(async ({ slug, versions }) => {
+      // If drafts are enabled
+      if (versions) {
+        const VersionsModel = payload.versions[slug];
+
+        const updatedGlobalVersion = await VersionsModel.findOneAndUpdate(
+          {},
+          { latest: true },
+          {
+            sort: { updatedAt: -1 },
+            new: true,
+          },
+        ).exec();
+
+        console.log(updatedGlobalVersion);
+      }
+    }),
+  );
+
+  console.log('Done!');
+  process.exit(0);
+};
+
+boostVersionsQueryPerformance();
