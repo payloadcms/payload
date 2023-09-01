13e708e974f09b563381d565a3899c827a4e062f
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2023-09-01 19:08:23 +0300

    Fix credentials not in allowed headers

--- src/express/middleware/corsHeaders.ts
+++ src/express/middleware/corsHeaders.ts
@@ -4,15 +4,20 @@ import { SanitizedConfig } from '../../config/types';
 export default (config: SanitizedConfig) => (
   (req: Request, res: Response, next: NextFunction) => {
     if (config.cors) {
+      let headers = ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Content-Encoding', 'x-apollo-tracing'];
+
       res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, GET, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Encoding, x-apollo-tracing');
+      
 
       if (config.cors === '*') {
         res.setHeader('Access-Control-Allow-Origin', '*');
       } else if (Array.isArray(config.cors) && config.cors.indexOf(req.headers.origin) > -1) {
         res.header('Access-Control-Allow-Credentials', 'true');
         res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        headers.push("credentials");
       }
+
+      res.header('Access-Control-Allow-Headers', headers.join(","));
     }
 
     next();
