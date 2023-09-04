56bb3ecf0d14bb426e5af82c34ed8b1de4c7d1f0
Jacob Fletcher <jacobsfletch@gmail.com>
2023-09-04 12:15:11 -0400

    chore(templates/website): removes cached .next directory

--- templates/website/.next/server/middleware-build-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={"polyfillFiles":["static/chunks/polyfills.js"],"devFiles":["static/chunks/webpack.js","static/chunks/react-refresh.js"],"ampDevFiles":[],"lowPriorityFiles":["static/development/_buildManifest.js","static/development/_ssgManifest.js"],"rootMainFiles":["static/chunks/webpack.js","static/chunks/main-app.js"],"pages":{"/_app":[]},"ampFirstPages":[]}
--- templates/website/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {}\n}"
