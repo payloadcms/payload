09101965b8eb2f25ab4cdf7c0b56a6e2cee6caab
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-09-05 23:11:21 -0400

    fix(#3248): align admin depth param with local API

--- src/admin/components/views/collections/Edit/index.tsx
+++ src/admin/components/views/collections/Edit/index.tsx
@@ -110,7 +110,7 @@ const EditView: React.FC<IndexProps> = (props) => {
   }
 
   const apiURL = `${serverURL}${api}/${slug}/${id}?locale=${locale}${collection.versions.drafts ? '&draft=true' : ''}`;
-  const action = `${serverURL}${api}/${slug}${isEditing ? `/${id}` : ''}?locale=${locale}&depth=0&fallback-locale=null`;
+  const action = `${serverURL}${api}/${slug}${isEditing ? `/${id}` : ''}?locale=${locale}&fallback-locale=null`;
   const hasSavePermission = (isEditing && docPermissions?.update?.permission) || (!isEditing && (docPermissions as CollectionPermission)?.create?.permission);
   const isLoading = !internalState || !docPermissions || isLoadingData;
 
