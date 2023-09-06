8efdb2eb202983e8476e437d209de374dd388670
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-09-06 01:41:43 -0400

    chore: fix custom component test example

--- test/fields/collections/Array/components/AddCustomBlocks/index.tsx
+++ test/fields/collections/Array/components/AddCustomBlocks/index.tsx
@@ -8,9 +8,9 @@ const baseClass = 'custom-blocks-field-management';
 
 export const AddCustomBlocks: React.FC = () => {
   const { addFieldRow, replaceFieldRow } = useForm();
-  const { value } = useField({ path: 'customBlocks' });
+  const { value } = useField<[]>({ path: 'customBlocks' });
 
-  const nextIndex = typeof value === 'number' ? value + 1 : 0;
+  const nextIndex = Array.isArray(value) ? value.length : 0;
 
   return (
     <div className={baseClass}>
