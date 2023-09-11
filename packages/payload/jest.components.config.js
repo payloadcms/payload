41347bef5f5e3ef931cbe4f0986d59c37bf06046
Elliot DeNolf <denolfe@gmail.com>
2023-09-11 16:49:35 -0400

    chore: fix components tests

--- packages/payload/jest.components.config.js
+++ packages/payload/jest.components.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/bundlers/mocks/fileMock.js',
   },
-  setupFilesAfterEnv: ['<rootDir>/test/componentsSetup.js'],
+  setupFilesAfterEnv: ['../../test/componentsSetup.js'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['node_modules', 'dist'],
   testRegex: '(/src/admin/.*\\.(test|spec))\\.[jt]sx?$',
