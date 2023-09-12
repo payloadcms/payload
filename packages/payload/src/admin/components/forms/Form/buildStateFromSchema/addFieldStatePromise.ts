74f333e213bf11265d2ef198baf1f655611be379
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-09-12 11:14:53 -0400

    fix(2.0): ensures relationTo as array fields load returned data from form correctly

--- packages/payload/src/admin/components/forms/Form/buildStateFromSchema/addFieldStatePromise.ts
+++ packages/payload/src/admin/components/forms/Form/buildStateFromSchema/addFieldStatePromise.ts
@@ -269,6 +269,15 @@ export const addFieldStatePromise = async ({
         if (field.hasMany) {
           const relationshipValue = Array.isArray(valueWithDefault)
             ? valueWithDefault.map((relationship) => {
+                if (Array.isArray(field.relationTo)) {
+                  return {
+                    relationTo: relationship.relationTo,
+                    value:
+                      typeof relationship.value === 'string'
+                        ? relationship.value
+                        : relationship.value?.id,
+                  }
+                }
                 if (typeof relationship === 'object' && relationship !== null) {
                   return relationship.id
                 }
@@ -278,8 +287,24 @@ export const addFieldStatePromise = async ({
 
           fieldState.value = relationshipValue
           fieldState.initialValue = relationshipValue
-
-          state[`${path}${field.name}`] = fieldState
+        } else if (Array.isArray(field.relationTo)) {
+          if (
+            valueWithDefault &&
+            typeof valueWithDefault === 'object' &&
+            'relationTo' in valueWithDefault &&
+            'value' in valueWithDefault
+          ) {
+            const value =
+              typeof valueWithDefault?.value === 'object' && 'id' in valueWithDefault.value
+                ? valueWithDefault.value.id
+                : valueWithDefault.value
+            const relationshipValue = {
+              relationTo: valueWithDefault?.relationTo,
+              value,
+            }
+            fieldState.value = relationshipValue
+            fieldState.initialValue = relationshipValue
+          }
         } else {
           const relationshipValue =
             valueWithDefault && typeof valueWithDefault === 'object' && 'id' in valueWithDefault
@@ -287,9 +312,9 @@ export const addFieldStatePromise = async ({
               : valueWithDefault
           fieldState.value = relationshipValue
           fieldState.initialValue = relationshipValue
-
-          state[`${path}${field.name}`] = fieldState
         }
+
+        state[`${path}${field.name}`] = fieldState
 
         break
       }
