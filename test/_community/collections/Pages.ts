ac8ca9120d92cdc3a8b96cc7f260d9297c9369c1
Sil <52583398+silveltman@users.noreply.github.com>
2023-09-13 17:36:23 +0000

    added pages collectiom

---
+++ test/_community/collections/Pages.ts
@@ -0,0 +1,12 @@
+const Collection = {
+    slug: 'pages',
+    fields: [
+        {
+            name: 'blocks',
+            type: 'blocks',
+            blocks: [],
+        },
+    ],
+}
+
+export default Collection
