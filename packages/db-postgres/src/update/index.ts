b271936b64e0e66f51198d0bdeedd221ca279a65
Dan Ribbens <dan.ribbens@gmail.com>
2023-09-18 14:38:59 -0400

    chore: fix postgres updateOne

--- packages/db-postgres/src/update/index.ts
+++ packages/db-postgres/src/update/index.ts
@@ -6,17 +6,17 @@ import buildQuery from '../queries/buildQuery';
 import { upsertRow } from '../upsertRow';
 
 export const updateOne: UpdateOne = async function updateOne({
+  id,
   collection: collectionSlug,
   data,
   draft,
-  id,
   locale,
   req,
   where: whereArg,
 }) {
   const db = req.transactionID ? this.sessions[req.transactionID] : this.db;
   const collection = this.payload.collections[collectionSlug].config;
-  const tableName = toSnakeCase(collection);
+  const tableName = toSnakeCase(collectionSlug);
   const whereToUse = whereArg || { id: { equals: id } };
 
   const { where } = await buildQuery({
@@ -28,11 +28,11 @@ export const updateOne: UpdateOne = async function updateOne({
   });
 
   const result = await upsertRow({
+    id,
     adapter: this,
     data,
     db,
     fields: collection.fields,
-    id,
     operation: 'update',
     tableName: toSnakeCase(collectionSlug),
     where,
