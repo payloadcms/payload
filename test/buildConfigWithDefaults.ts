7a61db9eeaba4dc29e1319b601635a7b607727a8
Elliot DeNolf <denolfe@gmail.com>
2023-09-18 15:24:56 -0400

    chore: make bundlers use named exports

--- test/buildConfigWithDefaults.ts
+++ test/buildConfigWithDefaults.ts
@@ -2,8 +2,8 @@ import path from 'path'
 
 import type { Config, SanitizedConfig } from '../packages/payload/src/config/types'
 
-// import viteBundler from '../packages/bundler-vite/src'
-import webpackBundler from '../packages/bundler-webpack/src'
+// import { viteBundler } from '../packages/bundler-vite/src'
+import { webpackBundler } from '../packages/bundler-webpack/src'
 import { mongooseAdapter } from '../packages/db-mongodb/src/index'
 import { postgresAdapter } from '../packages/db-postgres/src/index'
 import { buildConfig as buildPayloadConfig } from '../packages/payload/src/config/build'
