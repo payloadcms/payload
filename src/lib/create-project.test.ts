35a6daa10d3668831c9ea14c962ef63ef4fcdc8b
Elliot DeNolf <denolfe@gmail.com>
2023-09-19 15:42:04 -0400

    test: reorganize tests

--- src/lib/create-project.test.ts
+++ src/lib/create-project.test.ts
@@ -53,12 +53,34 @@ describe('createProject', () => {
       expect(packageJson.name).toEqual(projectName)
     })
 
-    it.each([
-      ['mongodb', 'webpack'],
-      ['postgres', 'webpack'],
-    ])(
-      'should properly replace dependencies and update payload config when %s and %s are selected',
-      async (db, bundler) => {
+    it('creates plugin template', async () => {
+      const projectName = 'plugin'
+      const template: ProjectTemplate = {
+        name: 'plugin',
+        type: 'plugin',
+        url: 'https://github.com/payloadcms/payload-plugin-template',
+        description: 'Template for creating a Payload plugin',
+      }
+      await createProject({
+        cliArgs: args,
+        projectName,
+        projectDir,
+        template,
+        packageManager,
+      })
+
+      const packageJsonPath = path.resolve(projectDir, 'package.json')
+      const packageJson = fse.readJsonSync(packageJsonPath)
+
+      // Check package name and description
+      expect(packageJson.name).toEqual(projectName)
+    })
+
+    describe('db adapters and bundlers', () => {
+      it.each([
+        ['mongodb', 'webpack'],
+        ['postgres', 'webpack'],
+      ])('update config and deps: %s, %s', async (db, bundler) => {
         const projectName = 'starter-project'
         const template: ProjectTemplate = {
           name: 'blank',
@@ -106,30 +128,7 @@ describe('createProject', () => {
 
         expect(content).not.toContain('// bundler-config')
         expect(content).toContain(bundlerReplacement.configReplacement)
-      },
-    )
-
-    it('creates plugin template', async () => {
-      const projectName = 'plugin'
-      const template: ProjectTemplate = {
-        name: 'plugin',
-        type: 'plugin',
-        url: 'https://github.com/payloadcms/payload-plugin-template',
-        description: 'Template for creating a Payload plugin',
-      }
-      await createProject({
-        cliArgs: args,
-        projectName,
-        projectDir,
-        template,
-        packageManager,
       })
-
-      const packageJsonPath = path.resolve(projectDir, 'package.json')
-      const packageJson = fse.readJsonSync(packageJsonPath)
-
-      // Check package name and description
-      expect(packageJson.name).toEqual(projectName)
     })
   })
 })
