2ac7d6b2dd9283674c3054c849f7293401889224
Elliot DeNolf <denolfe@gmail.com>
2023-09-19 09:40:22 -0400

    feat(templates): update blank template

--- templates/blank/src/payload.config.ts
+++ templates/blank/src/payload.config.ts
@@ -1,7 +1,11 @@
-import { buildConfig } from 'payload/config'
 import path from 'path'
-import Users from './collections/Users'
+
 import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
+import { webpackBundler } from '@payloadcms/bundler-webpack' // bundler-import
+import { buildConfig } from 'payload/config'
+
+import Users from './collections/Users'
 
 export default buildConfig({
   admin: {
@@ -15,4 +19,10 @@ export default buildConfig({
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
   plugins: [payloadCloud()],
+  // database-adapter-config-start
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+  // database-adapter-config-end
+  bundler: webpackBundler(), // bundler-config
 })
