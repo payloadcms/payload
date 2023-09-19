e96e9898447085409034395d52c655d6867c794d
Alessio Gravili <alessio@gravili.de>
2023-09-19 09:54:14 +0200

    chore: auto-generated pointer files

--- packages/payload/components.d.ts
+++ packages/payload/components.d.ts
@@ -2,6 +2,8 @@ export { default as Banner } from './dist/admin/components/elements/Banner';
 export { default as Button } from './dist/admin/components/elements/Button';
 export { default as Pill } from './dist/admin/components/elements/Pill';
 export { default as Popup } from './dist/admin/components/elements/Popup';
+export { ShimmerEffect } from './dist/admin/components/elements/ShimmerEffect';
+export { default as Tooltip } from './dist/admin/components/elements/Tooltip';
 export { default as Check } from './dist/admin/components/icons/Check';
 export { default as Chevron } from './dist/admin/components/icons/Chevron';
 export { default as Menu } from './dist/admin/components/icons/Menu';
