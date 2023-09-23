90cb9f821efc1a4537b7412a810930a59266e2b5
Alessio Gravili <alessio@gravili.de>
2023-09-24 01:19:42 +0200

    fix: cloneDeep function not handling all data types well

--- packages/richtext-lexical/src/field/lexical/utils/cloneDeep.ts
+++ packages/richtext-lexical/src/field/lexical/utils/cloneDeep.ts
@@ -1,11 +1,23 @@
 export function cloneDeep<T>(object: T): T {
   if (object === null) return null
 
+  // Handle Date
+  if (object instanceof Date) {
+    return new Date(object.getTime()) as unknown as T
+  }
+
+  // Handle RegExp
+  if (object instanceof RegExp) {
+    return new RegExp(object.source, object.flags) as unknown as T
+  }
+
+  // Handle Array
   if (Array.isArray(object)) {
     return object.map((item) => cloneDeep(item)) as unknown as T
   }
 
-  if (typeof object === 'object') {
+  // Handle plain Object
+  if (typeof object === 'object' && object !== null) {
     const clonedObject: any = {}
     for (const key in object) {
       if (object.hasOwnProperty(key)) {
@@ -15,5 +27,6 @@ export function cloneDeep<T>(object: T): T {
     return clonedObject as T
   }
 
+  // Handle all other cases
   return object
 }
