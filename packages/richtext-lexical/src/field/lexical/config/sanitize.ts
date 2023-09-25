c27439139459b125d3de1020f37b27fb34bc3d52
Alessio Gravili <alessio@gravili.de>
2023-09-25 22:39:34 +0200

    chore: improve key generation for plugins

--- packages/richtext-lexical/src/field/lexical/config/sanitize.ts
+++ packages/richtext-lexical/src/field/lexical/config/sanitize.ts
@@ -23,13 +23,13 @@ export const sanitizeFeatures = (features: ResolvedFeatureMap): SanitizedFeature
       sanitized.nodes = sanitized.nodes.concat(feature.nodes)
     }
     if (feature.plugins?.length) {
-      for (const plugin of feature.plugins) {
+      feature.plugins.forEach((plugin, i) => {
         sanitized.plugins.push({
           Component: plugin.Component,
-          key: feature.key,
+          key: feature.key + i,
           position: plugin.position,
         })
-      }
+      })
     }
     if (feature.markdownTransformers?.length) {
       sanitized.markdownTransformers = sanitized.markdownTransformers.concat(
