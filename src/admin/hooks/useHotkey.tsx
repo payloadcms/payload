b43d8cfd38307df5f0dd3eddf71b4b1901aa2083
Alessio Gravili <alessio@gravili.de>
2023-09-27 23:25:37 +0200

    fix: hotkey's pressed keys are not unset when window is not focused

--- src/admin/hooks/useHotkey.tsx
+++ src/admin/hooks/useHotkey.tsx
@@ -5,7 +5,13 @@ import { setsAreEqual } from '../utilities/setsAreEqual';
 
 // Required to be outside of hook, else debounce would be necessary
 // and then one could not prevent the default behaviour.
-const pressedKeys = new Set<string>([]);
+// It maps the pressed keys with the time they were pressed, in order to implement a maximum time
+// for the user to press the next key in the sequence
+
+// This is necessary to prevent a bug where the keyup event, which unsets the key as pressed
+// is not fired when the window is not focused.
+// When the user then comes back to the window, the key is still registered as pressed, even though it's not.
+const pressedKeys = new Map<string, number>([]);
 
 const map = {
   metaleft: 'meta',
@@ -35,10 +41,10 @@ const pushToKeys = (code: string) => {
   // There is a weird bug with macos that if the keys are not cleared they remain in the
   // pressed keys set.
   if (key === 'meta') {
-    pressedKeys.forEach((pressedKey) => pressedKey !== 'meta' && pressedKeys.delete(pressedKey));
+    pressedKeys.forEach((time, pressedKey) => pressedKey !== 'meta' && pressedKeys.delete(pressedKey));
   }
 
-  pressedKeys.add(key);
+  pressedKeys.set(key, Date.now());
 };
 
 const removeFromKeys = (code: string) => {
@@ -76,9 +82,17 @@ const useHotkey = (options: {
     }
     if (e.code) pushToKeys(e.code);
 
+    // Filter out pressed keys which have been pressed > 3 seconds ago
+    pressedKeys.forEach((time, key) => {
+      if (Date.now() - time > 3000) {
+        pressedKeys.delete(key);
+        console.warn(`Removed ${key} from pressedKeys`);
+      }
+    });
+
     // Check for Mac and iPad
     const hasCmd = window.navigator.userAgent.includes('Mac OS X');
-    const pressedWithoutModifier = [...pressedKeys].filter((key) => !['meta', 'ctrl', 'alt', 'shift'].includes(key));
+    const pressedWithoutModifier = [...pressedKeys.keys()].filter((key) => !['meta', 'ctrl', 'alt', 'shift'].includes(key));
 
     // Check whether arrays contain the same values (regardless of number of occurrences)
     if (
