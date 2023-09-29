90a8c046e6f394d8b07b8cc7e9edea5f0dd22366
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-09-29 11:34:13 -0400

    chore: adds basic query input

--- packages/payload/src/admin/components/views/RestAPI/index.tsx
+++ packages/payload/src/admin/components/views/RestAPI/index.tsx
@@ -4,28 +4,10 @@ import { useTranslation } from 'react-i18next'
 
 import { requests } from '../../../api'
 import { Gutter } from '../../elements/Gutter'
+import TextInput from '../../forms/field-types/Text/Input'
 import { useConfig } from '../../utilities/Config'
 import { useDocumentInfo } from '../../utilities/DocumentInfo'
 import './index.scss'
-
-const testNestedObjects = {
-  a: 1,
-  b: {
-    c: 2,
-    d: {
-      e: 3,
-      f: {
-        array: [1, 2, 3],
-        g: 4,
-        h: {
-          i: 5,
-        },
-        string:
-          'then push so I dont have to hear nate whine about it then push so I dont have to hear nate whine about it then push so I dont have to hear nate whine about it then push so I dont have to hear nate whine about it',
-      },
-    },
-  },
-}
 
 const baseClass = 'query-inspector'
 
@@ -34,19 +16,23 @@ export const QueryInspector = () => {
   const { serverURL } = useConfig()
   const { i18n } = useTranslation()
 
+  const [query, setQuery] = React.useState<string>('')
   const containerRef = React.useRef<HTMLDivElement>(null)
 
   React.useEffect(() => {
     const fetchData = async () => {
       if (id) {
-        const request = await requests.get(`${serverURL}/api/${collection.slug}/${id}`, {
-          headers: {
-            'Accept-Language': i18n.language,
+        const request = await requests.get(
+          `${serverURL}/api/${collection.slug}/${id}${query ? `${query}` : ''}`,
+          {
+            headers: {
+              'Accept-Language': i18n.language,
+            },
           },
-        })
+        )
 
         const json = await request.json()
-        const formatter = new JsonFormatter(testNestedObjects, Infinity)
+        const formatter = new JsonFormatter(json, Infinity)
         const formattedJSON = formatter.render()
 
         containerRef.current.innerHTML = '' // Clear previous content
@@ -55,11 +41,20 @@ export const QueryInspector = () => {
     }
 
     fetchData()
-  }, [id, collection, serverURL, i18n.language])
+  }, [id, collection, serverURL, i18n.language, query])
 
   return (
     <Gutter className={baseClass} right={false}>
-      <div>Editable section</div>
+      <div className={`${baseClass}__configuration`}>
+        <h3 className={`${baseClass}__configuration-title`}>Query Builder</h3>
+        <TextInput
+          name="query"
+          onChange={(e) => setQuery(e.target.value)}
+          path="query"
+          placeholder="?depth=1"
+          value={query}
+        />
+      </div>
 
       <div className={`${baseClass}__results-container`} ref={containerRef} />
     </Gutter>
