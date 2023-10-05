db98df3b4e39fd4ac2946d2b620d8294fa832a42
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-10-05 01:01:18 -0400

    chore: merge conflict resolution

--- packages/payload/src/admin/components/elements/EditUpload/index.tsx
+++ packages/payload/src/admin/components/elements/EditUpload/index.tsx
@@ -31,7 +31,7 @@ const EditUpload: React.FC<{
   fileSrc: string
   showCrop?: boolean
   showFocalPoint?: boolean
-}> = ({ fileName, fileSrc, showCrop, showFocalPoint: hasFocalPoint }) => {
+}> = ({ fileName, fileSrc, showCrop, showFocalPoint }) => {
   const { closeModal } = useModal()
 
   const { formQueryParams, setFormQueryParams } = useFormQueryParams()
@@ -48,10 +48,9 @@ const EditUpload: React.FC<{
     x: uploadEdits?.focalPoint?.x || 50,
     y: uploadEdits?.focalPoint?.y || 50,
   })
+  const [checkBounds, setCheckBounds] = useState<boolean>(false)
 
-  const [isDragging, setIsDragging] = useState<boolean>(false)
-  const showFocalPoint = hasFocalPoint && !isDragging
-
+  const focalWrapRef = useRef<HTMLDivElement | undefined>()
   const imageRef = useRef<HTMLImageElement | undefined>()
   const cropRef = useRef<HTMLDivElement | undefined>()
 
@@ -78,8 +77,8 @@ const EditUpload: React.FC<{
       const updatedCrop = {
         ...crop,
         [dimension]: val,
-        ...(maxCrop.width && name !== 'height' ? { x: 100 - val } : {}),
-        ...(maxCrop.height && name !== 'width' ? { y: 100 - val } : {}),
+        ...(maxCrop.width && name === 'width' ? { x: 100 - val } : {}),
+        ...(maxCrop.height && name === 'height' ? { y: 100 - val } : {}),
       }
 
       setCrop(updatedCrop)
@@ -92,16 +91,6 @@ const EditUpload: React.FC<{
     }
   }
 
-  const setFocalPoint = (e: React.MouseEvent<HTMLDivElement>) => {
-    const rect =
-      cropRef.current?.getBoundingClientRect() || imageRef.current?.getBoundingClientRect()
-
-    const x = Math.max(0, Math.min(100, ((e.clientX - rect.left) / rect.width) * 100))
-    const y = Math.max(0, Math.min(100, ((e.clientY - rect.top) / rect.height) * 100))
-
-    setPointPosition({ x, y })
-  }
-
   const saveEdits = () => {
     setFormQueryParams({
       ...formQueryParams,
@@ -111,6 +100,21 @@ const EditUpload: React.FC<{
       },
     })
     closeModal(editDrawerSlug)
+  }
+
+  const onDragEnd = React.useCallback(({ x, y }) => {
+    setPointPosition({ x, y })
+    setCheckBounds(false)
+  }, [])
+
+  const centerFocalPoint = () => {
+    const containerRect = focalWrapRef.current.getBoundingClientRect()
+    const boundsRect = cropRef.current.getBoundingClientRect()
+    const xCenter =
+      ((boundsRect.left - containerRect.left + boundsRect.width / 2) / containerRect.width) * 100
+    const yCenter =
+      ((boundsRect.top - containerRect.top + boundsRect.height / 2) / containerRect.height) * 100
+    setPointPosition({ x: xCenter, y: yCenter })
   }
 
   return (
@@ -136,134 +140,240 @@ const EditUpload: React.FC<{
         </div>
       </div>
       <div className={`${baseClass}__toolWrap`}>
-        {showCrop ? (
-          <div className={`${baseClass}__crop`}>
-            <ReactCrop
-              className={`${baseClass}__reactCrop`}
-              crop={crop}
-              onChange={(_, c) => setCrop(c)}
-              onDragEnd={() => setIsDragging(false)}
-              onDragStart={() => setIsDragging(true)}
-              renderSelectionAddon={() => {
-                return (
-                  <div
-                    className={`${baseClass}__focalPoint`}
-                    onClick={setFocalPoint}
-                    ref={cropRef}
-                    role="presentation"
-                  >
-                    <div
-                      className={`${baseClass}__point`}
-                      style={{
-                        left: `${pointPosition.x}%`,
-                        opacity: showFocalPoint ? 1 : 0,
-                        top: `${pointPosition.y}%`,
-                      }}
-                    >
-                      <Plus />
-                    </div>
-                  </div>
-                )
-              }}
-            >
-              <img alt="Upload Preview" ref={imageRef} src={fileSrc} />
-            </ReactCrop>
-          </div>
-        ) : (
-          <div className={`${baseClass}__focalOnly`}>
-            <div
-              className={`${baseClass}__imageWrap`}
-              onClick={setFocalPoint}
-              ref={cropRef}
-              role="presentation"
-            >
-              <img alt="Upload Preview" ref={imageRef} src={fileSrc} />
-              <div
-                className={`${baseClass}__point`}
-                style={{
-                  left: `${pointPosition.x}%`,
-                  opacity: showFocalPoint ? 1 : 0,
-                  top: `${pointPosition.y}%`,
+        <div className={`${baseClass}__crop`}>
+          <div
+            className={`${baseClass}__focal-wrapper`}
+            style={{
+              aspectRatio: `${originalWidth / originalHeight}`,
+            }}
+            ref={focalWrapRef}
+          >
+            {showCrop ? (
+              <ReactCrop
+                className={`${baseClass}__reactCrop`}
+                crop={crop}
+                onChange={(_, c) => setCrop(c)}
+                onComplete={() => setCheckBounds(true)}
+                renderSelectionAddon={() => {
+                  return <div className={`${baseClass}__crop-window`} ref={cropRef} />
                 }}
               >
-                <Plus />
+                <img alt="Upload Preview" ref={imageRef} src={fileSrc} />
+              </ReactCrop>
+            ) : (
+              <img alt="Upload Preview" ref={imageRef} src={fileSrc} />
+            )}
+            <DraggableElement
+              className={`${baseClass}__focalPoint`}
+              onDragEnd={onDragEnd}
+              containerRef={focalWrapRef}
+              boundsRef={cropRef}
+              checkBounds={checkBounds}
+              setCheckBounds={setCheckBounds}
+              initialPosition={pointPosition}
+            >
+              <Plus />
+            </DraggableElement>
+          </div>
+        </div>
+        {(showCrop || showFocalPoint) && (
+          <div className={`${baseClass}__sidebar`}>
+            {showCrop && (
+              <div className={`${baseClass}__groupWrap`}>
+                <div>
+                  <div className={`${baseClass}__titleWrap`}>
+                    <h3>Crop</h3>
+                    <Button
+                      buttonStyle="none"
+                      className={`${baseClass}__reset`}
+                      onClick={() =>
+                        setCrop({
+                          height: 100,
+                          unit: '%',
+                          width: 100,
+                          x: 0,
+                          y: 0,
+                        })
+                      }
+                    >
+                      Reset
+                    </Button>
+                  </div>
+                  <span className={`${baseClass}__description`}>
+                    Draw an area to crop, adjust by dragging the corners or updating the values
+                    below.
+                  </span>
+                </div>
+                <div className={`${baseClass}__inputsWrap`}>
+                  <Input
+                    name="Width (px)"
+                    onChange={(value) => handleInputChange('width', value)}
+                    value={(originalWidth * crop.width).toFixed(0)}
+                  />
+                  <Input
+                    name="Height (px)"
+                    onChange={(value) => handleInputChange('height', value)}
+                    value={(originalHeight * crop.height).toFixed(0)}
+                  />
+                </div>
               </div>
-            </div>
+            )}
+
+            {showFocalPoint && (
+              <div className={`${baseClass}__groupWrap`}>
+                <div>
+                  <div className={`${baseClass}__titleWrap`}>
+                    <h3>Focal Point</h3>
+                    <Button
+                      buttonStyle="none"
+                      className={`${baseClass}__reset`}
+                      onClick={centerFocalPoint}
+                    >
+                      Reset
+                    </Button>
+                  </div>
+                  <span className={`${baseClass}__description`}>
+                    Click within the cropped area to position the focal point or adjust the values
+                    below.
+                  </span>
+                </div>
+                <div className={`${baseClass}__inputsWrap`}>
+                  <Input
+                    name="X %"
+                    onChange={(value) => handleInputChange('x', value)}
+                    value={pointPosition.x.toFixed(0)}
+                  />
+                  <Input
+                    name="Y %"
+                    onChange={(value) => handleInputChange('y', value)}
+                    value={pointPosition.y.toFixed(0)}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         )}
-        <div className={`${baseClass}__sidebar`}>
-          {showCrop && (
-            <div className={`${baseClass}__groupWrap`}>
-              <div className={`${baseClass}__titleWrap`}>
-                <h3>Crop</h3>
-                <Button
-                  buttonStyle="none"
-                  className={`${baseClass}__reset`}
-                  onClick={() =>
-                    setCrop({
-                      height: 100,
-                      unit: '%',
-                      width: 100,
-                      x: 0,
-                      y: 0,
-                    })
-                  }
-                >
-                  Reset
-                </Button>
-              </div>
-              <span className={`${baseClass}__description`}>
-                Draw an area to crop, adjust by dragging the corners or updating the values below.
-              </span>
-              <div className={`${baseClass}__inputsWrap`}>
-                <Input
-                  name="Width (px)"
-                  onChange={(value) => handleInputChange('width', value)}
-                  value={(originalWidth * crop.width).toFixed(0)}
-                />
-                <Input
-                  name="Height (px)"
-                  onChange={(value) => handleInputChange('height', value)}
-                  value={(originalHeight * crop.height).toFixed(0)}
-                />
-              </div>
-            </div>
-          )}
-
-          {showFocalPoint && (
-            <div className={`${baseClass}__groupWrap`}>
-              <div className={`${baseClass}__titleWrap`}>
-                <h3>Focal Point</h3>
-                <Button
-                  buttonStyle="none"
-                  className={`${baseClass}__reset`}
-                  onClick={() => setPointPosition({ x: 50, y: 50 })}
-                >
-                  Reset
-                </Button>
-              </div>
-              <span className={`${baseClass}__description`}>
-                Click within the cropped area to position the focal point or adjust the values
-                below.
-              </span>
-              <div className={`${baseClass}__inputsWrap`}>
-                <Input
-                  name="X %"
-                  onChange={(value) => handleInputChange('x', value)}
-                  value={pointPosition.x.toFixed(0)}
-                />
-                <Input
-                  name="Y %"
-                  onChange={(value) => handleInputChange('y', value)}
-                  value={pointPosition.y.toFixed(0)}
-                />
-              </div>
-            </div>
-          )}
-        </div>
       </div>
     </div>
   )
 }
 
 export default EditUpload
+
+const DraggableElement = ({
+  onDragEnd,
+  className,
+  children,
+  initialPosition = { x: 50, y: 50 },
+  checkBounds,
+  setCheckBounds,
+  containerRef,
+  boundsRef,
+}) => {
+  const [position, setPosition] = useState({ x: initialPosition.x, y: initialPosition.y })
+  const [isDragging, setIsDragging] = useState(false)
+  const dragRef = useRef<HTMLDivElement | undefined>()
+
+  const getCoordinates = React.useCallback(
+    (mouseXArg?: number, mouseYArg?: number, recenter?: boolean) => {
+      const containerRect = containerRef.current.getBoundingClientRect()
+      const boundsRect = boundsRef.current.getBoundingClientRect()
+      const mouseX = mouseXArg ?? boundsRect.left
+      const mouseY = mouseYArg ?? boundsRect.top
+
+      const xOutOfBounds = mouseX < boundsRect.left || mouseX > boundsRect.right
+      const yOutOfBounds = mouseY < boundsRect.top || mouseY > boundsRect.bottom
+
+      let x = ((mouseX - containerRect.left) / containerRect.width) * 100
+      let y = ((mouseY - containerRect.top) / containerRect.height) * 100
+      const xCenter =
+        ((boundsRect.left - containerRect.left + boundsRect.width / 2) / containerRect.width) * 100
+      const yCenter =
+        ((boundsRect.top - containerRect.top + boundsRect.height / 2) / containerRect.height) * 100
+      if (xOutOfBounds || yOutOfBounds) {
+        setIsDragging(false)
+        if (mouseX < boundsRect.left) {
+          x = ((boundsRect.left - containerRect.left) / containerRect.width) * 100
+        } else if (mouseX > boundsRect.right) {
+          x =
+            ((containerRect.width - (containerRect.right - boundsRect.right)) /
+              containerRect.width) *
+            100
+        }
+
+        if (mouseY < boundsRect.top) {
+          y = ((boundsRect.top - containerRect.top) / containerRect.height) * 100
+        } else if (mouseY > boundsRect.bottom) {
+          y =
+            ((containerRect.height - (containerRect.bottom - boundsRect.bottom)) /
+              containerRect.height) *
+            100
+        }
+
+        if (recenter) {
+          x = xOutOfBounds ? xCenter : x
+          y = yOutOfBounds ? yCenter : y
+        }
+      }
+
+      return { x, y }
+    },
+    [],
+  )
+
+  const handleMouseDown = (event) => {
+    event.preventDefault()
+    setIsDragging(true)
+  }
+
+  const handleMouseMove = (event) => {
+    if (!isDragging) return null
+    const { x, y } = getCoordinates(event.clientX, event.clientY)
+
+    setPosition({ x, y })
+  }
+
+  const onDrop = () => {
+    setIsDragging(false)
+    onDragEnd(position)
+  }
+
+  React.useEffect(() => {
+    if (isDragging || !dragRef.current) return
+    if (checkBounds) {
+      const { left, top, height, width } = dragRef.current.getBoundingClientRect()
+      const { x, y } = getCoordinates(left + width / 2, top + height / 2, true)
+      onDragEnd({ x, y })
+      setPosition({ x, y })
+      setCheckBounds(false)
+      return
+    }
+  }, [getCoordinates, isDragging, checkBounds, setCheckBounds, position.x, position.y])
+
+  React.useEffect(() => {
+    setPosition({ x: initialPosition.x, y: initialPosition.y })
+  }, [initialPosition.x, initialPosition.y])
+
+  return (
+    <div
+      onMouseMove={handleMouseMove}
+      className={[
+        `${baseClass}__draggable-container`,
+        isDragging && `${baseClass}__draggable-container--dragging`,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <div
+        className={[`${baseClass}__draggable`, className].filter(Boolean).join(' ')}
+        onMouseDown={handleMouseDown}
+        onMouseUp={onDrop}
+        style={{ position: 'absolute', left: `${position.x}%`, top: `${position.y}%` }}
+        ref={dragRef}
+      >
+        {children}
+      </div>
+      <div />
+    </div>
+  )
+}
