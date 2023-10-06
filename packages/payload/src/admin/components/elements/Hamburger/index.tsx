ca5dbb5cd4a1e8a11eb92dcb5757c12f585e07bc
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-10-06 12:21:10 -0400

    chore: adjusts collapse icon for collection navigation

--- packages/payload/src/admin/components/elements/Hamburger/index.tsx
+++ packages/payload/src/admin/components/elements/Hamburger/index.tsx
@@ -14,25 +14,31 @@ export const Hamburger: React.FC<{
   const { closeIcon = 'x', isActive = false } = props
 
   return (
-    <div className={[baseClass, isActive && `${baseClass}--active`].filter(Boolean).join(' ')}>
+    <div className={[baseClass].filter(Boolean).join(' ')}>
       <div className={`${baseClass}__icon`}>
-        <div className={`${baseClass}__line ${baseClass}__top`} />
-        <div className={`${baseClass}__line ${baseClass}__middle`} />
-        <div className={`${baseClass}__line ${baseClass}__bottom`} />
+        {!isActive && (
+          <div className={`${baseClass}__lines`}>
+            <div className={`${baseClass}__line ${baseClass}__top`} />
+            <div className={`${baseClass}__line ${baseClass}__middle`} />
+            <div className={`${baseClass}__line ${baseClass}__bottom`} />
+          </div>
+        )}
+
+        {isActive && (
+          <div className={`${baseClass}__close-icon`}>
+            {closeIcon === 'x' && (
+              <div>
+                <div className={`${baseClass}__line ${baseClass}__x-left`} />
+                <div className={`${baseClass}__line ${baseClass}__x-right`} />
+              </div>
+            )}
+
+            {closeIcon === 'collapse' && (
+              <Chevron className={`${baseClass}__collapse-chevron`} direction="left" />
+            )}
+          </div>
+        )}
       </div>
-      {closeIcon === 'x' && (
-        <div className={`${baseClass}__x`}>
-          <div className={`${baseClass}__line ${baseClass}__x-left`} />
-          <div className={`${baseClass}__line ${baseClass}__x-right`} />
-        </div>
-      )}
-      {closeIcon === 'collapse' && (
-        <div aria-label={t('collapse')} className={`${baseClass}__collapse`} title={t('collapse')}>
-          <div className={`${baseClass}__line ${baseClass}__collapse-left`} />
-          <Chevron className={`${baseClass}__collapse-chevron`} direction="left" />
-          <div className={`${baseClass}__collapse-label`}>{t('collapse')}</div>
-        </div>
-      )}
     </div>
   )
 }
