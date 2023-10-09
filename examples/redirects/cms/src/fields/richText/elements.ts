08f7497040f26c70ad15d891345f768e8aef861c
Jacob Fletcher <jacobsfletch@gmail.com>
2023-10-09 14:03:18 -0400

    chore(examples/redirects): migrates to 2.0 (#3514)

--- examples/redirects/cms/src/fields/richText/elements.ts
+++
@@ -1,5 +0,0 @@
-import type { RichTextElement } from 'payload/dist/fields/config/types'
-
-const elements: RichTextElement[] = ['blockquote', 'h2', 'h3', 'h4', 'h5', 'h6', 'link']
-
-export default elements
