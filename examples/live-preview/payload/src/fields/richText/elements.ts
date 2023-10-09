a72779b4314f5cf669fbb11367fae277ff2f42b5
Jacob Fletcher <jacobsfletch@gmail.com>
2023-10-09 00:02:21 -0400

    chore(examples/live-preview): adds live preview example

---
+++ examples/live-preview/payload/src/fields/richText/elements.ts
@@ -0,0 +1,5 @@
+import type { RichTextElement } from 'payload/dist/fields/config/types'
+
+const elements: RichTextElement[] = ['blockquote', 'h2', 'h3', 'h4', 'h5', 'h6', 'link']
+
+export default elements
