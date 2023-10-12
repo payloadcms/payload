b2c90a2ff41651f0dae985271d6527adc19b702d
Jacob Fletcher <jacobsfletch@gmail.com>
2023-10-12 11:10:39 -0400

    feat(live-preview): exports ready function

--- packages/live-preview/src/subscribe.ts
+++ packages/live-preview/src/subscribe.ts
@@ -15,17 +15,6 @@ export const subscribe = <T>(args: {
 
   if (typeof window !== 'undefined') {
     window.addEventListener('message', onMessage)
-
-    // This subscription may have been from either an iframe `src` or `window.open()`
-    // i.e. `window?.opener` || `window?.parent`
-
-    window?.opener?.postMessage(
-      JSON.stringify({
-        popupReady: true,
-        type: 'payload-live-preview',
-      }),
-      serverURL,
-    )
   }
 
   return onMessage
