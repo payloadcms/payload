e847955fa683e8463454ec1c01ba240035fa4216
Dan Ribbens <dan.ribbens@gmail.com>
2023-10-16 12:49:36 -0400

    fix: payload.verifyEmail local operation not using transaction

--- packages/payload/src/auth/operations/local/verifyEmail.ts
+++ packages/payload/src/auth/operations/local/verifyEmail.ts
@@ -3,10 +3,13 @@ import type { PayloadRequest } from '../../../express/types'
 import type { Payload } from '../../../payload'
 
 import { APIError } from '../../../errors'
+import { setRequestContext } from '../../../express/setRequestContext'
+import { i18nInit } from '../../../translations/init'
 import verifyEmail from '../verifyEmail'
 
 export type Options<T extends keyof GeneratedTypes['collections']> = {
   collection: T
+  req?: PayloadRequest
   token: string
 }
 
@@ -14,11 +17,8 @@ async function localVerifyEmail<T extends keyof GeneratedTypes['collections']>(
   payload: Payload,
   options: Options<T>,
 ): Promise<boolean> {
-  const { collection: collectionSlug, token } = options
-
-  const req = {
-    payload,
-  } as PayloadRequest
+  const { collection: collectionSlug, req = {} as PayloadRequest, token } = options
+  setRequestContext(options.req)
 
   const collection = payload.collections[collectionSlug]
 
@@ -27,6 +27,10 @@ async function localVerifyEmail<T extends keyof GeneratedTypes['collections']>(
       `The collection with slug ${String(collectionSlug)} can't be found. Verify Email Operation.`,
     )
   }
+
+  req.payload = payload
+  req.payloadAPI = req.payloadAPI || 'local'
+  req.i18n = i18nInit(payload.config.i18n)
 
   return verifyEmail({
     collection,
