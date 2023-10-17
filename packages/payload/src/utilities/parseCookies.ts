f7a5e8f8ca3318eacd64ca9f667436c771444692
PatrikKozak <patrik@trbl.design>
2023-10-17 17:33:54 -0400

    chore: wraps decodeURI in try / catch & only adds result to parsed cookie if succeeds

--- packages/payload/src/utilities/parseCookies.ts
+++ packages/payload/src/utilities/parseCookies.ts
@@ -7,6 +7,15 @@ export default function parseCookies(req: Request): { [key: string]: string } {
   if (rc) {
     rc.split(';').forEach((cookie) => {
       const parts = cookie.split('=')
+      const key = parts.shift().trim()
+      const encodedValue = parts.join('=')
+
+      try {
+        const decodedValue = decodeURI(encodedValue)
+        list[key] = decodedValue
+      } catch (e) {
+        console.error(`Error decoding cookie value for key ${key}: ${e}`)
+      }
       list[parts.shift().trim()] = decodeURI(parts.join('='))
     })
   }
