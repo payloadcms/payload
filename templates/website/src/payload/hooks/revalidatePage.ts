48472df7acd998113afbdad54fdd374f9d89655f
Jacob Fletcher <jacobsfletch@gmail.com>
2023-10-20 18:04:10 -0400

    fix(templates/website): missing env vars

--- templates/website/src/payload/hooks/revalidatePage.ts
+++ templates/website/src/payload/hooks/revalidatePage.ts
@@ -3,7 +3,7 @@ import type { AfterChangeHook } from 'payload/dist/collections/config/types'
 // ensure that the home page is revalidated at '/' instead of '/home'
 export const formatAppURL = ({ doc }): string => {
   const pathToUse = doc.slug === 'home' ? '' : doc.slug
-  const { pathname } = new URL(`${process.env.PAYLOAD_PUBLIC_SITE_URL}/${pathToUse}`)
+  const { pathname } = new URL(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/${pathToUse}`)
   return pathname
 }
 
@@ -16,7 +16,7 @@ export const revalidatePage: AfterChangeHook = ({ doc, req }) => {
     try {
       url = formatAppURL({ doc })
       const res = await fetch(
-        `${process.env.PAYLOAD_PUBLIC_SITE_URL}/api/revalidate?secret=${process.env.REVALIDATION_KEY}&revalidatePath=${url}`,
+        `${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/revalidate?secret=${process.env.REVALIDATION_KEY}&revalidatePath=${url}`,
       )
 
       if (res.ok) {
