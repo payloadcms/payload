88d8f1cd4b4dd42f5be5e3b412d3c1716f125545
Dan Ribbens <dan.ribbens@gmail.com>
2023-10-24 13:04:39 -0400

    chore: add comments and cleanup preferences useEffects

--- packages/payload/src/admin/components/utilities/Preferences/index.tsx
+++ packages/payload/src/admin/components/utilities/Preferences/index.tsx
@@ -8,6 +8,11 @@ import { useConfig } from '../Config'
 
 type PreferencesContext = {
   getPreference: <T = any>(key: string) => Promise<T> | T
+  /**
+   * @param key - a string identifier for the property being set
+   * @param value - preference data to store
+   * @param merge - when true will combine the existing preference object batch the change into one request for objects, default = false
+   */
   setPreference: <T = any>(key: string, value: T, merge?: boolean) => Promise<void>
 }
 
@@ -79,15 +84,18 @@ export const PreferencesProvider: React.FC<{ children?: React.ReactNode }> = ({ 
 
       let newValue = value
       const currentPreference = await getPreference(key)
+      // handle value objects where multiple values can be set under one key
       if (
         typeof value === 'object' &&
-        typeof preferencesRef.current[key] === 'object' &&
+        typeof currentPreference === 'object' &&
         typeof newValue === 'object'
       ) {
+        // merge the value with any existing preference for the key
         newValue = { ...(currentPreference || {}), ...value }
         if (isDeepEqual(newValue, currentPreference)) {
           return
         }
+        // add the requested changes to a pendingUpdate batch for the key
         pendingUpdate.current[key] = {
           ...pendingUpdate.current[key],
           ...(newValue as Record<string, unknown>),
@@ -100,15 +108,18 @@ export const PreferencesProvider: React.FC<{ children?: React.ReactNode }> = ({ 
       }
 
       const updatePreference = async () => {
+        // compare the value stored in context before sending to eliminate duplicate requests
         if (isDeepEqual(pendingUpdate.current[key], preferencesRef.current[key])) {
           return
         }
+        // preference set in context here to prevent other updatePreference at the same time
         preferencesRef.current[key] = pendingUpdate.current[key]
 
         await requests.post(
           `${serverURL}${api}/payload-preferences/${key}`,
           requestOptions(preferencesRef.current[key], i18n.language),
         )
+        // reset any changes for this key after sending the request
         delete pendingUpdate.current[key]
       }
 
