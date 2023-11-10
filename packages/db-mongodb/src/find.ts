c03322abbefe2b7ee8eeed0be3121fc0c7f0b90a
Alessio Gravili <alessio@gravili.de>
2023-11-10 16:05:50 +0100

    chore: disablePaginationCountIndexHint => disableIndexHints

--- packages/db-mongodb/src/find.ts
+++ packages/db-mongodb/src/find.ts
@@ -55,7 +55,7 @@ export const find: Find = async function find(
     useEstimatedCount,
   }
 
-  if (!useEstimatedCount && this.disablePaginationCountIndexHint !== true) {
+  if (!useEstimatedCount && this.disableIndexHints !== true) {
     // Improve the performance of the countDocuments query which is used if useEstimatedCount is set to false by adding a hint.
     paginationOptions.useCustomCountFn = () => {
       return Promise.resolve(
