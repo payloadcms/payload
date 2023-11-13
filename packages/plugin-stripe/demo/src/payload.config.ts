3e79f55d609104b005c165e111925e122a97d353
PatrikKozak <patrik@trbl.design>
2023-11-13 10:08:33 -0500

    chore: handles type errors

--- packages/plugin-stripe/demo/src/payload.config.ts
+++ packages/plugin-stripe/demo/src/payload.config.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { buildConfig } from 'payload/config'
-// import { viteBundler } from '@payloadcms/bundler-vite'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { viteBundler } from '@payloadcms/bundler-vite'
+// import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 
@@ -13,31 +13,72 @@ import { subscriptionCreatedOrUpdated } from './webhooks/subscriptionCreatedOrUp
 import { subscriptionDeleted } from './webhooks/subscriptionDeleted'
 import { syncPriceJSON } from './webhooks/syncPriceJSON'
 
+const mockModulePath = path.resolve(__dirname, './emptyModuleMock.js')
+
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_CMS_URL,
   admin: {
     user: Users.slug,
-    bundler: webpackBundler(),
-    webpack: (config) => {
-      const newConfig = {
-        ...config,
-        resolve: {
-          ...config.resolve,
-          alias: {
-            ...config.resolve.alias,
-            payload: path.join(__dirname, '../node_modules/payload'),
-            react: path.join(__dirname, '../node_modules/react'),
-            'react-dom': path.join(__dirname, '../node_modules/react-dom'),
-            [path.resolve(__dirname, '../../src/index')]: path.resolve(
-              __dirname,
-              '../../src/admin.ts',
-            ),
-          },
+    bundler: viteBundler({
+      resolve: {
+        alias: {
+          config: mockModulePath,
         },
-      }
+      },
+    }),
+    vite: (config) => ({
+      ...config,
+      define: {
+        ...config.define,
+      },
+      optimizeDeps: {
+        force: true,
+        ...config.optimizeDeps,
+        exclude: [...config.optimizeDeps.exclude],
+      },
+      resolve: {
+        ...(config?.resolve || {}),
+        alias: {
+          ...(config?.resolve?.alias || {}),
+          // remember, vite aliases are exact-match only
+          // stripePlugin: mockModulePath,
+          // 'payload-config': mockModulePath,
+          // './webhooks/subscriptionCreatedOrUpdated': mockModulePath,
+          // './webhooks/subscriptionDeleted': mockModulePath,
+          // './webhooks/syncPriceJSON': mockModulePath,
 
-      return newConfig
-    },
+          // 'payload-config': mockModulePath,
+          payload: path.join(__dirname, '../node_modules/payload'),
+          react: path.join(__dirname, '../node_modules/react'),
+          'react-dom': path.join(__dirname, '../node_modules/react-dom'),
+          [path.resolve(__dirname, '../../src/index')]: path.resolve(
+            __dirname,
+            '../../src/admin.ts',
+          ),
+        },
+      },
+    }),
+    // bundler: webpackBundler(),
+    // webpack: (config) => {
+    //   const newConfig = {
+    //     ...config,
+    //     resolve: {
+    //       ...config.resolve,
+    //       alias: {
+    //         ...config.resolve.alias,
+    //         payload: path.join(__dirname, '../node_modules/payload'),
+    //         react: path.join(__dirname, '../node_modules/react'),
+    //         'react-dom': path.join(__dirname, '../node_modules/react-dom'),
+    //         [path.resolve(__dirname, '../../src/index')]: path.resolve(
+    //           __dirname,
+    //           '../../src/admin.ts',
+    //         ),
+    //       },
+    //     },
+    //   }
+
+    //   return newConfig
+    // },
   },
   collections: [Users, Customers, Products],
   editor: lexicalEditor({}),
