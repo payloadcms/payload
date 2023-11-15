9c585421e884caadab44b612477eecb8268da84a
Take Weiland <diesieben07@gmail.com>
2023-11-15 21:09:54 +0100

    fix: fixes transaction handling for operations using getEntityPolicies

--- packages/payload/src/globals/operations/docAccess.ts
+++ packages/payload/src/globals/operations/docAccess.ts
@@ -3,7 +3,10 @@ import type { PayloadRequest } from '../../express/types'
 import type { AllOperations } from '../../types'
 import type { SanitizedGlobalConfig } from '../config/types'
 
+import { commitTransaction } from '../../utilities/commitTransaction'
 import { getEntityPolicies } from '../../utilities/getEntityPolicies'
+import { initTransaction } from '../../utilities/initTransaction'
+import { killTransaction } from '../../utilities/killTransaction'
 
 type Arguments = {
   globalConfig: SanitizedGlobalConfig
@@ -19,10 +22,17 @@ export async function docAccess(args: Arguments): Promise<GlobalPermission> {
     globalOperations.push('readVersions')
   }
 
-  return getEntityPolicies({
-    entity: globalConfig,
-    operations: globalOperations,
-    req,
-    type: 'global',
-  })
+  try {
+    const shouldCommit = await initTransaction(req)
+    const result = await getEntityPolicies({
+      entity: globalConfig,
+      operations: globalOperations,
+      req,
+      type: 'global',
+    })
+    if (shouldCommit) await commitTransaction(req)
+    return result
+  } catch (e: unknown) {
+    await killTransaction(req)
+  }
 }
