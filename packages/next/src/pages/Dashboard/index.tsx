32567e01ec5c70c1686954b7246cef9d2e4fd531
Jacob Fletcher <jacobsfletch@gmail.com>
2023-11-16 14:04:44 -0500

    chore: moves ui components

--- packages/next/src/pages/Dashboard/index.tsx
+++ packages/next/src/pages/Dashboard/index.tsx
@@ -1,15 +1,36 @@
 import { SanitizedConfig } from 'payload/types'
 import React from 'react'
 import { RootProvider } from '@payloadcms/ui/providers'
+import { RenderCustomComponent } from '@payloadcms/ui/elements'
 import { createClientConfig } from '../../createClientConfig'
+import { DefaultDashboard } from '@payloadcms/ui/views'
 
 export const Dashboard = ({ config: configPromise }: { config: Promise<SanitizedConfig> }) =>
   async function () {
-    const config = await createClientConfig(configPromise)
+    const config = await configPromise
+    const clientConfig = await createClientConfig(config)
+
+    const CustomDashboardComponent = config.admin.components.views.Dashboard
 
     return (
-      <RootProvider config={config}>
+      <RootProvider config={clientConfig}>
         <h1>Dashboard (rendered on server)</h1>
+        <RenderCustomComponent
+          CustomComponent={
+            typeof CustomDashboardComponent === 'function' ? CustomDashboardComponent : undefined
+          }
+          DefaultComponent={DefaultDashboard}
+          componentProps={
+            {
+              // collections: collections.filter(
+              //   (collection) => permissions?.collections?.[collection.slug]?.read?.permission,
+              // ),
+              // globals: filteredGlobals,
+              // permissions,
+              // user,
+            }
+          }
+        />
       </RootProvider>
     )
   }
