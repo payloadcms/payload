4360c00e57f6928e30f0144e0bfbf05f08848afa
Jacob Fletcher <jacobsfletch@gmail.com>
2023-11-29 15:51:33 -0500

    chore(examples/custom-server): removes unnused dotenv.js

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -6,11 +6,40 @@ export const postsSlug = 'posts'
 
 export const PostsCollection: CollectionConfig = {
   slug: postsSlug,
+  admin: {
+    defaultColumns: [
+      'text0',
+      'text1',
+      'text2',
+      'text3',
+      'text4',
+      'text5',
+      'text6',
+      'text7',
+      'text8',
+      'text9',
+      'text10',
+      'text11',
+      'text12',
+      'text13',
+      'text14',
+      'text15',
+      'text16',
+      'text17',
+      'text18',
+      'text19',
+    ],
+  },
   fields: [
     {
       name: 'text',
       type: 'text',
     },
+    // add 20 more text fields with unique names
+    ...Array.from(Array(20).keys()).map((i) => ({
+      name: `text${i}`,
+      type: 'text',
+    })),
     {
       name: 'associatedMedia',
       type: 'upload',
