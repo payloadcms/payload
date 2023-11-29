f1314cd07571efbdb3ed3d7aec84d8b4552ba34c
Alessio Gravili <alessio@bonfireleads.com>
2023-11-29 22:04:04 +0100

    chore: remove incorrect comment

--- packages/payload/src/admin/components/forms/field-types/RichText/types.ts
+++ packages/payload/src/admin/components/forms/field-types/RichText/types.ts
@@ -21,7 +21,6 @@ export type RichTextAdapter<
   CellComponent: () => React.FC<
     CellComponentProps<RichTextField<Value, AdapterProps, ExtraFieldProperties>>
   >
-  // Using a function to return the React.FC fixes the following issue: https://github.com/payloadcms/payload/issues/4282
   FieldComponent: () => React.FC<RichTextFieldProps<Value, AdapterProps, ExtraFieldProperties>>
   afterReadPromise?: ({
     field,
