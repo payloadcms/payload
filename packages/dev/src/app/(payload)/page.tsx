ab24796316562dc804a6507f43c4e062f69a05c4
Jacob Fletcher <jacobsfletch@gmail.com>
2023-12-06 11:59:14 -0500

    chore(next): exports root page and adds admin redirect

---
+++ packages/dev/src/app/(payload)/page.tsx
@@ -0,0 +1,6 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import { RootPage } from '@payloadcms/next/pages/Root'
+import config from 'payload-config'
+
+export default () => RootPage({ config })
