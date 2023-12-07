02d95e90b4290f2f2a553a1d9ed86bf7a34a61cf
Jacob Fletcher <jacobsfletch@gmail.com>
2023-12-07 16:12:07 -0500

    chore: scaffolds auth utility

---
+++ packages/dev/src/app/(payload)/admin/[collection]/[id]/page.tsx
@@ -0,0 +1,6 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import { Document } from '@payloadcms/next/pages/Document'
+import config from 'payload-config'
+
+export default ({ searchParams }) => Document({ searchParams, config })
