9222d6f2077fb918ca9d1ad2796ab3cf246b1579
Alessio Gravili <alessio@bonfireleads.com>
2023-12-11 16:35:26 +0100

    chore(richtext-lexical): remove unnecessary leftover Meta license headers, as they're already included in the LICENSE.md

--- packages/richtext-lexical/src/field/features/Link/nodes/LinkNode.ts
+++ packages/richtext-lexical/src/field/features/Link/nodes/LinkNode.ts
@@ -1,12 +1,3 @@
-/** @module @lexical/link */
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 import type { BaseSelection } from 'lexical'
 
 import { addClassNamesToElement, isHTMLAnchorElement } from '@lexical/utils'
