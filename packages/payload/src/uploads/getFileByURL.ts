fed2850ba29bad41a6500d318a960022fd273357
Jessica Chowdhury <jessica@trbl.design>
2023-12-12 12:44:09 +0000

    fix: reworks getFileByURL without node-fetch

--- packages/payload/src/uploads/getFileByURL.ts
+++ packages/payload/src/uploads/getFileByURL.ts
@@ -1,24 +1,21 @@
-import fetch from 'node-fetch'
 import path from 'path'
 
 import type { File } from './types'
 
 const getFileByURL = async (url: string): Promise<File> => {
   if (typeof url === 'string') {
-    const res = await fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'GET',
-    })
-    const data = await res.buffer()
+    const res = await fetch(url)
+    const blob = await res.blob()
+    const arrayBuffer = await blob.arrayBuffer()
+
+    const data = Buffer.from(arrayBuffer)
     const name = path.basename(url)
 
     return {
       name,
       data,
-      mimetype: res.headers.get('content-type') || undefined,
-      size: Number(res.headers.get('content-length')) || 0,
+      mimetype: blob.type || undefined,
+      size: blob.size || 0,
     }
   }
 }
