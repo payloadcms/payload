5eb00321c8c234a4fa5fe3c8133d2e2350b67d7d
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-12-13 17:12:14 -0500

    chore: forward all headers

--- packages/payload/src/uploads/getExternalFile.ts
+++ packages/payload/src/uploads/getExternalFile.ts
@@ -18,7 +18,7 @@ export const getExternalFile = async ({ req, data }: Args): Promise<File> => {
       credentials: 'include',
       method: 'GET',
       headers: {
-        cookie: req.headers.cookie,
+        ...req.headers,
       },
     })
 
