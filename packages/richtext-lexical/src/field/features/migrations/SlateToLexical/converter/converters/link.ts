31f8f3cac6bfd08f3adfa0a026a57c4b1b510045
Alessio Gravili <70709113+AlessioGr@users.noreply.github.com>
2023-12-13 13:16:58 +0100

    feat(richtext-lexical): Slate to Lexical converter: add blockquote conversion, convert custom link fields (#4486)

--- packages/richtext-lexical/src/field/features/migrations/SlateToLexical/converter/converters/link.ts
+++ packages/richtext-lexical/src/field/features/migrations/SlateToLexical/converter/converters/link.ts
@@ -14,7 +14,8 @@ export const SlateLinkConverter: SlateNodeConverter = {
       }),
       direction: 'ltr',
       fields: {
-        doc: slateNode.doc || undefined,
+        ...(slateNode.fields || {}),
+        doc: slateNode.doc || null,
         linkType: slateNode.linkType || 'custom',
         newTab: slateNode.newTab || false,
         url: slateNode.url || undefined,
