c54246079ad8c1716e1298e0f2c20413323de8d0
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-12-13 16:59:39 -0500

    fix: correctly fetches externally stored file for private buckets

---
+++ packages/payload/src/uploads/getExternalFile.ts
@@ -0,0 +1,36 @@
+import type { File, FileData } from './types'
+import { Request } from 'express'
+import { APIError } from '../errors'
+
+type Args = {
+  req: Request
+  data: FileData
+}
+export const getExternalFile = async ({ req, data }: Args): Promise<File> => {
+  const baseUrl = req.get('origin') || `${req.protocol}://${req.get('host')}`
+  const { url, filename } = data
+
+  if (typeof url === 'string') {
+    const fileURL = `${baseUrl}${url}`
+    const { default: fetch } = (await import('node-fetch')) as any
+
+    const res = await fetch(fileURL, {
+      credentials: 'include',
+      method: 'GET',
+      headers: {
+        cookie: req.headers.cookie,
+      },
+    })
+
+    if (!res.ok) throw new APIError(`Failed to fetch file from ${fileURL}`)
+
+    const data = await res.buffer()
+
+    return {
+      name: filename,
+      data,
+      mimetype: res.headers.get('content-type') || undefined,
+      size: Number(res.headers.get('content-length')) || 0,
+    }
+  }
+}
