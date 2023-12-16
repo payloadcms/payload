8a34be69469bfd67bafa92407155883a52e289ce
Ritsu <enjoythepain1337@gmail.com>
2023-12-16 23:52:17 +0200

    chore: simplify getTableName

--- packages/db-postgres/src/utilities/getTableName.ts
+++ packages/db-postgres/src/utilities/getTableName.ts
@@ -1,18 +1,14 @@
 import toSnakeCase from 'to-snake-case'
 
-export const getTableName = (
-  entityConfig: ({ enumName?: string } | { tableName?: string }) &
-    (
-      | {
-          name: string
-        }
-      | {
-          slug: string
-        }
-    ),
-) => {
-  if ('tableName' in entityConfig) return entityConfig.tableName
-  if ('enumName' in entityConfig) return entityConfig.enumName
-  if ('name' in entityConfig) return toSnakeCase(entityConfig.name)
-  return toSnakeCase(entityConfig.slug)
+type EntityConfig = {
+  enumName?: string
+  name?: string
+  slug?: string
+  tableName?: string
+}
+
+export const getTableName = ({ name, enumName, slug, tableName }: EntityConfig) => {
+  const generated = toSnakeCase(name ?? slug)
+  const custom = tableName ?? enumName
+  return custom ?? generated
 }
