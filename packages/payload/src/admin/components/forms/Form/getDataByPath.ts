d6a111013410d2a7781c16ded98e8d3a596acc5e
PatrikKozak <patrik@trbl.design>
2023-12-18 15:01:26 -0500

    fix: sets the return value to [] instead of 0 for arrays in getDataByPath

--- packages/payload/src/admin/components/forms/Form/getDataByPath.ts
+++ packages/payload/src/admin/components/forms/Form/getDataByPath.ts
@@ -6,14 +6,24 @@ const getDataByPath = <T = unknown>(fields: Fields, path: string): T => {
   const pathPrefixToRemove = path.substring(0, path.lastIndexOf('.') + 1)
   const name = path.split('.').pop()
 
+  let fieldHasEmptyRows = false
+
   const data = {}
   Object.keys(fields).forEach((key) => {
     if (!fields[key].disableFormData && (key.indexOf(`${path}.`) === 0 || key === path)) {
       data[key.replace(pathPrefixToRemove, '')] = fields[key].value
+
+      if (fields[key].rows && fields[key].rows.length === 0) {
+        fieldHasEmptyRows = true
+      }
     }
   })
 
   const unflattenedData = unflatten(data)
+
+  if (fieldHasEmptyRows) {
+    return [] as T
+  }
 
   return unflattenedData?.[name]
 }
