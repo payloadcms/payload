7ffab3ee5f9e76807af882d6428f785e3f5c3311
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-12-22 08:00:58 -0500

    chore: removes old files

--- packages/dev/src/app/(payload)/api/[collection]/access/route.ts
+++
@@ -1,6 +0,0 @@
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY it because it could be re-written at any time. */
-import { access } from '@payloadcms/next/routes/[collection]/access'
-import config from 'payload-config'
-
-export const GET = (request, context) => access({ config, collection: context.params.collection })
--- packages/dev/src/app/(payload)/api/access/route.ts
+++
@@ -1,6 +0,0 @@
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY it because it could be re-written at any time. */
-import { access } from '@payloadcms/next/routes/access'
-import config from 'payload-config'
-
-export const GET = access({ config })
