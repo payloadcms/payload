c22f4ef87cee46f0deb8e67a96198dcfb2dd8deb
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-01-10 11:03:20 -0500

    chore: simplify next/routes export and import paths

--- packages/dev/src/app/(payload)/api/[...slug]/route.ts
+++ packages/dev/src/app/(payload)/api/[...slug]/route.ts
@@ -1,6 +1,4 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY it because it could be re-written at any time. */
 
-export { GET, POST, DELETE, PATCH } from '@payloadcms/next/dist/routes/index'
-// use the next line for easier local development
-// export { GET, POST, DELETE, PATCH } from '@payloadcms/next/routes/index'
+export { GET, POST, DELETE, PATCH } from '@payloadcms/next/routes'
