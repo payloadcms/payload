f01ab5e2b1bb654a8cab41a215ee4b9ae0c3251a
Kendell Joseph <kendelljoseph@gmail.com>
2024-01-26 14:50:25 -0500

    chore: Adds Embedding collection

---
+++ examples/genai/src/collections/Embedding.ts
@@ -0,0 +1,30 @@
+import { CollectionConfig } from 'payload/types'
+
+export const Embedding: CollectionConfig = {
+  slug: 'embedding',
+  admin: {
+    useAsTitle: 'value',
+  },
+  fields: [
+    {
+      name: 'prompt',
+      type: 'relationship',
+      relationTo: 'prompt',
+    },
+    {
+      name: 'media',
+      type: 'relationship',
+      relationTo: 'media',
+    },
+    {
+      name: 'transformer',
+      type: 'relationship',
+      relationTo: 'transformer',
+    },
+    {
+      name: 'value',
+      type: 'text',
+      required: true,
+    },
+  ],
+}
