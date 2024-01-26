90463b040c820e377d868e400046153458bafdd7
Kendell Joseph <kendelljoseph@gmail.com>
2024-01-26 14:49:49 -0500

    chore: Adds Knowledge Graph collection

---
+++ examples/genai/src/collections/KnowledgeGraph.ts
@@ -0,0 +1,15 @@
+import { CollectionConfig } from 'payload/types'
+
+export const KnowledgeGraph: CollectionConfig = {
+  slug: 'knowledge-graph',
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+  ],
+}
