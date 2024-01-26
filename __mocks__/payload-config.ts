bd83d995faaffd9459b26aa90045c43fd32ad3c0
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-01-26 15:34:50 -0500

    chore: rm console log

--- __mocks__/payload-config.ts
+++ __mocks__/payload-config.ts
@@ -1,5 +1,3 @@
-console.log('PAYLOAD_CONFIG_PATH', process.env.PAYLOAD_CONFIG_PATH)
-
 export default typeof process.env.PAYLOAD_CONFIG_PATH === 'string'
   ? require(process.env.PAYLOAD_CONFIG_PATH)
   : {}
