f845842cfe3a6ff7024c4991500aa85522ae012b
Kendell Joseph <kendelljoseph@gmail.com>
2024-01-27 15:20:00 -0500

    chore: Adds virtual field

--- examples/genai/src/collections/KnowledgeGraph.ts
+++ examples/genai/src/collections/KnowledgeGraph.ts
@@ -1,3 +1,4 @@
+import * as neo4j from 'neo4j-driver'
 import { CollectionConfig } from 'payload/types'
 
 export const KnowledgeGraph: CollectionConfig = {
@@ -15,6 +16,47 @@ export const KnowledgeGraph: CollectionConfig = {
       name: 'resource',
       type: 'relationship',
       relationTo: 'resource',
+    },
+
+    // - This virtual field is populated by setting the query parameter 'genai=true'
+    // - This is a virtual field used to do Retrieval-Augmented Generation (RAG)
+    // - GenAI data is not stored on this field
+    {
+      name: 'genai',
+      type: 'text',
+      access: {
+        create: () => false,
+        update: () => false,
+      },
+      hooks: {
+        afterRead: [
+          async ({ data, req }) => {
+            const { id, url, prompts } = data
+
+            if (!req.query.genai) return // TODO: Requires 'fs' module
+
+            // Connect
+            let serverInfo = {}
+
+            try {
+              const driver = neo4j.driver(
+                process.env.NEO4J_URI,
+                neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
+              )
+              serverInfo = await driver.getServerInfo()
+            } catch (err) {
+              console.log(`Connection error\n${err}\nCause: ${err.cause}`)
+            }
+
+            return {
+              genai: true,
+              transactionInfo: {
+                serverInfo,
+              },
+            }
+          },
+        ],
+      },
     },
   ],
 }
