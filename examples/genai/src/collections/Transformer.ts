f376b032949bcf4bd88fb0320886f95c5af533eb
Kendell Joseph <kendelljoseph@gmail.com>
2024-01-28 13:53:02 -0500

    chore: Adds service option

--- examples/genai/src/collections/Transformer.ts
+++ examples/genai/src/collections/Transformer.ts
@@ -15,6 +15,10 @@ export const Transformer: CollectionConfig = {
           label: 'custom',
           value: 'custom',
         },
+        {
+          label: 'OpenAI',
+          value: 'openai',
+        },
       ],
     },
     {
