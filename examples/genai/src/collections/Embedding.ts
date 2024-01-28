ce034a8880cfb4bc8f98179f3c4278ca8b0e321d
Kendell Joseph <kendelljoseph@gmail.com>
2024-01-28 14:27:21 -0500

    chore: Adds virtual field

--- examples/genai/src/collections/Embedding.ts
+++ examples/genai/src/collections/Embedding.ts
@@ -1,14 +1,24 @@
 import { CollectionConfig } from 'payload/types'
 
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+import { OpenAIEmbeddings } from '@langchain/openai'
+
 export const Embedding: CollectionConfig = {
   slug: 'embedding',
   admin: {
-    useAsTitle: 'value',
+    useAsTitle: 'transformer',
   },
   fields: [
     {
+      name: 'transformer',
+      type: 'relationship',
+      relationTo: 'transformer',
+      required: true,
+    },
+    {
       name: 'source',
       type: 'array',
+      required: true,
       fields: [
         {
           name: 'prompt',
@@ -22,15 +32,65 @@ export const Embedding: CollectionConfig = {
         },
       ],
     },
+
+    // - This virtual field is populated by setting the query parameter 'genai=true'
+    // - This is a virtual field used to do Retrieval-Augmented Generation (RAG)
+    // - GenAI data is not stored on this field
     {
-      name: 'transformer',
-      type: 'relationship',
-      relationTo: 'transformer',
-    },
-    {
-      name: 'value',
+      name: 'genai',
       type: 'text',
-      required: true,
+      access: {
+        create: () => false,
+        update: () => false,
+      },
+      hooks: {
+        afterRead: [
+          async ({ data, req }) => {
+            const { id, url, source, transformer } = data
+
+            if (!req.query.genai) return
+
+            const transformerRecord = await req.payload.find({
+              req,
+              collection: 'transformer',
+              where: {
+                id: { equals: transformer },
+              },
+              limit: 1,
+            })
+
+            const resource = transformerRecord.docs[0].resource
+            const service = transformerRecord.docs[0].service
+            console.log(process.env.OPENAI_API_KEY)
+            const prompts = await Promise.all(
+              source.map(async ({ prompt }) => {
+                const record = await req.payload.find({
+                  req,
+                  collection: 'prompt',
+                  where: {
+                    id: { equals: prompt },
+                  },
+                  limit: 1,
+                })
+
+                return record.docs[0].prompt
+              }),
+            )
+
+            const embedding = { text: [], image: [], audio: [] }
+            if ('openai' === service) {
+              const openAIApiKey = resource?.key || process.env.OPENAI_API_KEY
+              const embeddings = new OpenAIEmbeddings({ openAIApiKey })
+              embedding.text = await embeddings.embedDocuments(prompts)
+            }
+
+            return {
+              generated: true,
+              embedding,
+            }
+          },
+        ],
+      },
     },
   ],
 }
