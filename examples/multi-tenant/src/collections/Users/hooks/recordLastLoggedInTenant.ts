c5abdaef517b55a4e0c586283d44c3056d0b8b3a
Jacob Fletcher <jacobsfletch@gmail.com>
2024-02-02 15:39:00 -0500

    chore(examples/multi-tenant): reset lastLoggedInTenant when none found

--- examples/multi-tenant/src/collections/Users/hooks/recordLastLoggedInTenant.ts
+++ examples/multi-tenant/src/collections/Users/hooks/recordLastLoggedInTenant.ts
@@ -2,7 +2,7 @@ import type { AfterLoginHook } from 'payload/dist/collections/config/types'
 
 export const recordLastLoggedInTenant: AfterLoginHook = async ({ req, user }) => {
   try {
-    const relatedOrg = await req.payload.find({
+    const relatedOrgs = await req.payload.find({
       collection: 'tenants',
       where: {
         'domains.domain': {
@@ -13,15 +13,15 @@ export const recordLastLoggedInTenant: AfterLoginHook = async ({ req, user }) =>
       limit: 1,
     })
 
-    if (relatedOrg.docs.length > 0) {
-      await req.payload.update({
-        id: user.id,
-        collection: 'users',
-        data: {
-          lastLoggedInTenant: relatedOrg.docs[0].id,
-        },
-      })
-    }
+    const relatedOrg = relatedOrgs?.docs?.[0]
+
+    await req.payload.update({
+      id: user.id,
+      collection: 'users',
+      data: {
+        lastLoggedInTenant: relatedOrg?.id || null,
+      },
+    })
   } catch (err: unknown) {
     req.payload.logger.error(`Error recording last logged in tenant for user ${user.id}: ${err}`)
   }
