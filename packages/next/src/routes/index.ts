aa1b98aef8fdc2e50ceb75e069a2e7e280a915ad
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-02-05 10:34:19 -0500

    remove old playground init, stub graphql handler location

--- packages/next/src/routes/index.ts
+++ packages/next/src/routes/index.ts
@@ -319,6 +319,8 @@ export const POST = async (
         default:
           return new Response('Route Not Found', { status: 404 })
       }
+    } else if (req.payloadAPI === 'GraphQL') {
+      // graphql handler
     } else {
       // root routes
       const customEndpointResponse = await handleCustomEndpoints({
