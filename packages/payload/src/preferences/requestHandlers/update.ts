cd4dc53afc53628289ccc05c047e011815525ba3
Jacob Fletcher <jacobsfletch@gmail.com>
2024-02-09 10:50:16 -0500

    fix(next): attaches params to req and properly assigns prefs key (#5042)

--- packages/payload/src/preferences/requestHandlers/update.ts
+++ packages/payload/src/preferences/requestHandlers/update.ts
@@ -4,10 +4,11 @@ import type { PayloadHandler } from '../../exports/config'
 
 import update from '../operations/update'
 
-export const updateHandler: PayloadHandler = async ({ req }) => {
+export const updateHandler: PayloadHandler = async ({ req, routeParams }) => {
   const payloadRequest = req
+
   const doc = await update({
-    key: req?.searchParams.get('key'),
+    key: routeParams?.key,
     req: payloadRequest,
     user: payloadRequest?.user,
     value: payloadRequest.data.value || payloadRequest.data,
