356975d06cb4faf7b795a412fcbd7fd9320e7e0e
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-02-12 16:44:20 -0500

    chore: migrate to new folder route for file GET requests

--- packages/dev/src/app/(payload)/media/[filename]/route.ts
+++
@@ -1,6 +0,0 @@
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY it because it could be re-written at any time. */
-
-import { GET as GET_file } from '@payloadcms/next/routes/fileHandler/[filename]/route'
-
-export const GET = GET_file('media')
