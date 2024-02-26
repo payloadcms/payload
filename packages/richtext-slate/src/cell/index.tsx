02cdc91fcd3b74ba8df50a1f768745790db2a50b
Alessio Gravili <alessio@gravili.de>
2024-02-26 14:55:17 -0500

    better code

--- packages/richtext-slate/src/cell/index.tsx
+++ packages/richtext-slate/src/cell/index.tsx
@@ -3,8 +3,8 @@ import type { CellComponentProps } from 'payload/types'
 
 import React from 'react'
 
-const RichTextCell: React.FC<CellComponentProps<any[]>> = (props) => {
-  const flattenedText = props?.cellData?.map((i) => i?.children?.map((c) => c.text)).join(' ')
+const RichTextCell: React.FC<CellComponentProps<any[]>> = ({ cellData }) => {
+  const flattenedText = cellData?.map((i) => i?.children?.map((c) => c.text)).join(' ')
 
   // Limiting the number of characters shown is done in a CSS rule
   return <span>{flattenedText}</span>
