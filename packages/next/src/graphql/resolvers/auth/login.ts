hanzo-apps/cms

chore: move all graphql code into next package

packages/next/src/graphql/resolvers/auth/login.ts
@@ -1,0 +1,25 @@
+import { loginOperation } from 'payload/operations'
+import type { Collection } from 'payload/types'
+
+import isolateTransactionID from '../../utilities/isolateTransactionID'
+
+function loginResolver(collection: Collection) {
+  async function resolver(_, args, context) {
+    const options = {
+      collection,
+      data: {
+        email: args.email,
+        password: args.password,
+      },
+      depth: 0,
+      req: isolateTransactionID(context.req),
+    }
+
+    const result = loginOperation(options)
+    return result
+  }
+
+  return resolver
+}
+
+export default loginResolver

packages/next/src/graphql/resolvers/collections/docAccess.ts
@@ -1,0 +1,26 @@
+import { docAccessOperation } from 'payload/operations'
+import type { PayloadRequest } from 'payload/types'
+import type { CollectionPermission, GlobalPermission } from 'payload/auth'
+
+import isolateTransactionID from '../../utilities/isolateTransactionID'
+
+export type Resolver = (
+  _: unknown,
+  args: {
+    id: number | string
+  },
+  context: {
+    req: PayloadRequest
+  },
+) => Promise<CollectionPermission | GlobalPermission>
+
+export function docAccessResolver(): Resolver {
+  async function resolver(_, args, context) {
+    return docAccessOperation({
+      id: args.id,
+      req: isolateTransactionID(context.req),
+    })
+  }
+
+  return resolver
+}

packages/next/src/graphql/resolvers/globals/findVersionByID.ts
@@ -1,0 +1,35 @@
+import { findVersionByIDOperationGlobal } from 'payload/operations'
+import type { Document, PayloadRequest, SanitizedGlobalConfig } from 'payload/types'
+
+import isolateTransactionID from '../../utilities/isolateTransactionID'
+
+export type Resolver = (
+  _: unknown,
+  args: {
+    draft?: boolean
+    fallbackLocale?: string
+    id: number | string
+    locale?: string
+  },
+  context: {
+    req: PayloadRequest
+  },
+) => Promise<Document>
+
+export default function findVersionByIDResolver(globalConfig: SanitizedGlobalConfig): Resolver {
+  return async function resolver(_, args, context) {
+    if (args.locale) context.req.locale = args.locale
+    if (args.fallbackLocale) context.req.fallbackLocale = args.fallbackLocale
+
+    const options = {
+      id: args.id,
+      depth: 0,
+      draft: args.draft,
+      globalConfig,
+      req: isolateTransactionID(context.req),
+    }
+
+    const result = await findVersionByIDOperationGlobal(options)
+    return result
+  }
+}

payloadcms/payload

packages/next/src/graphql/resolvers/auth/login.ts
@@ -1,0 +1,25 @@
+import { loginOperation } from 'payload/operations'
+import type { Collection } from 'payload/types'
+
+import isolateObjectProperty from '../../utilities/isolateObjectProperty'
+
+function loginResolver(collection: Collection) {
+  async function resolver(_, args, context) {
+    const options = {
+      collection,
+      data: {
+        email: args.email,
+        password: args.password,
+      },
+      depth: 0,
+      req: isolateObjectProperty(context.req, 'transactionID'),
+    }
+
+    const result = loginOperation(options)
+    return result
+  }
+
+  return resolver
+}
+
+export default loginResolver

packages/next/src/graphql/resolvers/collections/docAccess.ts
@@ -1,0 +1,26 @@
+import { docAccessOperation } from 'payload/operations'
+import type { PayloadRequest } from 'payload/types'
+import type { CollectionPermission, GlobalPermission } from 'payload/auth'
+
+import isolateObjectProperty from '../../utilities/isolateObjectProperty'
+
+export type Resolver = (
+  _: unknown,
+  args: {
+    id: number | string
+  },
+  context: {
+    req: PayloadRequest
+  },
+) => Promise<CollectionPermission | GlobalPermission>
+
+export function docAccessResolver(): Resolver {
+  async function resolver(_, args, context) {
+    return docAccessOperation({
+      id: args.id,
+      req: isolateObjectProperty<PayloadRequest>(context.req, 'transactionID'),
+    })
+  }
+
+  return resolver
+}

packages/next/src/graphql/resolvers/globals/findVersionByID.ts
@@ -1,0 +1,35 @@
+import { findVersionByIDOperationGlobal } from 'payload/operations'
+import type { Document, PayloadRequest, SanitizedGlobalConfig } from 'payload/types'
+
+import isolateObjectProperty from '../../utilities/isolateObjectProperty'
+
+export type Resolver = (
+  _: unknown,
+  args: {
+    draft?: boolean
+    fallbackLocale?: string
+    id: number | string
+    locale?: string
+  },
+  context: {
+    req: PayloadRequest
+  },
+) => Promise<Document>
+
+export default function findVersionByIDResolver(globalConfig: SanitizedGlobalConfig): Resolver {
+  return async function resolver(_, args, context) {
+    if (args.locale) context.req.locale = args.locale
+    if (args.fallbackLocale) context.req.fallbackLocale = args.fallbackLocale
+
+    const options = {
+      id: args.id,
+      depth: 0,
+      draft: args.draft,
+      globalConfig,
+      req: isolateObjectProperty<PayloadRequest>(context.req, 'transactionID'),
+    }
+
+    const result = await findVersionByIDOperationGlobal(options)
+    return result
+  }
+}