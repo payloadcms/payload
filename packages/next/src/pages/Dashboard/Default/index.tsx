6251e653d6e73e765717123f5f9cf32ab75ce5f8
Jacob Fletcher <jacobsfletch@gmail.com>
2024-02-27 16:36:06 -0500

    chore(next): supports admin.hidden property

--- packages/next/src/pages/Dashboard/Default/index.tsx
+++ packages/next/src/pages/Dashboard/Default/index.tsx
@@ -8,10 +8,14 @@ import './index.scss'
 
 const baseClass = 'dashboard'
 
-export const DefaultDashboard: React.FC<{
+export type DashboardProps = {
   Link: React.ComponentType<any>
   config: SanitizedConfig
-}> = (props) => {
+  visibleCollections: string[]
+  visibleGlobals: string[]
+}
+
+export const DefaultDashboard: React.FC<DashboardProps> = (props) => {
   const {
     Link,
     config: {
@@ -19,6 +23,8 @@ export const DefaultDashboard: React.FC<{
         components: { afterDashboard, beforeDashboard },
       },
     },
+    visibleCollections,
+    visibleGlobals,
   } = props
 
   return (
@@ -27,7 +33,11 @@ export const DefaultDashboard: React.FC<{
       <Gutter className={`${baseClass}__wrap`}>
         {Array.isArray(beforeDashboard) &&
           beforeDashboard.map((Component, i) => <Component key={i} />)}
-        <DefaultDashboardClient Link={Link} />
+        <DefaultDashboardClient
+          Link={Link}
+          visibleCollections={visibleCollections}
+          visibleGlobals={visibleGlobals}
+        />
         {Array.isArray(afterDashboard) &&
           afterDashboard.map((Component, i) => <Component key={i} />)}
       </Gutter>
