589d49af30efc4c23ce1b3c7365b9f0643d587f5
James <james@trbl.design>
2024-03-01 14:47:21 -0500

    chore: attempts pre-building next

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,1 +1,6 @@
-// Hello, world!
+export { AdminLayout } from './layouts/Admin'
+export { DocumentLayout } from './layouts/Document'
+export { RootLayout } from './layouts/Root'
+export { Dashboard as DashboardPage, generateMetadata as dashboardMeta } from './pages/Dashboard'
+export { Login, generateMetadata as loginMeta } from './pages/Login'
+export { RootPage } from './pages/Root'
