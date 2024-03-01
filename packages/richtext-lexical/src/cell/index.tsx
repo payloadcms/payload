ae0dd7b9c00d228f2411abce9e50aa698cd2d3f6
Alessio Gravili <alessio@gravili.de>
2024-03-01 15:03:18 -0500

    feat(richtext-lexical): working Cell

--- packages/richtext-lexical/src/cell/index.tsx
+++ packages/richtext-lexical/src/cell/index.tsx
@@ -4,36 +4,103 @@ import type { EditorConfig as LexicalEditorConfig } from 'lexical/LexicalEditor'
 import { createHeadlessEditor } from '@lexical/headless'
 import { useTableCell } from '@payloadcms/ui/elements'
 import { $getRoot } from 'lexical'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
+import type { FeatureProviderClient } from '../field/features/types'
 import type { SanitizedClientEditorConfig } from '../field/lexical/config/types'
+import type { GeneratedFeatureProviderComponent } from '../types'
 
 import { useFieldPath } from '../../../ui/src/forms/FieldPathProvider'
 import { useClientFunctions } from '../../../ui/src/providers/ClientFunction'
 import { defaultEditorLexicalConfig } from '../field/lexical/config/client/default'
+import { loadClientFeatures } from '../field/lexical/config/client/loader'
 import { sanitizeClientEditorConfig } from '../field/lexical/config/client/sanitize'
 import { getEnabledNodes } from '../field/lexical/nodes'
 
 export const RichTextCell: React.FC<{
   lexicalEditorConfig: LexicalEditorConfig
-}> = ({ lexicalEditorConfig }) => {
+}> = (props) => {
+  const { lexicalEditorConfig } = props
+
   const [preview, setPreview] = React.useState('Loading...')
   const { schemaPath } = useFieldPath()
-  const clientFunctions = useClientFunctions()
 
-  const { cellData, cellProps, columnIndex, richTextComponentMap, rowData } = useTableCell()
+  const { cellData, richTextComponentMap } = useTableCell()
+
+  const clientFunctions = useClientFunctions()
+  const [hasLoadedFeatures, setHasLoadedFeatures] = useState(false)
+
+  const [featureProviders, setFeatureProviders] = useState<FeatureProviderClient<unknown>[]>([])
+
+  const [finalSanitizedEditorConfig, setFinalSanitizedEditorConfig] =
+    useState<SanitizedClientEditorConfig>(null)
+
+  let featureProviderComponents: GeneratedFeatureProviderComponent[] =
+    richTextComponentMap.get('features')
+  // order by order
+  featureProviderComponents = featureProviderComponents.sort((a, b) => a.order - b.order)
+
+  const featureComponentsWithFeaturesLength =
+    Array.from(richTextComponentMap.keys()).filter(
+      (key) => key.startsWith(`feature.`) && !key.includes('.fields.'),
+    ).length + featureProviderComponents.length
+
+  useEffect(() => {
+    if (!hasLoadedFeatures) {
+      const featureProvidersLocal: FeatureProviderClient<unknown>[] = []
+      let featureProvidersAndComponentsLoaded = 0
+
+      Object.entries(clientFunctions).forEach(([key, plugin]) => {
+        if (key.startsWith(`lexicalFeature.${schemaPath}.`)) {
+          if (!key.includes('.components.')) {
+            featureProvidersLocal.push(plugin)
+          }
+          featureProvidersAndComponentsLoaded++
+        }
+      })
+
+      if (featureProvidersAndComponentsLoaded === featureComponentsWithFeaturesLength) {
+        setFeatureProviders(featureProvidersLocal)
+        setHasLoadedFeatures(true)
+
+        /**
+         * Loaded feature provided => create the final sanitized editor config
+         */
+
+        const resolvedClientFeatures = loadClientFeatures({
+          clientFunctions,
+          schemaPath,
+          unSanitizedEditorConfig: {
+            features: featureProvidersLocal,
+            lexical: lexicalEditorConfig,
+          },
+        })
+
+        setFinalSanitizedEditorConfig(
+          sanitizeClientEditorConfig(
+            lexicalEditorConfig ? lexicalEditorConfig : defaultEditorLexicalConfig,
+            resolvedClientFeatures,
+          ),
+        )
+      }
+    }
+  }, [
+    hasLoadedFeatures,
+    clientFunctions,
+    schemaPath,
+    featureProviderComponents.length,
+    featureProviders,
+    finalSanitizedEditorConfig,
+    lexicalEditorConfig,
+    featureComponentsWithFeaturesLength,
+  ])
 
   useEffect(() => {
     let dataToUse = cellData
-    if (dataToUse == null) {
+    if (dataToUse == null || !hasLoadedFeatures || !finalSanitizedEditorConfig) {
       setPreview('')
       return
     }
-
-    const finalSanitizedEditorConfig: SanitizedClientEditorConfig = sanitizeClientEditorConfig(
-      lexicalEditorConfig ? lexicalEditorConfig : defaultEditorLexicalConfig,
-      null,
-    )
 
     // Transform data through load hooks
     if (finalSanitizedEditorConfig?.features?.hooks?.load?.length) {
@@ -74,7 +141,35 @@ export const RichTextCell: React.FC<{
 
     // Limiting the number of characters shown is done in a CSS rule
     setPreview(textContent)
-  }, [cellData, lexicalEditorConfig])
+  }, [cellData, lexicalEditorConfig, hasLoadedFeatures, finalSanitizedEditorConfig])
+
+  if (!hasLoadedFeatures) {
+    return (
+      <React.Fragment>
+        {Array.isArray(featureProviderComponents) &&
+          featureProviderComponents.map((featureProvider) => {
+            // get all components starting with key feature.${FeatureProvider.key}.components.{featureComponentKey}
+            const featureComponentKeys = Array.from(richTextComponentMap.keys()).filter((key) =>
+              key.startsWith(`feature.${featureProvider.key}.components.`),
+            )
+            const featureComponents: React.ReactNode[] = featureComponentKeys.map((key) => {
+              return richTextComponentMap.get(key)
+            })
+
+            return (
+              <React.Fragment key={featureProvider.key}>
+                {featureComponents?.length
+                  ? featureComponents.map((FeatureComponent) => {
+                      return FeatureComponent
+                    })
+                  : null}
+                {featureProvider.ClientComponent}
+              </React.Fragment>
+            )
+          })}
+      </React.Fragment>
+    )
+  }
 
   return <span>{preview}</span>
 }
--- packages/richtext-lexical/src/exports/components.ts
+++ packages/richtext-lexical/src/exports/components.ts
@@ -1,6 +1,6 @@
 export { RichTextCell } from '../cell'
 export { RichTextField } from '../field'
 
-export { defaultEditorLexicalConfig } from '../field/lexical/config/client/defaultClient'
+export { defaultEditorLexicalConfig } from '../field/lexical/config/client/default'
 export { ToolbarButton } from '../field/lexical/plugins/FloatingSelectToolbar/ToolbarButton'
 export { ToolbarDropdown } from '../field/lexical/plugins/FloatingSelectToolbar/ToolbarDropdown/index'
