f2239decca20e5d309b83903a54309bf3c9dd924
James <james@trbl.design>
2024-03-04 16:17:29 -0500

    chore: abstracts next get route

--- app/(payload)/api/[...slug]/route.ts
+++ app/(payload)/api/[...slug]/route.ts
@@ -3,7 +3,9 @@
 import config from '@payload-config'
 import { REST_DELETE, REST_GET, REST_PATCH, REST_POST } from '@payloadcms/next/routes'
 
-export const GET = REST_GET(config)
+export const GET = async (request: Request, options: { params: { slug: string[] } }) => {
+  return REST_GET({ config, options, request })
+}
 export const POST = REST_POST(config)
 export const DELETE = REST_DELETE(config)
 export const PATCH = REST_PATCH(config)
