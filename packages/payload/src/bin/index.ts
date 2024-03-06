e7adaecb0ff326c5ec279ab2ddb4c2819ee171db
James <james@trbl.design>
2024-03-06 11:06:14 -0500

    chore: converts bin script to commonjs, which imports esm

--- packages/payload/src/bin/index.ts
+++ packages/payload/src/bin/index.ts
@@ -1,9 +1,4 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-var-requires */
-import swcRegister from '@swc/register'
-import { getTsconfig as getTSconfig } from 'get-tsconfig'
 import minimist from 'minimist'
-import path from 'path'
 
 import type { BinScript } from '../config/types'
 
@@ -13,41 +8,6 @@ import { loadEnv } from './loadEnv'
 import { migrate } from './migrate'
 
 loadEnv()
-
-const tsConfig = getTSconfig()
-
-const swcOptions = {
-  ignore: [
-    /.*[\\/]node_modules[\\/].*/, // parse everything besides files within node_modules
-  ],
-  jsc: {
-    baseUrl: path.resolve(),
-    parser: {
-      syntax: 'typescript',
-      tsx: true,
-    },
-    paths: undefined,
-  },
-  module: {
-    type: 'commonjs',
-  },
-  sourceMaps: 'inline',
-}
-
-if (tsConfig?.config?.compilerOptions?.paths) {
-  swcOptions.jsc.paths = tsConfig.config.compilerOptions.paths
-
-  if (tsConfig?.config?.compilerOptions?.baseUrl) {
-    swcOptions.jsc.baseUrl = path.resolve(tsConfig.config.compilerOptions.baseUrl)
-  }
-}
-
-// Allow disabling SWC for debugging
-if (process.env.DISABLE_SWC !== 'true') {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore - bad @swc/register types
-  swcRegister(swcOptions)
-}
 
 const executeBin = async () => {
   const args = minimist(process.argv.slice(2))
