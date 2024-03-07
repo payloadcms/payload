51e6c3566b9f37abd868e201b991e2bf0d86e6bb
Ed Torba <eduards.torba@gmail.com>
2024-03-07 15:46:21 +0000

    chore(auth): revert status code back to forbidden

--- packages/payload/src/auth/operations/logout.ts
+++ packages/payload/src/auth/operations/logout.ts
@@ -20,7 +20,7 @@ export const logoutOperation = async (incomingArgs: Arguments): Promise<boolean>
 
   if (!user) throw new APIError('No User', httpStatus.BAD_REQUEST)
   if (user.collection !== collectionConfig.slug)
-    throw new APIError('Incorrect collection', httpStatus.INTERNAL_SERVER_ERROR)
+    throw new APIError('Incorrect collection', httpStatus.FORBIDDEN)
 
   await collectionConfig.hooks.afterLogout.reduce(async (priorHook, hook) => {
     await priorHook
