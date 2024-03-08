dc5366ca3671fc9455a7d94f113e83f856570ae0
Willian Souza <willianaru@gmail.com>
2024-03-08 12:32:08 -0300

    feat(plugin-seo): add pt-br translations

--- packages/plugin-seo/src/translations/index.ts
+++ packages/plugin-seo/src/translations/index.ts
@@ -3,10 +3,13 @@ import es from './es.json'
 import fa from './fa.json'
 import fr from './fr.json'
 import pl from './pl.json'
+import ptBr from './pt-br.json'
+
 
 export default {
   en,
   es,
+  "pt-br": ptBr,
   fa,
   fr,
   pl,
