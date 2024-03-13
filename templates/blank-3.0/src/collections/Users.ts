80c8f014b50aef84c8a490b3af2148331d491871
PatrikKozak <patrik@payloadcms.com>
2024-03-13 09:32:58 -0400

    chore: scaffolds out blank 3.0 template

---
+++ templates/blank-3.0/src/collections/Users.ts
@@ -0,0 +1,15 @@
+import type { CollectionConfig } from 'payload/types'
+
+const Users: CollectionConfig = {
+  slug: 'users',
+  admin: {
+    useAsTitle: 'email',
+  },
+  auth: true,
+  fields: [
+    // Email added by default
+    // Add more fields as needed
+  ],
+}
+
+export default Users
