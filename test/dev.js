881fcd4e9b49a9385cedfef07a8fc645127fec58
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-03-13 13:00:24 -0400

    chore: alter @payload-config tsconfig for e2e tests

--- test/dev.js
+++ test/dev.js
@@ -1,16 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import { existsSync, promises, promises as _promises, promises as __promises } from 'fs'
-import json5 from 'json5'
 import minimist from 'minimist'
 import { nextDev } from 'next/dist/cli/next-dev.js'
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
+import { resolve } from 'path'
 
-const { readFile } = promises
-const { writeFile } = _promises
-const { rm } = __promises
-
-const { parse } = json5
+import { beforeTest } from './beforeTest.js'
 
 main()
 
@@ -26,28 +18,7 @@ async function main() {
     process.env.TURBOPACK = '1'
   }
 
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = dirname(__filename)
-
-  const testSuite = testSuiteArg || '_community'
-
-  console.log('\nUsing config:', testSuite, '\n')
-
-  // Delete next webpack cache
-  const nextWebpackCache = resolve(__dirname, '..', '.next/cache/webpack')
-  if (existsSync(nextWebpackCache)) {
-    await rm(nextWebpackCache, { recursive: true })
-  }
-
-  // Set path.'payload-config' in tsconfig.json
-  const tsConfigPath = resolve(__dirname, '..', 'tsconfig.json')
-  const tsConfig = await parse(await readFile(tsConfigPath, 'utf8'))
-  tsConfig.compilerOptions.paths['@payload-config'] = [`./test/${testSuite}/config.ts`]
-
-  await writeFile(tsConfigPath, JSON.stringify(tsConfig, null, 2))
-
-  const PAYLOAD_CONFIG_PATH = resolve(testSuite, 'config')
-  process.env.PAYLOAD_CONFIG_PATH = PAYLOAD_CONFIG_PATH
+  beforeTest(testSuiteArg)
 
   nextDev({ _: [resolve(__dirname, '..')], port: process.env.PORT || 3000 })
 }
--- test/helpers/configHelpers.ts
+++ test/helpers/configHelpers.ts
@@ -1,17 +1,14 @@
+import { promises as _promises } from 'fs'
 import getPort from 'get-port'
-import json5 from 'json5'
 import { nextDev } from 'next/dist/cli/next-dev.js'
-import path, { resolve } from 'path'
-const { parse } = json5
-import { promises as _promises, promises } from 'fs'
-const { readFile } = promises
-const { writeFile } = _promises
+import path from 'path'
 
 import type { SanitizedConfig } from '../../packages/payload/src/config/types.js'
 import type { Payload } from '../../packages/payload/src/index.js'
 
 import { getPayload } from '../../packages/payload/src/index.js'
 import wait from '../../packages/payload/src/utilities/wait.js'
+import { beforeTest } from '../beforeTest.js'
 
 type Args = {
   config: Promise<SanitizedConfig>
@@ -24,22 +21,14 @@ type Result = {
 }
 
 export async function initPayloadE2E({ config, dirname }: Args): Promise<Result> {
+  const testSuiteName = dirname.split('/').pop()
+  await beforeTest(testSuiteName)
+
   const port = await getPort()
   const serverURL = `http://localhost:${port}`
   process.env.NODE_OPTIONS = '--no-deprecation'
-  const configPath = path.resolve(dirname, './config.js')
-  process.env.PAYLOAD_CONFIG_PATH = configPath
   process.env.PAYLOAD_DROP_DATABASE = 'true'
   process.env.PORT = String(port)
-
-  const testSuite = configPath.split('/test/')[1].split('/')[0]
-
-  // Set path.'payload-config' in tsconfig.json
-  const tsConfigPath = resolve(process.cwd(), 'tsconfig.json')
-  const tsConfig = await parse(await readFile(tsConfigPath, 'utf8'))
-  tsConfig.compilerOptions.paths['@payload-config'] = [`./test/${testSuite}/config.ts`]
-
-  await writeFile(tsConfigPath, JSON.stringify(tsConfig, null, 2))
 
   const payload = await getPayload({ config })
 
