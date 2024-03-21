d511e80b0100233cef319437fe4caaef0888f7ef
Alessio Gravili <alessio@gravili.de>
2024-03-20 23:03:09 -0400

    chore: new payload/no-relative-monorepo-imports eslint rule

--- packages/eslint-plugin-payload/index.js
+++ packages/eslint-plugin-payload/index.js
@@ -2,5 +2,6 @@
 module.exports = {
   rules: {
     'no-non-retryable-assertions': require('./customRules/no-non-retryable-assertions'),
+    'no-relative-monorepo-imports': require('./customRules/no-relative-monorepo-imports'),
   },
 }
