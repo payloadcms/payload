b6912a487447e8ade1a6a413aa4fd3f375982cde
Alessio Gravili <alessio@gravili.de>
2024-03-22 10:11:15 -0400

    chore: new payload/no-flaky-assertions and payload/no-wait-function eslint rules

--- test/.eslintrc.cjs
+++ test/.eslintrc.cjs
@@ -63,6 +63,8 @@ module.exports = {
         'jest/require-top-level-describe': 'off',
         'jest-dom/prefer-to-have-attribute': 'off',
         'playwright/prefer-web-first-assertions': 'error',
+        'payload/no-flaky-assertions': 'warn',
+        'payload/no-wait-function': 'warn',
         // Enable the no-non-retryable-assertions rule ONLY for hunting for flakes
         // 'payload/no-non-retryable-assertions': 'error',
       },
