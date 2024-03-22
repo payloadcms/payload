af2af6687e6fdcf03d5cd1fe7f7e44f1b44f0b0a
Elliot DeNolf <denolfe@gmail.com>
2024-03-22 01:09:48 -0400

    chore(eslint): add no-jsx-import-statements custom rule

--- packages/eslint-plugin-payload/index.js
+++ packages/eslint-plugin-payload/index.js
@@ -1,6 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   rules: {
+    'no-jsx-import-statements': require('./customRules/no-jsx-import-statements'),
     'no-non-retryable-assertions': require('./customRules/no-non-retryable-assertions'),
     'no-relative-monorepo-imports': require('./customRules/no-relative-monorepo-imports'),
   },
