7af22a7c09f6da60fd274ad61ceb3b4bc481d0d1
Dan Ribbens <dan.ribbens@gmail.com>
2024-03-25 15:45:06 -0400

    fix(db-postgres): deleteOne handle joins

--- packages/db-postgres/src/deleteOne.ts
+++ packages/db-postgres/src/deleteOne.ts
@@ -1,47 +1,65 @@
 import type { DeleteOne } from 'payload/database'
 import type { PayloadRequest } from 'payload/types'
 
+import { eq } from 'drizzle-orm'
 import toSnakeCase from 'to-snake-case'
 
 import type { PostgresAdapter } from './types.js'
 
 import { buildFindManyArgs } from './find/buildFindManyArgs.js'
 import buildQuery from './queries/buildQuery.js'
+import { selectDistinct } from './queries/selectDistinct.js'
 import { transform } from './transform/read/index.js'
 
 export const deleteOne: DeleteOne = async function deleteOne(
   this: PostgresAdapter,
-  { collection, req = {} as PayloadRequest, where: incomingWhere },
+  { collection: collectionSlug, req = {} as PayloadRequest, where: whereArg },
 ) {
   const db = this.sessions[req.transactionID]?.db || this.drizzle
-  const collectionConfig = this.payload.collections[collection].config
-  const tableName = toSnakeCase(collection)
+  const collection = this.payload.collections[collectionSlug].config
+  const tableName = toSnakeCase(collectionSlug)
+  let docToDelete: Record<string, unknown>
 
-  const { where } = await buildQuery({
+  const { joinAliases, joins, selectFields, where } = await buildQuery({
     adapter: this,
-    fields: collectionConfig.fields,
+    fields: collection.fields,
+    locale: req.locale,
     tableName,
-    where: incomingWhere,
+    where: whereArg,
   })
 
-  const findManyArgs = buildFindManyArgs({
+  const selectDistinctResult = selectDistinct({
     adapter: this,
-    depth: 0,
-    fields: collectionConfig.fields,
+    chainedMethods: [{ args: [1], method: 'limit' }],
+    joinAliases,
+    joins,
+    selectFields,
     tableName,
+    where,
   })
 
-  findManyArgs.where = where
+  if (selectDistinctResult?.[0]?.id) {
+    docToDelete = await db.query[tableName].findFirst({
+      where: eq(this.tables[tableName].id, selectDistinctResult[0].id),
+    })
+  } else {
+    const findManyArgs = buildFindManyArgs({
+      adapter: this,
+      depth: 0,
+      fields: collection.fields,
+      tableName,
+    })
 
-  const docToDelete = await db.query[tableName].findFirst(findManyArgs)
+    docToDelete = await db.query[tableName].findFirst(findManyArgs)
+  }
 
   const result = transform({
     config: this.payload.config,
     data: docToDelete,
-    fields: collectionConfig.fields,
+    fields: collection.fields,
   })
 
-  await db.delete(this.tables[tableName]).where(where)
+  await db.delete(this.tables[tableName]).where(eq(this.tables[tableName].id, docToDelete.id))
 
   return result
 }
