c3e8e6d17360ed67416bdd5fe66c15cdfadf3f1a
Jacob Fletcher <jacobsfletch@gmail.com>
2024-03-27 10:19:27 -0400

    fix(ui): prevents field errors from flashing when used outside of field props context

--- packages/ui/src/forms/Form/index.tsx
+++ packages/ui/src/forms/Form/index.tsx
@@ -262,6 +262,7 @@ export const Form: React.FC<FormProps> = (props) => {
 
         if (res.status < 400) {
           setSubmitted(false)
+          console.log(json)
 
           if (typeof onSuccess === 'function') onSuccess(json)
 
