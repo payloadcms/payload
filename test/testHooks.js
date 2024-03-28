33c9f12539d69f01bd2247f90e75cf75b339d921
Alessio Gravili <alessio@gravili.de>
2024-03-28 14:35:29 -0400

    chore: obliterate next cache before starting e2e/int/dev

--- test/testHooks.js
+++ test/testHooks.js
@@ -10,26 +10,23 @@ const dirname = path.dirname(filename)
 export const createTestHooks = async (testSuiteName = '_community') => {
   const tsConfigPath = path.resolve(dirname, '../tsconfig.json')
   const tsConfig = await json5.parse(await readFile(tsConfigPath, 'utf8'))
-  const originalPayloadConfigTsValue =
-    tsConfig.compilerOptions.paths['@payload-config'] ?? './_community/config.ts'
 
   return {
     /**
      * Clear next webpack cache and set '@payload-config' path in tsconfig.json
      */
     beforeTest: async () => {
-      // Delete next webpack cache
-      const nextWebpackCache = path.resolve(dirname, '../.next/cache/webpack')
-      if (existsSync(nextWebpackCache)) {
-        await rm(nextWebpackCache, { recursive: true })
+      // Delete entire .next cache folder
+      const nextCache = path.resolve(dirname, '../.next')
+      if (existsSync(nextCache)) {
+        await rm(nextCache, { recursive: true })
       }
 
       // Set '@payload-config' in tsconfig.json
       tsConfig.compilerOptions.paths['@payload-config'] = [`./test/${testSuiteName}/config.ts`]
       await writeFile(tsConfigPath, JSON.stringify(tsConfig, null, 2))
 
-      const PAYLOAD_CONFIG_PATH = path.resolve(testSuiteName, 'config')
-      process.env.PAYLOAD_CONFIG_PATH = PAYLOAD_CONFIG_PATH
+      process.env.PAYLOAD_CONFIG_PATH = path.resolve(testSuiteName, 'config')
     },
     /**
      * Reset the changes made to tsconfig.json
