e4f0b9e79bc3774329f6097649e2e21895db19eb
Jacob Fletcher <jacobsfletch@gmail.com>
2024-04-01 12:00:30 -0400

    fix(next): estalblishes pattern for preview urls

---
+++ packages/next/src/routes/rest/collections/preview.ts
@@ -0,0 +1,45 @@
+import httpStatus from 'http-status'
+import { findByIDOperation } from 'payload/operations'
+import { isNumber } from 'payload/utilities'
+
+import type { CollectionRouteHandlerWithID } from '../types.js'
+
+import { routeError } from '../routeError.js'
+
+export const preview: CollectionRouteHandlerWithID = async ({ id, collection, req }) => {
+  const { searchParams } = req
+  const depth = searchParams.get('depth')
+
+  const result = await findByIDOperation({
+    id,
+    collection,
+    depth: isNumber(depth) ? Number(depth) : undefined,
+    draft: searchParams.get('draft') === 'true',
+    req,
+  })
+
+  let previewURL: string
+
+  const generatePreviewURL = req.payload.config.collections.find(
+    (config) => config.slug === collection.config.slug,
+  )?.admin?.preview
+
+  if (typeof generatePreviewURL === 'function') {
+    try {
+      previewURL = await generatePreviewURL(result, {
+        locale: req.locale,
+        token: req.user?.token,
+      })
+    } catch (err) {
+      routeError({
+        collection,
+        err,
+        req,
+      })
+    }
+  }
+
+  return Response.json(previewURL, {
+    status: httpStatus.OK,
+  })
+}
---
+++ packages/ui/src/elements/PreviewButton/usePreviewURL.tsx
@@ -0,0 +1,70 @@
+'use client'
+import { useCallback, useRef, useState } from 'react'
+import { toast } from 'react-toastify'
+
+import { useConfig } from '../../providers/Config/index.js'
+import { useDocumentInfo } from '../../providers/DocumentInfo/index.js'
+import { useLocale } from '../../providers/Locale/index.js'
+import { useTranslation } from '../../providers/Translation/index.js'
+
+export const usePreviewURL = (): {
+  generatePreviewURL: ({ openPreviewWindow }: { openPreviewWindow?: boolean }) => void
+  isLoading: boolean
+  label: string
+  previewURL: string
+} => {
+  const { id, collectionSlug, globalSlug } = useDocumentInfo()
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [previewURL, setPreviewURL] = useState('')
+  const { code: locale } = useLocale()
+
+  const {
+    routes: { api },
+    serverURL,
+  } = useConfig()
+
+  const { t } = useTranslation()
+
+  const isGeneratingPreviewURL = useRef(false)
+
+  // we need to regenerate the preview URL every time the button is clicked
+  // to do this we need to fetch the document data fresh from the API
+  // this will ensure the latest data is used when generating the preview URL
+  const generatePreviewURL = useCallback(
+    async ({ openPreviewWindow = false }) => {
+      if (isGeneratingPreviewURL.current) return
+      isGeneratingPreviewURL.current = true
+
+      try {
+        setIsLoading(true)
+
+        let url = `${serverURL}${api}`
+        if (collectionSlug) url = `${url}/${collectionSlug}/${id}/preview`
+        if (globalSlug) url = `${url}/globals/${globalSlug}/preview`
+
+        const res = await fetch(`${url}${locale ? `?locale=${locale}` : ''}`)
+
+        if (!res.ok) throw new Error()
+        const newPreviewURL = await res.json()
+        if (!newPreviewURL) throw new Error()
+        setPreviewURL(newPreviewURL)
+        setIsLoading(false)
+        isGeneratingPreviewURL.current = false
+        if (openPreviewWindow) window.open(newPreviewURL, '_blank')
+      } catch (err) {
+        setIsLoading(false)
+        isGeneratingPreviewURL.current = false
+        toast.error(t('error:previewing'))
+      }
+    },
+    [serverURL, api, collectionSlug, globalSlug, id, locale, t],
+  )
+
+  return {
+    generatePreviewURL,
+    isLoading,
+    label: isLoading ? t('general:loading') : t('version:preview'),
+    previewURL,
+  }
+}
