42212b409a4d4e782ab8cddf712175f8ec47c187
Alessio Gravili <alessio@gravili.de>
2024-04-02 10:19:24 -0400

    chore: remove console log

--- test/buildConfigWithDefaults.ts
+++ test/buildConfigWithDefaults.ts
@@ -36,7 +36,6 @@ import { localAPIEndpoint } from './helpers/sdk/endpoint.js'
 export async function buildConfigWithDefaults(
   testConfig?: Partial<Config>,
 ): Promise<SanitizedConfig> {
-  console.log('MEMORY URI', process.env.MONGODB_MEMORY_SERVER_URI)
   const databaseAdapters = {
     mongodb: mongooseAdapter({
       url:
