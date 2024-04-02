7054ae8a8866b20fcf36165cc996177e519d803b
Alessio Gravili <alessio@gravili.de>
2024-04-02 12:00:32 -0400

    chore: unit/int test CI stuff

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,28 +1,29 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
+import jestBaseConfig from '../jest.config.js'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  ...jestBaseConfig,
+  testMatch: ['<rootDir>/**/*int.spec.ts'],
+  setupFilesAfterEnv: [
+    //path.resolve(dirname, './helpers/startMemoryDB.ts'),
+    '<rootDir>/jest.setup.ts',
+  ],
+  globalSetup: path.resolve(dirname, './helpers/startMemoryDB.ts'),
   moduleNameMapper: {
     '\\.(css|scss)$': '<rootDir>/helpers/mocks/emptyModule.js',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/test/helpers/mocks/fileMock.js',
+      '<rootDir>/helpers/mocks/fileMock.js',
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  reporters: ['default', ['github-actions', { silent: false }], 'summary'],
-  testEnvironment: 'node',
-  globalSetup: path.resolve(dirname, './helpers/startMemoryDB.ts'),
-  testMatch: ['<rootDir>/**/*int.spec.ts'],
-  testTimeout: 90000,
-  transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
-  },
-  verbose: true,
+}
+
+if (process.env.CI) {
+  customJestConfig.reporters = [['github-actions', { silent: false }], 'summary']
 }
 
 export default customJestConfig
