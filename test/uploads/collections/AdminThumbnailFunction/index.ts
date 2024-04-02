1779bc4db7b3496214cd0b29bff8ef5db48b9b42
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-04-02 15:53:30 -0400

    chore: fix dirname for upload test

--- test/uploads/collections/AdminThumbnailFunction/index.ts
+++ test/uploads/collections/AdminThumbnailFunction/index.ts
@@ -1,13 +1,16 @@
 import type { CollectionConfig } from 'payload/types'
 
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 import { adminThumbnailFunctionSlug } from '../../shared.js'
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 export const AdminThumbnailFunction: CollectionConfig = {
   slug: adminThumbnailFunctionSlug,
   upload: {
-    staticDir: path.resolve(process.cwd(), 'test/uploads/media'),
+    staticDir: path.resolve(dirname, 'test/uploads/media'),
     adminThumbnail: () => 'https://payloadcms.com/images/universal-truth.jpg',
   },
   fields: [],
