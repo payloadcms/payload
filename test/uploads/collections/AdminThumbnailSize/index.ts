dddfa64a28df7653187c556b74fd1cb6735ad685
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-04-02 15:53:30 -0400

    chore: fix dirname for upload test

--- test/uploads/collections/AdminThumbnailSize/index.ts
+++ test/uploads/collections/AdminThumbnailSize/index.ts
@@ -1,13 +1,16 @@
 import type { CollectionConfig } from 'payload/types'
 
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 import { adminThumbnailSizeSlug } from '../../shared.js'
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 export const AdminThumbnailSize: CollectionConfig = {
   slug: adminThumbnailSizeSlug,
   upload: {
-    staticDir: path.resolve(process.cwd(), 'test/uploads/media'),
+    staticDir: path.resolve(dirname, 'test/uploads/media'),
     adminThumbnail: 'small',
     imageSizes: [
       {
