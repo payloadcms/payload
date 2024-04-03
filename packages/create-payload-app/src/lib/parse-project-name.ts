9f2aea1d7ce16f5c3d2cc387fa0264eb6291e1d1
Elliot DeNolf <denolfe@gmail.com>
2024-04-03 11:57:30 -0400

    chore(cpa): ensure project name is slugified

--- packages/create-payload-app/src/lib/parse-project-name.ts
+++ packages/create-payload-app/src/lib/parse-project-name.ts
@@ -1,10 +1,11 @@
 import * as p from '@clack/prompts'
+import slugify from '@sindresorhus/slugify'
 
 import type { CliArgs } from '../types.js'
 
 export async function parseProjectName(args: CliArgs): Promise<string> {
-  if (args['--name']) return args['--name']
-  if (args._[0]) return args._[0]
+  if (args['--name']) return slugify(args['--name'])
+  if (args._[0]) return slugify(args._[0])
 
   const projectName = await p.text({
     message: 'Project name?',
@@ -15,5 +16,5 @@ export async function parseProjectName(args: CliArgs): Promise<string> {
   if (p.isCancel(projectName)) {
     process.exit(0)
   }
-  return projectName
+  return slugify(projectName)
 }
