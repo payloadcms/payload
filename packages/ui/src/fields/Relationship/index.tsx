2a4e680da9264b32a0d6fbaf7d616323479ccd85
Alessio Gravili <alessio@gravili.de>
2024-04-04 14:26:01 -0400

    console logs!

--- packages/ui/src/fields/Relationship/index.tsx
+++ packages/ui/src/fields/Relationship/index.tsx
@@ -115,6 +115,8 @@ const RelationshipField: React.FC<RelationshipFieldProps> = (props) => {
       sort,
       value: valueArg,
     }) => {
+      console.log('inside getResults')
+
       if (!permissions) {
         return
       }
@@ -381,9 +383,12 @@ const RelationshipField: React.FC<RelationshipFieldProps> = (props) => {
     // If the menu is open while filterOptions changes
     // due to latency of getFormState and fast clicking into this field,
     // re-fetch options
+    console.log('in useEffect')
 
     if (hasLoadedFirstPageRef.current && menuIsOpen.current) {
       setIsLoading(true)
+      console.log('in useEffect > getResults')
+
       void getResults({
         lastLoadedPage: {},
         onSuccess: () => {
@@ -533,9 +538,11 @@ const RelationshipField: React.FC<RelationshipFieldProps> = (props) => {
             }}
             onMenuOpen={() => {
               menuIsOpen.current = true
+              console.log('onMenuOpen')
 
               if (!hasLoadedFirstPageRef.current) {
                 setIsLoading(true)
+                console.log('Getting results...')
                 void getResults({
                   lastLoadedPage: {},
                   onSuccess: () => {
