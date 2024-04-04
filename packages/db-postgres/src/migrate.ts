73408112051c91386e5d243d7b6922f0cc0bfb23
Elliot DeNolf <denolfe@gmail.com>
2024-04-04 19:33:51 -0400

    fix: db migrations esm

--- packages/db-postgres/src/migrate.ts
+++ packages/db-postgres/src/migrate.ts
@@ -3,6 +3,7 @@ import type { Payload } from 'payload'
 import type { Migration } from 'payload/database'
 import type { PayloadRequest } from 'payload/types'
 
+import { createRequire } from 'module'
 import {
   commitTransaction,
   initTransaction,
@@ -16,6 +17,8 @@ import type { PostgresAdapter } from './types.js'
 import { createMigrationTable } from './utilities/createMigrationTable.js'
 import { migrationTableExists } from './utilities/migrationTableExists.js'
 import { parseError } from './utilities/parseError.js'
+
+const require = createRequire(import.meta.url)
 
 export async function migrate(this: PostgresAdapter): Promise<void> {
   const { payload } = this
@@ -82,9 +85,7 @@ export async function migrate(this: PostgresAdapter): Promise<void> {
 }
 
 async function runMigrationFile(payload: Payload, migration: Migration, batch: number) {
-  const { generateDrizzleJson } = require
-    ? require('drizzle-kit/payload')
-    : await import('drizzle-kit/payload')
+  const { generateDrizzleJson } = require('drizzle-kit/payload')
 
   const start = Date.now()
   const req = { payload } as PayloadRequest
