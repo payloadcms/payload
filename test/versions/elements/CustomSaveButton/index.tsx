444245e510732632222d81679dc938fb87ba1034
PatrikKozak <patrik@payloadcms.com>
2024-04-05 11:06:00 -0400

    fix: updates type for custom publish button in versions test suite

--- test/versions/elements/CustomSaveButton/index.tsx
+++ test/versions/elements/CustomSaveButton/index.tsx
@@ -1,12 +1,12 @@
 'use client'
-import type { CustomPublishButtonProps } from 'payload/types'
+import type { CustomPublishButton as CustomPublishButtonType } from 'payload/types'
 
 import { DefaultPublishButton } from '@payloadcms/ui/elements/Publish'
 import * as React from 'react'
 
 import classes from './index.module.scss'
 
-export const CustomPublishButton: CustomPublishButtonProps = () => {
+export const CustomPublishButton: CustomPublishButtonType = () => {
   return (
     <div className={classes.customButton}>
       <DefaultPublishButton />
