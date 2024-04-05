76d37fec2385258e2d7009ceac549a97b383cc00
Alessio Gravili <alessio@gravili.de>
2024-04-05 11:16:51 -0400

    chore: reduce playwright e2e test timeout, so we see failing tests faster

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from '@playwright/test'
 
-export const EXPECT_TIMEOUT = 45000
+export const EXPECT_TIMEOUT = 6000
 export const POLL_TOPASS_TIMEOUT = EXPECT_TIMEOUT * 4 // That way expect.poll() or expect().toPass can retry 4 times. 4x higher than default expect timeout => can retry 4 times if retryable expects are used inside
 
 export default defineConfig({
   // Look for test files in the "test" directory, relative to this configuration file
   testDir: '',
   testMatch: '*e2e.spec.ts',
-  timeout: 240000, // 4 minutes
+  timeout: 40000, // 40 seconds
   use: {
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
