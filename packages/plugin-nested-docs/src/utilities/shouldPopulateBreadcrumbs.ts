6a85c3e0366bca552efb7a16b33d9d6a97546c6b
cuquo <francisco@cuquo.com>
2024-04-05 21:52:33 -0600

    remove eslint comments

--- packages/plugin-nested-docs/src/utilities/shouldPopulateBreadcrumbs.ts
+++ packages/plugin-nested-docs/src/utilities/shouldPopulateBreadcrumbs.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import type { CollectionConfig } from 'payload/types'
 
 import type { PluginConfig } from '../types.js'
