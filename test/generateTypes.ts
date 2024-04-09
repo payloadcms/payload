abf285d7130b17344fcc3c8184aa3476e9eceba0
Alessio Gravili <alessio@gravili.de>
2024-04-09 14:12:23 -0400

    chore: get dev:generate-types to work again (#5750)

--- test/generateTypes.ts
+++ test/generateTypes.ts
@@ -1,29 +1,41 @@
 import fs from 'fs'
 import path from 'path'
+import { generateTypes } from 'payload/node'
 
-import { generateTypes } from '../packages/payload/src/bin/generateTypes.js'
 import { setTestEnvPaths } from './helpers/setTestEnvPaths.js'
 
 const [testConfigDir] = process.argv.slice(2)
 
 import { fileURLToPath } from 'url'
+
+import { load } from './loader/load.js'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+const loadConfig = async (configPath: string) => {
+  const configPromise = await load(configPath)
+  return configPromise.default
+}
 
 let testDir
 if (testConfigDir) {
   testDir = path.resolve(dirname, testConfigDir)
+
+  const config = await loadConfig(path.resolve(testDir, 'config.ts'))
+
   setTestEnvPaths(testDir)
-  generateTypes()
+  generateTypes(config)
 } else {
   // Generate types for entire directory
   testDir = dirname
+
+  const config = await loadConfig(path.resolve(testDir, 'config.ts'))
 
   fs.readdirSync(dirname, { withFileTypes: true })
     .filter((f) => f.isDirectory())
     .forEach((dir) => {
       const suiteDir = path.resolve(testDir, dir.name)
       const configFound = setTestEnvPaths(suiteDir)
-      if (configFound) generateTypes()
+      if (configFound) generateTypes(config)
     })
 }
