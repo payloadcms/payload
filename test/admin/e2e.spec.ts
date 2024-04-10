b1ee709121f00e3c7734871faccdc9ff7a0dd2af
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-04-10 16:21:13 -0400

    chore: more flake fixes

--- test/admin/e2e.spec.ts
+++ test/admin/e2e.spec.ts
@@ -429,7 +429,9 @@ describe('admin', () => {
 
     test('collection — should render `id` as `useAsTitle` fallback', async () => {
       const { id } = await createPost()
-      await page.goto(postsUrl.edit(id))
+      const postURL = postsUrl.edit(id)
+      await page.goto(postURL)
+      await page.waitForURL(postURL)
       await page.locator('#field-title')?.fill('')
       await expect(page.locator('.doc-header__title.render-title:has-text("ID:")')).toBeVisible()
       await saveDocAndAssert(page)
