71758f2d545f7e310f64548c6d5d55d260677557
§ <git@viveur.uk>
2024-04-10 14:40:49 +0100

    Use tooltip--error instead of separate stylesheet.

--- packages/payload/src/admin/components/forms/Error/index.tsx
+++ packages/payload/src/admin/components/forms/Error/index.tsx
@@ -3,16 +3,13 @@ import React from 'react'
 import type { Props } from './types'
 
 import Tooltip from '../../elements/Tooltip'
-import './index.scss'
-
-const baseClass = 'field-error'
 
 const Error: React.FC<Props> = (props) => {
   const { alignCaret = 'right', message, showError = false } = props
 
   if (showError) {
     return (
-      <Tooltip alignCaret={alignCaret} className={baseClass} delay={0}>
+      <Tooltip alignCaret={alignCaret} className="tooltip--error" delay={0}>
         {message}
       </Tooltip>
     )
