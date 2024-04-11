f3e25f32774357f55051cc4eb45d2affaf56ff0d
James <james@trbl.design>
2024-04-11 11:17:59 -0400

    more de-flake

--- test/fields/e2e.spec.ts
+++ test/fields/e2e.spec.ts
@@ -746,8 +746,7 @@ describe('fields', () => {
         .locator('.file-field__upload input[type="file"]')
         .setInputFiles(path.resolve(dirname, './collections/Upload/payload.jpg'))
       await expect(page.locator('.file-field .file-field__filename')).toHaveValue('payload.jpg')
-      await page.locator('#action-save').click()
-      await expect(page.locator('.Toastify')).toContainText('successfully')
+      await saveDocAndAssert(page)
     }
 
     // eslint-disable-next-line playwright/expect-expect
@@ -766,7 +765,7 @@ describe('fields', () => {
 
     test('should upload using the document drawer', async () => {
       await uploadImage()
-      await wait(500)
+      await wait(1000)
       // Open the media drawer and create a png upload
 
       await openDocDrawer(page, '.field-type.upload .upload__toggler.doc-drawer__toggler')
