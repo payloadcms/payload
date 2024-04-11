2063a628071371ec5a686a596ca2187398063478
Elliot DeNolf <denolfe@gmail.com>
2024-04-10 22:17:43 -0400

    chore(cpa): add or operator to process.env.DATABASE_URI [skip ci]

--- packages/create-payload-app/src/lib/packages.ts
+++ packages/create-payload-app/src/lib/packages.ts
@@ -10,14 +10,18 @@ const mongodbReplacement: DbAdapterReplacement = {
   importReplacement: "import { mongooseAdapter } from '@payloadcms/db-mongodb'",
   packageName: '@payloadcms/db-mongodb',
   // Replacement between `// database-adapter-config-start` and `// database-adapter-config-end`
-  configReplacement: ['  db: mongooseAdapter({', '    url: process.env.DATABASE_URI,', '  }),'],
+  configReplacement: [
+    '  db: mongooseAdapter({',
+    "    url: process.env.DATABASE_URI || '',",
+    '  }),',
+  ],
 }
 
 const postgresReplacement: DbAdapterReplacement = {
   configReplacement: [
     '  db: postgresAdapter({',
     '    pool: {',
-    '      connectionString: process.env.DATABASE_URI,',
+    "      connectionString: process.env.DATABASE_URI || '',",
     '    },',
     '  }),',
   ],
