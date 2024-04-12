d98d0fd5bd0f736b1d3b03e4cf9666e3af787bef
Elliot DeNolf <denolfe@gmail.com>
2024-04-12 09:28:32 -0400

    chore(pcs): use proper getFilePrefix

--- packages/plugin-cloud-storage/src/adapters/vercelBlob/staticHandler.ts
+++ packages/plugin-cloud-storage/src/adapters/vercelBlob/staticHandler.ts
@@ -4,6 +4,8 @@ import type { CollectionConfig, PayloadRequest, UploadConfig } from 'payload/typ
 import { head } from '@vercel/blob'
 import path from 'path'
 
+import { getFilePrefix } from '../../utilities/getFilePrefix.js'
+
 type StaticHandlerArgs = {
   baseUrl: string
   token: string
@@ -47,35 +49,4 @@ export const getStaticHandler = (
       return new Response('Internal Server Error', { status: 500 })
     }
   }
-}
-
-async function getFilePrefix({
-  collection,
-  req,
-}: {
-  collection: CollectionConfig
-  req: PayloadRequest
-}): Promise<string> {
-  const imageSizes = (collection?.upload as UploadConfig)?.imageSizes || []
-  const { routeParams } = req
-  const filename = routeParams?.['filename']
-
-  const files = await req.payload.find({
-    collection: collection.slug,
-    depth: 0,
-    limit: 1,
-    pagination: false,
-    where: {
-      or: [
-        {
-          filename: { equals: filename },
-        },
-        ...imageSizes.map((imageSize) => ({
-          [`sizes.${imageSize.name}.filename`]: { equals: filename },
-        })),
-      ],
-    },
-  })
-  const prefix = files?.docs?.[0]?.prefix
-  return prefix ? (prefix as string) : ''
 }
