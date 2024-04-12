70fcd6bf409b132bfbf310f6a52f6cd141b09fdf
Elliot DeNolf <denolfe@gmail.com>
2024-04-12 10:51:19 -0400

    feat: rework image dimensions, use image-size

--- packages/payload/src/uploads/getImageSize.ts
+++ packages/payload/src/uploads/getImageSize.ts
@@ -1,14 +1,31 @@
 import fs from 'fs'
-import probeImageSize from 'probe-image-size'
+import sizeOfImport from 'image-size'
+import { temporaryFileTask } from 'tempy'
+import { promisify } from 'util'
 
 import type { PayloadRequest } from '../types/index.js'
 import type { ProbedImageSize } from './types.js'
 
-export function getImageSize(file: PayloadRequest['file']): ProbedImageSize {
+const { imageSize } = sizeOfImport
+const imageSizePromise = promisify(imageSize)
+
+export async function getImageSize(file: PayloadRequest['file']): Promise<ProbedImageSize> {
   if (file.tempFilePath) {
-    const data = fs.readFileSync(file.tempFilePath)
-    return probeImageSize.sync(data)
+    return imageSizePromise(file.tempFilePath)
   }
 
-  return probeImageSize.sync(file.data)
+  // Tiff file do not support buffers or streams, so we must write to file first
+  // then retrieve dimensions. https://github.com/image-size/image-size/issues/103
+  if (file.mimetype === 'image/tiff') {
+    const dimensions = await temporaryFileTask(
+      async (filepath) => {
+        fs.writeFileSync(filepath, file.data)
+        return imageSizePromise(filepath)
+      },
+      { extension: 'tiff' },
+    )
+    return dimensions
+  }
+
+  return imageSize(file.data)
 }
