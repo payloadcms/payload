db6ebba5a1a40650467d81b3e16994de9fb00ced
Paul Popus <paul@nouance.io>
2024-04-15 19:03:19 -0300

    fix: check if password and confirm-password match on registering the first user

--- packages/next/src/routes/rest/auth/registerFirstUser.ts
+++ packages/next/src/routes/rest/auth/registerFirstUser.ts
@@ -1,11 +1,21 @@
 import httpStatus from 'http-status'
 import { generatePayloadCookie } from 'payload/auth'
+import { ValidationError } from 'payload/errors'
 import { registerFirstUserOperation } from 'payload/operations'
 
 import type { CollectionRouteHandler } from '../types.js'
 
 export const registerFirstUser: CollectionRouteHandler = async ({ collection, req }) => {
   const data = req.data
+
+  if (data?.password !== data['confirm-password']) {
+    throw new ValidationError([
+      {
+        field: 'confirm-password',
+        message: req.t('Password and confirm password fields do not match.'),
+      },
+    ])
+  }
 
   const result = await registerFirstUserOperation({
     collection,
@@ -26,8 +36,7 @@ export const registerFirstUser: CollectionRouteHandler = async ({ collection, re
   return Response.json(
     {
       exp: result.exp,
-      // TODO(translate)
-      message: 'Successfully registered first user.',
+      message: req.t('Successfully registered first user.'),
       token: result.token,
       user: result.user,
     },
