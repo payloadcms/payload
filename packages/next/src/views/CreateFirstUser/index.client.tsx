842b85abf5dd0fe1958b1460a2e8131c87dcc2ef
Paul Popus <paul@nouance.io>
2024-04-16 10:33:30 -0300

    chore: move field map to server side

--- packages/next/src/views/CreateFirstUser/index.client.tsx
+++ packages/next/src/views/CreateFirstUser/index.client.tsx
@@ -1,6 +1,5 @@
 'use client'
 import type { FieldMap } from '@payloadcms/ui/utilities/buildComponentMap'
-import type { FieldTypes } from 'payload/config'
 
 import { RenderFields } from '@payloadcms/ui/forms/RenderFields'
 import { useComponentMap } from '@payloadcms/ui/providers/ComponentMap'
@@ -14,29 +13,9 @@ export const CreateFirstUserFields: React.FC<{
 
   const fieldMap = getFieldMap({ collectionSlug: userSlug })
 
-  const firstUserFieldMapWithPasswordFields = createFirstUserFieldMap.map((field) => {
-    if (field.name === 'password') {
-      const type: keyof FieldTypes = 'password'
-
-      return {
-        ...field,
-        type,
-      }
-    }
-    if (field.name === 'confirm-password') {
-      const type: keyof FieldTypes = 'confirmPassword'
-
-      return {
-        ...field,
-        type,
-      }
-    }
-    return field
-  })
-
   return (
     <RenderFields
-      fieldMap={[...(firstUserFieldMapWithPasswordFields || []), ...(fieldMap || [])]}
+      fieldMap={[...(createFirstUserFieldMap || []), ...(fieldMap || [])]}
       operation="create"
       path=""
       readOnly={false}
