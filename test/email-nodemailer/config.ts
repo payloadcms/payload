83c617b4528fcc71c3eae1fd00769b299c59fd25
Elliot DeNolf <denolfe@gmail.com>
2024-04-18 14:17:29 -0400

    test: clean up email-nodemailer config

--- test/email-nodemailer/config.ts
+++ test/email-nodemailer/config.ts
@@ -1,13 +1,7 @@
 import { createNodemailerAdapter } from '@payloadcms/email-nodemailer'
-import path from 'path'
-import { getFileByPath } from 'payload/uploads'
-import { fileURLToPath } from 'url'
 
 import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
 import { devUser } from '../credentials.js'
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
 
 export default buildConfigWithDefaults({
   // ...extend config here
@@ -27,14 +21,6 @@ export default buildConfigWithDefaults({
       subject: 'This was sent on init',
     })
 
-    // Create image
-    const imageFilePath = path.resolve(dirname, '../uploads/image.png')
-    const imageFile = await getFileByPath(imageFilePath)
-
-    await payload.create({
-      collection: 'media',
-      data: {},
-      file: imageFile,
-    })
+    payload.logger.info({ msg: 'Email sent', email })
   },
 })
