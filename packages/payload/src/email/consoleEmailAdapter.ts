bb3bd9c39543bb77e929ca4f819b4d54ccedf58d
Elliot DeNolf <denolfe@gmail.com>
2024-04-22 15:42:21 -0400

    chore: adjust email adapter messaging

--- packages/payload/src/email/consoleEmailAdapter.ts
+++ packages/payload/src/email/consoleEmailAdapter.ts
@@ -3,12 +3,12 @@ import type { EmailAdapter } from './types.js'
 import { emailDefaults } from './defaults.js'
 import { getStringifiedToAddress } from './getStringifiedToAddress.js'
 
-export const stdoutAdapter: EmailAdapter<void> = ({ payload }) => ({
+export const consoleEmailAdapter: EmailAdapter<void> = ({ payload }) => ({
   defaultFromAddress: emailDefaults.defaultFromAddress,
   defaultFromName: emailDefaults.defaultFromName,
   sendEmail: async (message) => {
     const stringifiedTo = getStringifiedToAddress(message)
-    const res = `EMAIL NON-DELIVERY. To: '${stringifiedTo}', Subject: '${message.subject}'`
+    const res = `Email attempted without being configured. To: '${stringifiedTo}', Subject: '${message.subject}'`
     payload.logger.info({ msg: res })
     return Promise.resolve()
   },
