e4d024cd0d2a754cbb94ba1f68d7e9259b5c2a96
James <james@trbl.design>
2024-04-23 12:43:25 -0400

    chore: properly destroys db in postgres

--- packages/db-postgres/src/destroy.ts
+++ packages/db-postgres/src/destroy.ts
@@ -2,13 +2,13 @@ import type { Destroy } from 'payload/database'
 
 import type { PostgresAdapter } from './types.js'
 
-import { pushDevSchema } from './utilities/pushDevSchema.js'
-
+// eslint-disable-next-line @typescript-eslint/require-await
 export const destroy: Destroy = async function destroy(this: PostgresAdapter) {
-  if (process.env.NODE_ENV !== 'production') {
-    await pushDevSchema(this)
-  } else {
-    // TODO: this hangs test suite for some reason
-    // await this.pool.end()
-  }
+  this.enums = {}
+  this.schema = {}
+  this.tables = {}
+  this.relations = {}
+  this.blockTableNames = {}
+  this.fieldConstraints = {}
+  this.drizzle = undefined
 }
