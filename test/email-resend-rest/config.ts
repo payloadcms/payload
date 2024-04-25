d3e83f3b6a7bba14bf95ce65ec652b5cbe7d0711
Elliot DeNolf <denolfe@gmail.com>
2024-04-24 22:15:06 -0400

    test: add email-resend-rest

---
+++ test/email-resend-rest/config.ts
@@ -0,0 +1,31 @@
+import { resendAdapter } from '@payloadcms/email-resend-rest'
+
+import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
+import { devUser } from '../credentials.js'
+
+export default buildConfigWithDefaults({
+  // ...extend config here
+  collections: [],
+  email: resendAdapter({
+    defaultFromAddress: 'dev@payloadcms.com',
+    defaultFromName: 'Payload CMS',
+    resendApiKey: String(process.env.RESEND_API_KEY),
+  }),
+  onInit: async (payload) => {
+    await payload.create({
+      collection: 'users',
+      data: {
+        email: devUser.email,
+        password: devUser.password,
+      },
+    })
+
+    const email = await payload.sendEmail({
+      to: 'elliot@payloadcms.com',
+      subject: 'This was sent on init',
+      text: 'This is my message body',
+    })
+
+    payload.logger.info({ msg: 'Email sent', email })
+  },
+})
