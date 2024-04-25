2fb15cf63b70ba8014c3cc4bd7dac41d3abf9078
Patrik Kozak <patrik@payloadcms.com>
2024-04-25 16:26:56 -0400

    chore: restricts int tests to only run on mongoose

--- test/plugin-relationship-object-ids/int.spec.ts
+++ test/plugin-relationship-object-ids/int.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-if */
 import payload from '../../packages/payload/src'
 import { initPayloadTest } from '../helpers/configHelpers'
 
@@ -10,26 +11,27 @@ describe('Relationship Object IDs Plugin', () => {
   })
 
   it('seeds data accordingly', async () => {
-    const relationsQuery = await payload.find({
-      collection: 'relations',
-      sort: 'createdAt',
-    })
+    if (payload.db.name === 'mongoose') {
+      const relationsQuery = await payload.find({
+        collection: 'relations',
+        sort: 'createdAt',
+      })
 
-    relations = relationsQuery.docs
+      relations = relationsQuery.docs
 
-    const postsQuery = await payload.find({
-      collection: 'posts',
-      sort: 'createdAt',
-    })
+      const postsQuery = await payload.find({
+        collection: 'posts',
+        sort: 'createdAt',
+      })
 
-    posts = postsQuery.docs
+      posts = postsQuery.docs
 
-    expect(relationsQuery.totalDocs).toEqual(2)
-    expect(postsQuery.totalDocs).toEqual(2)
+      expect(relationsQuery.totalDocs).toEqual(2)
+      expect(postsQuery.totalDocs).toEqual(2)
+    }
   })
 
   it('stores relations as object ids', async () => {
-    // eslint-disable-next-line jest/no-if
     if (payload.db.name === 'mongoose') {
       const docs = await payload.db.collections.relations.find()
       expect(typeof docs[0].hasOne).toBe('object')
@@ -41,53 +43,61 @@ describe('Relationship Object IDs Plugin', () => {
   })
 
   it('can query by relationship id', async () => {
-    const { totalDocs } = await payload.find({
-      collection: 'relations',
-      where: {
-        hasOne: {
-          equals: posts[0].id,
+    if (payload.db.name === 'mongoose') {
+      const { totalDocs } = await payload.find({
+        collection: 'relations',
+        where: {
+          hasOne: {
+            equals: posts[0].id,
+          },
         },
-      },
-    })
+      })
 
-    expect(totalDocs).toStrictEqual(1)
+      expect(totalDocs).toStrictEqual(1)
+    }
   })
 
   it('populates relations', () => {
-    const populatedPostTitle =
-      // eslint-disable-next-line jest/no-if
-      typeof relations[0].hasOne === 'object' ? relations[0].hasOne.title : undefined
-    expect(populatedPostTitle).toBeDefined()
+    if (payload.db.name === 'mongoose') {
+      const populatedPostTitle =
+        // eslint-disable-next-line jest/no-if
+        typeof relations[0].hasOne === 'object' ? relations[0].hasOne.title : undefined
+      expect(populatedPostTitle).toBeDefined()
 
-    const populatedUploadFilename =
-      typeof relations[0].upload === 'object' ? relations[0].upload.filename : undefined
+      const populatedUploadFilename =
+        typeof relations[0].upload === 'object' ? relations[0].upload.filename : undefined
 
-    expect(populatedUploadFilename).toBeDefined()
+      expect(populatedUploadFilename).toBeDefined()
+    }
   })
 
   it('can query by nested property', async () => {
-    const { totalDocs } = await payload.find({
-      collection: 'relations',
-      where: {
-        'hasOne.title': {
-          equals: 'post 1',
+    if (payload.db.name === 'mongoose') {
+      const { totalDocs } = await payload.find({
+        collection: 'relations',
+        where: {
+          'hasOne.title': {
+            equals: 'post 1',
+          },
         },
-      },
-    })
+      })
 
-    expect(totalDocs).toStrictEqual(1)
+      expect(totalDocs).toStrictEqual(1)
+    }
   })
 
   it('can query using the "in" operator', async () => {
-    const { totalDocs } = await payload.find({
-      collection: 'relations',
-      where: {
-        hasMany: {
-          in: [posts[0].id],
+    if (payload.db.name === 'mongoose') {
+      const { totalDocs } = await payload.find({
+        collection: 'relations',
+        where: {
+          hasMany: {
+            in: [posts[0].id],
+          },
         },
-      },
-    })
+      })
 
-    expect(totalDocs).toStrictEqual(1)
+      expect(totalDocs).toStrictEqual(1)
+    }
   })
 })
