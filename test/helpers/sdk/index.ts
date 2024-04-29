250d84ba430c190a9acae9ffd218b810544b0278
Jacob Fletcher <jacobsfletch@gmail.com>
2024-04-29 18:05:36 -0400

    removes jakes negligence

--- test/helpers/sdk/index.ts
+++ test/helpers/sdk/index.ts
@@ -75,7 +75,6 @@ export class PayloadTestSDK<TGeneratedTypes extends GeneratedTypes<TGeneratedTyp
     jwt,
     ...args
   }: LoginArgs<TGeneratedTypes, T>) => {
-    console.log('login', args)
     return this.fetch<TGeneratedTypes['collections'][T]>({
       operation: 'login',
       args,
