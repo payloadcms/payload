8220b6fc3e51087feec37fa875372eabfab1d659
Alessio Gravili <alessio@gravili.de>
2024-05-01 15:46:25 -0400

    test out github reporter

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -28,5 +28,5 @@ export default defineConfig({
   },
   workers: 16,
   maxFailures: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'list' : 'list',
+  reporter: process.env.CI ? 'github' : [['list', { printSteps: true }]],
 })
