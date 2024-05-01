50db9e53a4f8ee445f785a2a162d4d2b612970f5
Alessio Gravili <alessio@gravili.de>
2024-05-01 11:41:04 -0400

    reduce test timeout

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -8,14 +8,14 @@ const dirname = path.dirname(filename)
 
 dotenv.config({ path: path.resolve(dirname, 'test.env') })
 
-export const EXPECT_TIMEOUT = 45000
+export const EXPECT_TIMEOUT = 8000
 export const POLL_TOPASS_TIMEOUT = EXPECT_TIMEOUT * 4 // That way expect.poll() or expect().toPass can retry 4 times. 4x higher than default expect timeout => can retry 4 times if retryable expects are used inside
 
 export default defineConfig({
   // Look for test files in the "test" directory, relative to this configuration file
   testDir: '',
   testMatch: '*e2e.spec.ts',
-  timeout: 120000,
+  timeout: 60000, // 1 minute
   use: {
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
