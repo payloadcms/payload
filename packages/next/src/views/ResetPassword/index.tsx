c0ae287d462cd795d600fbf0fcf0542af56ce624
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2024-05-02 15:08:47 -0400

    fix: reset password validations (#6153)
    Co-authored-by: Elliot DeNolf <denolfe@gmail.com>
    Co-authored-by: James <james@trbl.design>
    Co-authored-by: Alessio Gravili <alessio@gravili.de>

--- packages/next/src/views/ResetPassword/index.tsx
+++ packages/next/src/views/ResetPassword/index.tsx
@@ -2,15 +2,11 @@ import type { AdminViewProps } from 'payload/types'
 
 import { Button } from '@payloadcms/ui/elements/Button'
 import { Translation } from '@payloadcms/ui/elements/Translation'
-import { ConfirmPassword } from '@payloadcms/ui/fields/ConfirmPassword'
-import { HiddenInput } from '@payloadcms/ui/fields/HiddenInput'
-import { Password } from '@payloadcms/ui/fields/Password'
-import { Form } from '@payloadcms/ui/forms/Form'
-import { FormSubmit } from '@payloadcms/ui/forms/Submit'
 import { MinimalTemplate } from '@payloadcms/ui/templates/Minimal'
 import LinkImport from 'next/link.js'
 import React from 'react'
 
+import { ResetPasswordClient } from './index.client.js'
 import './index.scss'
 
 export const resetPasswordBaseClass = 'reset-password'
@@ -22,7 +18,9 @@ export { generateResetPasswordMetadata } from './meta.js'
 export const ResetPassword: React.FC<AdminViewProps> = ({ initPageResult, params }) => {
   const { req } = initPageResult
 
-  const { token } = params
+  const {
+    segments: [_, token],
+  } = params
 
   const {
     i18n,
@@ -31,20 +29,8 @@ export const ResetPassword: React.FC<AdminViewProps> = ({ initPageResult, params
   } = req
 
   const {
-    admin: { user: userSlug },
-    routes: { admin, api },
-    serverURL,
+    routes: { admin },
   } = config
-
-  // const onSuccess = async (data) => {
-  //   if (data.token) {
-  //     await fetchFullUser()
-  //     history.push(`${admin}`)
-  //   } else {
-  //     history.push(`${admin}/login`)
-  //     toast.success(i18n.t('general:updatedSuccessfully'), { autoClose: 3000 })
-  //   }
-  // }
 
   if (user) {
     return (
@@ -73,22 +59,7 @@ export const ResetPassword: React.FC<AdminViewProps> = ({ initPageResult, params
     <MinimalTemplate className={resetPasswordBaseClass}>
       <div className={`${resetPasswordBaseClass}__wrap`}>
         <h1>{i18n.t('authentication:resetPassword')}</h1>
-        <Form
-          action={`${serverURL}${api}/${userSlug}/reset-password`}
-          method="POST"
-          // onSuccess={onSuccess}
-          redirect={admin}
-        >
-          <Password
-            autoComplete="off"
-            label={i18n.t('authentication:newPassword')}
-            name="password"
-            required
-          />
-          <ConfirmPassword />
-          <HiddenInput forceUsePathFromProps name="token" value={token} />
-          <FormSubmit>{i18n.t('authentication:resetPassword')}</FormSubmit>
-        </Form>
+        <ResetPasswordClient token={token} />
       </div>
     </MinimalTemplate>
   )
