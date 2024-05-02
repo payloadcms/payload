6d642fe9b9b8770bf2f415206fc9b9c110e193eb
Yiannis Demetriades <yiannis.demetriades@gmail.com>
2024-05-02 18:30:58 +0300

    fix(templates): adds back missing CSS import in blank 3.0 template (#6183)

--- templates/blank-3.0/src/app/(payload)/layout.tsx
+++ templates/blank-3.0/src/app/(payload)/layout.tsx
@@ -1,5 +1,6 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 import configPromise from '@payload-config'
+import '@payloadcms/next/css'
 import { RootLayout } from '@payloadcms/next/layouts'
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import React from 'react'
