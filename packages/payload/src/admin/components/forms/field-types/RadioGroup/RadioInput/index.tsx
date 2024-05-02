7d676a0a6486515fcfc0086b9c2a9558a2650b56
Patrik Kozak <patrik@payloadcms.com>
2024-05-02 10:14:39 -0400

    fix: appends editDepth value to radio & checkbox fields when inside drawer

--- packages/payload/src/admin/components/forms/field-types/RadioGroup/RadioInput/index.tsx
+++ packages/payload/src/admin/components/forms/field-types/RadioGroup/RadioInput/index.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import type { Props } from './types'
 
 import { getTranslation } from '../../../../../../utilities/getTranslation'
+import { useEditDepth } from '../../../../utilities/EditDepth'
 import './index.scss'
 
 const baseClass = 'radio-input'
@@ -12,9 +13,12 @@ const RadioInput: React.FC<Props> = (props) => {
   const { isSelected, onChange, option, path } = props
   const { i18n } = useTranslation()
 
+  const editDepth = useEditDepth()
+
   const classes = [baseClass, isSelected && `${baseClass}--is-selected`].filter(Boolean).join(' ')
 
-  const id = `field-${path}-${option.value}`
+  const id =
+    editDepth > 1 ? `field-${path}-${option.value}-${editDepth}` : `field-${path}-${option.value}`
 
   return (
     <label htmlFor={id}>
