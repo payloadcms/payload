070d8e1731ffac691e691e1cc810f8e0adae2b8b
Jacob Fletcher <jacobsfletch@gmail.com>
2024-05-02 13:50:32 -0400

    feat(next): supports custom login redirects in initPage

--- packages/next/src/utilities/initPage/handleAuthRedirect.ts
+++ packages/next/src/utilities/initPage/handleAuthRedirect.ts
@@ -1,10 +1,11 @@
 import { redirect } from 'next/navigation.js'
 import QueryString from 'qs'
 
-import { isAdminAuthRoute } from './shared.js'
+import { isAdminAuthRoute, isAdminRoute } from './shared.js'
 
 export const handleAuthRedirect = ({
   adminRoute,
+  redirectUnauthenticatedUser,
   route,
   searchParams,
 }: {
@@ -16,10 +17,31 @@ export const handleAuthRedirect = ({
   if (!isAdminAuthRoute(route, adminRoute)) {
     if (searchParams && 'redirect' in searchParams) delete searchParams.redirect
 
-    const stringifiedSearchParams = Object.keys(searchParams ?? {}).length
-      ? `?${QueryString.stringify(searchParams)}`
-      : ''
+    const redirectRoute = encodeURIComponent(
+      route + Object.keys(searchParams ?? {}).length
+        ? `${QueryString.stringify(searchParams, { addQueryPrefix: true })}`
+        : undefined,
+    )
 
-    redirect(`${adminRoute}/login?redirect=${route + stringifiedSearchParams}`)
+    const adminLoginRoute = `${adminRoute}/login`
+
+    const customLoginRoute =
+      typeof redirectUnauthenticatedUser === 'string' ? redirectUnauthenticatedUser : undefined
+
+    const loginRoute = isAdminRoute(route, adminRoute)
+      ? adminLoginRoute
+      : customLoginRoute || '/login'
+
+    const parsedLoginRouteSearchParams = QueryString.parse(loginRoute.split('?')[1] ?? '')
+
+    const searchParamsWithRedirect = `${QueryString.stringify(
+      {
+        ...parsedLoginRouteSearchParams,
+        ...(redirectRoute ? { redirect: redirectRoute } : {}),
+      },
+      { addQueryPrefix: true },
+    )}`
+
+    redirect(`${loginRoute.split('?')[0]}${searchParamsWithRedirect}`)
   }
 }
