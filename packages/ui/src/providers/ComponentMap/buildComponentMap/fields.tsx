e16cdb1c7cc68aed8b549b82e05de0c886e323f6
r1tsuu <enjoythepain1337@gmail.com>
2024-05-04 04:42:32 +0300

    chore(ui): pass localized to baseFieldProps

--- packages/ui/src/providers/ComponentMap/buildComponentMap/fields.tsx
+++ packages/ui/src/providers/ComponentMap/buildComponentMap/fields.tsx
@@ -205,6 +205,7 @@ export const mapFields = (args: {
           disabled: 'admin' in field && 'disabled' in field.admin ? field.admin?.disabled : false,
           errorProps,
           labelProps,
+          localized: 'localized' in field ? field.localized : undefined,
           path,
           required: 'required' in field ? field.required : undefined,
         }
@@ -595,8 +596,6 @@ export const mapFields = (args: {
                 <WithServerSideProps Component={RichTextCellComponent} />
               )
             }
-
-            fieldComponentProps = richTextField
 
             break
           }
