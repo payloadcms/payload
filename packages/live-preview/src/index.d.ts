a8525ff5984d5139e71c957f972408a6cfa0a63c
Jacob Fletcher <jacobsfletch@gmail.com>
2024-05-06 12:43:15 -0400

    removes built ts files

--- packages/live-preview/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { handleMessage } from './handleMessage.js'
-export { mergeData } from './mergeData.js'
-export { ready } from './ready.js'
-export { subscribe } from './subscribe.js'
-export { unsubscribe } from './unsubscribe.js'
-//# sourceMappingURL=index.d.ts.map
