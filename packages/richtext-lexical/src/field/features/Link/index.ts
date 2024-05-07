77ee87898232fd6e0e1d2f4fac23dbd2cf8f39f0
Alessio Gravili <alessio@gravili.de>
2024-05-07 09:19:56 -0400

    feat(richtext-lexical): add maxDepth property to various lexical features

--- packages/richtext-lexical/src/field/features/Link/index.ts
+++ packages/richtext-lexical/src/field/features/Link/index.ts
@@ -52,6 +52,11 @@ export type LinkFeatureProps = ExclusiveLinkCollectionsProps & {
         i18n: i18n
       }) => FieldWithRichTextRequiredEditor[])
     | FieldWithRichTextRequiredEditor[]
+  /**
+   * Sets a maximum population depth for the internal doc default field of link, regardless of the remaining depth when the field is reached.
+   * This behaves exactly like the maxDepth properties of relationship and upload fields.
+   */
+  maxDepth?: number
 }
 
 export const LinkFeature = (props: LinkFeatureProps): FeatureProvider => {
