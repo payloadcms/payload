7e4f50a01c6e0181bcdaa560652c945243cbd9c1
Jacob Fletcher <jacobsfletch@gmail.com>
2024-05-13 16:27:53 -0400

    feat(ui): exports png favicons

--- packages/ui/src/assets/index.ts
+++ packages/ui/src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as payloadFavicon } from '../assets/favicon.svg'
+export { default as payloadFaviconDark } from '../assets/favicon-dark.png'
+export { default as payloadFaviconLight } from '../assets/favicon-light.png'
 export { default as payloadOgImage } from '../assets/og-image.png'
