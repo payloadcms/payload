4bd3bb9400fac5ef6f4a996f18bb5248810b488c
Jessica Chowdhury <67977755+JessChowdhury@users.noreply.github.com>
2024-05-13 12:31:36 +0100

    chore: adds translations for checkbox field in collection view (#6167)

--- packages/payload/src/admin/components/views/collections/List/Cell/field-types/Checkbox/index.tsx
+++ packages/payload/src/admin/components/views/collections/List/Cell/field-types/Checkbox/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import type { CheckboxField } from '../../../../../../../../exports/types'
 import type { CellComponentProps } from '../../types'
@@ -6,9 +7,13 @@ import type { CellComponentProps } from '../../types'
 import './index.scss'
 
 // Handles boolean values
-const Checkbox: React.FC<CellComponentProps<CheckboxField>> = ({ data }) => (
-  <code className="bool-cell">
-    <span>{JSON.stringify(data)}</span>
-  </code>
-)
+const Checkbox: React.FC<CellComponentProps<CheckboxField>> = ({ data }) => {
+  const { t } = useTranslation('general')
+  if (typeof data !== 'boolean') return null
+  return (
+    <code className="bool-cell">
+      <span>{t(`${data}`).toLowerCase()}</span>
+    </code>
+  )
+}
 export default Checkbox
