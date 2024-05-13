12cfc4cc6f8595eb89a72946f1bf6c7c2decbb0b
Paul Popus <paul@nouance.io>
2024-05-13 11:24:52 -0300

    chore: add test

--- packages/payload/src/utilities/configToJSONSchema.spec.ts
+++ packages/payload/src/utilities/configToJSONSchema.spec.ts
@@ -5,6 +5,7 @@ import { configToJSONSchema } from './configToJSONSchema.js'
 
 describe('configToJSONSchema', () => {
   it('should handle optional arrays with required fields', async () => {
+    // @ts-expect-error
     const config: Config = {
       collections: [
         {
@@ -54,6 +55,93 @@ describe('configToJSONSchema', () => {
         },
       },
       required: ['id'],
+      title: 'Test',
+      type: 'object',
+    })
+  })
+
+  it('should handle tabs and named tabs with required fields', async () => {
+    // @ts-expect-error
+    const config: Config = {
+      collections: [
+        {
+          fields: [
+            {
+              type: 'tabs',
+              tabs: [
+                {
+                  label: 'unnamedTab',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'fieldInUnnamedTab',
+                    },
+                  ],
+                },
+                {
+                  label: 'namedTab',
+                  name: 'namedTab',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'fieldInNamedTab',
+                    },
+                  ],
+                },
+                {
+                  label: 'namedTabWithRequired',
+                  name: 'namedTabWithRequired',
+                  fields: [
+                    {
+                      type: 'text',
+                      name: 'fieldInNamedTab',
+                      required: true,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+          slug: 'test',
+          timestamps: false,
+        },
+      ],
+    }
+
+    const sanitizedConfig = await sanitizeConfig(config)
+    const schema = configToJSONSchema(sanitizedConfig, 'text')
+
+    expect(schema?.definitions?.test).toStrictEqual({
+      additionalProperties: false,
+      properties: {
+        id: {
+          type: 'string',
+        },
+        fieldInUnnamedTab: {
+          type: ['string', 'null'],
+        },
+        namedTab: {
+          additionalProperties: false,
+          type: 'object',
+          properties: {
+            fieldInNamedTab: {
+              type: ['string', 'null'],
+            },
+          },
+          required: [],
+        },
+        namedTabWithRequired: {
+          additionalProperties: false,
+          type: 'object',
+          properties: {
+            fieldInNamedTab: {
+              type: 'string',
+            },
+          },
+          required: ['fieldInNamedTab'],
+        },
+      },
+      required: ['id', 'namedTabWithRequired'],
       title: 'Test',
       type: 'object',
     })
