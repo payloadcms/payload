ad38011348633ac4d93123ceffb76d26e14c48c8
Jessica Chowdhury <67977755+JessChowdhury@users.noreply.github.com>
2024-05-14 16:09:17 +0100

    chore: fix api preview indentation (#6152)

--- packages/payload/src/admin/components/views/API/index.tsx
+++ packages/payload/src/admin/components/views/API/index.tsx
@@ -64,9 +64,9 @@ const RecursivelyRenderObjectData = ({
   const objectKeys = Object.keys(object)
   const objectLength = objectKeys.length
   const [isOpen, setIsOpen] = React.useState<boolean>(true)
-
+  const isNestedAndEmpty = isEmpty && (parentType === 'object' || parentType === 'array')
   return (
-    <li>
+    <li className={isNestedAndEmpty ? `${baseClass}__row-line--nested` : ''}>
       <button
         aria-label="toggle"
         className={`${baseClass}__list-toggle ${isEmpty ? `${baseClass}__list-toggle--empty` : ''}`}
