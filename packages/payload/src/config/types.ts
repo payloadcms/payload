952704ccacf5307959287a485cf8b4ce9d9e2cfc
Alessio Gravili <alessio@gravili.de>
2024-05-14 11:39:12 -0400

    fix: clean-up generics and fix req.user type

--- packages/payload/src/config/types.ts
+++ packages/payload/src/config/types.ts
@@ -2,7 +2,7 @@ import type { DefaultTranslationsObject, I18nOptions, TFunction } from '@payload
 import type { Options as ExpressFileUploadOptions } from 'express-fileupload'
 import type GraphQL from 'graphql'
 import type { Metadata as NextMetadata } from 'next'
-import type { DestinationStream, LoggerOptions, P } from 'pino'
+import type { DestinationStream, LoggerOptions } from 'pino'
 import type React from 'react'
 import type { default as sharp } from 'sharp'
 import type { DeepRequired } from 'ts-essentials'
@@ -10,7 +10,6 @@ import type { DeepRequired } from 'ts-essentials'
 import type { RichTextAdapterProvider } from '../admin/RichText.js'
 import type { DocumentTab, RichTextAdapter } from '../admin/types.js'
 import type { AdminView, ServerSideEditViewProps } from '../admin/views/types.js'
-import type { User } from '../auth/types.js'
 import type {
   AfterErrorHook,
   Collection,
@@ -162,19 +161,19 @@ export type InitOptions = {
  */
 export type AccessResult = Where | boolean
 
-export type AccessArgs<T = any, U = any> = {
+export type AccessArgs<TData = any> = {
   /**
    * The relevant resource that is being accessed.
    *
    * `data` is null when a list is requested
    */
-  data?: T
+  data?: TData
   /** ID of the resource being accessed */
   id?: number | string
   /** If true, the request is for a static file */
   isReadingStaticFile?: boolean
   /** The original request that requires an access check */
-  req: PayloadRequestWithData<U>
+  req: PayloadRequestWithData
 }
 
 /**
@@ -183,17 +182,15 @@ export type AccessArgs<T = any, U = any> = {
  *
  * @see https://payloadcms.com/docs/access-control/overview
  */
-export type Access<T = any, U = any> = (
-  args: AccessArgs<T, U>,
-) => AccessResult | Promise<AccessResult>
+export type Access<TData = any> = (args: AccessArgs<TData>) => AccessResult | Promise<AccessResult>
 
-/** Web Request/Response model, but the the req has more payload specific properties added to it. */
+/** Web Request/Response model, but the req has more payload specific properties added to it. */
 export type PayloadHandler = (req: PayloadRequest) => Promise<Response> | Response
 
 /**
  * Docs: https://payloadcms.com/docs/rest-api/overview#custom-endpoints
  */
-export type Endpoint<U = User> = {
+export type Endpoint = {
   /** Extension point to add your custom data. */
   custom?: Record<string, any>
 
@@ -256,7 +253,9 @@ export type ServerProps = {
 
 export const serverProps: (keyof ServerProps)[] = ['payload']
 
-export type CustomComponent<T extends any = any> = React.ComponentType<T & ServerProps>
+export type CustomComponent<TAdditionalProps extends any = any> = React.ComponentType<
+  TAdditionalProps & ServerProps
+>
 
 export type Locale = {
   /**
@@ -461,7 +460,7 @@ export type Config = {
     }
     /** The route for the logout page. */
     logoutRoute?: string
-    /** Base meta data to use for the Admin Panel. Included properties are titleSuffix, ogImage, and favicon. */
+    /** Base metadata to use for the Admin Panel. Included properties are titleSuffix, ogImage, and favicon. */
     meta?: {
       /**
        * An array of Next.js metadata objects that represent icons to be used by devices and browsers.
@@ -482,7 +481,7 @@ export type Config = {
        */
       titleSuffix?: string
     }
-    /** The slug of a Collection that you want be used to log in to the Admin dashboard. */
+    /** The slug of a Collection that you want to be used to log in to the Admin dashboard. */
     user?: string
   }
   /** Custom Payload bin scripts can be injected via the config. */
