e8d1d369cf8641c35f7cb67a5fa3908162426965
Patrik <patrik@payloadcms.com>
2024-05-15 15:10:51 -0400

    fix(db-postgres): filter with ID not_in AND queries  (#6359)
    ## Description
    
    v2 PR [here](https://github.com/payloadcms/payload/pull/6358)
    
    - [x] I have read and understand the
    [CONTRIBUTING.md](https://github.com/payloadcms/payload/blob/main/CONTRIBUTING.md)
    document in this repository.
    
    ## Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist:
    
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] Existing test suite passes locally with my changes

--- packages/db-postgres/src/queries/parseParams.ts
+++ packages/db-postgres/src/queries/parseParams.ts
@@ -195,10 +195,10 @@ export async function parseParams({
                   operator === 'not_in'
                 ) {
                   constraints.push(
-                    sql`${notInArray(table[columnName], queryValue)} OR
+                    sql`(${notInArray(table[columnName], queryValue)} OR
                     ${table[columnName]}
                     IS
-                    NULL`,
+                    NULL)`,
                   )
 
                   break
