d053778bf213f8e0c5d4adc2e3724d1232587c15
Paul <paul@payloadcms.com>
2024-05-15 21:22:54 -0300

    chore: update form builder example (#6376)
    Updates the form builder example

--- examples/form-builder/payload/.eslintrc.cjs
+++ examples/form-builder/payload/.eslintrc.cjs
@@ -1,4 +1,21 @@
 module.exports = {
+  extends: ['plugin:@next/next/core-web-vitals', '@payloadcms'],
+  ignorePatterns: ['**/payload-types.ts'],
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['*.js', '*.cjs', '*.json', '*.md', '*.yml', '*.yaml'],
+    },
+    {
+      files: ['./src/**/*.ts', './src/**/*.tsx'],
+      rules: {
+        'no-restricted-exports': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
   root: true,
-  extends: ['@payloadcms'],
 }
