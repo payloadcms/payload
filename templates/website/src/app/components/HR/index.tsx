b7591c03724151bb133ab3c67fb1787ec3d05d83
Paul Popus <paul@nouance.io>
2024-05-21 20:40:54 -0300

    remove some dead components

--- templates/website/src/app/components/HR/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const HR: React.FC<{
-  className?: string
-}> = (props) => {
-  const { className } = props
-
-  return <hr className={[className, ''].filter(Boolean).join(' ')} />
-}
--- templates/website/src/app/components/PageRange/index.tsx
+++ templates/website/src/app/components/PageRange/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import classes from './index.module.scss'
-
 const defaultLabels = {
   plural: 'Docs',
   singular: 'Doc',
@@ -44,7 +42,7 @@ export const PageRange: React.FC<{
     collectionLabelsFromProps || defaultCollectionLabels[collection || ''] || defaultLabels || {}
 
   return (
-    <div className={[className, classes.pageRange].filter(Boolean).join(' ')}>
+    <div className={[className, 'font-semibold'].filter(Boolean).join(' ')}>
       {(typeof totalDocs === 'undefined' || totalDocs === 0) && 'Search produced no results.'}
       {typeof totalDocs !== 'undefined' &&
         totalDocs > 0 &&
