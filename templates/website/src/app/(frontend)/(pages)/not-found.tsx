d6a40bcf0b0b1a4602e61a13eab2a91430bcd84a
Paul Popus <paul@nouance.io>
2024-05-23 10:55:54 -0300

    style 404

--- templates/website/src/app/(frontend)/(pages)/not-found.tsx
+++ templates/website/src/app/(frontend)/(pages)/not-found.tsx
@@ -5,9 +5,11 @@ import { Button } from '../../components/ui/button'
 
 export default function NotFound() {
   return (
-    <div className="py-28">
-      <h1 style={{ marginBottom: 0 }}>404</h1>
-      <p>This page could not be found.</p>
+    <div className="container py-28">
+      <div className="prose max-w-none">
+        <h1 style={{ marginBottom: 0 }}>404</h1>
+        <p className="mb-4">This page could not be found.</p>
+      </div>
       <Button asChild variant="default">
         <Link href="/">Go home</Link>
       </Button>
