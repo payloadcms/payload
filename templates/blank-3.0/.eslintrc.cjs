365660764d540df7f811c3da2cc08630970acb74
Elliot DeNolf <denolfe@users.noreply.github.com>
2024-05-24 11:36:50 -0400

    chore(templates): enable next lint on blank (#6494)
    Enables next linting on blank template
    
    Closes #6481

--- templates/blank-3.0/.eslintrc.cjs
+++ templates/blank-3.0/.eslintrc.cjs
@@ -1,5 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
+  extends: ['next/core-web-vitals'],
   parserOptions: {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
