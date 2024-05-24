b4092f59ae53ca12914d4472b8874fabc38a8aa0
Paul <paul@payloadcms.com>
2024-05-24 11:10:29 -0300

    chore: fix seed data validation in website template (#6491)
    Fixes an issue with data validation in lexical for the seed script

--- templates/website/src/payload.config.ts
+++ templates/website/src/payload.config.ts
@@ -59,6 +59,25 @@ export default buildConfig({
         ItalicFeature(),
         LinkFeature({
           enabledCollections: ['pages', 'posts'],
+          fields: ({ defaultFields }) => {
+            const defaultFieldsWithoutUrl = defaultFields.filter((field) => {
+              if ('name' in field && field.name === 'url') return false
+              return true
+            })
+
+            return [
+              ...defaultFieldsWithoutUrl,
+              {
+                name: 'url',
+                type: 'text',
+                admin: {
+                  condition: ({ linkType }) => linkType !== 'internal',
+                },
+                label: ({ t }) => t('fields:enterURL'),
+                required: true,
+              },
+            ]
+          },
         }),
       ]
     },
