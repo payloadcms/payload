c70a131a7b0eeb373b3cce19e3198aee56fb38be
Alessio Gravili <alessio@gravili.de>
2024-05-30 11:04:32 -0400

    fixes

--- packages/richtext-lexical/src/exports/components.ts
+++ packages/richtext-lexical/src/exports/components.ts
@@ -9,9 +9,5 @@ export { ToolbarButton } from '../field/features/toolbars/shared/ToolbarButton/i
 export { ToolbarDropdown } from '../field/features/toolbars/shared/ToolbarDropdown/index.js'
 
 export { RichTextField } from '../field/index.js'
-export {
-  type EditorFocusContextType,
-  EditorFocusProvider,
-  useEditorFocus,
-} from '../field/lexical/EditorFocusProvider.js'
+
 export { defaultEditorLexicalConfig } from '../field/lexical/config/client/default.js'
