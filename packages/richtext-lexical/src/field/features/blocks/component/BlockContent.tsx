c13f9bae72732434bfa9f47272bc604159b362d2
Alessio Gravili <alessio@gravili.de>
2024-06-02 14:43:57 -0400

    chore: remove console.log's

--- packages/richtext-lexical/src/field/features/blocks/component/BlockContent.tsx
+++ packages/richtext-lexical/src/field/features/blocks/component/BlockContent.tsx
@@ -129,8 +129,6 @@ export const BlockContent: React.FC<Props> = (props) => {
       // Only update if the data has actually changed. Otherwise, we may be triggering an unnecessary value change,
       // which would trigger the "Leave without saving" dialog unnecessarily
       if (!isDeepEqual(formData, newFormData)) {
-        console.log('old', formData)
-        console.log('new', newFormData)
         // Running this in the next tick in the meantime fixes this issue: https://github.com/payloadcms/payload/issues/4108
         // I don't know why. When this is called immediately, it might focus out of a nested lexical editor field if an update is made there.
         // My hypothesis is that the nested editor might not have fully finished its update cycle yet. By updating in the next tick, we
