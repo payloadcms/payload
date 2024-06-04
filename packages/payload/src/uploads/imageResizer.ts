bcd277eaaacb19d517301820676b88c5bc158d00
Patrik <patrik@payloadcms.com>
2024-06-04 13:57:46 -0400

    fix: resizing animated images (#6623)
    Fixes resizing of animated images
    
    V2 PR [here](https://github.com/payloadcms/payload/pull/6621)

--- packages/payload/src/uploads/imageResizer.ts
+++ packages/payload/src/uploads/imageResizer.ts
@@ -1,4 +1,4 @@
-import type { OutputInfo } from 'sharp'
+import type { OutputInfo, Sharp, SharpOptions } from 'sharp'
 
 import fileType from 'file-type'
 const { fromBuffer } = fileType
@@ -219,7 +219,7 @@ const sanitizeResizeConfig = (resizeConfig: ImageSize): ImageSize => {
  *
  * The image will be resized according to the provided
  * resize config. If no image sizes are requested, the resolved data will be empty.
- * For every image that dos not need to be resized, an result object with `null`
+ * For every image that does not need to be resized, a result object with `null`
  * parameters will be returned.
  *
  * @param resizeConfig - the resize config
@@ -256,7 +256,13 @@ export async function resizeAndTransformImageSizes({
     return defaultResult
   }
 
-  const sharpBase = sharp(file.tempFilePath || file.data).rotate() // pass rotate() to auto-rotate based on EXIF data. https://github.com/payloadcms/payload/pull/3081
+  // Determine if the file is animated
+  const fileIsAnimated = ['image/avif', 'image/gif', 'image/webp'].includes(file.mimetype)
+  const sharpOptions: SharpOptions = {}
+
+  if (fileIsAnimated) sharpOptions.animated = true
+
+  const sharpBase: Sharp | undefined = sharp(file.tempFilePath || file.data, sharpOptions).rotate() // pass rotate() to auto-rotate based on EXIF data. https://github.com/payloadcms/payload/pull/3081
 
   const results: ImageSizesResult[] = await Promise.all(
     imageSizes.map(async (imageResizeConfig): Promise<ImageSizesResult> => {
@@ -271,6 +277,8 @@ export async function resizeAndTransformImageSizes({
 
       const imageToResize = sharpBase.clone()
       let resized = imageToResize
+
+      const metadata = await sharpBase.metadata()
 
       if (incomingFocalPoint && applyPayloadAdjustments(imageResizeConfig, dimensions)) {
         const { height: resizeHeight, width: resizeWidth } = imageResizeConfig
@@ -293,14 +301,18 @@ export async function resizeAndTransformImageSizes({
         const safeOffsetX = Math.min(Math.max(0, leftFocalEdge), maxOffsetX)
 
         const safeResizeHeight = resizeHeight ?? scaledImageInfo.height
-        const maxOffsetY = scaledImageInfo.height - safeResizeHeight
+
+        const maxOffsetY = fileIsAnimated
+          ? resizeHeight - safeResizeHeight
+          : scaledImageInfo.height - safeResizeHeight
+
         const topFocalEdge = Math.round(
           scaledImageInfo.height * (incomingFocalPoint.y / 100) - safeResizeHeight / 2,
         )
         const safeOffsetY = Math.min(Math.max(0, topFocalEdge), maxOffsetY)
 
         // extract the focal area from the scaled image
-        resized = scaledImage.extract({
+        resized = (fileIsAnimated ? imageToResize : scaledImage).extract({
           height: safeResizeHeight,
           left: safeOffsetX,
           top: safeOffsetY,
@@ -354,7 +366,7 @@ export async function resizeAndTransformImageSizes({
         name: imageResizeConfig.name,
         filename: imageNameWithDimensions,
         filesize: size,
-        height,
+        height: fileIsAnimated ? height / metadata.pages : height,
         mimeType: mimeInfo?.mime || mimeType,
         sizesToSave: [{ buffer: bufferData, path: imagePath }],
         width,
