52c81ad52597d7ab8717c4c9cc091f16acf04d22
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2024-06-07 15:22:03 -0400

    feat: adds draft validation option (#6677)
    ## Description
    
    Allows draft validation to be enabled at the config level.
    
    You can enable this by:
    ```ts
    // ...collectionConfig
    versions: {
      drafts: {
        validate: true // defaults to false
      }
    }
    ```

---
+++ test/field-error-states/collections/ValidateDraftsOn/index.ts
@@ -0,0 +1,20 @@
+import type { CollectionConfig } from 'payload/types'
+
+import { slugs } from '../../shared.js'
+
+export const ValidateDraftsOn: CollectionConfig = {
+  slug: slugs.validateDraftsOn,
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+  ],
+  versions: {
+    drafts: {
+      autosave: true,
+      validate: true,
+    },
+  },
+}
--- test/field-error-states/config.ts
+++ test/field-error-states/config.ts
@@ -2,9 +2,18 @@ import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
 import { devUser } from '../credentials.js'
 import { ErrorFieldsCollection } from './collections/ErrorFields/index.js'
 import Uploads from './collections/Upload/index.js'
+import { ValidateDraftsOff } from './collections/ValidateDraftsOff/index.js'
+import { ValidateDraftsOn } from './collections/ValidateDraftsOn/index.js'
+import { ValidateDraftsOnAndAutosave } from './collections/ValidateDraftsOnAutosave/index.js'
 
 export default buildConfigWithDefaults({
-  collections: [ErrorFieldsCollection, Uploads],
+  collections: [
+    ErrorFieldsCollection,
+    Uploads,
+    ValidateDraftsOn,
+    ValidateDraftsOff,
+    ValidateDraftsOnAndAutosave,
+  ],
   onInit: async (payload) => {
     await payload.create({
       collection: 'users',
