65a853a89605c7f250f1361f9e7d0af46da25209
Paul Popus <paul@nouance.io>
2024-06-07 10:23:38 -0400

    feat(plugin-redirects)!: update fields overrides to use a function instead

--- packages/plugin-redirects/src/index.ts
+++ packages/plugin-redirects/src/index.ts
@@ -1,80 +1,85 @@
 import type { Config } from 'payload/config'
+import type { CollectionConfig, Field } from 'payload/types'
 
 import type { RedirectsPluginConfig } from './types.js'
 
-import deepMerge from './deepMerge.js'
-
 export const redirectsPlugin =
   (pluginConfig: RedirectsPluginConfig) =>
-  (incomingConfig: Config): Config => ({
-    ...incomingConfig,
-    collections: [
-      ...(incomingConfig?.collections || []),
-      deepMerge(
-        {
-          slug: 'redirects',
-          access: {
-            read: (): boolean => true,
-          },
-          admin: {
-            defaultColumns: ['from', 'to.type', 'createdAt'],
-          },
-          fields: [
-            {
-              name: 'from',
-              type: 'text',
-              index: true,
-              label: 'From URL',
-              required: true,
+  (incomingConfig: Config): Config => {
+    const defaultFields: Field[] = [
+      {
+        name: 'from',
+        type: 'text',
+        index: true,
+        label: 'From URL',
+        required: true,
+      },
+      {
+        name: 'to',
+        type: 'group',
+        fields: [
+          {
+            name: 'type',
+            type: 'radio',
+            admin: {
+              layout: 'horizontal',
             },
-            {
-              name: 'to',
-              type: 'group',
-              fields: [
-                {
-                  name: 'type',
-                  type: 'radio',
-                  admin: {
-                    layout: 'horizontal',
-                  },
-                  defaultValue: 'reference',
-                  label: 'To URL Type',
-                  options: [
-                    {
-                      label: 'Internal link',
-                      value: 'reference',
-                    },
-                    {
-                      label: 'Custom URL',
-                      value: 'custom',
-                    },
-                  ],
-                },
-                {
-                  name: 'reference',
-                  type: 'relationship',
-                  admin: {
-                    condition: (_, siblingData) => siblingData?.type === 'reference',
-                  },
-                  label: 'Document to redirect to',
-                  relationTo: pluginConfig?.collections || [],
-                  required: true,
-                },
-                {
-                  name: 'url',
-                  type: 'text',
-                  admin: {
-                    condition: (_, siblingData) => siblingData?.type === 'custom',
-                  },
-                  label: 'Custom URL',
-                  required: true,
-                },
-              ],
-              label: false,
+            defaultValue: 'reference',
+            label: 'To URL Type',
+            options: [
+              {
+                label: 'Internal link',
+                value: 'reference',
+              },
+              {
+                label: 'Custom URL',
+                value: 'custom',
+              },
+            ],
+          },
+          {
+            name: 'reference',
+            type: 'relationship',
+            admin: {
+              condition: (_, siblingData) => siblingData?.type === 'reference',
             },
-          ],
-        },
-        pluginConfig?.overrides || {},
-      ),
-    ],
-  })
+            label: 'Document to redirect to',
+            relationTo: pluginConfig?.collections || [],
+            required: true,
+          },
+          {
+            name: 'url',
+            type: 'text',
+            admin: {
+              condition: (_, siblingData) => siblingData?.type === 'custom',
+            },
+            label: 'Custom URL',
+            required: true,
+          },
+        ],
+        label: false,
+      },
+    ]
+
+    const redirectsCollection: CollectionConfig = {
+      ...(pluginConfig?.overrides || {}),
+      slug: pluginConfig?.overrides?.slug || 'redirects',
+      access: {
+        read: () => true,
+        ...(pluginConfig?.overrides?.access || {}),
+      },
+      admin: {
+        defaultColumns: ['from', 'to.type', 'createdAt'],
+        ...(pluginConfig?.overrides?.admin || {}),
+      },
+      fields:
+        pluginConfig?.overrides?.fields && typeof pluginConfig?.overrides?.fields === 'function'
+          ? pluginConfig?.overrides.fields({ defaultFields })
+          : defaultFields,
+    }
+
+    return {
+      ...incomingConfig,
+      collections: [...(incomingConfig?.collections || []), redirectsCollection],
+    }
+  }
