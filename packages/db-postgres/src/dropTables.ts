8ce8800327d8782e6dcb85e1b5608f1a96cdce01
Dan Ribbens <dan.ribbens@gmail.com>
2024-06-09 22:06:01 -0400

    chore(db-postgres): add if exists to drop schema

--- packages/db-postgres/src/dropTables.ts
+++ packages/db-postgres/src/dropTables.ts
@@ -3,7 +3,7 @@ import type { DropTables } from './types.js'
 export const dropTables: DropTables = async function dropTables({ adapter }) {
   await adapter.execute({
     drizzle: adapter.drizzle,
-    raw: `drop schema ${this.schemaName || 'public'} cascade;
+    raw: `drop schema if exists ${this.schemaName || 'public'} cascade;
     create schema ${this.schemaName || 'public'};`,
   })
 }
