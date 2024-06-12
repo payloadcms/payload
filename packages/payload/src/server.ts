583e99e0433c5cb9ce76befab11e686e82e11fc5
Alessio Gravili <alessio@gravili.de>
2024-06-12 15:40:05 -0400

    get bundled payload to work

--- packages/payload/src/server.ts
+++ packages/payload/src/server.ts
@@ -2,7 +2,6 @@
  * WARNING: This file contains exports that can only be safely used on the server.
  */
 
-export { generateTypes } from './bin/generateTypes.js'
 export { loadEnv } from './bin/loadEnv.js'
 export { findConfig } from './config/find.js'
 export type {
