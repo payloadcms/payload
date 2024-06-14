payloadcms/payload

commit bc9e591e377ec93423938ee3995d477d6ec5d1ae

chore: removes letter spacing in loading overlay for script languages (#4769)

packages/payload/src/admin/components/utilities/LanguageWrap/index.tsx
@@ -1,0 +1,14 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import '../../../scss/app.scss'
+
+const scriptLanguages = ['ar', 'fa']
+
+export const LanguageWrap: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  const { i18n } = useTranslation()
+  const currentLanguage = i18n?.language
+  const isScriptLanguage = currentLanguage && scriptLanguages.includes(currentLanguage)
+
+  return <div className={isScriptLanguage ? `script-language` : ''}>{children}</div>
+}

packages/payload/src/admin/scss/app.scss
@@ -200,4 +200,11 @@ dialog {
   z-index: var(--z-modal);
 }
 
+.script-language {
+  & > *,
+  & > * > * {
+    letter-spacing: 0 !important;
+  }
+}
+
 @import '~payload-user-css';

packages/payload/src/admin/components/elements/Loading/index.tsx
@@ -24,7 +24,6 @@ export const LoadingOverlay: React.FC<Props> = ({
   show = true,
 }) => {
   const { t } = useTranslation('general')
-
   return (
     <div
       className={[
@@ -59,25 +58,25 @@ type UseLoadingOverlayToggleT = {
 }
 export const LoadingOverlayToggle: React.FC<UseLoadingOverlayToggleT> = ({
   name: key,
+  type = 'fullscreen',
   loadingText,
   show,
-  type = 'fullscreen',
 }) => {
   const { toggleLoadingOverlay } = useLoadingOverlay()
 
   React.useEffect(() => {
     toggleLoadingOverlay({
+      type,
       isLoading: show,
       key,
       loadingText: loadingText || undefined,
-      type,
     })
 
     return () => {
       toggleLoadingOverlay({
+        type,
         isLoading: false,
         key,
-        type,
       })
     }
   }, [show, toggleLoadingOverlay, key, type, loadingText])
@@ -94,10 +93,10 @@ type FormLoadingOverlayToggleT = {
 }
 export const FormLoadingOverlayToggle: React.FC<FormLoadingOverlayToggleT> = ({
   name,
+  type = 'fullscreen',
   action,
   formIsLoading = false,
   loadingSuffix,
-  type = 'fullscreen',
 }) => {
   const isProcessing = useFormProcessing()
   const { i18n, t } = useTranslation('general')

packages/payload/src/admin/Root.tsx
@@ -19,6 +19,7 @@ import { ConfigProvider } from './components/utilities/Config'
 import { CustomProvider } from './components/utilities/CustomProvider'
 import { DocumentEventsProvider } from './components/utilities/DocumentEvents'
 import { I18n } from './components/utilities/I18n'
+import { LanguageWrap } from './components/utilities/LanguageWrap'
 import { LoadingOverlayProvider } from './components/utilities/LoadingOverlay'
 import { LocaleProvider } from './components/utilities/Locale'
 import { PreferencesProvider } from './components/utilities/Preferences'
@@ -46,21 +47,23 @@ const Root = ({ config: incomingConfig }: { config?: SanitizedConfig }) => {
                 <AuthProvider>
                   <PreferencesProvider>
                     <ThemeProvider>
-                      <SearchParamsProvider>
-                        <LocaleProvider>
-                          <StepNavProvider>
-                            <LoadingOverlayProvider>
-                              <DocumentEventsProvider>
-                                <NavProvider>
-                                  <CustomProvider>
-                                    <Routes />
-                                  </CustomProvider>
-                                </NavProvider>
-                              </DocumentEventsProvider>
-                            </LoadingOverlayProvider>
-                          </StepNavProvider>
-                        </LocaleProvider>
-                      </SearchParamsProvider>
+                      <LanguageWrap>
+                        <SearchParamsProvider>
+                          <LocaleProvider>
+                            <StepNavProvider>
+                              <LoadingOverlayProvider>
+                                <DocumentEventsProvider>
+                                  <NavProvider>
+                                    <CustomProvider>
+                                      <Routes />
+                                    </CustomProvider>
+                                  </NavProvider>
+                                </DocumentEventsProvider>
+                              </LoadingOverlayProvider>
+                            </StepNavProvider>
+                          </LocaleProvider>
+                        </SearchParamsProvider>
+                      </LanguageWrap>
                     </ThemeProvider>
                     <ModalContainer />
                   </PreferencesProvider>

commit 4e0725f7c61f16167c3731ce27eceb7608c8fc1b

chore: fixes view height (#6779)
Fixes issue where view height was not 100% by default. Regression from
https://github.com/payloadcms/payload/pull/4769

packages/payload/src/admin/components/utilities/LanguageWrap/index.scss
@@ -1,0 +1,8 @@
+.script-language {
+  height: 100%;
+
+  & > *,
+  & > * > * {
+    letter-spacing: 0 !important;
+  }
+}

packages/payload/src/admin/components/utilities/LanguageWrap/index.tsx
@@ -10,5 +10,9 @@ export const LanguageWrap: React.FC<{ children?: React.ReactNode }> = ({ childre
   const currentLanguage = i18n?.language
   const isScriptLanguage = currentLanguage && scriptLanguages.includes(currentLanguage)
 
-  return <div className={isScriptLanguage ? `script-language` : ''}>{children}</div>
+  if (isScriptLanguage) {
+    return <div className="script-language">{children}</div>
+  }
+
+  return <React.Fragment>{children}</React.Fragment>
 }

packages/payload/src/admin/scss/app.scss
@@ -200,11 +200,4 @@ dialog {
   z-index: var(--z-modal);
 }
 
-.script-language {
-  & > *,
-  & > * > * {
-    letter-spacing: 0 !important;
-  }
-}
-
 @import '~payload-user-css';