e036d4efab3c0e7f2bd794ef5edca5a66a260a9e
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2024-06-21 15:45:52 -0400

    chore: updated 3.0 graphql docs (#6859)

--- packages/graphql/src/exports/types.ts
+++ packages/graphql/src/exports/types.ts
@@ -1,1 +1,3 @@
 export { GraphQLJSON, GraphQLJSONObject } from '../packages/graphql-type-json/index.js'
+export { buildPaginatedListType } from '../schema/buildPaginatedListType.js'
+export { default as GraphQL } from 'graphql'
