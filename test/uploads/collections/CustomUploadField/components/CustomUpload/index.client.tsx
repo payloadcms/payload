65d6e4f984698f5f2a0ff298c4cc88051ae586d6
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-06-25 17:04:19 -0400

    chore: updates custom upload component example

--- test/uploads/collections/CustomUploadField/components/CustomUpload/index.client.tsx
+++ test/uploads/collections/CustomUploadField/components/CustomUpload/index.client.tsx
@@ -8,12 +8,19 @@ export const CustomUploadClient = () => {
 
   return (
     <div>
-      <h3>Upload component provided via the plugin</h3>
+      <h3>This text was rendered on the client</h3>
       <Upload
         collectionSlug={collectionSlug}
         initialState={initialState}
         uploadConfig={'upload' in docConfig ? docConfig.upload : undefined}
       />
+      <h4>
+        And that{' '}
+        <span aria-label="point up" role="img">
+          👆
+        </span>{' '}
+        is re-used from payload components
+      </h4>
     </div>
   )
 }
