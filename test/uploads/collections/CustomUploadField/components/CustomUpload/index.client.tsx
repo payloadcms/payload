305309745d67a1f707df3f63080da3b547f4b72c
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-06-26 12:02:14 -0400

    feat: ability to pass uploadActions to the Upload component

--- test/uploads/collections/CustomUploadField/components/CustomUpload/index.client.tsx
+++ test/uploads/collections/CustomUploadField/components/CustomUpload/index.client.tsx
@@ -1,7 +1,27 @@
 'use client'
 
-import { Upload, useDocumentInfo } from '@payloadcms/ui'
+import { Drawer, DrawerToggler, TextField, Upload, useDocumentInfo } from '@payloadcms/ui'
 import React from 'react'
+
+const customDrawerSlug = 'custom-upload-drawer'
+
+const CustomDrawer = () => {
+  return (
+    <Drawer slug={customDrawerSlug}>
+      <h1>Custom Drawer</h1>
+      <TextField name="alt" path="alt" />
+    </Drawer>
+  )
+}
+
+const CustomDrawerToggler = () => {
+  return (
+    <React.Fragment>
+      <DrawerToggler slug={customDrawerSlug}>Custom Drawer</DrawerToggler>
+      <CustomDrawer />
+    </React.Fragment>
+  )
+}
 
 export const CustomUploadClient = () => {
   const { collectionSlug, docConfig, initialState } = useDocumentInfo()
@@ -11,6 +31,7 @@ export const CustomUploadClient = () => {
       <h3>This text was rendered on the client</h3>
       <Upload
         collectionSlug={collectionSlug}
+        customActions={[<CustomDrawerToggler />]}
         initialState={initialState}
         uploadConfig={'upload' in docConfig ? docConfig.upload : undefined}
       />
