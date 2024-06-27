dcb20dc5cdfa00d72d37a9be0a5637fc37265037
James <james@trbl.design>
2024-06-27 16:48:31 -0400

    feat: adds me and refresh hooks

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -5,6 +5,35 @@ import { mediaSlug } from '../Media'
 export const postsSlug = 'posts'
 
 export const PostsCollection: CollectionConfig = {
+  hooks: {
+    beforeValidate: [
+      async ({ data, req }) => {
+        const postsQuery1 = await req.payload.find({
+          collection: 'posts',
+          where: {
+            text: {
+              equals: 'hello',
+            },
+          },
+        })
+
+        console.log({ postsQuery1 })
+
+        const postsQuery2 = await req.payload.find({
+          collection: 'posts',
+          where: {
+            text: {
+              equals: 'goodbye',
+            },
+          },
+        })
+
+        console.log({ postsQuery2 })
+
+        return data
+      },
+    ],
+  },
   fields: [
     {
       name: 'text',
