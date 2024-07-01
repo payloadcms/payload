d94e4551d0b5dbf6e05b5a68e641dc36a238f592
Paul Popus <paul@nouance.io>
2024-07-01 13:38:22 -0400

    chore: make generate email types generics for the user type

--- packages/payload/src/auth/types.ts
+++ packages/payload/src/auth/types.ts
@@ -74,26 +74,28 @@ export type User = {
  */
 export type ClientUser = Omit<User, 'collection'>
 
-type GenerateVerifyEmailHTML = (args: {
+type GenerateVerifyEmailHTML<TUser = any> = (args: {
   req: PayloadRequestWithData
   token: string
-  user: any
-}) => Promise<string> | string
-type GenerateVerifyEmailSubject = (args: {
-  req: PayloadRequestWithData
-  token: string
-  user: any
+  user: TUser
 }) => Promise<string> | string
 
-type GenerateForgotPasswordEmailHTML = (args?: {
-  req?: PayloadRequestWithData
-  token?: string
-  user?: unknown
+type GenerateVerifyEmailSubject<TUser = any> = (args: {
+  req: PayloadRequestWithData
+  token: string
+  user: TUser
 }) => Promise<string> | string
-type GenerateForgotPasswordEmailSubject = (args?: {
+
+type GenerateForgotPasswordEmailHTML<TUser = any> = (args?: {
   req?: PayloadRequestWithData
   token?: string
-  user?: any
+  user?: TUser
+}) => Promise<string> | string
+
+type GenerateForgotPasswordEmailSubject<TUser = any> = (args?: {
+  req?: PayloadRequestWithData
+  token?: string
+  user?: TUser
 }) => Promise<string> | string
 
 export type AuthStrategyFunctionArgs = {
