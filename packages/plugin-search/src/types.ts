8bae28e886bb8d93df118d21359630bd6703da12
Paul <paul@payloadcms.com>
2024-07-10 11:22:12 -0400

    feat(plugin-search)!: make search collection fields override into a function that provides defaultFields inline with other plugins (#7095)
    searchPlugin's searchOverrides for the collection now takes in a fields
    function instead of array similar to other plugins and patterns we use
    to allow you to map over existing fields as well if needed.
    
    ```ts
    // before
    searchPlugin({
      searchOverrides: {
        slug: 'search-results',
        fields: [
          {
            name: 'excerpt',
            type: 'textarea',
            admin: {
              position: 'sidebar',
            },
          },
        ]
      },
    }),
    
    // current
    searchPlugin({
      searchOverrides: {
        slug: 'search-results',
        fields: ({ defaultFields }) => [
          ...defaultFields,
          {
            name: 'excerpt',
            type: 'textarea',
            admin: {
              position: 'sidebar',
            },
          },
        ]
      },
    }),
    ```

--- packages/plugin-search/src/types.ts
+++ packages/plugin-search/src/types.ts
@@ -1,4 +1,10 @@
-import type { CollectionAfterChangeHook, CollectionConfig, Payload, PayloadRequest } from 'payload'
+import type {
+  CollectionAfterChangeHook,
+  CollectionConfig,
+  Field,
+  Payload,
+  PayloadRequest,
+} from 'payload'
 
 export type DocToSync = {
   [key: string]: any
@@ -18,6 +24,8 @@ export type BeforeSync = (args: {
   searchDoc: DocToSync
 }) => DocToSync | Promise<DocToSync>
 
+export type FieldsOverride = (args: { defaultFields: Field[] }) => Field[]
+
 export type SearchPluginConfig = {
   beforeSync?: BeforeSync
   collections?: string[]
@@ -25,15 +33,15 @@ export type SearchPluginConfig = {
     [collection: string]: ((doc: any) => Promise<number> | number) | number
   }
   deleteDrafts?: boolean
-  searchOverrides?: Partial<CollectionConfig>
+  searchOverrides?: { fields?: FieldsOverride } & Partial<Omit<CollectionConfig, 'fields'>>
   syncDrafts?: boolean
 }
 
 // Extend the `CollectionAfterChangeHook` with more function args
 // Convert the `collection` arg from `SanitizedCollectionConfig` to a string
 export type SyncWithSearch = (
-  Args: Omit<Parameters<CollectionAfterChangeHook>[0], 'collection'> & {
+  Args: {
     collection: string
     pluginConfig: SearchPluginConfig
-  },
+  } & Omit<Parameters<CollectionAfterChangeHook>[0], 'collection'>,
 ) => ReturnType<CollectionAfterChangeHook>
