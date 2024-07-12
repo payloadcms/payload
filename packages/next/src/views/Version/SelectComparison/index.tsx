c01996927193bdf045839c845d9b58624246037b
Jessica Chowdhury <67977755+JessChowdhury@users.noreply.github.com>
2024-07-12 12:48:18 -0400

    feat(ui): updates version status UI to be more informative (#6661)
    ## Description
    
    Improves the status pill in the version archive and version comparison
    views.
    
    - [X] I have read and understand the
    [CONTRIBUTING.md](https://github.com/payloadcms/payload/blob/main/CONTRIBUTING.md)
    document in this repository.
    
    ## Type of change
    
    - [X] New feature (non-breaking change which adds functionality)
    
    ## Checklist:
    
    - [X] Existing test suite passes locally with my changes

--- packages/next/src/views/Version/SelectComparison/index.tsx
+++ packages/next/src/views/Version/SelectComparison/index.tsx
@@ -1,37 +1,44 @@
 'use client'
+
 import type { PaginatedDocs, Where } from 'payload'
 
-import {
-  ReactSelect,
-  fieldBaseClass,
-  useConfig,
-  useDocumentInfo,
-  useTranslation,
-} from '@payloadcms/ui'
+import { ReactSelect, fieldBaseClass, useConfig, useTranslation } from '@payloadcms/ui'
 import { formatDate } from '@payloadcms/ui/shared'
 import * as qs from 'qs-esm'
 import React, { useCallback, useEffect, useState } from 'react'
 
 import type { Props } from './types.js'
 
-import { mostRecentVersionOption, publishedVersionOption } from '../shared.js'
+import { renderPill } from '../../Versions/cells/AutosaveCell/index.js'
 import './index.scss'
 
 const baseClass = 'compare-version'
 
 const maxResultsPerRequest = 10
 
-const baseOptions = [mostRecentVersionOption]
+const baseOptions = []
 
 export const SelectComparison: React.FC<Props> = (props) => {
-  const { baseURL, onChange, parentID, publishedDoc, value, versionID } = props
+  const {
+    baseURL,
+    latestDraftVersion,
+    latestPublishedVersion,
+    onChange,
+    parentID,
+    value,
+    versionID,
+  } = props
 
   const {
     admin: { dateFormat },
   } = useConfig()
 
-  const { docConfig } = useDocumentInfo()
-  const [options, setOptions] = useState(baseOptions)
+  const [options, setOptions] = useState<
+    {
+      label: React.ReactNode | string
+      value: string
+    }[]
+  >(baseOptions)
   const [lastLoadedPage, setLastLoadedPage] = useState(1)
   const [errorLoading, setErrorLoading] = useState('')
   const { i18n, t } = useTranslation()
@@ -58,14 +65,6 @@ export const SelectComparison: React.FC<Props> = (props) => {
         },
       }
 
-      if (docConfig.versions?.drafts) {
-        query.where.and.push({
-          latest: {
-            not_equals: true,
-          },
-        })
-      }
-
       if (parentID) {
         query.where.and.push({
           parent: {
@@ -85,14 +84,41 @@ export const SelectComparison: React.FC<Props> = (props) => {
 
       if (response.ok) {
         const data: PaginatedDocs = await response.json()
+
         if (data.docs.length > 0) {
-          setOptions((existingOptions) => [
-            ...existingOptions,
-            ...data.docs.map((doc) => ({
-              label: formatDate({ date: doc.updatedAt, i18n, pattern: dateFormat }),
+          const versionInfo = {
+            draft: {
+              currentLabel: t('version:currentDraft'),
+              latestVersion: latestDraftVersion,
+              pillStyle: undefined,
+              previousLabel: t('version:draft'),
+            },
+            published: {
+              currentLabel: t('version:currentPublishedVersion'),
+              latestVersion: latestPublishedVersion,
+              pillStyle: 'success',
+              previousLabel: t('version:previouslyPublished'),
+            },
+          }
+
+          const additionalOptions = data.docs.map((doc) => {
+            const status = doc.version._status
+            const { currentLabel, latestVersion, pillStyle, previousLabel } =
+              versionInfo[status] || {}
+
+            return {
+              label: (
+                <div>
+                  {formatDate({ date: doc.updatedAt, i18n, pattern: dateFormat })}
+                  &nbsp;&nbsp;
+                  {renderPill(doc, latestVersion, currentLabel, previousLabel, pillStyle)}
+                </div>
+              ),
               value: doc.id,
-            })),
-          ])
+            }
+          })
+
+          setOptions((existingOptions) => [...existingOptions, ...additionalOptions])
 
           if (!data.hasNextPage) {
             loadedAllOptionsRef.current = true
@@ -103,12 +129,22 @@ export const SelectComparison: React.FC<Props> = (props) => {
         setErrorLoading(t('error:unspecific'))
       }
     },
-    [dateFormat, baseURL, parentID, versionID, t, i18n, docConfig.versions?.drafts],
+    [dateFormat, baseURL, parentID, versionID, t, i18n, latestDraftVersion, latestPublishedVersion],
   )
 
   useEffect(() => {
     void getResults({ lastLoadedPage: 1 })
   }, [getResults])
+
+  const filteredOptions = options.filter(
+    (option, index, self) => self.findIndex((t) => t.value === option.value) === index,
+  )
+
+  useEffect(() => {
+    if (filteredOptions.length > 0 && !value) {
+      onChange(filteredOptions[0])
+    }
+  }, [filteredOptions, value, onChange])
 
   return (
     <div
@@ -125,10 +161,7 @@ export const SelectComparison: React.FC<Props> = (props) => {
           onMenuScrollToBottom={() => {
             void getResults({ lastLoadedPage: lastLoadedPage + 1 })
           }}
-          options={[
-            ...(publishedDoc?._status === 'published' ? [publishedVersionOption] : []),
-            ...options,
-          ]}
+          options={filteredOptions}
           placeholder={t('version:selectVersionToCompare')}
           value={value}
         />
--- packages/next/src/views/Versions/buildColumns.tsx
+++ packages/next/src/views/Versions/buildColumns.tsx
@@ -13,12 +13,16 @@ export const buildVersionColumns = ({
   docID,
   globalConfig,
   i18n: { t },
+  latestDraftVersion,
+  latestPublishedVersion,
 }: {
   collectionConfig?: SanitizedCollectionConfig
   config: SanitizedConfig
   docID?: number | string
   globalConfig?: SanitizedGlobalConfig
   i18n: I18n
+  latestDraftVersion?: string
+  latestPublishedVersion?: string
 }): Column[] => {
   const entityConfig = collectionConfig || globalConfig
 
@@ -64,8 +68,13 @@ export const buildVersionColumns = ({
       accessor: '_status',
       active: true,
       components: {
-        Cell: <AutosaveCell />,
-        Heading: <SortColumn Label={t('version:type')} disable name="autosave" />,
+        Cell: (
+          <AutosaveCell
+            latestDraftVersion={latestDraftVersion}
+            latestPublishedVersion={latestPublishedVersion}
+          />
+        ),
+        Heading: <SortColumn Label={t('version:status')} disable name="status" />,
       },
     })
   }
