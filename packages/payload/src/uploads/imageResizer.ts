2646bc11cd32eaebc5958dc8e19984d7cd9f8179
PatrikKozak <patrik@payloadcms.com>
2024-07-15 13:27:16 -0400

    fix: calculates correct height for resized animated image filenames

--- packages/payload/src/uploads/imageResizer.ts
+++ packages/payload/src/uploads/imageResizer.ts
@@ -1,4 +1,4 @@
-import type { OutputInfo, Sharp, SharpOptions } from 'sharp'
+import type { Metadata, OutputInfo, Sharp, SharpOptions } from 'sharp'
 
 import { fileTypeFromBuffer } from 'file-type'
 import fs from 'fs'
@@ -72,7 +72,12 @@ const createImageName = (
   outputImageName: string,
   { height, width }: OutputInfo,
   extension: string,
-) => `${outputImageName}-${width}x${height}.${extension}`
+  metadata: Metadata,
+) => {
+  // Adjust height if the file is animated
+  const adjustedHeight = metadata.pages ? height / metadata.pages : height
+  return `${outputImageName}-${width}x${adjustedHeight}.${extension}`
+}
 
 type CreateResultArgs = {
   filename?: FileSize['filename']
@@ -363,6 +368,7 @@ export async function resizeAndTransformImageSizes({
         sanitizedImage.name,
         bufferInfo,
         mimeInfo?.ext || sanitizedImage.ext,
+        metadata,
       )
 
       const imagePath = `${staticPath}/${imageNameWithDimensions}`
