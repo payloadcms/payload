846244d24df97f03562e4d9da23fc34988734e02
Alessio Gravili <alessio@gravili.de>
2024-07-15 17:03:33 -0400

    Revert "chore: run esbuild scripts in sync, hopefully fixing publishing issues"
    This reverts commit 82c2c83b9b4f2abf68ec6f61f33c8e88366e05ab.

--- packages/next/bundleScss.js
+++ packages/next/bundleScss.js
@@ -1,52 +1,45 @@
 import * as esbuild from 'esbuild'
 import fs from 'fs'
-
+import path from 'path'
+import { fileURLToPath } from 'url'
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 import { sassPlugin } from 'esbuild-sass-plugin'
 
 // Bundle only the .scss files into a single css file
-esbuild.buildSync({
-  entryPoints: ['src/esbuildEntry.ts'],
-  bundle: true,
-  minify: true,
-  outdir: 'dist/prod',
-  packages: 'external',
-  plugins: [sassPlugin({ css: 'external' })],
-})
+await esbuild
+  .build({
+    entryPoints: ['src/esbuildEntry.ts'],
+    bundle: true,
+    minify: true,
+    outdir: 'dist/prod',
+    packages: 'external',
+    plugins: [sassPlugin({ css: 'external' })],
+  })
+  .then(() => {
+    fs.rename('dist/prod/esbuildEntry.css', 'dist/prod/styles.css', (err) => {
+      if (err) console.error(`Error while renaming index.css: ${err}`)
+    })
 
-try {
-  fs.renameSync('dist/prod/esbuildEntry.css', 'dist/prod/styles.css')
-} catch (err) {
-  console.error(`Error while renaming index.css: ${err}`)
-}
+    fs.unlink('dist/esbuildEntry.js', (err) => {
+      if (err) console.error(`Error while deleting dist/esbuildEntry.js: ${err}`)
+    })
 
-try {
-  fs.unlinkSync('dist/esbuildEntry.js')
-} catch (err) {
-  console.error(`Error while deleting dist/esbuildEntry.js: ${err}`)
-}
+    fs.unlink('dist/prod/esbuildEntry.js', (err) => {
+      if (err) console.error(`Error while deleting dist/prod/esbuildEntry.js: ${err}`)
+    })
 
-try {
-  fs.unlinkSync('dist/prod/esbuildEntry.js')
-} catch (err) {
-  console.error(`Error while deleting dist/prod/esbuildEntry.js: ${err}`)
-}
-
-try {
-  fs.unlinkSync('dist/esbuildEntry.d.ts')
-} catch (err) {
-  console.error(`Error while deleting dist/esbuildEntry.d.ts: ${err}`)
-}
-
-try {
-  fs.unlinkSync('dist/esbuildEntry.d.ts.map')
-} catch (err) {
-  console.error(`Error while deleting dist/esbuildEntry.d.ts.map: ${err}`)
-}
-
-try {
-  fs.unlinkSync('dist/esbuildEntry.js.map')
-} catch (err) {
-  console.error(`Error while deleting dist/esbuildEntry.js.map: ${err}`)
-}
-
-console.log('styles.css bundled successfully')
+    fs.unlink('dist/esbuildEntry.d.ts', (err) => {
+      if (err) console.error(`Error while deleting dist/esbuildEntry.d.ts: ${err}`)
+    })
+    fs.unlink('dist/esbuildEntry.d.ts.map', (err) => {
+      if (err) console.error(`Error while deleting dist/esbuildEntry.d.ts.map: ${err}`)
+    })
+    fs.unlink('dist/esbuildEntry.js.map', (err) => {
+      if (err) console.error(`Error while deleting dist/esbuildEntry.js.map: ${err}`)
+    })
+    console.log('styles.css bundled successfully')
+  })
+  .catch((e) => {
+    throw e
+  })
