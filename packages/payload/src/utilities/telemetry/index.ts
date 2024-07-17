4c3a9a3f236e0394ea60dd038a3825f1fb1bc659
Alessio Gravili <alessio@gravili.de>
2024-07-17 13:19:08 -0400

    fix: maxListenersExceeded warning due to atomically, which is a peerdep of conf (#7182)
    The conf dependency being bundled (not even executed) causes frequent
    HMR runs (around 10+) to throw multiple MaxListenersExceeded warnings in
    the console.
    
    This PR
    - fixes telemetry which was previously broken (threw an error which we
    ignored) due to a conf version upgrade
    - Removes the conf dependency (which is large and comes with a lot of
    unneeded dependencies from functionality we don't need, like dot
    notation or ajv validation). The important parts of the source code were
    copied over - it's now dependency-free
    - makes sure we only register the Next.js HMR websocket listener once,
    by adding it to the cache
    
    Before this PR:
    ![CleanShot 2024-07-16 at 19 35
    22](https://github.com/user-attachments/assets/cfd926b7-fe5d-440a-9b35-91f61eaa69fd)
    
    
    After this PR:
    
    ![CleanShot 2024-07-16 at 19 37
    41](https://github.com/user-attachments/assets/f5d0f0f3-4e00-4d28-8e32-be42db2f5f6c)
    
    Canary: 3.0.0-canary.ca3dd1c

--- packages/payload/src/utilities/telemetry/index.ts
+++ packages/payload/src/utilities/telemetry/index.ts
@@ -1,6 +1,5 @@
 import { execSync } from 'child_process'
 import ciInfo from 'ci-info'
-import Conf from 'conf'
 import { randomBytes } from 'crypto'
 import { findUp } from 'find-up'
 import fs from 'fs'
@@ -11,6 +10,7 @@ import type { Payload } from '../../types/index.js'
 import type { AdminInitEvent } from './events/adminInit.js'
 import type { ServerInitEvent } from './events/serverInit.js'
 
+import { Conf } from './conf/index.js'
 import { oneWayHash } from './oneWayHash.js'
 
 export type BaseEvent = {
