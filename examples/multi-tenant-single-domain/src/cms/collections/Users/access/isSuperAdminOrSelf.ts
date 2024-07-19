18063bd2565fa2f213e2d6b08f9073849a837040
Jacob Fletcher <jacobsfletch@gmail.com>
2024-07-19 13:18:13 -0400

    chore(examples): proper module resolution and migrations for multi-tenant single-domain example (#7240)

--- examples/multi-tenant-single-domain/src/cms/collections/Users/access/isSuperAdminOrSelf.ts
+++
@@ -1,6 +0,0 @@
-import type { Access } from 'payload'
-
-import { isSuperAdmin } from '../../../access/isSuperAdmin.js'
-import { isAccessingSelf } from './isAccessingSelf.js'
-
-export const isSuperAdminOrSelf: Access = (args) => isSuperAdmin(args) || isAccessingSelf(args)
