f18079c907b771370e7157c3f1f9066fa6ee30d6
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-07-19 13:48:13 -0400

    chore: fixes graphql enum change

--- packages/graphql/src/utilities/buildOptionEnums.ts
+++ packages/graphql/src/utilities/buildOptionEnums.ts
@@ -6,14 +6,18 @@ import formatName from './formatName.js'
 export function buildOptionEnums(options: Option[]): ThunkObjMap<GraphQLEnumValueConfig> {
   return options.reduce((acc, option) => {
     if (typeof option === 'string') {
-      acc[formatName(option)] = option
+      acc[formatName(option)] = {
+        value: option,
+      }
     } else if ('options' in option) {
       acc = {
         ...acc,
         ...buildOptionEnums(option.options),
       }
     } else {
-      acc[formatName(option.value)] = option.value
+      acc[formatName(option.value)] = {
+        value: option.value,
+      }
     }
     return acc
   }, {})
