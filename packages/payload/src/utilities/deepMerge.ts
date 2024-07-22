fd69e4f84cfe295324a85921e1d53d35dd0b3b8f
Alessio Gravili <alessio@gravili.de>
2024-07-21 23:55:40 -0400

    properly export deepMerge

--- packages/payload/src/utilities/deepMerge.ts
+++ packages/payload/src/utilities/deepMerge.ts
@@ -1,18 +1,15 @@
-import deepmerge from 'deepmerge'
+import deepMerge from 'deepmerge'
 
 import { isPlainObject } from './isPlainObject.js'
 
-export function deepMerge(obj1: object, obj2: object): object {
-  return deepmerge(obj1, obj2)
-}
-
+export { deepMerge }
 /**
  * Fully-featured deepMerge.
  *
  * Array handling: Arrays in the target object are combined with the source object's arrays.
  */
 export function deepMergeWithCombinedArrays<T = object>(obj1: object, obj2: object): T {
-  return deepmerge<T>(obj1, obj2, {
+  return deepMerge<T>(obj1, obj2, {
     arrayMerge: (target, source, options) => {
       const destination = target.slice()
 
@@ -20,7 +17,7 @@ export function deepMergeWithCombinedArrays<T = object>(obj1: object, obj2: obje
         if (typeof destination[index] === 'undefined') {
           destination[index] = options.cloneUnlessOtherwiseSpecified(item, options)
         } else if (options.isMergeableObject(item)) {
-          destination[index] = deepmerge(target[index], item, options)
+          destination[index] = deepMerge(target[index], item, options)
         } else if (target.indexOf(item) === -1) {
           destination.push(item)
         }
@@ -36,14 +33,14 @@ export function deepMergeWithCombinedArrays<T = object>(obj1: object, obj2: obje
  * Array handling: Arrays in the target object are replaced by the source object's arrays.
  */
 export function deepMergeWithSourceArrays<T = object>(obj1: object, obj2: object): T {
-  return deepmerge<T>(obj1, obj2, { arrayMerge: (_, source) => source })
+  return deepMerge<T>(obj1, obj2, { arrayMerge: (_, source) => source })
 }
 
 /**
  * Fully-featured deepMerge. Does not clone React components by default.
  */
 export function deepMergeWithReactComponents<T = object>(obj1: object, obj2: object): T {
-  return deepmerge<T>(obj1, obj2, {
+  return deepMerge<T>(obj1, obj2, {
     isMergeableObject: isPlainObject,
   })
 }
