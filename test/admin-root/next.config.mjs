8a55136dca44359c54b3c2e42054a71d26781785
Jacob Fletcher <jacobsfletch@gmail.com>
2024-07-22 15:03:20 -0400

    adds new admin-root test suite

---
+++ test/admin-root/next.config.mjs
@@ -0,0 +1,31 @@
+import bundleAnalyzer from '@next/bundle-analyzer'
+
+import withPayload from '../../packages/next/src/withPayload.js'
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+// eslint-disable-next-line no-restricted-exports
+export default withBundleAnalyzer(
+  withPayload({
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    images: {
+      domains: ['localhost'],
+    },
+    webpack: (webpackConfig) => {
+      webpackConfig.resolve.extensionAlias = {
+        '.cjs': ['.cts', '.cjs'],
+        '.js': ['.ts', '.tsx', '.js', '.jsx'],
+        '.mjs': ['.mts', '.mjs'],
+      }
+
+      return webpackConfig
+    },
+  }),
+)
