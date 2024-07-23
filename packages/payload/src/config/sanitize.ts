1a45f477587a6c04be71bad55de3631ad7604f31
Paul Popus <paul@nouance.io>
2024-07-23 16:49:41 -0400

    fix: some email adapters not working if they're promises due to config sanitisation

--- packages/payload/src/config/sanitize.ts
+++ packages/payload/src/config/sanitize.ts
@@ -151,6 +151,9 @@ export const sanitizeConfig = async (incomingConfig: Config): Promise<SanitizedC
     new Set(config.collections.map((c) => c.upload?.adapter).filter(Boolean)),
   )
 
+  // Pass through the email config as is so adapters don't break
+  if (incomingConfig.email) config.email = incomingConfig.email
+
   /*
     Execute richText sanitization
    */
