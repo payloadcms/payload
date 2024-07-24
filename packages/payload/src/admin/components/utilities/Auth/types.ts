8259611ce60e23f6298a07564d5f6dd2966d61ff
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2024-07-24 12:30:10 -0400

    fix: fetches and sets permissions before setting user (#7337)
    Fixes https://github.com/payloadcms/payload/issues/7330
    
    Ensures permissions are always present before setting the user.

--- packages/payload/src/admin/components/utilities/Auth/types.ts
+++ packages/payload/src/admin/components/utilities/Auth/types.ts
@@ -2,12 +2,12 @@ import type { Permissions, User } from '../../../../auth/types'
 
 export type AuthContext<T = User> = {
   fetchFullUser: () => Promise<void>
-  logOut: () => void
+  logOut: () => Promise<void>
   permissions?: Permissions
   refreshCookie: (forceRefresh?: boolean) => void
   refreshCookieAsync: () => Promise<User>
   refreshPermissions: ({ locale }?: { locale?: string }) => Promise<void>
-  setUser: (user: T) => void
+  setUser: (user: T) => Promise<void>
   strategy?: string
   token?: string
   tokenExpiration?: number
