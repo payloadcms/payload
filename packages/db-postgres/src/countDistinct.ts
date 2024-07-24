7a4bd68e401af580860c980dbc930204c14de315
Dan Ribbens <dan.ribbens@gmail.com>
2024-07-24 12:12:28 -0400

    chore: remove ts-expect-errors

--- packages/db-postgres/src/countDistinct.ts
+++ packages/db-postgres/src/countDistinct.ts
@@ -22,12 +22,10 @@ export const countDistinct: CountDistinct = async function countDistinct(
     methods: chainedMethods,
     query: (db as TransactionPg)
       .select({
-        // @ts-expect-error generic string is not matching union type from drizzle
         count: sql<string>`count
             (DISTINCT ${this.tables[tableName].id})`,
       })
       .from(this.tables[tableName])
-      // @ts-expect-error where is picking up libsql types
       .where(where),
   })
 
