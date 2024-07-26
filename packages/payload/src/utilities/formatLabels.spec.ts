eb191f335b210e10e8d6fe1a9451822234c45cde
Paul Popus <paul@nouance.io>
2024-07-26 13:52:32 -0400

    dont uppercase

--- packages/payload/src/utilities/formatLabels.spec.ts
+++ packages/payload/src/utilities/formatLabels.spec.ts
@@ -43,7 +43,7 @@ describe('formatLabels', () => {
 describe('formatErrorLabels', () => {
   it('should format array', () => {
     expect(formatErrorLabels(['test', 1, 'field', 'array', 3, 'final'])).toBe(
-      'Test (1) > Field > Array (3) > Final',
+      'test (1) > field > array (3) > final',
     )
   })
 })
