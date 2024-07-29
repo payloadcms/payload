c2cd6bbaecc8c2679ca0de868321c1e8f7b060fb
Paul Popus <paul@nouance.io>
2024-07-29 17:04:22 -0400

    fix test error

--- test/fields/e2e.spec.ts
+++ test/fields/e2e.spec.ts
@@ -102,7 +102,7 @@ describe('fields', () => {
 
       // toast error
       await expect(page.locator('.payload-toast-container')).toContainText(
-        'The following field is invalid: UniqueText',
+        'The following field is invalid: uniqueText',
       )
 
       await expect.poll(() => page.url(), { timeout: POLL_TOPASS_TIMEOUT }).toContain('create')
