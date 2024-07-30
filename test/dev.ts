ec878597899dda829366da5bb500337a3d232f35
Alessio Gravili <alessio@gravili.de>
2024-07-30 16:00:50 -0400

    init importMap in dev.ts

--- test/dev.ts
+++ test/dev.ts
@@ -1,21 +1,20 @@
+import chalk from 'chalk'
 import minimist from 'minimist'
 import { nextDev } from 'next/dist/cli/next-dev.js'
-import open from 'open'
-import { getNextJSRootDir } from './helpers/getNextJSRootDir.js'
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
 import fs from 'node:fs'
-import chalk from 'chalk'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import open from 'open'
+import { generateImportMap } from 'payload'
 
+import { getNextJSRootDir } from './helpers/getNextJSRootDir.js'
+import { load } from './loader/load.js'
 import { createTestHooks } from './testHooks.js'
-import { initImportMap } from './initImportMap.js'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 process.env.PAYLOAD_DROP_DATABASE = 'true'
-
-await initImportMap()
 
 const {
   _: [testSuiteArg],
@@ -38,6 +37,23 @@ await beforeTest()
 
 const { rootDir, adminRoute } = getNextJSRootDir(testSuiteArg)
 
+/*
+Gen importMap
+ */
+const testDir = path.resolve(dirname, testSuiteArg)
+
+const pathWithConfig = path.resolve(testDir, 'config.ts')
+console.log('Generating import map for config:', pathWithConfig)
+
+const config = await load(pathWithConfig)
+
+process.env.NEXT_PUBLIC_ROOT_DIR = path.resolve(dirname, '..')
+
+await generateImportMap(config, { log: true, force: true })
+/*
+Gen importMap end
+ */
+
 // Open the admin if the -o flag is passed
 if (args.o) {
   await open(`http://localhost:3000${adminRoute}`)
@@ -47,4 +63,4 @@ if (args.o) {
 await nextDev({ port: process.env.PORT || 3000, dirname: rootDir }, 'default', rootDir)
 
 // fetch the admin url to force a render
-fetch(`http://localhost:${process.env.PORT || 3000}${adminRoute}`)
+void fetch(`http://localhost:${process.env.PORT || 3000}${adminRoute}`)
