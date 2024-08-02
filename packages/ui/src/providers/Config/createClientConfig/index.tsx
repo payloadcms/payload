e18ea2b3a240cc5358f3e5af8b2c027a2a347f7e
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-02 12:35:33 -0400

    transfers root components to client config

--- packages/ui/src/providers/Config/createClientConfig/index.tsx
+++ packages/ui/src/providers/Config/createClientConfig/index.tsx
@@ -1,8 +1,21 @@
 import type { I18nClient } from '@payloadcms/translations'
-import type { ClientCollectionConfig, ClientGlobalConfig , LivePreviewConfig , Payload , SanitizedCollectionConfig , SanitizedConfig, SanitizedGlobalConfig, ServerOnlyLivePreviewProperties } from 'payload'
+import type {
+  ClientCollectionConfig,
+  ClientGlobalConfig,
+  LivePreviewConfig,
+  MappedComponent,
+  Payload,
+  SanitizedCollectionConfig,
+  SanitizedConfig,
+  SanitizedGlobalConfig,
+  ServerOnlyLivePreviewProperties,
+} from 'payload'
 
 import { createClientCollectionConfigs } from './collections.js'
 import { createClientGlobalConfigs } from './globals.js'
+import type { ImportMap } from 'packages/payload/src/bin/generateImportMap/index.js'
+import { PayloadIcon } from '@payloadcms/ui'
+import { getCreateMappedComponent, PayloadLogo } from '@payloadcms/ui/shared'
 
 export type ServerOnlyRootProperties = keyof Pick<
   SanitizedConfig,
@@ -28,7 +41,16 @@ export type ServerOnlyRootAdminProperties = keyof Pick<SanitizedConfig['admin'],
 export type ClientConfig = {
   admin: {
     livePreview?: Omit<LivePreviewConfig, ServerOnlyLivePreviewProperties>
-  } & Omit<SanitizedConfig['admin'], 'livePreview' & ServerOnlyRootAdminProperties>
+    components: {
+      actions: MappedComponent[]
+      LogoutButton: MappedComponent
+      graphics: {
+        Icon: MappedComponent
+        Logo: MappedComponent
+      }
+      Avatar: MappedComponent
+    }
+  } & Omit<SanitizedConfig['admin'], 'livePreview' | 'components'>
   collections: ClientCollectionConfig[]
   custom?: Record<string, any>
   globals: ClientGlobalConfig[]
@@ -53,21 +75,29 @@ const serverOnlyConfigProperties: readonly Partial<ServerOnlyRootProperties>[] =
   // `admin`, `onInit`, `localization`, `collections`, and `globals` are all handled separately
 ]
 
-const serverOnlyAdminProperties: readonly Partial<ServerOnlyRootAdminProperties>[] = ['components']
-
 export const createClientConfig = async ({
   children,
   config,
   i18n,
   payload,
+  importMap,
 }: {
   children: React.ReactNode
   config: SanitizedConfig
   i18n: I18nClient
   payload: Payload
+  importMap: ImportMap
   // eslint-disable-next-line @typescript-eslint/require-await
 }): Promise<{ clientConfig: ClientConfig; render: React.ReactNode }> => {
   const clientConfig: ClientConfig = { ...config }
+
+  const createMappedComponent = getCreateMappedComponent({
+    importMap,
+    serverProps: {
+      i18n,
+      payload,
+    },
+  })
 
   for (const key of serverOnlyConfigProperties) {
     if (key in clientConfig) {
@@ -86,11 +116,31 @@ export const createClientConfig = async ({
   if ('admin' in clientConfig) {
     clientConfig.admin = { ...clientConfig.admin }
 
-    for (const key of serverOnlyAdminProperties) {
-      if (key in clientConfig.admin) {
-        delete clientConfig.admin[key]
-      }
-    }
+    clientConfig.admin.components.graphics.Icon = createMappedComponent(
+      config.admin?.components?.graphics?.Icon,
+      undefined,
+      PayloadIcon,
+    )
+
+    clientConfig.admin.components.graphics.Logo = createMappedComponent(
+      config.admin?.components?.graphics?.Logo,
+      undefined,
+      PayloadLogo,
+    )
+
+    clientConfig.admin.components.actions = createMappedComponent(config.admin?.components?.actions)
+
+    clientConfig.admin.components.LogoutButton = createMappedComponent(
+      config.admin?.components?.logout?.Button,
+    )
+
+    clientConfig.admin.components.Avatar = createMappedComponent(
+      config.admin?.avatar &&
+        typeof config.admin?.avatar === 'object' &&
+        config.admin?.avatar &&
+        'Component' in config.admin?.avatar &&
+        config.admin?.avatar.Component,
+    )
 
     if ('livePreview' in clientConfig.admin) {
       clientConfig.admin.livePreview = { ...clientConfig.admin.livePreview }
