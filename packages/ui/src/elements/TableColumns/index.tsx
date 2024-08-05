12f1539fa90c5bcb6ea77b32ffd89a89a56738d6
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-05 13:30:55 -0400

    fixes column selector onclick

--- packages/ui/src/elements/TableColumns/index.tsx
+++ packages/ui/src/elements/TableColumns/index.tsx
@@ -125,7 +125,7 @@ export const TableColumnsProvider: React.FC<Props> = ({
       const toggledColumns = tableColumns.map((col) => {
         return {
           ...col,
-          active: col?.name === column ? !col.active : col.active,
+          active: col?.cellProps?.clientFieldConfig?.name === column ? !col.active : col.active,
         }
       })
 
