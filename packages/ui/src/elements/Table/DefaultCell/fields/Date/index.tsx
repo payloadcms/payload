04f640aa17034a4338c74a84e200c9c9a5db61d0
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-05 13:35:18 -0400

    fixes date cell formatting

--- packages/ui/src/elements/Table/DefaultCell/fields/Date/index.tsx
+++ packages/ui/src/elements/Table/DefaultCell/fields/Date/index.tsx
@@ -1,6 +1,7 @@
 'use client'
-import type { DefaultCellComponentProps } from 'payload'
+import type { ClientFieldConfig, DefaultCellComponentProps } from 'payload'
 
+import { useConfig } from '@payloadcms/ui'
 import React from 'react'
 
 import { useTranslation } from '../../../../../providers/Translation/index.js'
@@ -9,9 +10,17 @@ import { formatDate } from '../../../../../utilities/formatDate.js'
 export const DateCell: React.FC<DefaultCellComponentProps<Date | number | string>> = ({
   cellData,
   clientFieldConfig: {
-    admin: { dateFormat },
+    admin: { dateFormat: dateFormatFromField } = {} as ClientFieldConfig['admin'],
   },
 }) => {
+  const {
+    config: {
+      admin: { dateFormat: dateFormatFromRoot },
+    },
+  } = useConfig()
+
+  const dateFormat = dateFormatFromField || dateFormatFromRoot
+
   const { i18n } = useTranslation()
 
   return <span>{cellData && formatDate({ date: cellData, i18n, pattern: dateFormat })}</span>
