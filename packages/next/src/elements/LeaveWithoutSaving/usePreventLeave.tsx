f52fe0bb2a0643f8640104604afaa43753b67e76
PatrikKozak <patrik@payloadcms.com>
2024-08-14 10:43:34 -0400

    fix: remove reliance on useModal in usePreventLeave hook

--- packages/next/src/elements/LeaveWithoutSaving/usePreventLeave.tsx
+++ packages/next/src/elements/LeaveWithoutSaving/usePreventLeave.tsx
@@ -1,7 +1,6 @@
 // Credit: @Taiki92777
 //    - Source: https://github.com/vercel/next.js/discussions/32231#discussioncomment-7284386
 // Credit: `react-use` maintainers
-import { useModal } from '@payloadcms/ui'
 //    -  Source: https://github.com/streamich/react-use/blob/ade8d3905f544305515d010737b4ae604cc51024/src/useBeforeUnload.ts#L2
 import { useRouter } from 'next/navigation.js'
 import { useCallback, useEffect, useRef } from 'react'
@@ -58,14 +57,14 @@ export const useBeforeUnload = (enabled: (() => boolean) | boolean = true, messa
 export const usePreventLeave = ({
   hasAccepted = false,
   message = 'Are you sure want to leave this page?',
-  modalSlug,
+  onAccept,
   onPrevent,
   prevent = true,
 }: {
   hasAccepted: boolean
   // if no `onPrevent` is provided, the message will be displayed in a confirm dialog
   message?: string
-  modalSlug?: string
+  onAccept?: () => void
   // to use a custom confirmation dialog, provide a function that returns a boolean
   onPrevent?: () => void
   prevent: boolean
@@ -73,7 +72,6 @@ export const usePreventLeave = ({
   // check when page is about to be reloaded
   useBeforeUnload(prevent, message)
 
-  const { closeModal } = useModal()
   const router = useRouter()
   const cancelledURL = useRef<string>('')
 
@@ -146,8 +144,8 @@ export const usePreventLeave = ({
 
   useEffect(() => {
     if (hasAccepted && cancelledURL.current) {
-      if (modalSlug) closeModal(modalSlug)
+      if (onAccept) onAccept()
       router.push(cancelledURL.current)
     }
-  }, [closeModal, hasAccepted, modalSlug, router])
+  }, [hasAccepted, onAccept, router])
 }
