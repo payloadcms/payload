caccec14ef9fc9f3f991c4e09734355c180231e9
Paul Popus <paul@nouance.io>
2024-08-14 10:30:06 -0600

    chore: add command to run inside the importMap error

--- packages/ui/src/providers/Config/createClientConfig/getComponent.ts
+++ packages/ui/src/providers/Config/createClientConfig/getComponent.ts
@@ -37,11 +37,15 @@ export const getComponent = <
   const Component = importMap[key]
 
   if (!Component && !silent) {
-    console.error(`getComponent: Component not found in importMap`, {
-      identifier,
-      key,
-      payloadComponent,
-    })
+    console.error(
+      `getComponent: Component not found in importMap`,
+      {
+        identifier,
+        key,
+        payloadComponent,
+      },
+      'You may need to run the `payload generate:importmap` command to generate the importMap ahead of runtime.',
+    )
   }
 
   return {
