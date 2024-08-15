b9fa144fd422f7a8268843d2e6f3c93727651793
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-15 13:22:41 -0400

    adds back community fields

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -17,6 +17,57 @@ export const PostsCollection: CollectionConfig = {
         },
       },
     },
+    {
+      admin: {
+        components: {
+          Label: '/collections/Posts/MyComponent.js#MyComponent',
+        },
+        description: 'This is a description',
+      },
+      name: 'text',
+      type: 'text',
+    },
+    {
+      name: 'richText',
+      type: 'richText',
+    },
+    {
+      name: 'myBlocks',
+      type: 'blocks',
+      blocks: [
+        {
+          slug: 'test',
+          fields: [
+            {
+              name: 'test',
+              type: 'text',
+            },
+          ],
+        },
+        {
+          slug: 'someBlock2',
+          fields: [
+            {
+              name: 'test2',
+              type: 'text',
+            },
+          ],
+        },
+      ],
+    },
+    // {
+    //   type: 'row',
+    //   fields: [],
+    // },
+    // {
+    //   name: 'associatedMedia',
+    //   type: 'upload',
+    //   access: {
+    //     create: () => true,
+    //     update: () => false,
+    //   },
+    //   relationTo: mediaSlug,
+    // },
   ],
   versions: {
     drafts: true,
