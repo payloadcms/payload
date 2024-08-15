195667281e738b3ccdf87570bdcaee554234d9fa
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-15 16:12:43 -0400

    fetches data using the api and adds pagination

--- test/_community/collections/Posts/TableField/MyTable.tsx
+++ test/_community/collections/Posts/TableField/MyTable.tsx
@@ -2,8 +2,8 @@
 
 import type { ClientCollectionConfig } from 'payload'
 
-import { Table, useListQuery } from '@payloadcms/ui'
-import React from 'react'
+import { Pagination, Table, useListQuery } from '@payloadcms/ui'
+import React, { Fragment } from 'react'
 
 type RelationshipTableComponentProps = {
   readonly collectionConfig: ClientCollectionConfig
@@ -12,16 +12,29 @@ type RelationshipTableComponentProps = {
 export const MyTableComponent: React.FC<RelationshipTableComponentProps> = ({
   collectionConfig,
 }) => {
-  const { data } = useListQuery()
+  const { data, handlePageChange } = useListQuery()
 
   return (
-    <Table
-      customCellContext={{
-        collectionSlug: collectionConfig.slug,
-        uploadConfig: collectionConfig.upload,
-      }}
-      data={data.docs}
-      fields={collectionConfig.fields}
-    />
+    <Fragment>
+      <Table
+        customCellContext={{
+          collectionSlug: collectionConfig.slug,
+          uploadConfig: collectionConfig.upload,
+        }}
+        data={data.docs}
+        fields={collectionConfig.fields}
+      />
+      <Pagination
+        hasNextPage={data.hasNextPage}
+        hasPrevPage={data.hasPrevPage}
+        limit={data.limit}
+        nextPage={data.nextPage || undefined}
+        numberOfNeighbors={1}
+        onChange={() => handlePageChange}
+        page={data.page}
+        prevPage={data.prevPage || undefined}
+        totalPages={data.totalPages}
+      />
+    </Fragment>
   )
 }
--- test/_community/collections/Posts/TableField/index.tsx
+++ test/_community/collections/Posts/TableField/index.tsx
@@ -2,14 +2,19 @@
 import type { ClientCollectionConfig, ClientField, PaginatedDocs, Where } from 'payload'
 
 import {
+  FieldLabel,
   ListQueryProvider,
+  LoadingOverlay,
+  Pagination,
   TableColumnsProvider,
   useConfig,
   useDocumentDrawer,
   useListDrawer,
+  usePayloadAPI,
 } from '@payloadcms/ui'
-import React, { useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 
+import { hoistQueryParamsToAnd } from '../../../../../packages/ui/src/elements/ListDrawer/DrawerContent.js'
 import { MyCell } from './MyCell.js'
 import { MyTableComponent } from './MyTable.js'
 import './index.scss'
@@ -17,22 +22,18 @@ import './index.scss'
 const baseClass = 'table-field-header'
 
 type RelationshipTableComponentProps = {
-  readonly field: {
-    label: string
-    name: string
-    relationTo: string
-  }
+  readonly field: ClientField
 }
 
 const initialData: PaginatedDocs = {
   docs: [
-    {
-      id: '123',
-      context: {},
-      createdAt: '2021-01-01T00:00:00.000Z',
-      title: 'Hello',
-      updatedAt: '2021-01-01T00:00:00.000Z',
-    },
+    // {
+    //   id: '123',
+    //   context: {},
+    //   createdAt: '2021-01-01T00:00:00.000Z',
+    //   title: 'Hello',
+    //   updatedAt: '2021-01-01T00:00:00.000Z',
+    // },
   ],
   hasNextPage: false,
   hasPrevPage: false,
@@ -43,18 +44,87 @@ const initialData: PaginatedDocs = {
   totalPages: 0,
 }
 
-export const TableField: React.FC<RelationshipTableComponentProps> = ({
-  field,
-  field: { relationTo = 'posts' },
-}) => {
+const relationTo = 'posts'
+
+const filterOptions: Where = {}
+
+export const TableField: React.FC<RelationshipTableComponentProps> = (props) => {
+  const { field } = props
+
+  const {
+    config: {
+      routes: { api },
+      serverURL,
+    },
+    getEntityConfig,
+  } = useConfig()
+
   const [limit, setLimit] = useState<number>()
   const [sort, setSort] = useState<string | undefined>(undefined)
   const [page, setPage] = useState<number>(1)
   const [where, setWhere] = useState<Where | null>(null)
   const [search, setSearch] = useState<string>('')
 
-  const { getEntityConfig } = useConfig()
   const collectionConfig = getEntityConfig({ collectionSlug: relationTo }) as ClientCollectionConfig
+
+  const apiURL = `${serverURL}${api}/${collectionConfig.slug}`
+
+  const [cacheBust, dispatchCacheBust] = useReducer((state) => state + 1, 0) // used to force a re-fetch even when apiURL is unchanged
+
+  const [{ data, isError, isLoading: isLoadingList }, { setParams }] = usePayloadAPI(apiURL, {
+    initialData,
+  })
+
+  console.log(data)
+
+  useEffect(() => {
+    const {
+      admin: { listSearchableFields, useAsTitle } = {} as ClientCollectionConfig['admin'],
+      versions,
+    } = collectionConfig
+
+    const params: {
+      cacheBust?: number
+      draft?: string
+      limit?: number
+      page?: number
+      search?: string
+      sort?: string
+      where?: unknown
+    } = {}
+
+    let copyOfWhere = { ...(where || {}) }
+
+    if (filterOptions && typeof filterOptions !== 'boolean') {
+      copyOfWhere = hoistQueryParamsToAnd(copyOfWhere, filterOptions)
+    }
+
+    if (search) {
+      const searchAsConditions = (listSearchableFields || [useAsTitle]).map((fieldName) => {
+        return {
+          [fieldName]: {
+            like: search,
+          },
+        }
+      }, [])
+
+      if (searchAsConditions.length > 0) {
+        const searchFilter: Where = {
+          or: [...searchAsConditions],
+        }
+
+        copyOfWhere = hoistQueryParamsToAnd(copyOfWhere, searchFilter)
+      }
+    }
+
+    if (page) params.page = page
+    if (sort) params.sort = sort
+    if (cacheBust) params.cacheBust = cacheBust
+    if (copyOfWhere) params.where = copyOfWhere
+    if (versions?.drafts) params.draft = 'true'
+
+    setParams(params)
+  }, [page, sort, where, search, cacheBust, collectionConfig, setParams])
 
   const [DocumentDrawer, DocumentDrawerToggler] = useDocumentDrawer({
     collectionSlug: relationTo,
@@ -66,17 +136,31 @@ export const TableField: React.FC<RelationshipTableComponentProps> = ({
 
   const preferenceKey = `${relationTo}-list`
 
+  if (isLoadingList) {
+    return <LoadingOverlay />
+  }
+
   return (
     <div>
       <div className={baseClass}>
-        <h4>{field?.label || field?.name || 'Untitled'}</h4>
+        <h4>
+          <FieldLabel
+            label={
+              'label' in field && field.label
+                ? field.label
+                : 'name' in field
+                  ? field.name
+                  : 'Untitled'
+            }
+          />
+        </h4>
         <div className={`${baseClass}__actions`}>
           <DocumentDrawerToggler>Create new</DocumentDrawerToggler>
           <ListDrawerToggler>Add existing</ListDrawerToggler>
         </div>
       </div>
       <ListQueryProvider
-        data={initialData}
+        data={data}
         defaultLimit={limit || collectionConfig?.admin?.pagination?.defaultLimit}
         defaultSort={sort}
         handlePageChange={setPage}
