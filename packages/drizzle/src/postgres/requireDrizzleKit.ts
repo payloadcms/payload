f34168e2a4b384a9e1844e15e57168d7fb71a0d2
Elliot DeNolf <denolfe@gmail.com>
2024-08-16 15:41:03 -0400

    chore: lint fix

--- packages/drizzle/src/postgres/requireDrizzleKit.ts
+++ packages/drizzle/src/postgres/requireDrizzleKit.ts
@@ -1,5 +1,6 @@
-import type { RequireDrizzleKit } from '@payloadcms/drizzle/types'
-
 import { createRequire } from 'module'
+
+import type { RequireDrizzleKit } from '../types.js'
+
 const require = createRequire(import.meta.url)
 export const requireDrizzleKit: RequireDrizzleKit = () => require('drizzle-kit/api')
