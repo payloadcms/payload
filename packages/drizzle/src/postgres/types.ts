fc349bdd2a57dbc03ef7901e1d305c4fa52aefdc
Elliot DeNolf <denolfe@gmail.com>
2024-08-16 13:53:34 -0400

    feat: abstracted postgres common into drizzle/postgres

--- packages/drizzle/src/postgres/types.ts
+++ packages/drizzle/src/postgres/types.ts
@@ -1,9 +1,3 @@
-import type { Operators } from '@payloadcms/drizzle'
-import type {
-  BuildQueryJoinAliases,
-  DrizzleAdapter,
-  TransactionPg,
-} from '@payloadcms/drizzle/types'
 import type { DrizzleSnapshotJSON } from 'drizzle-kit/api'
 import type {
   ColumnBaseConfig,
@@ -25,29 +19,10 @@ import type {
 } from 'drizzle-orm/pg-core'
 import type { PgTableFn } from 'drizzle-orm/pg-core/table'
 import type { Payload, PayloadRequest } from 'payload'
-import type { Pool, PoolConfig, QueryResult } from 'pg'
+import type { QueryResult } from 'pg'
 
-export type Args = {
-  idType?: 'serial' | 'uuid'
-  localesSuffix?: string
-  logger?: DrizzleConfig['logger']
-  migrationDir?: string
-  pool: PoolConfig
-  prodMigrations?: {
-    down: (args: MigrateDownArgs) => Promise<void>
-    name: string
-    up: (args: MigrateUpArgs) => Promise<void>
-  }[]
-  push?: boolean
-  relationshipsSuffix?: string
-  /**
-   * The schema name to use for the database
-   * @experimental This only works when there are not other tables or enums of the same name in the database under a different schema. Awaiting fix from Drizzle.
-   */
-  schemaName?: string
-  transactionOptions?: PgTransactionConfig | false
-  versionsSuffix?: string
-}
+import type { Operators } from '../index.js'
+import type { BuildQueryJoinAliases, DrizzleAdapter, TransactionPg } from '../types.js'
 
 export type GenericColumn = PgColumn<
   ColumnBaseConfig<ColumnDataType, string>,
@@ -120,15 +95,15 @@ export type PostgresAdapter = {
    * Used for returning properly formed errors from unique fields
    */
   fieldConstraints: Record<string, Record<string, string>>
-  idType: Args['idType']
+  idType: 'serial' | 'uuid'
   initializing: Promise<void>
   insert: Insert
   localesSuffix?: string
   logger: DrizzleConfig['logger']
   operators: Operators
   pgSchema?: Schema
-  pool: Pool
-  poolOptions: Args['pool']
+  // pool: Pool
+  // poolOptions: Args['pool']
   prodMigrations?: {
     down: (args: MigrateDownArgs) => Promise<void>
     name: string
@@ -139,7 +114,7 @@ export type PostgresAdapter = {
   relations: Record<string, GenericRelation>
   relationshipsSuffix?: string
   resolveInitializing: () => void
-  schemaName?: Args['schemaName']
+  schemaName?: string
   sessions: {
     [id: string]: {
       db: PostgresDB | TransactionPg
@@ -152,42 +127,19 @@ export type PostgresAdapter = {
   versionsSuffix?: string
 } & PostgresDrizzleAdapter
 
+export type PostgresDrizzleAdapter = Omit<
+  DrizzleAdapter,
+  | 'countDistinct'
+  | 'deleteWhere'
+  | 'drizzle'
+  | 'dropDatabase'
+  | 'execute'
+  | 'insert'
+  | 'operators'
+  | 'relations'
+>
+
 export type IDType = 'integer' | 'numeric' | 'uuid' | 'varchar'
 
 export type MigrateUpArgs = { payload: Payload; req?: Partial<PayloadRequest> }
 export type MigrateDownArgs = { payload: Payload; req?: Partial<PayloadRequest> }
-
-declare module 'payload' {
-  export interface DatabaseAdapter
-    extends Omit<Args, 'idType' | 'logger' | 'migrationDir' | 'pool'>,
-      DrizzleAdapter {
-    beginTransaction: (options?: PgTransactionConfig) => Promise<null | number | string>
-    drizzle: PostgresDB
-    enums: Record<string, GenericEnum>
-    /**
-     * An object keyed on each table, with a key value pair where the constraint name is the key, followed by the dot-notation field name
-     * Used for returning properly formed errors from unique fields
-     */
-    fieldConstraints: Record<string, Record<string, string>>
-    idType: Args['idType']
-    initializing: Promise<void>
-    localesSuffix?: string
-    logger: DrizzleConfig['logger']
-    pgSchema?: { table: PgTableFn } | PgSchema
-    pool: Pool
-    poolOptions: Args['pool']
-    prodMigrations?: {
-      down: (args: MigrateDownArgs) => Promise<void>
-      name: string
-      up: (args: MigrateUpArgs) => Promise<void>
-    }[]
-    push: boolean
-    rejectInitializing: () => void
-    relationshipsSuffix?: string
-    resolveInitializing: () => void
-    schema: Record<string, unknown>
-    schemaName?: Args['schemaName']
-    tableNameMap: Map<string, string>
-    versionsSuffix?: string
-  }
-}
