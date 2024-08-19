690108dd582d4abcfed80b0432cb92adfd3028f5
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-19 16:39:54 -0400

    adds more tests

--- test/admin/collections/CustomFields/fields/Text/Description.tsx
+++
@@ -1,9 +0,0 @@
-import type { TextFieldDescriptionServerComponent } from 'payload'
-
-import React from 'react'
-
-export const CustomDescription: TextFieldDescriptionServerComponent = (props) => {
-  return (
-    <div id="custom-field-description">{`Description: the max length of this field is: ${props?.field?.maxLength}`}</div>
-  )
-}
--- test/admin/collections/CustomFields/fields/Text/Label.tsx
+++
@@ -1,9 +0,0 @@
-import type { TextFieldLabelServerComponent } from 'payload'
-
-import React from 'react'
-
-export const CustomLabel: TextFieldLabelServerComponent = (props) => {
-  return (
-    <div id="custom-field-label">{`Label: the max length of this field is: ${props?.field?.maxLength}`}</div>
-  )
-}
