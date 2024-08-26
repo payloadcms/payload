fed7f2fa5b22454cf2873d49da870d945d9fb02d
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-26 17:50:29 -0400

    fix: sanitizes modifyResponseHeaders from client config (#7876)

--- packages/payload/src/index.ts
+++ packages/payload/src/index.ts
@@ -663,6 +663,7 @@ export type { ClientCollectionConfig } from './collections/config/client.js'
 export type {
   ServerOnlyCollectionAdminProperties,
   ServerOnlyCollectionProperties,
+  ServerOnlyUploadProperties,
 } from './collections/config/client.js'
 export type {
   AfterChangeHook as CollectionAfterChangeHook,
