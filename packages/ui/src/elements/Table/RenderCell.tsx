ab8b9319bb6b3316985deb79346f8a2a620c0213
Jacob Fletcher <jacobsfletch@gmail.com>
2024-08-29 13:25:03 -0400

    uses deepmergesimple

--- packages/ui/src/elements/Table/RenderCell.tsx
+++ packages/ui/src/elements/Table/RenderCell.tsx
@@ -1,11 +1,11 @@
 import type { CellComponentProps } from 'payload'
 
-import deepMerge from 'deepmerge'
 import React, { useMemo } from 'react'
 
 import type { Column } from './index.js'
 
 import { RenderComponent } from '../../providers/Config/RenderComponent.js'
+import { deepMergeSimple } from '../../utilities/deepMerge.js'
 import { TableCellProvider } from './TableCellProvider/index.js'
 
 export const RenderCell: React.FC<{
@@ -18,7 +18,7 @@ export const RenderCell: React.FC<{
   const { cellProps: cellPropsFromProps, col, colIndex, customCellContext, row } = props
 
   const cellProps: Partial<CellComponentProps> = useMemo(
-    () => deepMerge(col?.cellProps || {}, cellPropsFromProps || {}) as Partial<CellComponentProps>,
+    () => deepMergeSimple(col?.cellProps || {}, cellPropsFromProps || {}),
     [cellPropsFromProps, col.cellProps],
   )
 
