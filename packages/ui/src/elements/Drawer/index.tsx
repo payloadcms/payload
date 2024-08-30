64c371c2b2bd457e072edaaca4b89b520bbf03dd
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-08-30 15:41:18 -0400

    chore: swap EditDepthProviders

--- packages/ui/src/elements/Drawer/index.tsx
+++ packages/ui/src/elements/Drawer/index.tsx
@@ -5,7 +5,7 @@ import React, { useCallback, useEffect, useState } from 'react'
 import type { Props, TogglerProps } from './types.js'
 
 import { XIcon } from '../../icons/X/index.js'
-import { EditDepthContext, useEditDepth } from '../../providers/EditDepth/index.js'
+import { EditDepthProvider, useEditDepth } from '../../providers/EditDepth/index.js'
 import { useTranslation } from '../../providers/Translation/index.js'
 import { Gutter } from '../Gutter/index.js'
 import './index.scss'
@@ -102,7 +102,7 @@ export const Drawer: React.FC<Props> = ({
         >
           <div className={`${baseClass}__blur-bg-content`} />
           <Gutter className={`${baseClass}__content-children`} left={gutter} right={gutter}>
-            <EditDepthContext.Provider value={drawerDepth + 1}>
+            <EditDepthProvider>
               {Header}
               {Header === undefined && (
                 <div className={`${baseClass}__header`}>
@@ -124,7 +124,7 @@ export const Drawer: React.FC<Props> = ({
                 </div>
               )}
               {children}
-            </EditDepthContext.Provider>
+            </EditDepthProvider>
           </Gutter>
         </div>
       </Modal>
