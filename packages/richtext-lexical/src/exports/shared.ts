d523bf74f589c74f57fdc6c530b7b0434668c65e
Alessio Gravili <alessio@gravili.de>
2024-09-03 14:50:50 -0400

    expose jsx helper function in a safe way

---
+++ packages/richtext-lexical/src/exports/shared.ts
@@ -0,0 +1,7 @@
+export { extractPropsFromJSXPropsString } from '../utilities/jsx/extractPropsFromJSXPropsString.js'
+export {
+  extractFrontmatter,
+  frontmatterToObject,
+  objectToFrontmatter,
+  propsToJSXString,
+} from '../utilities/jsx/jsx.js'
