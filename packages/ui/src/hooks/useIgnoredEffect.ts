2d95ed56ecda9034ee9863c1bb317200503ef645
Jacob Fletcher <jacobsfletch@gmail.com>
2024-09-04 17:21:05 -0400

    adds jsdocs to useIgnoredEffect hook

--- packages/ui/src/hooks/useIgnoredEffect.ts
+++ packages/ui/src/hooks/useIgnoredEffect.ts
@@ -1,16 +1,24 @@
 import { dequal } from 'dequal/lite'
 import { useEffect, useRef } from 'react'
 
-// This hook is used to run an effect only when the dependencies change, but ignore some of them.
-// This is helpful when you're effect has a dependency that you don't want to trigger the effect.
+/**
+ * This hook is used to run an effect only when the dependencies change, but ignore some of them.
+ * This is helpful when you're effect has a dependency that you don't want to trigger the effect.
+ * @constructor
+ * @param {React.EffectCallback} effect - The effect to run
+ * @param {React.DependencyList} deps - Dependencies that should trigger the effect
+ * @param {React.DependencyList} ignoredDeps - Dependencies that should _not_ trigger the effect
+ * @param {Object} options - Additional options to configure the hook
+ * @param {boolean} options.runOnFirstRender - Whether the effect should run on the first render
+ * @example
+ * useIgnoredEffect(() => {
+ *  console.log('This will run when `foo` changes, but not when `bar` changes')
+ * }, [foo], [bar])
+ */
 export function useIgnoredEffect(
-  /** The effect to run */
   effect: React.EffectCallback,
-  /** Dependencies that should trigger the effect */
   deps: React.DependencyList,
-  /** Dependencies that should _not_ trigger the effect */
   ignoredDeps: React.DependencyList,
-  /** Additional options to configure the hook */
   options?: { runOnFirstRender?: boolean },
 ) {
   const hasInitialized = useRef(
