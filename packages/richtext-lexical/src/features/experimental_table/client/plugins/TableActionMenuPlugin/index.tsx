5e8a10833d2d371d655770f1bbbd4a67c9fab0da
rilrom <rilrom2@hotmail.com>
2024-09-06 01:58:05 +0930

    fix: merging cells onto already merged cells throws error

--- packages/richtext-lexical/src/features/experimental_table/client/plugins/TableActionMenuPlugin/index.tsx
+++ packages/richtext-lexical/src/features/experimental_table/client/plugins/TableActionMenuPlugin/index.tsx
@@ -58,14 +58,10 @@ function computeSelectionCount(selection: TableSelection): {
   }
 }
 
-// This is important when merging cells as there is no good way to re-merge weird shapes (a result
-// of selecting merged cells and non-merged)
-function isTableSelectionRectangular(selection: TableSelection): boolean {
+// This prevents the merge cells button from appearing when attempting to merge unmerged cells with
+// already merged cells
+function isTableSelectionValid(selection: TableSelection): boolean {
   const nodes = selection.getNodes()
-  const currentRows: Array<number> = []
-  let currentRow = null
-  let expectedColumns = null
-  let currentColumns = 0
   for (let i = 0; i < nodes.length; i++) {
     const node = nodes[i]
     if ($isTableCellNode(node)) {
@@ -73,30 +69,12 @@ function isTableSelectionRectangular(selection: TableSelection): boolean {
       if (!$isTableRowNode(row)) {
         throw new Error('Expected CellNode to have a RowNode parent')
       }
-      if (currentRow !== row) {
-        if (expectedColumns !== null && currentColumns !== expectedColumns) {
-          return false
-        }
-        if (currentRow !== null) {
-          expectedColumns = currentColumns
-        }
-        currentRow = row
-        currentColumns = 0
+      if (node.__colSpan > 1 || node.__rowSpan > 1) {
+        return false
       }
-      const colSpan = node.__colSpan
-      for (let j = 0; j < colSpan; j++) {
-        if (currentRows[currentColumns + j] === undefined) {
-          currentRows[currentColumns + j] = 0
-        }
-        currentRows[currentColumns + j] += node.__rowSpan
-      }
-      currentColumns += colSpan
     }
   }
-  return (
-    (expectedColumns === null || currentColumns === expectedColumns) &&
-    currentRows.every((v) => v === currentRows[0])
-  )
+  return true
 }
 
 function $canUnmerge(): boolean {
@@ -184,7 +162,7 @@ function TableActionMenu({
         const currentSelectionCounts = computeSelectionCount(selection)
         updateSelectionCounts(computeSelectionCount(selection))
         setCanMergeCells(
-          isTableSelectionRectangular(selection) &&
+          isTableSelectionValid(selection) &&
             (currentSelectionCounts.columns > 1 || currentSelectionCounts.rows > 1),
         )
       }
