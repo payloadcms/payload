042c1d8984790d3d212c1c8736efc278f19b5b97
PatrikKozak <patrik@payloadcms.com>
2024-09-06 12:51:38 -0400

    feat: adds int tests for locked-documents

--- test/locked-documents/int.spec.ts
+++ test/locked-documents/int.spec.ts
@@ -1,63 +1,72 @@
-import type { Payload } from 'payload'
-
 import path from 'path'
+import { APIError, NotFound, type Payload } from 'payload'
 import { fileURLToPath } from 'url'
 
 import type { NextRESTClient } from '../helpers/NextRESTClient.js'
-import type { Page, Post } from './payload-types.js'
+import type { Menu, Page, Post } from './payload-types.js'
 
-import { devUser, regularUser } from '../credentials.js'
+import { devUser } from '../credentials.js'
 import { initPayloadInt } from '../helpers/initPayloadInt.js'
 import { pagesSlug } from './collections/Pages/index.js'
 import { postsSlug } from './collections/Posts/index.js'
+import { menuSlug } from './globals/Menu/index.js'
+
+const lockedDocumentCollection = 'payload-locked-documents'
 
 let payload: Payload
 let token: string
 let restClient: NextRESTClient
 
-const { email: devEmail, password: devPassword } = devUser
-const { email: regEmail, password: regPassword } = regularUser
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
-describe('Locked Documents', () => {
-  let post1: Post
-  let page1: Page
-  let user1: any
+describe('Locked documents', () => {
+  let post: Post
+  let page: Page
+  let menu: Menu
+  let user: any
   let user2: any
 
   beforeAll(async () => {
-    ;({ payload } = await initPayloadInt(dirname))
-  })
+    ;({ payload, restClient } = await initPayloadInt(dirname))
 
-  beforeEach(async () => {
-    post1 = await payload.create({
-      collection: postsSlug,
+    const loginResult = await payload.login({
+      collection: 'users',
       data: {
-        text: 'SOME POST',
+        email: devUser.email,
+        password: devUser.password,
       },
     })
 
-    page1 = await payload.create({
-      collection: pagesSlug,
-      data: {
-        text: 'SOME PAGE',
-      },
-    })
+    user = loginResult.user
+    token = loginResult.token
 
     user2 = await payload.create({
       collection: 'users',
       data: {
-        email: regEmail,
-        password: regPassword,
+        email: 'test@payloadcms.com',
+        password: 'test',
       },
     })
 
-    user1 = await payload.login({
-      collection: 'users',
+    post = await payload.create({
+      collection: postsSlug,
       data: {
-        email: devEmail,
-        password: devPassword,
+        text: 'some post',
+      },
+    })
+
+    page = await payload.create({
+      collection: pagesSlug,
+      data: {
+        text: 'some page',
+      },
+    })
+
+    menu = await payload.updateGlobal({
+      slug: menuSlug,
+      data: {
+        globalText: 'global text',
       },
     })
   })
@@ -68,29 +77,373 @@ describe('Locked Documents', () => {
     }
   })
 
-  it('local API example', async () => {
-    const newPost = await payload.create({
+  // Update unlocked documents (collection & global)
+
+  it('update unlocked document - collection', async () => {
+    const updatedPost = await payload.update({
       collection: postsSlug,
       data: {
-        text: 'LOCAL API EXAMPLE',
+        text: 'updated post',
+      },
+      id: post.id,
+    })
+
+    expect(updatedPost.text).toEqual('updated post')
+  })
+
+  it('update unlocked document - global', async () => {
+    const updatedGlobalMenu = await payload.updateGlobal({
+      slug: menuSlug,
+      data: {
+        globalText: 'updated global text',
       },
     })
 
-    expect(newPost.text).toEqual('LOCAL API EXAMPLE')
+    expect(updatedGlobalMenu.globalText).toEqual('updated global text')
   })
 
-  it('rest API example', async () => {
-    const data = await restClient
-      .POST(`/${postsSlug}`, {
-        body: JSON.stringify({
-          text: 'REST API EXAMPLE',
-        }),
-        headers: {
-          Authorization: `JWT ${token}`,
-        },
-      })
-      .then((res) => res.json())
+  // Delete unlocked document (collection)
 
-    expect(data.doc.text).toEqual('REST API EXAMPLE')
+  it('delete unlocked document - collection', async () => {
+    const { docs } = await payload.find({
+      collection: postsSlug,
+      depth: 0,
+    })
+
+    expect(docs).toHaveLength(2)
+
+    await payload.delete({
+      collection: postsSlug,
+      id: post.id,
+    })
+
+    const { docs: deletedResults } = await payload.find({
+      collection: postsSlug,
+      depth: 0,
+    })
+
+    expect(deletedResults).toHaveLength(1)
+  })
+
+  // Try to update locked document (collection & global)
+
+  it('should not allow update of locked document - collection', async () => {
+    const newPost = await payload.create({
+      collection: postsSlug,
+      data: {
+        text: 'some post',
+      },
+    })
+
+    // Give locking ownership to another user
+    await payload.create({
+      collection: lockedDocumentCollection,
+      data: {
+        _lastEdited: {
+          editedAt: new Date().toISOString(),
+          user: {
+            relationTo: 'users',
+            value: user2.id,
+          },
+        },
+        document: {
+          relationTo: 'posts',
+          value: newPost.id,
+        },
+        globalSlug: undefined,
+        isLocked: true,
+      },
+    })
+
+    try {
+      await payload.update({
+        collection: postsSlug,
+        data: {
+          text: 'updated post',
+        },
+        id: newPost.id,
+      })
+    } catch (error) {
+      expect(error).toBeInstanceOf(APIError)
+      expect(error.message).toMatch(/currently locked by another user and cannot be updated/)
+    }
+
+    // Should not allow update - expect data not to change
+    expect(newPost.text).toEqual('some post')
+  })
+
+  it('update locked document - global', async () => {
+    // Give locking ownership to another user
+    const globalDocInstance = await payload.create({
+      collection: lockedDocumentCollection,
+      data: {
+        _lastEdited: {
+          editedAt: new Date().toISOString(),
+          user: {
+            relationTo: 'users',
+            value: user2.id,
+          },
+        },
+        document: undefined,
+        globalSlug: menuSlug,
+        isLocked: true,
+      },
+    })
+
+    try {
+      await payload.updateGlobal({
+        data: {
+          globalText: 'updated global text',
+        },
+        slug: menuSlug,
+      })
+    } catch (error) {
+      expect(error).toBeInstanceOf(APIError)
+      expect(error.message).toMatch(/currently locked by another user and cannot be updated/)
+    }
+
+    // Should not allow update - expect data not to change
+    expect(menu.globalText).toEqual('global text')
+
+    await payload.delete({
+      collection: lockedDocumentCollection,
+      id: globalDocInstance.id,
+    })
+  })
+
+  // Try to update locked document (stale) (collection & global)
+
+  it('update stale locked document - collection', async () => {
+    const newPost2 = await payload.create({
+      collection: postsSlug,
+      data: {
+        text: 'new post 2',
+      },
+    })
+
+    // Subtract 3.5 minutes (210 seconds) from the current time
+    const pastEditedAt = new Date()
+    pastEditedAt.setMinutes(pastEditedAt.getMinutes() - 3)
+    pastEditedAt.setSeconds(pastEditedAt.getSeconds() - 30)
+
+    // Give locking ownership to another user
+    const lockedDocInstance = await payload.create({
+      collection: lockedDocumentCollection,
+      data: {
+        _lastEdited: {
+          editedAt: pastEditedAt.toISOString(), // stale date
+          user: {
+            relationTo: 'users',
+            value: user2.id,
+          },
+        },
+        document: {
+          relationTo: 'posts',
+          value: newPost2.id,
+        },
+        globalSlug: undefined,
+        isLocked: true,
+      },
+    })
+
+    const updateLockedDoc = await payload.update({
+      collection: postsSlug,
+      data: {
+        text: 'updated post 2',
+      },
+      id: newPost2.id,
+    })
+
+    // Should allow update since editedAt date is past expiration duration.
+    // Therefore the document is considered stale
+    expect(updateLockedDoc.text).toEqual('updated post 2')
+
+    // Check to make sure the document does not exist in payload-locked-documents anymore
+    try {
+      await payload.findByID({
+        collection: lockedDocumentCollection,
+        id: lockedDocInstance.id,
+      })
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFound)
+    }
+
+    const findDocs = await payload.find({
+      collection: lockedDocumentCollection,
+      where: {
+        id: { equals: lockedDocInstance.id },
+      },
+    })
+
+    expect(findDocs.docs).toHaveLength(0)
+
+    // Updating a document with the local API should not keep a stored doc
+    // in the payload-locked-documents collection
+  })
+
+  it('update stale locked document - global', async () => {
+    // Subtract 5.5 minutes (330 seconds) from the current time
+    const pastEditedAt = new Date()
+    pastEditedAt.setMinutes(pastEditedAt.getMinutes() - 5)
+    pastEditedAt.setSeconds(pastEditedAt.getSeconds() - 30)
+
+    // Give locking ownership to another user
+    const lockedGlobalInstance = await payload.create({
+      collection: lockedDocumentCollection,
+      data: {
+        _lastEdited: {
+          editedAt: pastEditedAt.toISOString(), // stale date
+          user: {
+            relationTo: 'users',
+            value: user2.id,
+          },
+        },
+        document: undefined,
+        globalSlug: menuSlug,
+        isLocked: true,
+      },
+    })
+
+    const updateGlobalLockedDoc = await payload.updateGlobal({
+      data: {
+        globalText: 'global text 2',
+      },
+      slug: menuSlug,
+    })
+
+    // Should allow update since editedAt date is past expiration duration.
+    // Therefore the document is considered stale
+    expect(updateGlobalLockedDoc.globalText).toEqual('global text 2')
+
+    // Check to make sure the document does not exist in payload-locked-documents anymore
+    try {
+      await payload.findByID({
+        collection: lockedDocumentCollection,
+        id: lockedGlobalInstance.id,
+      })
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFound)
+    }
+
+    const findDocs = await payload.find({
+      collection: lockedDocumentCollection,
+      where: {
+        id: { equals: lockedGlobalInstance.id },
+      },
+    })
+
+    expect(findDocs.docs).toHaveLength(0)
+
+    // Updating a document with the local API should not keep a stored doc
+    // in the payload-locked-documents collection
+  })
+
+  // Try to delete locked document (collection)
+  it('delete locked document - collection', async () => {
+    const newPost3 = await payload.create({
+      collection: postsSlug,
+      data: {
+        text: 'new post 3',
+      },
+    })
+
+    // Give locking ownership to another user
+    await payload.create({
+      collection: lockedDocumentCollection,
+      data: {
+        _lastEdited: {
+          editedAt: new Date().toISOString(),
+          user: {
+            relationTo: 'users',
+            value: user2.id,
+          },
+        },
+        document: {
+          relationTo: 'posts',
+          value: newPost3.id,
+        },
+        globalSlug: undefined,
+        isLocked: true,
+      },
+    })
+
+    try {
+      await payload.delete({
+        collection: postsSlug,
+        id: newPost3.id,
+      })
+    } catch (error) {
+      expect(error).toBeInstanceOf(APIError)
+      expect(error.message).toMatch(/currently locked and cannot be deleted/)
+    }
+  })
+
+  // Try to delete locked document (stale) (collection)
+
+  it('delete stale locked document - collection', async () => {
+    const newPost4 = await payload.create({
+      collection: postsSlug,
+      data: {
+        text: 'new post 4',
+      },
+    })
+
+    // Subtract 3.5 minutes (210 seconds) from the current time
+    const pastEditedAt = new Date()
+    pastEditedAt.setMinutes(pastEditedAt.getMinutes() - 3)
+    pastEditedAt.setSeconds(pastEditedAt.getSeconds() - 30)
+
+    // Give locking ownership to another user
+    const lockedDocInstance = await payload.create({
+      collection: lockedDocumentCollection,
+      data: {
+        _lastEdited: {
+          editedAt: pastEditedAt.toISOString(), // stale date
+          user: {
+            relationTo: 'users',
+            value: user2.id,
+          },
+        },
+        document: {
+          relationTo: 'posts',
+          value: newPost4.id,
+        },
+        globalSlug: undefined,
+        isLocked: true,
+      },
+    })
+
+    await payload.delete({
+      collection: postsSlug,
+      id: newPost4.id,
+    })
+
+    const findPostDocs = await payload.find({
+      collection: postsSlug,
+      where: {
+        id: { equals: newPost4.id },
+      },
+    })
+
+    expect(findPostDocs.docs).toHaveLength(0)
+
+    // Check to make sure the document does not exist in payload-locked-documents anymore
+    try {
+      await payload.findByID({
+        collection: lockedDocumentCollection,
+        id: lockedDocInstance.id,
+      })
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFound)
+    }
+
+    const findDocs = await payload.find({
+      collection: lockedDocumentCollection,
+      where: {
+        id: { equals: lockedDocInstance.id },
+      },
+    })
+
+    expect(findDocs.docs).toHaveLength(0)
   })
 })
