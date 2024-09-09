bc2763f8dcc02da4c8376a22ddadd5855cfa835e
PatrikKozak <patrik@payloadcms.com>
2024-09-09 11:52:13 -0400

    chore: revert link button changes in richtext-slate

--- packages/richtext-slate/src/field/elements/link/Button/index.tsx
+++ packages/richtext-slate/src/field/elements/link/Button/index.tsx
@@ -73,39 +73,37 @@ export const LinkButton: React.FC = () => {
 
   const fields = richTextComponentMap.get(linkFieldsSchemaPath)
 
-  const handleClick = async () => {
-    if (isElementActive(editor, 'link')) {
-      unwrapLink(editor)
-    } else {
-      openModal(drawerSlug)
-      const isCollapsed = editor.selection && Range.isCollapsed(editor.selection)
-
-      if (!isCollapsed) {
-        const data = {
-          text: editor.selection ? Editor.string(editor, editor.selection) : '',
-        }
-
-        const { state } = await getFormState({
-          apiRoute: config.routes.api,
-          body: {
-            data,
-            operation: 'update',
-            schemaPath: `${schemaPath}.${linkFieldsSchemaPath}`,
-          },
-          serverURL: config.serverURL,
-        })
-
-        setInitialState(state)
-      }
-    }
-  }
-
   return (
     <Fragment>
       <ElementButton
         className="link"
         format="link"
-        onClick={void handleClick}
+        onClick={async () => {
+          if (isElementActive(editor, 'link')) {
+            unwrapLink(editor)
+          } else {
+            openModal(drawerSlug)
+            const isCollapsed = editor.selection && Range.isCollapsed(editor.selection)
+
+            if (!isCollapsed) {
+              const data = {
+                text: editor.selection ? Editor.string(editor, editor.selection) : '',
+              }
+
+              const { state } = await getFormState({
+                apiRoute: config.routes.api,
+                body: {
+                  data,
+                  operation: 'update',
+                  schemaPath: `${schemaPath}.${linkFieldsSchemaPath}`,
+                },
+                serverURL: config.serverURL,
+              })
+
+              setInitialState(state)
+            }
+          }
+        }}
         tooltip={t('fields:addLink')}
       >
         <LinkIcon />
