0c563ebd7350e03d513dbbb2b2ccb4632a6eefa3
Sasha <enjoythepain1337@gmail.com>
2024-09-10 15:44:38 +0300

    fix(db-postgres): querying on array wtihin a relationship field (#8152)
    ## Description
    
    Fixes https://github.com/payloadcms/payload/issues/6037
    
    - [x] I have read and understand the
    [CONTRIBUTING.md](https://github.com/payloadcms/payload/blob/main/CONTRIBUTING.md)
    document in this repository.
    
    ## Type of change
    
    <!-- Please delete options that are not relevant. -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist:
    
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] Existing test suite passes locally with my changes

--- test/relationships/payload-types.ts
+++ test/relationships/payload-types.ts
@@ -24,6 +24,8 @@ export interface Config {
     movieReviews: MovieReview;
     'polymorphic-relationships': PolymorphicRelationship;
     tree: Tree;
+    pages: Page;
+    'rels-to-pages': RelsToPage;
     users: User;
     'payload-preferences': PayloadPreference;
     'payload-migrations': PayloadMigration;
@@ -221,6 +223,31 @@ export interface Tree {
   id: string;
   text?: string | null;
   parent?: (string | null) | Tree;
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "pages".
+ */
+export interface Page {
+  id: string;
+  menu?:
+    | {
+        label?: string | null;
+        id?: string | null;
+      }[]
+    | null;
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "rels-to-pages".
+ */
+export interface RelsToPage {
+  id: string;
+  page?: (string | null) | Page;
   updatedAt: string;
   createdAt: string;
 }
