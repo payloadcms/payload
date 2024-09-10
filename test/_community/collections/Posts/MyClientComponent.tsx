12a30a05850cb5c703d9e109dec51f5275942609
Jacob Fletcher <jacobsfletch@gmail.com>
2024-09-10 10:42:22 -0400

    fix: extends server props onto field component types (#8155)

--- test/_community/collections/Posts/MyClientComponent.tsx
+++ test/_community/collections/Posts/MyClientComponent.tsx
@@ -1,9 +1,9 @@
 'use client'
-import type { TextFieldClientComponent } from 'payload'
+import type { TextFieldLabelClientComponent } from 'payload'
 
 import React from 'react'
 
-export const MyClientComponent: TextFieldClientComponent = (props) => {
+export const MyClientComponent: TextFieldLabelClientComponent = (props) => {
   const { field } = props
   return <p>{`The name of this field is: ${field.name}`}</p>
 }
--- test/_community/collections/Posts/MyServerComponent.tsx
+++ test/_community/collections/Posts/MyServerComponent.tsx
@@ -1,9 +1,13 @@
-import type { TextFieldServerComponent } from 'payload'
+import type { TextFieldLabelServerComponent } from 'payload'
 
 import React from 'react'
 
-export const MyServerComponent: TextFieldServerComponent = (props) => {
+export const MyServerComponent: TextFieldLabelServerComponent = (props) => {
   const { field } = props
 
-  return <p>{`The name of this field is: ${field.name}`}</p>
+  return (
+    <div>
+      <p>{`The name of this field is: ${field.name}`}</p>
+    </div>
+  )
 }
