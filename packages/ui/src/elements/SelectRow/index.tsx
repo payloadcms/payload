ef2e8d68aa02916c8a2c31fd9da45bdf18e98392
Paul Popus <paul@nouance.io>
2024-09-12 15:35:03 -0600

    remove unused hook

--- packages/ui/src/elements/SelectRow/index.tsx
+++ packages/ui/src/elements/SelectRow/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useMemo } from 'react'
+import React from 'react'
 
 import { useTableCell } from '../../elements/Table/TableCellProvider/index.js'
 import { CheckboxInput } from '../../fields/Checkbox/Input.js'
