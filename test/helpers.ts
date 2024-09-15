7905df21dad5022adebb47b6a24b12ca5ca062ac
Tsemach Hadad <tsemachh@gmail.com>
2024-09-15 14:47:06 +0300

    Merge PNPM deps upgrade

--- test/helpers.ts
+++ test/helpers.ts
@@ -198,9 +198,7 @@ export async function openNav(page: Page): Promise<void> {
   // check to see if the nav is already open and if not, open it
   // use the `--nav-open` modifier class to check if the nav is open
   // this will prevent clicking nav links that are bleeding off the screen
-  if (await page.locator('.template-default.template-default--nav-open').isVisible()) {
-    return
-  }
+  if (await page.locator('.template-default.template-default--nav-open').isVisible()) {return}
   // playwright: get first element with .nav-toggler which is VISIBLE (not hidden), could be 2 elements with .nav-toggler on mobile and desktop but only one is visible
   await page.locator('.nav-toggler >> visible=true').click()
   await expect(page.locator('.template-default.template-default--nav-open')).toBeVisible()
@@ -223,9 +221,7 @@ export async function openCreateDocDrawer(page: Page, fieldSelector: string): Pr
 }
 
 export async function closeNav(page: Page): Promise<void> {
-  if (!(await page.locator('.template-default.template-default--nav-open').isVisible())) {
-    return
-  }
+  if (!(await page.locator('.template-default.template-default--nav-open').isVisible())) {return}
   await page.locator('.nav-toggler >> visible=true').click()
   await expect(page.locator('.template-default.template-default--nav-open')).toBeHidden()
 }
