302f1b557bad7e4ed1b974b0198af40d2abb19ee
Tsemach Hadad <tsemachh@gmail.com>
2024-09-16 12:24:37 +0300

    Export LivePreviewView and LivePreviewClient and useLivePreviewContext

--- packages/next/src/exports/views.ts
+++ packages/next/src/exports/views.ts
@@ -1,5 +1,6 @@
 export { DefaultEditView as EditView } from '../views/Edit/Default/index.js'
 export { DefaultListView as ListView } from '../views/List/Default/index.js'
+export { useLivePreviewContext } from '../views/LivePreview/Context/context'
 export { LivePreviewClient } from '../views/LivePreview/index.client.js'
 export { LivePreviewView } from '../views/LivePreview/index.js'
 export { NotFoundPage } from '../views/NotFound/index.js'
