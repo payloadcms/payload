37e1adfa5c17b1b9e3d9d7c615525392ed5d9c0c
Sasha <enjoythepain1337@gmail.com>
2024-09-18 19:39:58 +0300

    fix: findByID adjust type to null if disableErrors: true is passed (#8282)
    Fixes https://github.com/payloadcms/payload/issues/8280
    
    Now, the result type of this operation:
    ```ts
    const post = await payload.findByID({
      collection: "posts",
      id,
      disableErrors: true
    })
    ```
    is `Post | null` instead of `Post` when `disableErrors: true` is passed
    
    Adds test for the `disableErrors` property and docs.

--- packages/payload/src/collections/operations/local/findByID.ts
+++ packages/payload/src/collections/operations/local/findByID.ts
@@ -6,7 +6,7 @@ import { APIError } from '../../../errors/index.js'
 import { createLocalReq } from '../../../utilities/createLocalReq.js'
 import { findByIDOperation } from '../findByID.js'
 
-export type Options<TSlug extends CollectionSlug> = {
+export type Options<TSlug extends CollectionSlug = CollectionSlug> = {
   collection: TSlug
   /**
    * context, which will then be passed to req.context, which can be read by hooks
@@ -26,10 +26,14 @@ export type Options<TSlug extends CollectionSlug> = {
   user?: Document
 }
 
-export default async function findByIDLocal<TSlug extends CollectionSlug>(
+export default async function findByIDLocal<TOptions extends Options>(
   payload: Payload,
-  options: Options<TSlug>,
-): Promise<DataFromCollectionSlug<TSlug>> {
+  options: TOptions,
+): Promise<
+  TOptions['disableErrors'] extends true
+    ? DataFromCollectionSlug<TOptions['collection']> | null
+    : DataFromCollectionSlug<TOptions['collection']>
+> {
   const {
     id,
     collection: collectionSlug,
@@ -50,7 +54,7 @@ export default async function findByIDLocal<TSlug extends CollectionSlug>(
     )
   }
 
-  return findByIDOperation<TSlug>({
+  return findByIDOperation<TOptions['collection']>({
     id,
     collection,
     currentDepth,
