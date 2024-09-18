c656ebc59d95d6ba2e3f50ba3b617176e4fac2f5
Tsemach Hadad <tsemachh@gmail.com>
2024-09-18 12:35:33 +0300

    Demonstrate the bug in payload traverseRichText

--- test/live-preview/collections/Pages.ts
+++ test/live-preview/collections/Pages.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from 'payload'
 
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { BlocksFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
 import { slateEditor } from '@payloadcms/richtext-slate'
 
 import { Archive } from '../blocks/ArchiveBlock/index.js'
@@ -85,7 +85,14 @@ export const Pages: CollectionConfig = {
               label: 'Rich Text — Lexical',
               type: 'richText',
               name: 'richTextLexical',
-              editor: lexicalEditor({}),
+              editor: lexicalEditor({
+                features: ({ defaultFeatures }) => [
+                  ...defaultFeatures,
+                  BlocksFeature({
+                    blocks: [CallToAction, Content, MediaBlock, Archive],
+                  }),
+                ],
+              }),
             },
             {
               name: 'relationshipAsUpload',
