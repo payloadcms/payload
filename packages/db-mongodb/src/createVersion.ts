70973b8dfe1cf937ff09931932bccf9ae3b839ac
Dan Ribbens <dan.ribbens@gmail.com>
2024-09-26 13:30:15 -0400

    fix: unset latest versions where id is string or object id

--- packages/db-mongodb/src/createVersion.ts
+++ packages/db-mongodb/src/createVersion.ts
@@ -1,3 +1,5 @@
+import ObjectIdImport from 'bson-objectid'
+import { isValidObjectId } from 'mongoose'
 import {
   buildVersionCollectionFields,
   type CreateVersion,
@@ -10,6 +12,8 @@ import type { MongooseAdapter } from './index.js'
 import { sanitizeRelationshipIDs } from './utilities/sanitizeRelationshipIDs.js'
 import { withSession } from './withSession.js'
 
+const ObjectId = (ObjectIdImport.default ||
+  ObjectIdImport) as unknown as typeof ObjectIdImport.default
 export const createVersion: CreateVersion = async function createVersion(
   this: MongooseAdapter,
   {
@@ -47,6 +51,23 @@ export const createVersion: CreateVersion = async function createVersion(
 
   const [doc] = await VersionModel.create([data], options, req)
 
+  const parentQuery = {
+    or: [
+      {
+        parent: {
+          $eq: data.parent,
+        },
+      },
+    ],
+  }
+  if (isValidObjectId(data.parent)) {
+    parentQuery.or.push({
+      parent: {
+        $eq: ObjectId(data.parent as string),
+      },
+    })
+  }
+
   await VersionModel.updateMany(
     {
       $and: [
@@ -56,9 +77,7 @@ export const createVersion: CreateVersion = async function createVersion(
           },
         },
         {
-          parent: {
-            $eq: data.parent,
-          },
+          parentQuery,
         },
         {
           latest: {
