3e02537ffe90fe3600d697fc323dfbe4cbf551ab
Sasha <enjoythepain1337@gmail.com>
2024-09-27 22:44:10 +0300

    upd template

--- templates/with-vercel-postgres/src/payload.config.ts
+++ templates/with-vercel-postgres/src/payload.config.ts
@@ -32,7 +32,7 @@ export default buildConfig({
   plugins: [
     vercelBlobStorage({
       collections: {
-        [Media.slug]: true,
+        slug: true,
       },
       token: process.env.BLOB_READ_WRITE_TOKEN || '',
     }),
