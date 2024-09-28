c3a2ea0bc1592ad33f26b8d7c6f27ed2109508ed
Alessio Gravili <alessio@gravili.de>
2024-09-28 18:16:45 +0200

    fix(richtext-lexical): tables: fix table row/column index when resizing merged cells
    Backports https://github.com/facebook/lexical/pull/6630/files

--- packages/richtext-lexical/src/features/experimental_table/client/plugins/TableCellResizerPlugin/index.tsx
+++ packages/richtext-lexical/src/features/experimental_table/client/plugins/TableCellResizerPlugin/index.tsx
@@ -154,7 +154,8 @@ function TableCellResizer({ editor }: { editor: LexicalEditor }): JSX.Element {
 
           const tableNode = $getTableNodeFromLexicalNodeOrThrow(tableCellNode)
 
-          const tableRowIndex = $getTableRowIndexFromTableCellNode(tableCellNode)
+          const tableRowIndex =
+            $getTableRowIndexFromTableCellNode(tableCellNode) + tableCellNode.getRowSpan() - 1
 
           const tableRows = tableNode.getChildren()
 
@@ -218,7 +219,7 @@ function TableCellResizer({ editor }: { editor: LexicalEditor }): JSX.Element {
     for (let row = 0; row < tableMap.length; row++) {
       for (let column = 0; column < tableMap[row].length; column++) {
         if (tableMap[row][column].cell === tableCellNode) {
-          return column
+          return column + tableCellNode.getColSpan() - 1
         }
       }
     }
