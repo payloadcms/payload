4424904f5848e17c6ef11867db7ed6101a7b46fe
Jacob Fletcher <jacobsfletch@gmail.com>
2024-10-01 16:48:50 -0400

    excludes examples until released

--- examples/hierarchy/src/app/(payload)/layout.tsx
+++ examples/hierarchy/src/app/(payload)/layout.tsx
@@ -1,32 +1,16 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import type { ClientServerFunction } from 'payload'
-
-import config from '@payload-config'
+import configPromise from '@payload-config'
+import '@payloadcms/next/css'
 import { RootLayout } from '@payloadcms/next/layouts'
-import { handleServerFunctions } from '@payloadcms/next/utilities'
 import React from 'react'
 
-import { importMap } from './admin/importMap.js'
 import './custom.scss'
 
 type Args = {
   children: React.ReactNode
 }
 
-const serverFunctions: ClientServerFunction = async function (args) {
-  'use server'
-  return await handleServerFunctions({
-    ...args,
-    config,
-    importMap,
-  })
-}
-
-const Layout = ({ children }: Args) => (
-  <RootLayout config={config} importMap={importMap} serverFunctions={serverFunctions}>
-    {children}
-  </RootLayout>
-)
+const Layout = ({ children }: Args) => <RootLayout config={configPromise}>{children}</RootLayout>
 
 export default Layout
