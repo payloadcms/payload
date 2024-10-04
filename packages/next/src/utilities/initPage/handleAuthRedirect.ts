a5d7fc996dbb5476830a2229b75c2d5d00ccf9e8
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-10-04 13:33:20 -0400

    fix: ensures users can get to the verify view

--- packages/next/src/utilities/initPage/handleAuthRedirect.ts
+++ packages/next/src/utilities/initPage/handleAuthRedirect.ts
@@ -2,19 +2,12 @@ import { formatAdminURL } from '@payloadcms/ui/shared'
 import { redirect } from 'next/navigation.js'
 import * as qs from 'qs-esm'
 
-import { isAdminAuthRoute, isAdminRoute } from './shared.js'
-
-export const handleAuthRedirect = ({
-  config,
-  redirectUnauthenticatedUser,
-  route,
-  searchParams,
-}: {
+type Args = {
   config
-  redirectUnauthenticatedUser: boolean | string
   route: string
   searchParams: { [key: string]: string | string[] }
-}) => {
+}
+export const handleAuthRedirect = ({ config, route, searchParams }: Args) => {
   const {
     admin: {
       routes: { login: loginRouteFromConfig },
@@ -22,36 +15,27 @@ export const handleAuthRedirect = ({
     routes: { admin: adminRoute },
   } = config
 
-  if (!isAdminAuthRoute({ adminRoute, config, route })) {
-    if (searchParams && 'redirect' in searchParams) {
-      delete searchParams.redirect
-    }
-
-    const redirectRoute = encodeURIComponent(
-      route + Object.keys(searchParams ?? {}).length
-        ? `${qs.stringify(searchParams, { addQueryPrefix: true })}`
-        : undefined,
-    )
-
-    const adminLoginRoute = formatAdminURL({ adminRoute, path: loginRouteFromConfig })
-
-    const customLoginRoute =
-      typeof redirectUnauthenticatedUser === 'string' ? redirectUnauthenticatedUser : undefined
-
-    const loginRoute = isAdminRoute({ adminRoute, config, route })
-      ? adminLoginRoute
-      : customLoginRoute || loginRouteFromConfig
-
-    const parsedLoginRouteSearchParams = qs.parse(loginRoute.split('?')[1] ?? '')
-
-    const searchParamsWithRedirect = `${qs.stringify(
-      {
-        ...parsedLoginRouteSearchParams,
-        ...(redirectRoute ? { redirect: redirectRoute } : {}),
-      },
-      { addQueryPrefix: true },
-    )}`
-
-    redirect(`${loginRoute.split('?')[0]}${searchParamsWithRedirect}`)
+  if (searchParams && 'redirect' in searchParams) {
+    delete searchParams.redirect
   }
+
+  const redirectRoute = encodeURIComponent(
+    route + Object.keys(searchParams ?? {}).length
+      ? `${qs.stringify(searchParams, { addQueryPrefix: true })}`
+      : undefined,
+  )
+
+  const loginRoute = formatAdminURL({ adminRoute, path: loginRouteFromConfig })
+
+  const parsedLoginRouteSearchParams = qs.parse(loginRoute.split('?')[1] ?? '')
+
+  const searchParamsWithRedirect = `${qs.stringify(
+    {
+      ...parsedLoginRouteSearchParams,
+      ...(redirectRoute ? { redirect: redirectRoute } : {}),
+    },
+    { addQueryPrefix: true },
+  )}`
+
+  redirect(`${loginRoute.split('?')[0]}${searchParamsWithRedirect}`)
 }
