a3bf938862f54f858458c538596d3fa199da7936
Sasha <64744993+r1tsuu@users.noreply.github.com>
2024-10-10 16:10:11 +0300

    chore: do not use withSentryConfig for dev/e2e next config if not needed (#8629)
    This fixes this error when executing particular e2e test with playwright
    extension
    <img width="347" alt="image"
    src="https://github.com/user-attachments/assets/5f7608fc-7896-4014-b312-a0c36f21b7d1">

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NEXT_PUBLIC_SENTRY_DSN) {
     await import('./sentry.server.config.js')
   }
 }
