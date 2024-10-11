8daac4e6706fb8c29eb971cc90e7ac7546e27138
Sasha <64744993+r1tsuu@users.noreply.github.com>
2024-10-11 17:01:21 +0300

    fix: properly store timestamps in versions (#8646)
    This PR makes a more clear gap between `version_createdAt` /
    `version_updatedAt` and `createdAt` / `updatedAt` columns / fields in
    mongodb.
    
    - `createdAt` - This should be a new value in a new version. Before this
    change it was the same all the time. Should remain the same on autosave.
    - The same for `updatedAt`, but it should be updated on every change
    (including autosave)
    - `version_createdAt` - Should remain equal to `createdAt` from the
    parent collection / table
    - `version_updatedAt` - On a latest version it makes sense this be the
    same as `updatedAt` from the parent collection / table, as all the
    `version_*` fields should be just synced with it

--- packages/drizzle/src/createVersion.ts
+++ packages/drizzle/src/createVersion.ts
@@ -13,10 +13,12 @@ export async function createVersion<T extends TypeWithID>(
   {
     autosave,
     collectionSlug,
+    createdAt,
     parent,
     publishedLocale,
     req = {} as PayloadRequest,
     snapshot,
+    updatedAt,
     versionData,
   }: CreateVersionArgs<T>,
 ) {
@@ -33,15 +35,13 @@ export async function createVersion<T extends TypeWithID>(
 
   const data: Record<string, unknown> = {
     autosave,
+    createdAt,
     latest: true,
     parent,
     publishedLocale,
     snapshot,
+    updatedAt,
     version,
-  }
-
-  if ('createdAt' in version) {
-    data.createdAt = version.createdAt
   }
 
   const result = await upsertRow<TypeWithVersion<T>>({
