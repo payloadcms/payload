d15032cac1d5becfee7da90a1fda93542c1a6b4d
Paul Popus <paul@nouance.io>
2024-10-14 10:18:33 -0600

    change lib to scss file

--- packages/ui/src/elements/DatePicker/DatePicker.tsx
+++ packages/ui/src/elements/DatePicker/DatePicker.tsx
@@ -12,7 +12,7 @@ import { CalendarIcon } from '../../icons/Calendar/index.js'
 import { XIcon } from '../../icons/X/index.js'
 import { useTranslation } from '../../providers/Translation/index.js'
 import { getFormattedLocale } from './getFormattedLocale.js'
-import './library.css'
+import './library.scss'
 import './index.scss'
 
 const baseClass = 'date-time-picker'
