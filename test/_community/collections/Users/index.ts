7426e73f811ec7fc3d88661812bc700ec6ab90e2
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-10-16 10:58:29 -0400

    chore: adds users collection

---
+++ test/_community/collections/Users/index.ts
@@ -0,0 +1,7 @@
+import type { CollectionConfig } from 'payload'
+
+export const UsersCollection: CollectionConfig = {
+  slug: 'users',
+  auth: true,
+  fields: [],
+}
--- test/_community/config.ts
+++ test/_community/config.ts
@@ -6,13 +6,16 @@ import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
 import { devUser } from '../credentials.js'
 import { MediaCollection } from './collections/Media/index.js'
 import { PostsCollection, postsSlug } from './collections/Posts/index.js'
+import { UsersCollection } from './collections/Users/index.js'
 import { MenuGlobal } from './globals/Menu/index.js'
+
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfigWithDefaults({
   // ...extend config here
   collections: [
+    UsersCollection,
     PostsCollection,
     {
       slug: 'simple',
@@ -26,6 +29,7 @@ export default buildConfigWithDefaults({
     MediaCollection,
   ],
   admin: {
+    user: 'users',
     importMap: {
       baseDir: path.resolve(dirname),
     },
