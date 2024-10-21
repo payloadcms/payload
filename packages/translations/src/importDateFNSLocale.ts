d5a792e0b8f2f5836875da209de095fc81d3312f
Gregor Gabrič <gregor.gabric@outlook.com>
2024-10-21 21:59:14 +0200

    added sl locale

--- packages/translations/src/importDateFNSLocale.ts
+++ packages/translations/src/importDateFNSLocale.ts
@@ -96,6 +96,10 @@ export const importDateFNSLocale = async (locale: string): Promise<Locale> => {
       result = (await import('date-fns/locale/sk')).sk
 
       break
+    case 'sl':
+      result = (await import('date-fns/locale/sl')).sl
+
+      break
     case 'sv':
       result = (await import('date-fns/locale/sv')).sv
 
@@ -120,9 +124,9 @@ export const importDateFNSLocale = async (locale: string): Promise<Locale> => {
       result = (await import('date-fns/locale/zh-CN')).zhCN
 
       break
+
     case 'zh-TW':
       result = (await import('date-fns/locale/zh-TW')).zhTW
-
       break
   }
 
