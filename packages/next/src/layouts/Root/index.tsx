6cdb576abe0fd513f548af071a7ff41ca739c112
Elliot DeNolf <denolfe@gmail.com>
2024-10-21 21:50:51 -0400

    feat(next): bump next.js to 15.0.0-rc.1

--- packages/next/src/layouts/Root/index.tsx
+++ packages/next/src/layouts/Root/index.tsx
@@ -66,7 +66,7 @@ export const RootLayout = async ({
       dependencyVersions: {
         next: {
           required: false,
-          version: '>=15.0.0-canary.173',
+          version: '>=15.0.0-rc.1',
         },
         react: {
           customVersionParser: customReactVersionParser,
