c7cd6e3fbb8075f50744096c8ec71a5eb27d32d7
Jacob Fletcher <jacobsfletch@gmail.com>
2024-10-28 10:10:57 -0400

    fix upload field allowCreate logic

--- packages/ui/src/fields/Upload/index.tsx
+++ packages/ui/src/fields/Upload/index.tsx
@@ -64,7 +64,7 @@ export function UploadComponent(props: UploadFieldClientProps) {
     readOnlyFromField ||
     formProcessing ||
     formInitializing ||
-    !allowCreate
+    allowCreate === false
 
   return (
     <UploadInput
