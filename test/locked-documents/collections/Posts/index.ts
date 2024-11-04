114777f09fa701d990205f0c87464ab77363a754
Patrik Kozak <patrik@payloadcms.com>
2024-11-04 16:05:58 -0500

    chore: adds e2e test

--- test/locked-documents/collections/Posts/index.ts
+++ test/locked-documents/collections/Posts/index.ts
@@ -15,6 +15,20 @@ export const PostsCollection: CollectionConfig = {
       name: 'text',
       type: 'text',
     },
+    {
+      name: 'documentLoaded',
+      label: 'Document loaded',
+      type: 'date',
+      admin: {
+        date: {
+          displayFormat: 'yyyy-MM-dd HH:mm:ss',
+        },
+        readOnly: true,
+        components: {
+          Field: '/collections/Posts/fields/DocumentLoaded.tsx#DocumentLoaded',
+        },
+      },
+    },
   ],
   versions: {
     drafts: true,
