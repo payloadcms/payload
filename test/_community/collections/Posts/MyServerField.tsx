e50a899d92efb4a1727d5d9d279c820ff0b46b8b
Jacob Fletcher <jacobsfletch@gmail.com>
2024-11-05 13:00:58 -0500

    fix custom server field in _community

--- test/_community/collections/Posts/MyServerField.tsx
+++ test/_community/collections/Posts/MyServerField.tsx
@@ -6,29 +6,26 @@ import React from 'react'
 export const MyServerFieldComponent: TextFieldServerComponent = (args) => {
   const {
     clientField,
-    fieldState,
     indexPath,
     parentPath,
     parentSchemaPath,
     path,
-    permissions,
     schemaPath,
     siblingData,
+    value,
   } = args
 
   return (
     <React.Fragment>
-      <h1>{String(fieldState.value)}</h1>
+      <h1>{String(value)}</h1>
       <p>{JSON.stringify(siblingData)}</p>
       <p>{path}</p>
       <TextField
         field={clientField}
-        fieldState={fieldState}
         indexPath={indexPath}
         parentPath={parentPath}
         parentSchemaPath={parentSchemaPath}
         path={path}
-        permissions={permissions}
         schemaPath={schemaPath}
       />
     </React.Fragment>
