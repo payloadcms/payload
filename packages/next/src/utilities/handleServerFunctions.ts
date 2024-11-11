dc8b1fe023ca54363c2f84b4143c246962a8fa15
Jacob Fletcher <jacobsfletch@gmail.com>
2024-11-11 12:12:31 -0500

    removes old custom server function reduce

--- packages/next/src/utilities/handleServerFunctions.ts
+++ packages/next/src/utilities/handleServerFunctions.ts
@@ -4,17 +4,9 @@ import { buildFormStateHandler } from '@payloadcms/ui/utilities/buildFormState'
 import { buildTableStateHandler } from '@payloadcms/ui/utilities/buildTableState'
 
 import { renderDocumentHandler } from '../views/Document/handleServerFunction.js'
+import { renderDocumentSlotsHandler } from '../views/Document/renderDocumentSlots.js'
 import { renderListHandler } from '../views/List/handleServerFunction.js'
 import { initReq } from './initReq.js'
-import { renderDocumentSlotsHandler } from '../views/Document/renderDocumentSlots.js'
-
-const defaultFunctions = {
-  'form-state': buildFormStateHandler as any as ServerFunction,
-  'render-document': renderDocumentHandler as any as ServerFunction,
-  'render-document-slots': renderDocumentSlotsHandler as any as ServerFunction,
-  'render-list': renderListHandler as any as ServerFunction,
-  'table-state': buildTableStateHandler as any as ServerFunction,
-}
 
 export const handleServerFunctions: ServerFunctionHandler = async (args) => {
   const { name: fnKey, args: fnArgs, config: configPromise, importMap } = args
@@ -27,14 +19,12 @@ export const handleServerFunctions: ServerFunctionHandler = async (args) => {
     req,
   }
 
-  const serverFunctions: {
-    [key: string]: ServerFunction
-  } = {
-    ...defaultFunctions,
-    ...req.payload?.config?.admin?.serverFunctions?.reduce((acc, fnConfig) => {
-      acc[fnConfig.name] = fnConfig.fn
-      return acc
-    }, {}),
+  const serverFunctions = {
+    'form-state': buildFormStateHandler as any as ServerFunction,
+    'render-document': renderDocumentHandler as any as ServerFunction,
+    'render-document-slots': renderDocumentSlotsHandler as any as ServerFunction,
+    'render-list': renderListHandler as any as ServerFunction,
+    'table-state': buildTableStateHandler as any as ServerFunction,
   }
 
   const fn = serverFunctions[fnKey]
