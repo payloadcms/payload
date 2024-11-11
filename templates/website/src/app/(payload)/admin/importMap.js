35ef5fc521fa7182b0e74eb4fdfb1994dae7b8cb
James <james@trbl.design>
2024-11-11 12:52:02 -0500

    chore: fixes website template build

--- templates/website/src/app/(payload)/admin/importMap.js
+++ templates/website/src/app/(payload)/admin/importMap.js
@@ -1,5 +1,5 @@
-import { RscEntryLexicalCell as RscEntryLexicalCell_9940332cc0fb632561dc34971af3b362 } from '@payloadcms/richtext-lexical/rscs'
-import { RscEntryLexicalField as RscEntryLexicalField_9940332cc0fb632561dc34971af3b362 } from '@payloadcms/richtext-lexical/rscs'
+import { RscEntryLexicalCell as RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { RscEntryLexicalField as RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
 import { InlineToolbarFeatureClient as InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { FixedToolbarFeatureClient as FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { HeadingFeatureClient as HeadingFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
@@ -21,10 +21,10 @@ import { default as default_1a7510af427896d367a49dbf838d2de6 } from '@/component
 import { default as default_8a7ab0eb7ab5c511aba12e68480bfe5e } from '@/components/BeforeLogin'
 
 export const importMap = {
-  '@payloadcms/richtext-lexical/rscs#RscEntryLexicalCell':
-    RscEntryLexicalCell_9940332cc0fb632561dc34971af3b362,
-  '@payloadcms/richtext-lexical/rscs#RscEntryLexicalField':
-    RscEntryLexicalField_9940332cc0fb632561dc34971af3b362,
+  '@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell':
+    RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
+  '@payloadcms/richtext-lexical/rsc#RscEntryLexicalField':
+    RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e,
   '@payloadcms/richtext-lexical/client#InlineToolbarFeatureClient':
     InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   '@payloadcms/richtext-lexical/client#FixedToolbarFeatureClient':
