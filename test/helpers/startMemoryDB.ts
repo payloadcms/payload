5c1d71b9b5938c23f18cc42b0960b0fdde46c4df
James <james@trbl.design>
2024-11-12 13:49:27 -0500

    chore: attempts to fix model overwriting

--- test/helpers/startMemoryDB.ts
+++ test/helpers/startMemoryDB.ts
@@ -1,4 +1,5 @@
 import { MongoMemoryReplSet } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
 
 // eslint-disable-next-line no-restricted-exports
 export default async () => {
@@ -10,10 +11,17 @@ export default async () => {
   process.env.NODE_OPTIONS = '--no-deprecation'
   process.env.DISABLE_PAYLOAD_HMR = 'true'
 
-  if (
-    (!process.env.PAYLOAD_DATABASE || process.env.PAYLOAD_DATABASE === 'mongodb') &&
-    !global._mongoMemoryServer
-  ) {
+  if (!process.env.PAYLOAD_DATABASE || process.env.PAYLOAD_DATABASE === 'mongodb') {
+    if (global._mongoMemoryServer) {
+      console.log('Stopping memorydb...')
+      await global._mongoMemoryServer.stop()
+      console.log('Stopped memorydb')
+    }
+
+    if (Object.keys(mongoose.models).length > 0) {
+      Object.keys(mongoose.models).map((model) => mongoose.deleteModel(model))
+    }
+
     const db = await MongoMemoryReplSet.create({
       replSet: {
         count: 3,
