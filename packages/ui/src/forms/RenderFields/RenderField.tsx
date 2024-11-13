1f4841e6ed6c52ebebc7badf65773c238f1a8a09
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-11-13 11:59:20 -0500

    chore: fix renderField in fieldSchemaToFormState props

--- packages/ui/src/forms/RenderFields/RenderField.tsx
+++ packages/ui/src/forms/RenderFields/RenderField.tsx
@@ -71,53 +71,69 @@ export function RenderField({
   }
 
   switch (clientFieldConfig.type) {
-    // named fields with subfields
     case 'array':
       return <ArrayField {...iterableFieldProps} field={clientFieldConfig} path={path} />
+
     case 'blocks':
       return <BlocksField {...iterableFieldProps} field={clientFieldConfig} path={path} />
-    // fields without subfields
+
     case 'checkbox':
       return <CheckboxField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'code':
       return <CodeField {...baseFieldProps} field={clientFieldConfig} path={path} />
 
     case 'collapsible':
       return <CollapsibleField {...iterableFieldProps} field={clientFieldConfig} path={path} />
+
     case 'date':
       return <DateTimeField {...baseFieldProps} field={clientFieldConfig} path={path} />
 
     case 'email':
       return <EmailField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'group':
       return <GroupField {...iterableFieldProps} field={clientFieldConfig} path={path} />
+
     case 'join':
       return <JoinField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'json':
       return <JSONField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'number':
       return <NumberField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'point':
       return <PointField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'radio':
       return <RadioGroupField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'relationship':
       return <RelationshipField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'richText':
       return <RichTextField {...baseFieldProps} field={clientFieldConfig} path={path} />
-    // unnamed fields with subfields
+
     case 'row':
       return <RowField {...iterableFieldProps} field={clientFieldConfig} />
+
     case 'select':
       return <SelectField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'tabs':
       return <TabsField {...iterableFieldProps} field={clientFieldConfig} path={path} />
+
     case 'text':
       return <TextField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'textarea':
       return <TextareaField {...baseFieldProps} field={clientFieldConfig} path={path} />
+
     case 'ui':
       return <UIField />
+
     case 'upload':
       return <UploadField {...baseFieldProps} field={clientFieldConfig} path={path} />
   }
