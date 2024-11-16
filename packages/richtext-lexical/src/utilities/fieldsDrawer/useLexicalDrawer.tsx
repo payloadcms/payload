65a58906c34a7cdf566ee85be31e4898982cdbbf
Alessio Gravili <alessio@gravili.de>
2024-11-16 14:38:12 -0700

    fix: reduce amount of times toggleModal from useLexicalDrawer rerenders. This then reduces the amount of times lexical blocks re-render when any modal is opened

--- packages/richtext-lexical/src/utilities/fieldsDrawer/useLexicalDrawer.tsx
+++ packages/richtext-lexical/src/utilities/fieldsDrawer/useLexicalDrawer.tsx
@@ -16,12 +16,7 @@ export const useLexicalDrawer = (slug: string, restoreLate?: boolean) => {
   const [selectionState, setSelectionState] = useState<BaseSelection | null>(null)
   const [wasOpen, setWasOpen] = useState<boolean>(false)
 
-  const {
-    closeModal: closeBaseModal,
-    isModalOpen: isBaseModalOpen,
-    modalState,
-    toggleModal: toggleBaseModal,
-  } = useModal()
+  const { closeModal: closeBaseModal, modalState, toggleModal: toggleBaseModal } = useModal()
 
   const storeSelection = useCallback(() => {
     editor.read(() => {
@@ -45,16 +40,17 @@ export const useLexicalDrawer = (slug: string, restoreLate?: boolean) => {
     //restoreSelection() // Should already be stored by the useEffect below
     closeBaseModal(slug)
   }, [closeBaseModal, slug])
+  const isModalOpen = modalState?.[slug]?.isOpen
 
   const toggleDrawer = useCallback(() => {
-    if (!isBaseModalOpen(slug)) {
+    if (!isModalOpen) {
       storeSelection()
     } else {
       restoreSelection()
     }
     setWasOpen(true)
     toggleBaseModal(slug)
-  }, [isBaseModalOpen, slug, storeSelection, toggleBaseModal, restoreSelection])
+  }, [slug, storeSelection, toggleBaseModal, restoreSelection, isModalOpen])
 
   // We need to handle drawer closing via a useEffect, as toggleDrawer / closeDrawer will not be triggered if the drawer
   // is closed by clicking outside of the drawer. This useEffect will handle everything.
