55d5edda6b4d5ef77a65009eeb19b85e2f6d55fc
Elliot DeNolf <denolfe@gmail.com>
2024-11-16 17:05:44 -0500

    ci: update pr action

--- scripts/generate-template-variations.ts
+++ scripts/generate-template-variations.ts
@@ -301,6 +301,7 @@ function log(message: string) {
 }
 function execSyncSafe(command: string, options?: Parameters<typeof execSync>[1]) {
   try {
+    console.log(`Executing: ${command}`)
     execSync(command, options)
   } catch (error) {
     if (error instanceof Error) {
@@ -317,5 +318,6 @@ function execSyncSafe(command: string, options?: Parameters<typeof execSync>[1])
     } else {
       console.error('An unexpected error occurred:', error)
     }
+    throw error
   }
 }
