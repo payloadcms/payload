8a226a4cccad01bef718dd204182cc0838b2fcb8
Violet Rosenzweig <rosenzweigv@leoncountyfl.gov>
2024-11-18 15:16:14 -0500

    fix(graphql): remove null entries from relationship field

--- packages/graphql/src/schema/buildObjectType.ts
+++ packages/graphql/src/schema/buildObjectType.ts
@@ -477,7 +477,7 @@ export function buildObjectType({
             }
 
             await Promise.all(resultPromises)
-            return results
+            return results.filter((result) => result !== null) // remove null entries for items that were unreadable
           }
 
           let id = value
