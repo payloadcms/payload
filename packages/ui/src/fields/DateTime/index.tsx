30947d217365d76751281aa671eff564b994d29f
Jacob Fletcher <jacobsfletch@gmail.com>
2024-11-18 10:03:26 -0500

    perf!: removes unnecessary field styles from initial page response (#9286)
    Optimizes initial page responses by removing unnecessary inline field
    styles that were being sent through the HTML response. The Client Config
    contains a large number of duplicates of the string:
    `"style\":{\"flex\":\"1 1 auto\"}`, one for every single field within
    the entirely of the config. This leads to hundreds or potentially
    thousands of instances of this same string, depending on the number of
    fields within the config itself. This is regardless of custom field
    widths being defined. Instead, we can do this entirely client-side,
    preventing this string from ever being transmitted over the network in
    the first place.
    
    ## Breaking Changes
    
    This only effects those who are importing Payload's field components
    into your own Custom Components or front-end application. The `width`
    prop no longer exists. It has been consolidated into the existing
    `style` prop. To migrate, simply move this prop as follows:
    
    ```diff
    import { TextInput } from '@payloadcms/ui
    
    export const MyCustomComponent = () => {
      return (
        <TextInput 
    -      width="60%"
           style={{
    +        width: "60%,
           }}
        />
      )
    }
    ```

--- packages/ui/src/fields/DateTime/index.tsx
+++ packages/ui/src/fields/DateTime/index.tsx
@@ -2,7 +2,7 @@
 import type { DateFieldClientComponent, DateFieldValidation } from 'payload'
 
 import { getTranslation } from '@payloadcms/translations'
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
 import { DatePickerField } from '../../elements/DatePicker/index.js'
 import { RenderCustomComponent } from '../../elements/RenderCustomComponent/index.js'
@@ -12,16 +12,17 @@ import { FieldLabel } from '../../fields/FieldLabel/index.js'
 import { useField } from '../../forms/useField/index.js'
 import { withCondition } from '../../forms/withCondition/index.js'
 import { useTranslation } from '../../providers/Translation/index.js'
-import { fieldBaseClass } from '../shared/index.js'
+import { mergeFieldStyles } from '../mergeFieldStyles.js'
 import './index.scss'
+import { fieldBaseClass } from '../shared/index.js'
 
 const baseClass = 'date-time-field'
 
 const DateTimeFieldComponent: DateFieldClientComponent = (props) => {
   const {
+    field,
     field: {
-      name,
-      admin: { className, date: datePickerProps, description, placeholder, style, width } = {},
+      admin: { className, date: datePickerProps, description, placeholder } = {},
       label,
       localized,
       required,
@@ -52,6 +53,8 @@ const DateTimeFieldComponent: DateFieldClientComponent = (props) => {
     validate: memoizedValidate,
   })
 
+  const styles = useMemo(() => mergeFieldStyles(field), [field])
+
   return (
     <div
       className={[
@@ -63,10 +66,7 @@ const DateTimeFieldComponent: DateFieldClientComponent = (props) => {
       ]
         .filter(Boolean)
         .join(' ')}
-      style={{
-        ...style,
-        width,
-      }}
+      style={styles}
     >
       <RenderCustomComponent
         CustomComponent={Label}
--- packages/ui/src/fields/Point/index.tsx
+++ packages/ui/src/fields/Point/index.tsx
@@ -2,7 +2,7 @@
 import type { PointFieldClientComponent, PointFieldValidation } from 'payload'
 
 import { getTranslation } from '@payloadcms/translations'
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 
 import { RenderCustomComponent } from '../../elements/RenderCustomComponent/index.js'
 import { FieldDescription } from '../../fields/FieldDescription/index.js'
@@ -11,16 +11,17 @@ import { FieldLabel } from '../../fields/FieldLabel/index.js'
 import { useField } from '../../forms/useField/index.js'
 import { withCondition } from '../../forms/withCondition/index.js'
 import { useTranslation } from '../../providers/Translation/index.js'
-import { fieldBaseClass } from '../shared/index.js'
+import { mergeFieldStyles } from '../mergeFieldStyles.js'
 import './index.scss'
+import { fieldBaseClass } from '../shared/index.js'
 
 const baseClass = 'point'
 
 export const PointFieldComponent: PointFieldClientComponent = (props) => {
   const {
+    field,
     field: {
-      name,
-      admin: { className, description, placeholder, step, style, width } = {},
+      admin: { className, description, placeholder, step } = {},
       label,
       localized,
       required,
@@ -71,6 +72,8 @@ export const PointFieldComponent: PointFieldClientComponent = (props) => {
     return `${fieldLabel}${fieldLabel ? ' - ' : ''}${suffix}`
   }
 
+  const styles = useMemo(() => mergeFieldStyles(field), [field])
+
   return (
     <div
       className={[
@@ -82,10 +85,7 @@ export const PointFieldComponent: PointFieldClientComponent = (props) => {
       ]
         .filter(Boolean)
         .join(' ')}
-      style={{
-        ...style,
-        width,
-      }}
+      style={styles}
     >
       <ul className={`${baseClass}__wrap`}>
         <li>
