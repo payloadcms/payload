efdf815032ac2b7463bb86f0e4fd9a0f594b94d2
Jacob Fletcher <jacobsfletch@gmail.com>
2024-11-18 23:43:54 -0500

    reset community types

--- test/_community/payload-types.ts
+++ test/_community/payload-types.ts
@@ -40,9 +40,9 @@ export interface Config {
   user: User & {
     collection: 'users';
   };
-  jobs?: {
+  jobs: {
     tasks: unknown;
-    workflows?: unknown;
+    workflows: unknown;
   };
 }
 export interface UserAuthOperations {
@@ -70,26 +70,6 @@ export interface UserAuthOperations {
 export interface Post {
   id: string;
   title?: string | null;
-  array?:
-    | {
-        richText?: {
-          root: {
-            type: string;
-            children: {
-              type: string;
-              version: number;
-              [k: string]: unknown;
-            }[];
-            direction: ('ltr' | 'rtl') | null;
-            format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
-            indent: number;
-            version: number;
-          };
-          [k: string]: unknown;
-        } | null;
-        id?: string | null;
-      }[]
-    | null;
   updatedAt: string;
   createdAt: string;
   _status?: ('draft' | 'published') | null;
@@ -222,14 +202,6 @@ export interface PayloadMigration {
  */
 export interface PostsSelect<T extends boolean = true> {
   title?: T;
-  array?:
-    | T
-    | {
-        richText?: T;
-        ui?: T;
-        id?: T;
-      };
-  ui?: T;
   updatedAt?: T;
   createdAt?: T;
   _status?: T;
