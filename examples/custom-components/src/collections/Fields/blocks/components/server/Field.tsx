037662d9f5b8be94dfb5c239dadaf12bdabfe33a
Jessica Chowdhury <67977755+JessChowdhury@users.noreply.github.com>
2024-11-19 11:40:20 -0500

    chore(examples): migrates `custom-component` example to latest beta [skip-lint] (#9170)
    Updates the `Custom Components` example, including packages, readme,
    lockfile, types and the custom fields.
    
    ---------
    
    Co-authored-by: Patrik Kozak <patrik@payloadcms.com>

--- examples/custom-components/src/collections/Fields/blocks/components/server/Field.tsx
+++ examples/custom-components/src/collections/Fields/blocks/components/server/Field.tsx
@@ -3,6 +3,7 @@ import type React from 'react'
 
 import { BlocksField } from '@payloadcms/ui'
 
-export const CustomBlocksFieldServer: BlocksFieldServerComponent = ({ clientField }) => {
-  return <BlocksField field={clientField} />
+export const CustomBlocksFieldServer: BlocksFieldServerComponent = (props) => {
+  const path = (props?.path || props?.field?.name || '') as string
+  return <BlocksField field={props?.clientField} path={path} permissions={props?.permissions} />
 }
