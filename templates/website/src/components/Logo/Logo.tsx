7f65c83a98a105abe3e5729af29568e7ebbf6058
Paul <paul@payloadcms.com>
2024-11-19 09:06:56 -0600

    fix(templates): website logo not showing (#9334)

--- templates/website/src/components/Logo/Logo.tsx
+++ templates/website/src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
-      className="max-w-[9.375rem] invert dark:invert-0"
+      className="max-w-[9.375rem] invert dark:invert-0 w-full"
       src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
     />
   )
