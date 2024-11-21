18b139b9de2b9971814236e63fda9ebdb7d79a3d
Paul <paul@payloadcms.com>
2024-11-21 15:42:29 -0600

    fix(templates): website template breaking change by renaming hero image to jpg (#9416)

--- templates/website/src/components/Logo/Logo.tsx
+++ templates/website/src/components/Logo/Logo.tsx
@@ -5,6 +5,8 @@ export const Logo = () => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
+      width={193}
+      height={43}
       className="max-w-[9.375rem] invert dark:invert-0 w-full"
       src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
     />
