75f39d4002b8cd52dd11a1ed12a3fe8533846b95
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2024-11-21 19:55:18 -0300

    fix path and samples for lexical-mdx tests

--- test/lexical-mdx/collections/Posts/shared.ts
+++ test/lexical-mdx/collections/Posts/shared.ts
@@ -1,4 +1,10 @@
-export const docsBasePath = '/Users/alessio/Documents/payloadcms-mdx-mock/docs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+export const docsBasePath =
+  typeof window === 'undefined'
+    ? path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../../sampleDocs')
+    : null // O maneja de otra forma para el cliente
 
 export const languages = {
   ts: 'TypeScript',
