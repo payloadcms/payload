23d54a76c1b825063b21f01dcffbd5fa126262a7
Paul <paul@payloadcms.com>
2024-11-22 17:53:06 -0600

    fix(templates): website template firefox logo fix and images fix on vercel (#9459)
    Fixes logo not showing up on firefox and images not loading properly
    when hosting with Vercel

--- templates/website/next.config.js
+++ templates/website/next.config.js
@@ -2,7 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
