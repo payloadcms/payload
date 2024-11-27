519bb790d8fa9d0e3218ef19aa2a04afb241761e
Alessio Gravili <alessio@gravili.de>
2024-11-26 22:56:22 -0700

    feat(richtext-lexical): fully-typed blocks in JSX serializer (#9554)
    This allows for full type-safety when using our official JSX converter
    with blocks:
    
    ![CleanShot 2024-11-26 at 21 35
    18@2x](https://github.com/user-attachments/assets/70ceb3e9-d5d1-4074-a5dd-bb9d514dc229)
    
    ![CleanShot 2024-11-26 at 21 35
    25@2x](https://github.com/user-attachments/assets/5100133c-8a91-4cfe-8e44-c091b2d86ffa)

--- packages/richtext-lexical/src/exports/react/components/RichText/index.tsx
+++ packages/richtext-lexical/src/exports/react/components/RichText/index.tsx
@@ -2,12 +2,22 @@ import type { SerializedEditorState } from 'lexical'
 
 import React from 'react'
 
+import type {
+  DefaultNodeTypes,
+  SerializedBlockNode,
+  SerializedInlineBlockNode,
+} from '../../../../nodeTypes.js'
 import type { JSXConverters } from './converter/types.js'
 
 import { defaultJSXConverters } from './converter/defaultConverters.js'
 import { convertLexicalToJSX } from './converter/index.js'
 
-export type JSXConvertersFunction = (args: { defaultConverters: JSXConverters }) => JSXConverters
+export type JSXConvertersFunction<
+  T extends { [key: string]: any; type?: string } =
+    | DefaultNodeTypes
+    | SerializedBlockNode<{ blockName?: null | string; blockType: string }>
+    | SerializedInlineBlockNode<{ blockName?: null | string; blockType: string }>,
+> = (args: { defaultConverters: JSXConverters<DefaultNodeTypes> }) => JSXConverters<T>
 
 type Props = {
   className?: string
