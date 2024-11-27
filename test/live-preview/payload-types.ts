d557f17b9a51de22eaf3d2c52027db54d11770fd
Alessio Gravili <alessio@gravili.de>
2024-11-27 12:49:49 -0700

    chore: update generated types for all test suites

--- test/live-preview/payload-types.ts
+++ test/live-preview/payload-types.ts
@@ -52,9 +52,9 @@ export interface Config {
   user: User & {
     collection: 'users';
   };
-  jobs?: {
+  jobs: {
     tasks: unknown;
-    workflows?: unknown;
+    workflows: unknown;
   };
 }
 export interface UserAuthOperations {
