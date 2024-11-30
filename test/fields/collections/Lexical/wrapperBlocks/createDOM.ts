538fdf64e79748223016a73719b0a58b1e9d9545
Alessio Gravili <alessio@gravili.de>
2024-11-29 21:34:19 -0700

    improve variable names

--- test/fields/collections/Lexical/wrapperBlocks/createDOM.ts
+++ test/fields/collections/Lexical/wrapperBlocks/createDOM.ts
@@ -3,14 +3,13 @@
 import type { CreateDOMFunction } from '@payloadcms/richtext-lexical'
 
 export const wrapperBlockCreateDOM: CreateDOMFunction = ({ editorConfig, editor, node }) => {
-  console.log('Node fields', node.getFields())
-  const a = document.createElement('span')
-  a.style.color = 'red'
-  return a
+  const span = document.createElement('span')
+  span.style.color = 'red'
+  return span
 }
 
 export const wrapperBlockColorCreateDOM: CreateDOMFunction = ({ editorConfig, editor, node }) => {
-  const a = document.createElement('span')
-  a.style.color = node.getFields().color
-  return a
+  const span = document.createElement('span')
+  span.style.color = node.getFields().color
+  return span
 }
