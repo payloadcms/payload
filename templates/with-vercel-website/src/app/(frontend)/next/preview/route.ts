2d5a8d131aec2db81bb0f50ccfbe4d2367bf4448
Paul Popus <paul@nouance.io>
2024-12-02 11:59:44 -0600

    commit updated templates

--- templates/with-vercel-website/src/app/(frontend)/next/preview/route.ts
+++ templates/with-vercel-website/src/app/(frontend)/next/preview/route.ts
@@ -70,6 +70,7 @@ export async function GET(
         collection,
         draft: true,
         limit: 1,
+        // pagination: false reduces overhead if you don't need totalDocs
         pagination: false,
         depth: 0,
         select: {},
