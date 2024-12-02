e285eca82aa4cd174c66a81716f57119c0125b2c
Jarrod Flesch <jarrodmflesch@gmail.com>
2024-12-02 08:36:59 -0500

    bring back createParams export

--- packages/ui/src/providers/SearchParams/index.tsx
+++ packages/ui/src/providers/SearchParams/index.tsx
@@ -1,4 +1,6 @@
 'use client'
+import type { ReadonlyURLSearchParams } from 'next/navigation.js'
+
 import { useSearchParams as useNextSearchParams } from 'next/navigation.js'
 import * as qs from 'qs-esm'
 import React, { createContext, useContext } from 'react'
@@ -16,6 +18,15 @@ const initialContext: SearchParamsContext = {
 }
 
 const Context = createContext(initialContext)
+
+export function createParams(params: ReadonlyURLSearchParams): State {
+  const search = params.toString()
+
+  return qs.parse(search, {
+    depth: 10,
+    ignoreQueryPrefix: true,
+  })
+}
 
 // TODO: abstract the `next/navigation` dependency out from this provider so that it can be used in other contexts
 export const SearchParamsProvider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
