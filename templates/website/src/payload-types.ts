bfab4ac25fac8c14622ba1d3e1cf6d5ba42f541e
Paul Popus <paul@nouance.io>
2024-12-03 17:10:24 -0600

    feat(templates): add new favicons and og images to website template

--- templates/website/src/payload-types.ts
+++ templates/website/src/payload-types.ts
@@ -214,6 +214,14 @@ export interface Media {
       filesize?: number | null;
       filename?: string | null;
     };
+    og?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
   };
 }
 /**
@@ -987,6 +995,16 @@ export interface MediaSelect<T extends boolean = true> {
               filename?: T;
             };
         xlarge?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        og?:
           | T
           | {
               url?: T;
