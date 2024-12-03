8beb7c163636ce4e044317c13ee45d20542af64a
Paul <paul@payloadcms.com>
2024-12-03 13:37:56 -0600

    fix(templates): vercel website template migrations wrong adapter import (#9708)

--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241203_183323_initial from './20241203_183323_initial'
+import * as migration_20241203_192242_initial from './20241203_192242_initial'
 
 export const migrations = [
   {
-    up: migration_20241203_183323_initial.up,
-    down: migration_20241203_183323_initial.down,
-    name: '20241203_183323_initial',
+    up: migration_20241203_192242_initial.up,
+    down: migration_20241203_192242_initial.down,
+    name: '20241203_192242_initial',
   },
 ]
