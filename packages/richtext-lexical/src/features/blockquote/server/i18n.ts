9a39e97eb1ddd1379e7e67b489385aced7a7a51f
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2024-12-04 19:44:31 +0400

    Update packages/richtext-lexical/src/features/blockquote/server/i18n.ts
    Co-authored-by: Thorarinn Stefansson <toro@hugsmidjan.is>

--- packages/richtext-lexical/src/features/blockquote/server/i18n.ts
+++ packages/richtext-lexical/src/features/blockquote/server/i18n.ts
@@ -41,7 +41,7 @@ export const i18n: Partial<GenericLanguages> = {
     label: 'Idézetblokk',
   },
   is: {
-    label: 'Tilvísunarblokk',
+    label: 'Tilvitnun',
   },
   it: {
     label: 'Citazione',
