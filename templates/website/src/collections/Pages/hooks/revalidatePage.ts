2321970fccb55c565b98a6b96edc11c34a648b67
Paul <paul@payloadcms.com>
2024-12-04 12:38:18 -0600

    templates: improve speed of seed script (#9748)
    Improves the speed of the seed script by moving operations to be async
    and disabling revalidation on them

--- templates/website/src/collections/Pages/hooks/revalidatePage.ts
+++ templates/website/src/collections/Pages/hooks/revalidatePage.ts
@@ -7,34 +7,37 @@ import type { Page } from '../../../payload-types'
 export const revalidatePage: CollectionAfterChangeHook<Page> = ({
   doc,
   previousDoc,
-  req: { payload },
+  req: { payload, context },
 }) => {
-  if (doc._status === 'published') {
-    const path = doc.slug === 'home' ? '/' : `/${doc.slug}`
+  if (!context.disableRevalidate) {
+    if (doc._status === 'published') {
+      const path = doc.slug === 'home' ? '/' : `/${doc.slug}`
 
-    payload.logger.info(`Revalidating page at path: ${path}`)
+      payload.logger.info(`Revalidating page at path: ${path}`)
 
-    revalidatePath(path)
-    revalidateTag('pages-sitemap')
+      revalidatePath(path)
+      revalidateTag('pages-sitemap')
+    }
+
+    // If the page was previously published, we need to revalidate the old path
+    if (previousDoc?._status === 'published' && doc._status !== 'published') {
+      const oldPath = previousDoc.slug === 'home' ? '/' : `/${previousDoc.slug}`
+
+      payload.logger.info(`Revalidating old page at path: ${oldPath}`)
+
+      revalidatePath(oldPath)
+      revalidateTag('pages-sitemap')
+    }
   }
-
-  // If the page was previously published, we need to revalidate the old path
-  if (previousDoc?._status === 'published' && doc._status !== 'published') {
-    const oldPath = previousDoc.slug === 'home' ? '/' : `/${previousDoc.slug}`
-
-    payload.logger.info(`Revalidating old page at path: ${oldPath}`)
-
-    revalidatePath(oldPath)
-    revalidateTag('pages-sitemap')
-  }
-
   return doc
 }
 
-export const revalidateDelete: CollectionAfterDeleteHook<Page> = ({ doc }) => {
-  const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
-  revalidatePath(path)
-  revalidateTag('pages-sitemap')
+export const revalidateDelete: CollectionAfterDeleteHook<Page> = ({ doc, req: { context } }) => {
+  if (!context.disableRevalidate) {
+    const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
+    revalidatePath(path)
+    revalidateTag('pages-sitemap')
+  }
 
   return doc
 }
