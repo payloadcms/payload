afd0b54d1b32c968b532af665ca1d817e9c0b16e
Sasha <64744993+r1tsuu@users.noreply.github.com>
2024-12-06 20:30:04 +0200

    feat: export `sanitizeSelectParam`, `sanitizePopulateParam`, `senitizeJoinParams` utils (#9777)
    ### What?
    * Exposes to `payload` these functions: `sanitizeSelectParam`,
    `sanitizePopulateParam`, `senitizeJoinParams`.
    * Refactors `sanitizeSelect` and `sanitizePopulate` to
    `sanitizeSelectParam` and `sanitizePopulateParam` for clarity.
    * Moves them from `@payloadcms/next` to `payload` as they aren't related
    to next.
    
    ### Why?
    To use these functions externally, for example in custom endpoints.

--- packages/next/src/routes/rest/collections/find.ts
+++ packages/next/src/routes/rest/collections/find.ts
@@ -1,16 +1,17 @@
 import type { JoinQuery, Where } from 'payload'
 
 import httpStatus from 'http-status'
-import { findOperation } from 'payload'
+import {
+  findOperation,
+  sanitizeJoinParams,
+  sanitizePopulateParam,
+  sanitizeSelectParam,
+} from 'payload'
 import { isNumber } from 'payload/shared'
 
 import type { CollectionRouteHandler } from '../types.js'
 
 import { headersWithCors } from '../../../utilities/headersWithCors.js'
-import { sanitizeJoinParams } from '../utilities/sanitizeJoinParams.js'
-import { sanitizePopulate } from '../utilities/sanitizePopulate.js'
-import { sanitizeSelect } from '../utilities/sanitizeSelect.js'
-
 export const find: CollectionRouteHandler = async ({ collection, req }) => {
   const { depth, draft, joins, limit, page, populate, select, sort, where } = req.query as {
     depth?: string
@@ -31,9 +32,9 @@ export const find: CollectionRouteHandler = async ({ collection, req }) => {
     joins: sanitizeJoinParams(joins),
     limit: isNumber(limit) ? Number(limit) : undefined,
     page: isNumber(page) ? Number(page) : undefined,
-    populate: sanitizePopulate(populate),
+    populate: sanitizePopulateParam(populate),
     req,
-    select: sanitizeSelect(select),
+    select: sanitizeSelectParam(select),
     sort: typeof sort === 'string' ? sort.split(',') : undefined,
     where,
   })
