300172dfaebda2fb000bbccf3aa5b34c7748d016
Alessio Gravili <alessio@gravili.de>
2024-12-10 11:03:57 -0700

    allow overriding full data used in buildFormState

--- packages/ui/src/forms/fieldSchemasToFormState/index.tsx
+++ packages/ui/src/forms/fieldSchemasToFormState/index.tsx
@@ -31,6 +31,16 @@ type Args = {
    * then the field schema map is not required.
    */
   fieldSchemaMap: FieldSchemaMap | undefined
+  /**
+   * By default, the full data will be the data passed to the form state, without being narrowed down to the sub-fields.
+   * In some cases, form state for sub fields will be generated independent from the parent form state. In these cases, the parent form state
+   * is not available and the full data would be the partial sub data.
+   *
+   * With fullDataOverride, we can pass the real full data, which will be used for `fullData` instead of the data passed to the form state request.
+   *
+   * This is useful for lexical blocks, as block fields there are not part of the parent form state, yet we still want full data to be available.
+   */
+  fullDataOverride?: Data
   id?: number | string
   operation?: 'create' | 'update'
   permissions: SanitizedFieldsPermissions
@@ -64,6 +74,7 @@ export const fieldSchemasToFormState = async (args: Args): Promise<FormState> =>
     data = {},
     fields,
     fieldSchemaMap,
+    fullDataOverride,
     operation,
     permissions,
     preferences,
@@ -88,6 +99,8 @@ export const fieldSchemasToFormState = async (args: Args): Promise<FormState> =>
       user: req.user,
     })
 
+    const fullData = fullDataOverride ?? dataWithDefaultValues
+
     await iterateFields({
       id,
       addErrorPathToParent: null,
@@ -96,7 +109,7 @@ export const fieldSchemasToFormState = async (args: Args): Promise<FormState> =>
       data: dataWithDefaultValues,
       fields,
       fieldSchemaMap,
-      fullData: dataWithDefaultValues,
+      fullData,
       operation,
       parentIndexPath: '',
       parentPassesCondition: true,
