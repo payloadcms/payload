563694d930601a562bf5305c7536558ffd965295
Patrik <patrik@payloadcms.com>
2024-12-10 11:39:15 -0500

    fix(ui): prevents unwanted data overrides when bulk editing (#9842)
    ### What?
    
    It became possible for fields to reset to a defined `defaultValue` when
    bulk editing from the `edit-many` drawer.
    
    ### Why?
    
    The form-state of all fields were being considered during a bulk edit -
    this also meant using their initial states - this meant any fields with
    default values or nested fields (`arrays`) would be overwritten with
    their initial states
    
    I.e. empty values or default values.
    
    ### How?
    
    Now - we only send through the form data of the fields specifically
    being edited in the edit-many drawer and ignore all other fields.
    
    Leaving all other fields stay their current values.
    
    Fixes #9590
    
    ---------
    
    Co-authored-by: Dan Ribbens <dan.ribbens@gmail.com>

--- test/admin/payload-types.ts
+++ test/admin/payload-types.ts
@@ -138,9 +138,31 @@ export interface Post {
         [k: string]: unknown;
       }[]
     | null;
+  arrayOfFields?:
+    | {
+        optional?: string | null;
+        innerArrayOfFields?:
+          | {
+              innerOptional?: string | null;
+              id?: string | null;
+            }[]
+          | null;
+        id?: string | null;
+      }[]
+    | null;
   group?: {
+    defaultValueField?: string | null;
     title?: string | null;
   };
+  someBlock?:
+    | {
+        textFieldForBlock?: string | null;
+        id?: string | null;
+        blockName?: string | null;
+        blockType: 'textBlock';
+      }[]
+    | null;
+  defaultValueField?: string | null;
   relationship?: (string | null) | Post;
   customCell?: string | null;
   sidebarField?: string | null;
@@ -484,11 +506,36 @@ export interface PostsSelect<T extends boolean = true> {
   description?: T;
   number?: T;
   richText?: T;
+  arrayOfFields?:
+    | T
+    | {
+        optional?: T;
+        innerArrayOfFields?:
+          | T
+          | {
+              innerOptional?: T;
+              id?: T;
+            };
+        id?: T;
+      };
   group?:
     | T
     | {
+        defaultValueField?: T;
         title?: T;
       };
+  someBlock?:
+    | T
+    | {
+        textBlock?:
+          | T
+          | {
+              textFieldForBlock?: T;
+              id?: T;
+              blockName?: T;
+            };
+      };
+  defaultValueField?: T;
   relationship?: T;
   customCell?: T;
   sidebarField?: T;
