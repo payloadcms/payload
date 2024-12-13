b75ac5692f69091152cec7cd0fa85df0b9575142
Dan Ribbens <dan.ribbens@gmail.com>
2024-12-13 11:31:55 -0500

    test(plugin-import-export): scaffolding plugin test

--- test/plugin-import-export/int.spec.ts
+++ test/plugin-import-export/int.spec.ts
@@ -14,7 +14,7 @@ let page: Page
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
-describe('@payloadcms/plugin-redirects', () => {
+describe('@payloadcms/plugin-import-export', () => {
   beforeAll(async () => {
     ;({ payload } = await initPayloadInt(dirname))
 
@@ -32,52 +32,5 @@ describe('@payloadcms/plugin-redirects', () => {
     }
   })
 
-  it('should add a redirects collection', async () => {
-    const redirect = await payload.find({
-      collection: 'redirects',
-      depth: 0,
-      limit: 1,
-    })
-
-    expect(redirect).toBeTruthy()
-  })
-
-  it('should add a redirect with to internal page', async () => {
-    const redirect = await payload.create({
-      collection: 'redirects',
-      data: {
-        from: '/test',
-        to: {
-          type: 'reference',
-          reference: {
-            relationTo: pagesSlug,
-            value: page.id,
-          },
-        },
-        type: '301',
-      },
-    })
-
-    expect(redirect).toBeTruthy()
-    expect(redirect.from).toBe('/test')
-    expect(redirect.to.reference.value).toMatchObject(page)
-  })
-
-  it('should add a redirect with to custom url', async () => {
-    const redirect = await payload.create({
-      collection: 'redirects',
-      data: {
-        from: '/test2',
-        to: {
-          type: 'custom',
-          url: '/test',
-        },
-        type: '301',
-      },
-    })
-
-    expect(redirect).toBeTruthy()
-    expect(redirect.from).toBe('/test2')
-    expect(redirect.to.url).toBe('/test')
-  })
+  it.todo('works')
 })
