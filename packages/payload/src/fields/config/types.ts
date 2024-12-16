c187bff581039a56405b2150ee5ae8f9d74a633f
Sasha <64744993+r1tsuu@users.noreply.github.com>
2024-12-16 16:57:21 +0200

    fix: remove `localized` property from `RowField` and `CollapsibleField` (#9672)
    ### What?
    Removes the `localized` property from typescript suggestion for row and
    collapsible fields.
    
    ### Why?
    Currently, this property doesn't do anything for them. This may be
    changed when/if we support `name` for them, but it'll work only with
    `name`.
    
    Fixes https://github.com/payloadcms/payload/issues/4720

--- packages/payload/src/fields/config/types.ts
+++ packages/payload/src/fields/config/types.ts
@@ -658,7 +658,7 @@ export type RowField = {
   admin?: Omit<Admin, 'description'>
   fields: Field[]
   type: 'row'
-} & Omit<FieldBase, 'admin' | 'label' | 'name' | 'validate' | 'virtual'>
+} & Omit<FieldBase, 'admin' | 'label' | 'localized' | 'name' | 'validate' | 'virtual'>
 
 export type RowFieldClient = {
   admin?: Omit<AdminClient, 'description'>
@@ -697,7 +697,7 @@ export type CollapsibleField = {
       label: Required<FieldBase['label']>
     }
 ) &
-  Omit<FieldBase, 'label' | 'name' | 'validate' | 'virtual'>
+  Omit<FieldBase, 'label' | 'localized' | 'name' | 'validate' | 'virtual'>
 
 export type CollapsibleFieldClient = {
   admin?: {
