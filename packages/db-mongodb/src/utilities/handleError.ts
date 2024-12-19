payloadcms/payload

commit 0e5bda9a744dff22ddfd3682a511cce7ad5cb8ae

feat: make `req` partial and optional in DB / Local API operations (#9935)
### What?
Previously, the `req` argument:
In database operations (e.g `payload.db`) was required and you needed to
pass the whole `req` with all the properties. This is confusing because
in database operations we never use its properties outside of
`req.transactionID` and `req.t`, both of which should be optional as
well.

Now, you don't have to do that cast:
```ts
payload.db.findOne({
  collection: 'posts',
  req: {} as PayloadRequest,
  where: {
    id: {
      equals: 1,
    },
  },
})
```
Becomes:
```ts
payload.db.findOne({
  collection: 'posts',
  where: {
    id: {
      equals: 1,
    },
  },
})
```

If you need to use transactions, you're not required to do the `as` cast
as well now, as the `req` not only optional but also partial -
`Partial<PayloadRequest>`.
`initTransaction`, `commitTransaction`, `killTransaction` utilities are
typed better now as well. They do not require to you pass all the
properties of `req`, but only `payload` -
`MarkRequired<Partial<PayloadRequest>, 'payload'>`
```ts
const req = { payload }
await initTransaction(req)
await payload.db.create({
  collection: "posts",
  data: {},
  req
})
await commitTransaction(req)
```

The same for the Local API. Internal operations (for example
`packages/payload/src/collections/operations/find.ts`) still accept the
whole `req`, but local ones
(`packages/payload/src/collections/operations/local/find.ts`) which are
used through `payload.` now accept `Partial<PayloadRequest>`, as they
pass it through to internal operations with `createLocalReq`.

So now, this is also valid, while previously you had to do `as` cast for
`req`.
```ts
const req = { payload }
await initTransaction(req)
await payload.create({
  collection: "posts",
  data: {},
  req
})
await commitTransaction(req)
```


Marked as deprecated `PayloadRequest['transactionIDPromise']` to remove

packages/db-mongodb/src/utilities/handleError.ts
@@ -1,3 +1,5 @@
+import type { PayloadRequest } from 'payload'
+
 import httpStatus from 'http-status'
 import { APIError, ValidationError } from 'payload'
 
@@ -8,28 +10,32 @@ export const handleError = ({
   req,
 }: {
   collection?: string
-  error
+  error: unknown
   global?: string
-  req
+  req?: Partial<PayloadRequest>
 }) => {
+  if (!error || typeof error !== 'object') {
+    throw error
+  }
+
+  const message = req?.t ? 'error:valueMustBeUnique' : 'Value must be unique'
+
   // Handle uniqueness error from MongoDB
-  if (error.code === 11000 && error.keyValue) {
+  if ('code' in error && error.code === 11000 && 'keyValue' in error && error.keyValue) {
     throw new ValidationError(
       {
         collection,
         errors: [
           {
-            message: req.t('error:valueMustBeUnique'),
+            message,
             path: Object.keys(error.keyValue)[0],
           },
         ],
         global,
       },
-      req.t,
+      req?.t,
     )
-  } else if (error.code === 11000) {
-    throw new APIError(req.t('error:valueMustBeUnique'), httpStatus.BAD_REQUEST)
-  } else {
-    throw error
   }
+
+  throw new APIError(message, httpStatus.BAD_REQUEST)
 }

packages/drizzle/src/upsertRow/index.ts
@@ -394,12 +394,12 @@ export const upsertRow = async <T extends Record<string, unknown> | TypeWithID>(
           id,
           errors: [
             {
-              message: req.t('error:valueMustBeUnique'),
+              message: req?.t ? 'error:valueMustBeUnique' : 'Value must be unique',
               path: fieldName,
             },
           ],
         },
-        req.t,
+        req?.t,
       )
     } else {
       throw error

packages/drizzle/src/postgres/predefinedMigrations/v2-v3/fetchAndResave/index.ts
@@ -1,7 +1,6 @@
 import type { FlattenedField, Payload, PayloadRequest } from 'payload'
 
-import type { TransactionPg } from '../../../../types.js'
-import type { BasePostgresAdapter } from '../../../types.js'
+import type { BasePostgresAdapter, PostgresDB } from '../../../types.js'
 import type { DocsToResave } from '../types.js'
 
 import { upsertRow } from '../../../../upsertRow/index.js'
@@ -10,14 +9,14 @@ import { traverseFields } from './traverseFields.js'
 type Args = {
   adapter: BasePostgresAdapter
   collectionSlug?: string
-  db: TransactionPg
+  db: PostgresDB
   debug: boolean
   docsToResave: DocsToResave
   fields: FlattenedField[]
   globalSlug?: string
   isVersions: boolean
   payload: Payload
-  req: PayloadRequest
+  req?: Partial<PayloadRequest>
   tableName: string
 }
 

packages/drizzle/src/postgres/predefinedMigrations/v2-v3/index.ts
@@ -6,10 +6,10 @@ import fs from 'fs'
 import { buildVersionCollectionFields, buildVersionGlobalFields } from 'payload'
 import toSnakeCase from 'to-snake-case'
 
-import type { TransactionPg } from '../../../types.js'
 import type { BasePostgresAdapter } from '../../types.js'
 import type { PathsToQuery } from './types.js'
 
+import { getTransaction } from '../../../utilities/getTransaction.js'
 import { groupUpSQLStatements } from './groupUpSQLStatements.js'
 import { migrateRelationships } from './migrateRelationships.js'
 import { traverseFields } from './traverseFields.js'
@@ -36,7 +36,7 @@ type Args = {
  */
 export const migratePostgresV2toV3 = async ({ debug, payload, req }: Args) => {
   const adapter = payload.db as unknown as BasePostgresAdapter
-  const db = adapter.sessions[await req.transactionID].db as TransactionPg
+  const db = await getTransaction(adapter, req)
   const dir = payload.db.migrationDir
 
   // get the drizzle migrateUpSQL from drizzle using the last schema

packages/payload/src/types/index.ts
@@ -62,6 +62,7 @@ export type CustomPayloadRequestProperties = {
   transactionID?: number | Promise<number | string> | string
   /**
    * Used to ensure consistency when multiple operations try to create a transaction concurrently on the same request
+   * @deprecated This is not used anywhere, instead `transactionID` is used for the above. Will be removed in next major version.
    */
   transactionIDPromise?: Promise<void>
   /** The signed-in user */

commit 0c57eef6211b378422070819441df7d43297eff9

fix: unique error message regression (#10064)
Regression from https://github.com/payloadcms/payload/pull/9935, we need
to call `req.t` with `''error:valueMustBeUnique''` instead of just
`''error:valueMustBeUnique''`

packages/db-mongodb/src/utilities/handleError.ts
@@ -18,7 +18,7 @@ export const handleError = ({
     throw error
   }
 
-  const message = req?.t ? 'error:valueMustBeUnique' : 'Value must be unique'
+  const message = req?.t ? req.t('error:valueMustBeUnique') : 'Value must be unique'
 
   // Handle uniqueness error from MongoDB
   if ('code' in error && error.code === 11000 && 'keyValue' in error && error.keyValue) {

packages/drizzle/src/upsertRow/index.ts
@@ -394,7 +394,7 @@ export const upsertRow = async <T extends Record<string, unknown> | TypeWithID>(
           id,
           errors: [
             {
-              message: req?.t ? 'error:valueMustBeUnique' : 'Value must be unique',
+              message: req?.t ? req.t('error:valueMustBeUnique') : 'Value must be unique',
               path: fieldName,
             },
           ],