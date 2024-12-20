2ea4ec364b19e9ae3f129f27b0ccfa7de6040eae
rik rose <rikrose@gmail.com>
2024-12-20 08:04:42 -0800

    fix(examples): fix access control model for Users. Only super-admins can create/edit/delete super-admins. Restrict tenant-admins from activities outside tenants they are admins for. Have not verified that this stops tenant-admins from editing users outside of their own tenants.

--- examples/multi-tenant/src/collections/Users/access/create.ts
+++ examples/multi-tenant/src/collections/Users/access/create.ts
@@ -6,7 +6,7 @@ import { isSuperAdmin } from '../../../access/isSuperAdmin'
 import { getTenantAdminTenantAccessIDs } from '../../../utilities/getTenantAccessIDs'
 
 export const createAccess: Access<User> = (args) => {
-  const { req } = args
+  const { req, data }: {req: any, data:User} = args
   if (!req.user) {
     return false
   }
@@ -17,9 +17,26 @@ export const createAccess: Access<User> = (args) => {
 
   const adminTenantAccessIDs = getTenantAdminTenantAccessIDs(req.user)
 
-  if (adminTenantAccessIDs.length > 0) {
-    return true
+  if (!data){
+    // Triggered by visit to the User collection on Admin console
+    // If we return true, then this enables the "Create New" button
+    // so, we do that if we're an admin of at least 1 tenant
+    return Boolean(adminTenantAccessIDs.length)
   }
 
-  return false
+  if (data.roles?.includes('super-admin')){
+    // No escalation of privs.
+    return false
+  }
+
+  if (data.tenants && data.tenants.length === 0){
+    // Gotta have at least 1 tenant
+    return false
+  }
+
+  // allow only if the tenants are where this user is an admin.
+  return data.tenants?.every((tenant) => 
+    adminTenantAccessIDs.includes(typeof(tenant.tenant) === 'string' ?
+       tenant.tenant : tenant.tenant.id)
+  )
 }
--- examples/multi-tenant/src/collections/Users/access/updateAndDelete.ts
+++ examples/multi-tenant/src/collections/Users/access/updateAndDelete.ts
@@ -2,9 +2,11 @@ import type { Access } from 'payload'
 
 import { isSuperAdmin } from '../../../access/isSuperAdmin'
 import { getTenantAdminTenantAccessIDs } from '../../../utilities/getTenantAccessIDs'
+import { User } from '@/payload-types'
 
 export const updateAndDeleteAccess: Access = (args) => {
-  const { req } = args
+  const { req, data } : {req:any, data:User} = args
+
   if (!req.user) {
     return false
   }
@@ -15,9 +17,20 @@ export const updateAndDeleteAccess: Access = (args) => {
 
   const adminTenantAccessIDs = getTenantAdminTenantAccessIDs(req.user)
 
-  return {
-    'tenants.tenant': {
-      in: adminTenantAccessIDs,
-    },
+  // if no data, then the GUI is asking if we are allowed to create users at all
+  if (adminTenantAccessIDs.length &&!data){
+    return true
   }
+  
+  // If you're only a tenant-admin, then you can't delete super-admins
+  if (data.roles?.includes('super-admin')){
+    return false
+  }
+
+  // Can delete a user, only if you're an admin in *all* of their tenants
+  return data.tenants?.every((tenant) => 
+    adminTenantAccessIDs.includes(typeof(tenant.tenant) === 'string' ?
+       tenant.tenant : tenant.tenant.id)
+  )
+
 }
