d3fa5bbbb838f48078947c2c28988062ad04ba23
rik rose <rikrose@gmail.com>
2024-12-20 10:13:53 -0800

    chore(formatting): fix formatting of multi-tenant example access control update.

--- examples/multi-tenant/src/collections/Users/access/updateAndDelete.ts
+++ examples/multi-tenant/src/collections/Users/access/updateAndDelete.ts
@@ -5,7 +5,7 @@ import { getTenantAdminTenantAccessIDs } from '../../../utilities/getTenantAcces
 import { User } from '@/payload-types'
 
 export const updateAndDeleteAccess: Access = (args) => {
-  const { req, data } : {req:any, data:User} = args
+  const { req, data }: { req: any; data: User } = args
 
   if (!req.user) {
     return false
@@ -17,20 +17,22 @@ export const updateAndDeleteAccess: Access = (args) => {
 
   const adminTenantAccessIDs = getTenantAdminTenantAccessIDs(req.user)
 
-  // if no data, then the GUI is asking if we are allowed to create users at all
-  if (adminTenantAccessIDs.length &&!data){
-    return true
+  if (!data) {
+    // Triggered by visit to the User collection on Admin console
+    // If we return true, then this enables the "Create New" button
+    // so, we do that if we're an admin of at least 1 tenant
+    return Boolean(adminTenantAccessIDs.length)
   }
-  
+
   // If you're only a tenant-admin, then you can't delete super-admins
-  if (data.roles?.includes('super-admin')){
+  if (data.roles?.includes('super-admin')) {
     return false
   }
 
   // Can delete a user, only if you're an admin in *all* of their tenants
-  return data.tenants?.every((tenant) => 
-    adminTenantAccessIDs.includes(typeof(tenant.tenant) === 'string' ?
-       tenant.tenant : tenant.tenant.id)
+  return data.tenants?.every((tenant) =>
+    adminTenantAccessIDs.includes(
+      typeof tenant.tenant === 'string' ? tenant.tenant : tenant.tenant.id,
+    ),
   )
-
 }
