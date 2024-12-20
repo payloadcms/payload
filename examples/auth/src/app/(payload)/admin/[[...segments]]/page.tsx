7292220109bb9c4c55c3f63c765b658144307541
Jacob Fletcher <jacobsfletch@gmail.com>
2024-12-19 23:54:24 -0500

    chore(examples): updates auth example to latest (#10090)
    The auth example was still on `v3.0.0-beta.24`, was missing its users
    collection config, and was not yet using the component paths pattern
    established here: #7246. This updates to latest and fixes these issues.
    This example can still use further improvements and housekeeping which
    will come in future PRs.

--- examples/auth/src/app/(payload)/admin/[[...segments]]/page.tsx
+++ examples/auth/src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -5,18 +5,20 @@ import type { Metadata } from 'next'
 import config from '@payload-config'
 import { generatePageMetadata, RootPage } from '@payloadcms/next/views'
 
-type Args = {
-  params: {
-    segments: string[]
-  }
-  searchParams: {
-    [key: string]: string | string[]
-  }
-}
+import { importMap } from '../importMap.js'
 
+type Args = {
+  params: Promise<{
+    segments: string[]
+  }>
+  searchParams: Promise<{
+    [key: string]: string | string[]
+  }>
+}
 export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> =>
   generatePageMetadata({ config, params, searchParams })
 
-const Page = ({ params, searchParams }: Args) => RootPage({ config, params, searchParams })
+const Page = ({ params, searchParams }: Args) =>
+  RootPage({ config, importMap, params, searchParams })
 
 export default Page
