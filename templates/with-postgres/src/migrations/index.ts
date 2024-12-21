728554ae97a01a3592ef5726e92b34f8f8d9627f
Alessio Gravili <alessio@gravili.de>
2024-12-21 01:09:22 -0700

    chore: pin templates to latest version

--- templates/with-postgres/src/migrations/index.ts
+++ templates/with-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241220_195023_initial from './20241220_195023_initial'
+import * as migration_20241221_080814_initial from './20241221_080814_initial'
 
 export const migrations = [
   {
-    up: migration_20241220_195023_initial.up,
-    down: migration_20241220_195023_initial.down,
-    name: '20241220_195023_initial',
+    up: migration_20241221_080814_initial.up,
+    down: migration_20241221_080814_initial.down,
+    name: '20241221_080814_initial',
   },
 ]
--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241220_195012_initial from './20241220_195012_initial'
+import * as migration_20241221_080802_initial from './20241221_080802_initial'
 
 export const migrations = [
   {
-    up: migration_20241220_195012_initial.up,
-    down: migration_20241220_195012_initial.down,
-    name: '20241220_195012_initial',
+    up: migration_20241221_080802_initial.up,
+    down: migration_20241221_080802_initial.down,
+    name: '20241221_080802_initial',
   },
 ]
