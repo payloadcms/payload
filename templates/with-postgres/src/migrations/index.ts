1679e3dd4fcc5b213dccdf22363689389adde223
Elliot DeNolf <denolfe@gmail.com>
2024-12-21 08:15:47 -0500

    chore: templates back to latest

--- templates/with-postgres/src/migrations/index.ts
+++ templates/with-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241221_130423_initial from './20241221_130423_initial'
+import * as migration_20241221_131510_initial from './20241221_131510_initial'
 
 export const migrations = [
   {
-    up: migration_20241221_130423_initial.up,
-    down: migration_20241221_130423_initial.down,
-    name: '20241221_130423_initial',
+    up: migration_20241221_131510_initial.up,
+    down: migration_20241221_131510_initial.down,
+    name: '20241221_131510_initial',
   },
 ]
--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241221_130412_initial from './20241221_130412_initial'
+import * as migration_20241221_131507_initial from './20241221_131507_initial'
 
 export const migrations = [
   {
-    up: migration_20241221_130412_initial.up,
-    down: migration_20241221_130412_initial.down,
-    name: '20241221_130412_initial',
+    up: migration_20241221_131507_initial.up,
+    down: migration_20241221_131507_initial.down,
+    name: '20241221_131507_initial',
   },
 ]
