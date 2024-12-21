f1901f66482125d8288da5b138805169cb835222
Alessio Gravili <alessio@gravili.de>
2024-12-21 01:18:03 -0700

    chore: pin templates to latest version (#10116)
    pinning to an exact version number is clearer more reliable than using
    "latest".

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241220_194958_initial from './20241220_194958_initial'
+import * as migration_20241221_080749_initial from './20241221_080749_initial'
 
 export const migrations = [
   {
-    up: migration_20241220_194958_initial.up,
-    down: migration_20241220_194958_initial.down,
-    name: '20241220_194958_initial',
+    up: migration_20241221_080749_initial.up,
+    down: migration_20241221_080749_initial.down,
+    name: '20241221_080749_initial',
   },
 ]
