760a46b06a6164783599ae3bc134c10e95027f04
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2024-12-22 16:16:09 -0300

    Merge branch 'example-localization'

---
+++ examples/localization/src/components/ui/input.tsx
@@ -0,0 +1,23 @@
+import { cn } from 'src/utilities/cn'
+import * as React from 'react'
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ type, className, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          'flex h-10 w-full rounded border border-border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          className,
+        )}
+        ref={ref}
+        type={type}
+        {...props}
+      />
+    )
+  },
+)
+Input.displayName = 'Input'
+
+export { Input }
