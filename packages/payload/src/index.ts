466f109152bc6bf9a67e7d7bafd38c7d57a881de
Jacob Fletcher <jacobsfletch@gmail.com>
2024-12-23 13:19:52 -0500

    chore(live-preview): strongly types message events (#10148)
    Live Preview message events were typed with the generic `MessageEvent`
    interface without passing any of the Live Preview specific properties,
    leading to unknown types upon use. To fix this, there is a new
    `LivePreviewMessageEvent` which properly extends the underlying
    `MessageEvent` interface, providing much needed type safety to these
    functions. In the same vein, the `UpdatedDocument` type was not being
    properly shared across packages, leading to multiple independent
    definitions of this type. This type is now exported from `payload`
    itself and renamed to `DocumentEvent` for improved semantics. Same with
    the `FieldSchemaJSON` type. This PR also adjusts where globally scoped
    variables are set, putting them within the shared `_payloadLivePreview`
    namespace instead of setting them individually at the top-level.

--- packages/payload/src/index.ts
+++ packages/payload/src/index.ts
@@ -1338,6 +1338,7 @@ export {
   type CustomVersionParser,
 } from './utilities/dependencies/dependencyChecker.js'
 export { getDependencies } from './utilities/dependencies/getDependencies.js'
+export type { FieldSchemaJSON } from './utilities/fieldSchemaToJSON.js'
 export {
   findUp,
   findUpSync,
