af1e67c5d0d2c9cbfcb8bd9dd65db746ac900b1a
Alessio Gravili <alessio@gravili.de>
2024-12-29 18:09:15 -0700

    fix regex

--- packages/richtext-lexical/src/utilities/jsx/lexicalMarkdownCopy.ts
+++ packages/richtext-lexical/src/utilities/jsx/lexicalMarkdownCopy.ts
@@ -18,8 +18,10 @@ const UNORDERED_LIST_REGEX = /^(\s*)[-*+]\s/
 const CHECK_LIST_REGEX = /^(\s*)(?:-\s)?\s?(\[(\s|x)?\])\s/i
 const HEADING_REGEX = /^(#{1,6})\s/
 const QUOTE_REGEX = /^>\s/
-const CODE_START_REGEX = /^[ \t]*```(\w+)?/
-const CODE_END_REGEX = /[ \t]*```$/
+// Match start of ``` or escaped \`\`\` code blocks
+const CODE_START_REGEX = /^[ \t]*(\\`\\`\\`|```)(\w+)?/
+// Match end of ``` or escaped \`\`\` code blocks
+const CODE_END_REGEX = /[ \t]*(\\`\\`\\`|```)$/
 const CODE_SINGLE_LINE_REGEX = /^[ \t]*```[^`]+(?:(?:`{1,2}|`{4,})[^`]+)*```(?:[^`]|$)/
 const TABLE_ROW_REG_EXP = /^\|(.+)\|\s?$/
 const TABLE_ROW_DIVIDER_REG_EXP = /^(\| ?:?-*:? ?)+\|\s?$/
