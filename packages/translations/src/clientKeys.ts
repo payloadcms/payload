1d6bf2a5dc38311bd5fe36fe2d5f546d678c994d
Francisco Lourenço <208149+franciscolourenco@users.noreply.github.com>
2024-12-31 18:10:22 +0800

    feat: add label to changes count

--- packages/translations/src/clientKeys.ts
+++ packages/translations/src/clientKeys.ts
@@ -348,6 +348,7 @@ export const clientTranslationKeys = createClientTranslationKeys([
   'version:autosavedSuccessfully',
   'version:autosavedVersion',
   'version:changed',
+  'version:changeCount',
   'version:confirmRevertToSaved',
   'version:compareVersion',
   'version:confirmPublish',
