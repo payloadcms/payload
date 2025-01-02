3a2a89703ce985f1859a05bd3647283dd0c1ef0d
Jacob Fletcher <jacobsfletch@gmail.com>
2025-01-02 13:45:46 -0500

    adds jsdocs

--- packages/ui/src/utilities/abortAndIgnore.ts
+++ packages/ui/src/utilities/abortAndIgnore.ts
@@ -8,6 +8,14 @@ export function abortAndIgnore(abortController: AbortController) {
   }
 }
 
+/**
+ * Use this function when an effect is triggered multiple times over and you want to cancel the previous effect.
+ * It will abort the previous effect and create a new AbortController for the next effect.
+ * Important: You must also _reset_ the `abortControllerRef` after the effect is done, otherwise the next effect will be aborted immediately.
+ * For example, run `abortControllerRef.current = null` in a `finally` block or after an awaited promise.
+ * @param abortControllerRef
+ * @returns {AbortController}
+ */
 export function handleAbortRef(
   abortControllerRef: React.RefObject<AbortController>,
 ): AbortController {
@@ -22,5 +30,6 @@ export function handleAbortRef(
   }
 
   abortControllerRef.current = newController
+
   return newController
 }
