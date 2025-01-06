6705036cfb568d1d9e9c379c55331492c7de927c
Patrik Kozak <patrik@payloadcms.com>
2025-01-06 10:13:36 -0500

    chore: adds test

--- test/uploads/payload-types.ts
+++ test/uploads/payload-types.ts
@@ -741,6 +741,14 @@ export interface Enlarge {
       filesize?: number | null;
       filename?: string | null;
     };
+    undefinedHeightWithoutEnlargement?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
   };
 }
 /**
@@ -1997,6 +2005,16 @@ export interface EnlargeSelect<T extends boolean = true> {
               filename?: T;
             };
         widthLowerHeightLarger?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        undefinedHeightWithoutEnlargement?:
           | T
           | {
               url?: T;
