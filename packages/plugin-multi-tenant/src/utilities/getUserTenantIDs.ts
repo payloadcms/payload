2797e1c2c856312901e0e076aa71f26426ac746f
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-01-09 14:05:04 -0500

    fix: getUserTenantIDs function

--- packages/plugin-multi-tenant/src/utilities/getUserTenantIDs.ts
+++ packages/plugin-multi-tenant/src/utilities/getUserTenantIDs.ts
@@ -6,22 +6,16 @@ import { extractID } from './extractID.js'
  * Returns array of all tenant IDs assigned to a user
  *
  * @param user - User object with tenants field
- * @param role - Optional role to filter by
  */
 export const getUserTenantIDs = <IDType extends number | string>(
   user: null | UserWithTenantsField,
-  role?: string,
 ): IDType[] => {
   if (!user) {
     return []
   }
 
   return (
-    user?.tenants?.reduce<IDType[]>((acc, { roles, tenant }) => {
-      if (role && !roles.includes(role)) {
-        return acc
-      }
-
+    user?.tenants?.reduce<IDType[]>((acc, { tenant }) => {
       if (tenant) {
         acc.push(extractID<IDType>(tenant as Tenant<IDType>))
       }
