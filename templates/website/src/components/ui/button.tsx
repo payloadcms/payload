1b9c7a7e518dfb89317fe56288e2e0495d244c92
Paul Popus <paul@nouance.io>
2025-01-10 17:34:03 -0600

    templates: fix issue with shadcn utilities alias across website template and localization example

--- templates/website/src/components/ui/button.tsx
+++ templates/website/src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import { cn } from 'src/utilities/cn'
+import { cn } from '@/utilities/ui'
 import { Slot } from '@radix-ui/react-slot'
 import { type VariantProps, cva } from 'class-variance-authority'
 import * as React from 'react'
