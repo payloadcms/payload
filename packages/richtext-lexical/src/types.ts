dc6d79e76e834adfe4041ad4606531f9e622d7df
Said Akhrarov <36972061+akhrarovsaid@users.noreply.github.com>
2025-01-12 16:28:10 -0500

    fix(richtext-lexical): adds support for localized placeholder (#10523)
    ### What?
    This PR fixes a minor issue in `richtext-lexical` where editor
    placeholders were not localized.
    
    ### Why?
    To allow users to localize placeholders accordingly with their language
    preferences in config.
    
    ### How?
    By evaluating the placeholder in the editor RSC, if any is provided. The
    `ContentEditable` component falls back to a default in the event that no
    placeholder was provided as this was the existing behavior.
    
    Fixes #10518

--- packages/richtext-lexical/src/types.ts
+++ packages/richtext-lexical/src/types.ts
@@ -2,11 +2,13 @@ import type { EditorConfig as LexicalEditorConfig, SerializedEditorState } from 
 import type {
   ClientField,
   DefaultCellComponentProps,
+  LabelFunction,
   RichTextAdapter,
   RichTextFieldClient,
   RichTextFieldClientProps,
   SanitizedConfig,
   ServerFieldBase,
+  StaticLabel,
 } from 'payload'
 
 import type {
@@ -25,8 +27,12 @@ export type LexicalFieldAdminProps = {
   /**
    * Changes the placeholder text in the editor if no content is present.
    */
-  placeholder?: string
+  placeholder?: LabelFunction | StaticLabel
 }
+
+export type LexicalFieldAdminClientProps = {
+  placeholder?: string
+} & Omit<LexicalFieldAdminProps, 'placeholder'>
 
 export type LexicalEditorProps = {
   admin?: LexicalFieldAdminProps
@@ -92,7 +98,7 @@ export type FeatureClientSchemaMap = {
 }
 
 export type LexicalRichTextFieldProps = {
-  admin: LexicalFieldAdminProps
+  admin?: LexicalFieldAdminClientProps
   // clientFeatures is added through the rsc field
   clientFeatures: {
     [featureKey: string]: {
