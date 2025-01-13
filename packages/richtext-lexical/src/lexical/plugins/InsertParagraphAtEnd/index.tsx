d17f0664e558286aa8e568172862c3f747a59abf
Alessio Gravili <alessio@gravili.de>
2025-01-12 18:53:29 -0700

    feat(richtext-lexical): add new paragraph button below the editor

---
+++ packages/richtext-lexical/src/lexical/plugins/InsertParagraphAtEnd/index.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { $createParagraphNode, $getRoot } from 'lexical'
+
+import './index.scss'
+import { useEditorConfigContext } from '../../config/client/EditorConfigProvider.js'
+const baseClass = 'insert-paragraph-at-end'
+
+export const InsertParagraphAtEndPlugin: React.FC = () => {
+  const [editor] = useLexicalComposerContext()
+  const { editorConfig } = useEditorConfigContext()
+
+  if (editorConfig?.admin?.hideInsertParagraphAtEnd) {
+    return null
+  }
+
+  const onClick = () => {
+    editor.update(() => {
+      const pNode = $createParagraphNode()
+      $getRoot().append(pNode)
+    })
+  }
+
+  return (
+    <div aria-label="Insert Paragraph" className={baseClass} onClick={onClick}>
+      <div className={`${baseClass}-inside`}>
+        <span>+</span>
+      </div>
+    </div>
+  )
+}
