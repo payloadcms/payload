09a1152afbff787c429ea808d5d4921f7038e907
Alessio Gravili <alessio@gravili.de>
2025-01-12 19:13:10 -0700

    disable eslint rules

--- packages/richtext-lexical/src/lexical/plugins/InsertParagraphAtEnd/index.tsx
+++ packages/richtext-lexical/src/lexical/plugins/InsertParagraphAtEnd/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 'use client'
 
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
