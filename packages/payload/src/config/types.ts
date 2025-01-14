34b3a520e767272858992b96c61bdda2e04814f3
Alessio Gravili <alessio@gravili.de>
2025-01-14 10:55:54 -0700

    Update types.ts

--- packages/payload/src/config/types.ts
+++ packages/payload/src/config/types.ts
@@ -801,9 +801,9 @@ export type Config = {
       /**
        * This removes the clientConfig.i18n property from the client config, reducing the amount of data sent to the client.
        *
-       * This will be enabled by default in 4.0, as the `useLocale` hook will be the recommended way to access i18n data.
+       * This will be enabled by default in 4.0, as the `useTranslation` hook will be the recommended way to access i18n data.
        *
-       * @example const { i18n } = useLocale()
+       * @example const { i18n } = useTranslation()
        *
        * @todo Remove in 4.0
        */
