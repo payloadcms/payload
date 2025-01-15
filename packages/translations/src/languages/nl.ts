44927112c5c289b1fff471866b35f6c5e93c6af8
Francisco Lourenço <208149+franciscolourenco@users.noreply.github.com>
2025-01-15 20:27:56 +0800

    chore(translations, next): add translations for changedFieldsCount
    Used in the version view

--- packages/translations/src/languages/nl.ts
+++ packages/translations/src/languages/nl.ts
@@ -447,6 +447,8 @@ export const nlTranslations: DefaultTranslationsObject = {
     autosavedSuccessfully: 'Succesvol automatisch bewaard.',
     autosavedVersion: 'Automatisch bewaarde versie',
     changed: 'Gewijzigd',
+    changedFieldsCount_one: '{{count}} gewijzigd veld',
+    changedFieldsCount_other: '{{count}} gewijzigde velden',
     compareVersion: 'Vergelijk versie met:',
     confirmPublish: 'Bevestig publiceren',
     confirmRevertToSaved: 'Bevestig terugdraaien naar bewaarde versie',
