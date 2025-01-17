05e0fb63b70ac9db20676ba241d96cf6c85418fd
Francisco Lourenço <208149+franciscolourenco@users.noreply.github.com>
2025-01-17 17:08:10 +0800

    feat: make tabs collapsible in versions view

--- packages/next/src/views/Version/RenderFieldsToDiff/fields/Tabs/index.tsx
+++ packages/next/src/views/Version/RenderFieldsToDiff/fields/Tabs/index.tsx
@@ -1,12 +1,13 @@
 'use client'
 import type { TabsFieldClient } from 'payload'
 
+import { getTranslation } from '@payloadcms/translations'
 import React from 'react'
 
 import type { DiffComponentProps } from '../types.js'
 
+import FieldDiffCollapser from '../../FieldDiffCollapser/index.js'
 import RenderFieldsToDiff from '../../index.js'
-import Nested from '../Nested/index.js'
 
 const baseClass = 'tabs-diff'
 
@@ -24,34 +25,36 @@ const Tabs: React.FC<DiffComponentProps<TabsFieldClient>> = ({
     <div className={baseClass}>
       <div className={`${baseClass}__wrap`}>
         {field.tabs.map((tab, i) => {
-          if ('name' in tab) {
-            return (
-              <Nested
-                comparison={comparison?.[tab.name]}
+          const comparisonUnwrapped = 'name' in tab ? comparison?.[tab.name] : comparison
+          const versionUnwrapped = 'name' in tab ? version?.[tab.name] : version
+
+          return (
+            <FieldDiffCollapser
+              comparison={comparisonUnwrapped}
+              key={i}
+              label={
+                'label' in tab &&
+                tab.label &&
+                typeof tab.label !== 'function' && (
+                  <span>
+                    {locale && <span className={`${baseClass}__locale-label`}>{locale}</span>}
+                    {getTranslation(tab.label, i18n)}
+                  </span>
+                )
+              }
+              version={versionUnwrapped}
+            >
+              <RenderFieldsToDiff
+                comparison={comparisonUnwrapped}
                 diffComponents={diffComponents}
-                field={field}
                 fieldPermissions={fieldPermissions}
                 fields={tab.fields}
                 i18n={i18n}
                 key={i}
-                locale={locale}
                 locales={locales}
-                version={version?.[tab.name]}
+                version={versionUnwrapped}
               />
-            )
-          }
-
-          return (
-            <RenderFieldsToDiff
-              comparison={comparison}
-              diffComponents={diffComponents}
-              fieldPermissions={fieldPermissions}
-              fields={tab.fields}
-              i18n={i18n}
-              key={i}
-              locales={locales}
-              version={version}
-            />
+            </FieldDiffCollapser>
           )
         })}
       </div>
