c342dbebb6aaf6af5d3e114777cbc5fc74e49970
Alessio Gravili <alessio@gravili.de>
2025-01-19 00:53:16 -0700

    importmap

--- packages/payload/src/bin/generateImportMap/iterateFields.ts
+++ packages/payload/src/bin/generateImportMap/iterateFields.ts
@@ -46,7 +46,7 @@ export function genImportMapIterateFields({
         addToImportMap,
         baseDir,
         config,
-        fields: field.blocks,
+        fields: field.blocks.filter((block) => typeof block !== 'string'),
         importMap,
         imports,
       })
