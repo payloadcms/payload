9977f6348e0b61fe145d69c48d845c4262855784
Paul Popus <paul@nouance.io>
2025-01-20 10:44:50 -0600

    templates: form fields will now respect 'required' flag from config on website template

--- templates/website/src/blocks/Form/Checkbox/index.tsx
+++ templates/website/src/blocks/Form/Checkbox/index.tsx
@@ -21,8 +21,8 @@ export const Checkbox: React.FC<
     register: UseFormRegister<FieldValues>
     setValue: any
   }
-> = ({ name, defaultValue, errors, label, register, required: requiredFromProps, width }) => {
-  const props = register(name, { required: requiredFromProps })
+> = ({ name, defaultValue, errors, label, register, required, width }) => {
+  const props = register(name, { required: required })
   const { setValue } = useFormContext()
 
   return (
@@ -36,9 +36,16 @@ export const Checkbox: React.FC<
             setValue(props.name, checked)
           }}
         />
-        <Label htmlFor={name}>{label}</Label>
+        <Label htmlFor={name}>
+          {required && (
+            <span className="required">
+              * <span className="sr-only">(required)</span>
+            </span>
+          )}
+          {label}
+        </Label>
       </div>
-      {requiredFromProps && errors[name] && <Error />}
+      {errors[name] && <Error />}
     </Width>
   )
 }
--- templates/website/src/blocks/Form/Text/index.tsx
+++ templates/website/src/blocks/Form/Text/index.tsx
@@ -17,17 +17,20 @@ export const Text: React.FC<
     >
     register: UseFormRegister<FieldValues>
   }
-> = ({ name, defaultValue, errors, label, register, required: requiredFromProps, width }) => {
+> = ({ name, defaultValue, errors, label, register, required, width }) => {
   return (
     <Width width={width}>
-      <Label htmlFor={name}>{label}</Label>
-      <Input
-        defaultValue={defaultValue}
-        id={name}
-        type="text"
-        {...register(name, { required: requiredFromProps })}
-      />
-      {requiredFromProps && errors[name] && <Error />}
+      <Label htmlFor={name}>
+        {label}
+
+        {required && (
+          <span className="required">
+            * <span className="sr-only">(required)</span>
+          </span>
+        )}
+      </Label>
+      <Input defaultValue={defaultValue} id={name} type="text" {...register(name, { required })} />
+      {errors[name] && <Error />}
     </Width>
   )
 }
