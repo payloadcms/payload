b95881c2a32fc9e73bb76aa572590a105873b3bb
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2025-01-22 18:20:03 +0400

    fix: migration filenames filter index files

--- packages/payload/src/database/migrations/readMigrationFiles.ts
+++ packages/payload/src/database/migrations/readMigrationFiles.ts
@@ -28,7 +28,7 @@ export const readMigrationFiles = async ({
     .readdirSync(payload.db.migrationDir)
     .sort()
     .filter((f) => {
-      return (f.endsWith('.ts') || f.endsWith('.js')) && !f.includes('index.')
+      return (f.endsWith('.ts') || f.endsWith('.js')) && f !== 'index.js' && file !== 'index.ts'
     })
     .map((file) => {
       return path.resolve(payload.db.migrationDir, file)
