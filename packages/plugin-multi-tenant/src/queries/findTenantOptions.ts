c4e832889b8a80c26b38c2a17dc20ece5c1601f6
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-01-23 13:14:53 -0500

    fix: adjust tenantOptions select

--- packages/plugin-multi-tenant/src/queries/findTenantOptions.ts
+++ packages/plugin-multi-tenant/src/queries/findTenantOptions.ts
@@ -18,7 +18,7 @@ export const findTenantOptions = async ({
     limit: 1,
     overrideAccess: false,
     select: {
-      id: true,
+      [useAsTitle]: true,
     },
     sort: useAsTitle,
     user,
