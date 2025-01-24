8793e0d326ea59c620e6fe7733f67b094be7c282
Patrik Kozak <patrik@payloadcms.com>
2025-01-24 14:52:07 -0500

    templates: styles blank template landing page

--- templates/blank/src/app/(frontend)/layout.tsx
+++ templates/blank/src/app/(frontend)/layout.tsx
@@ -1,10 +1,5 @@
-import { headers as getHeaders } from 'next/headers.js'
-import { getPayload } from 'payload'
 import React from 'react'
-
-import config from '@/payload.config'
-import { Header } from '@/components/Header'
-import '../../css/app.scss'
+import './globals.scss'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -13,14 +8,10 @@ export const metadata = {
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
-  const headers = await getHeaders()
-  const payload = await getPayload({ config })
-  const { user } = await payload.auth({ headers })
 
   return (
     <html lang="en">
       <body>
-        <Header user={user} />
         <main>{children}</main>
       </body>
     </html>
--- templates/blank/src/app/(frontend)/page.tsx
+++ templates/blank/src/app/(frontend)/page.tsx
@@ -1,14 +1,54 @@
-import React, { Fragment } from 'react'
+import { headers as getHeaders } from 'next/headers.js'
+import Image from 'next/image'
+import { getPayload } from 'payload'
+import React from 'react'
+import { fileURLToPath } from 'url'
 
-import { Gutter } from '@/components/Gutter'
+import config from '@/payload.config'
 import classes from './index.module.scss'
 
 export default async function HomePage() {
+  const headers = await getHeaders()
+  const payload = await getPayload({ config })
+  const { user } = await payload.auth({ headers })
+
+  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
+
   return (
-    <Fragment>
-      <Gutter className={classes.home}>
-        <h1>Payload Blank Template</h1>
-      </Gutter>
-    </Fragment>
+    <div className={classes.home}>
+      <div className={classes.content}>
+        <picture>
+          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
+          <Image
+            alt="Payload Logo"
+            height={13}
+            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
+            width={65}
+          />
+        </picture>
+        {!user && <h1>Welcome to your new project.</h1>}
+        {user && <h1>Welcome back, {user.email} </h1>}
+        <div className={classes.links}>
+          <a className={classes.admin} href="/admin" rel="noopener noreferrer" target="_blank">
+            Go to admin panel
+          </a>
+          <a
+            className={classes.docs}
+            href="https://payloadcms.com/docs"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Documentation
+          </a>
+        </div>
+      </div>
+
+      <p className={classes.footer}>
+        Update this page by editing{' '}
+        <a className={classes.codeLink} href={fileURL}>
+          <code>app/(frontend)/page.tsx</code>
+        </a>
+      </p>
+    </div>
   )
 }
