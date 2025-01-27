3aedb789609e3cbb1592769b9f067b0ef3b1d37c
Patrik Kozak <patrik@payloadcms.com>
2025-01-27 17:06:05 -0500

    chore: flattens css files

--- templates/with-vercel-mongodb/src/app/(frontend)/page.tsx
+++ templates/with-vercel-mongodb/src/app/(frontend)/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
-import classes from './index.module.scss'
+import './globals.css'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -15,8 +15,8 @@ export default async function HomePage() {
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
-    <div className={classes.home}>
-      <div className={classes.content}>
+    <div className="home">
+      <div className="content">
         <picture>
           <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
           <Image
@@ -28,12 +28,12 @@ export default async function HomePage() {
         </picture>
         {!user && <h1>Welcome to your new project.</h1>}
         {user && <h1>Welcome back, {user.email}</h1>}
-        <div className={classes.links}>
-          <a className={classes.admin} href="/admin" rel="noopener noreferrer" target="_blank">
+        <div className="links">
+          <a className="admin" href="/admin" rel="noopener noreferrer" target="_blank">
             Go to admin panel
           </a>
           <a
-            className={classes.docs}
+            className="docs"
             href="https://payloadcms.com/docs"
             rel="noopener noreferrer"
             target="_blank"
@@ -43,9 +43,9 @@ export default async function HomePage() {
         </div>
       </div>
 
-      <p className={classes.footer}>
+      <p className="footer">
         Update this page by editing{' '}
-        <a className={classes.codeLink} href={fileURL}>
+        <a className="codeLink" href={fileURL}>
           <code>app/(frontend)/page.tsx</code>
         </a>
       </p>
