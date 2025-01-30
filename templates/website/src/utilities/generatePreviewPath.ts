cf8fcc2a80926d35f9ecf3c780dcb15c3fdecd04
Jacob Fletcher <jacobsfletch@gmail.com>
2025-01-30 14:36:51 -0500

    simplifies generatePreviewPath

--- templates/website/src/utilities/generatePreviewPath.ts
+++ templates/website/src/utilities/generatePreviewPath.ts
@@ -12,22 +12,16 @@ type Props = {
 }
 
 export const generatePreviewPath = ({ collection, slug, req }: Props) => {
-  const path = `${collectionPrefixMap[collection]}/${slug}`
-
-  const params = {
+  const encodedParams = new URLSearchParams({
     slug,
     collection,
-    path,
-  }
-
-  const encodedParams = new URLSearchParams()
-
-  Object.entries(params).forEach(([key, value]) => {
-    encodedParams.append(key, value)
+    path: `${collectionPrefixMap[collection]}/${slug}`,
+    previewSecret: process.env.PREVIEW_SECRET || '',
   })
 
   const isProduction =
     process.env.NODE_ENV === 'production' || Boolean(process.env.VERCEL_PROJECT_PRODUCTION_URL)
+
   const protocol = isProduction ? 'https:' : req.protocol
 
   const url = `${protocol}//${req.host}/next/preview?${encodedParams.toString()}`
