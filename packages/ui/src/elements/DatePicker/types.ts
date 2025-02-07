65740238f75bd9518a6bbbbf82e783671c8d18fb
Paul Popus <paul@nouance.io>
2025-02-07 13:16:51 +0000

    remove types for datepicker props

--- packages/ui/src/elements/DatePicker/types.ts
+++ packages/ui/src/elements/DatePicker/types.ts
@@ -2,11 +2,8 @@ import type { DayPickerProps, SharedProps, TimePickerProps } from 'payload'
 
 export type Props = {
   onChange?: (val: Date) => void
-  onChangeTimezone?: (val: string) => void
   placeholder?: string
   readOnly?: boolean
-  timezone?: true
-  timezoneValue?: string
   value?: Date | string
 } & DayPickerProps &
   SharedProps &
