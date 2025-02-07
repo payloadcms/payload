4c6698b10e8371d9d7d36ca3b385aa1a5d8ae79c
Jessica Chowdhury <jessica@trbl.design>
2025-02-07 17:31:26 +0000

    chore: attempts to wireup preview table

--- packages/plugin-import-export/src/components/Preview/index.tsx
+++ packages/plugin-import-export/src/components/Preview/index.tsx
@@ -1,14 +1,13 @@
 'use client'
 import type { Column } from '@payloadcms/ui'
 
-import { Table, useConfig, useSelection } from '@payloadcms/ui'
-
-import './index.scss'
-
+import { Table, useConfig, useField } from '@payloadcms/ui'
+import * as qs from 'qs-esm'
 import React from 'react'
 
 import { reduceFields } from '../FieldsToExport/reduceFields.js'
 import { useImportExport } from '../ImportExportProvider/index.js'
+import './index.scss'
 
 const baseClass = 'Preview'
 
@@ -20,9 +19,10 @@ interface Field {
 }
 
 export const Preview = () => {
-  const { collection, columnsToExport, selected } = useImportExport()
+  const { collection, columnsToExport } = useImportExport()
   const { config } = useConfig()
-
+  const { value: whereIncoming } = useField({ path: 'where' })
+  const [dataToRender, setDataToRender] = React.useState<any[]>([])
   const collectionSlug = typeof collection === 'string' && collection
   const collectionConfig = config.collections.find(
     (collection) => collection.slug === collectionSlug,
@@ -30,6 +30,7 @@ export const Preview = () => {
   const reducedFields =
     collectionConfig && (reduceFields({ fields: collectionConfig.fields }) as any)
 
+  // TODO: this might be causing the table issue, need to check if there is a reusable function anywhere to format columns
   const columns: Column[] =
     reducedFields &&
     reducedFields.map((field: Field) => {
@@ -47,13 +48,39 @@ export const Preview = () => {
       }
     })
 
-  const dataToRender = {} as any
+  const whereQuery = qs.stringify(JSON.stringify(whereIncoming), { addQueryPrefix: true })
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      if (!collectionSlug || !whereIncoming) {
+        return
+      }
+
+      try {
+        const response = await fetch(`/api/${collectionSlug}${whereQuery}`, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'GET',
+        })
+
+        if (response.ok) {
+          const data = await response.json()
+          // TODO: check if this data is in the correct format for the table
+          setDataToRender(data.docs)
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    void fetchData()
+  }, [collectionSlug, whereIncoming])
+
   return (
-    <React.Fragment>
-      <div className={baseClass}>
-        <h3>Preview</h3>
-        {/* <Table columns={columns} data={dataToRender} /> */}
-      </div>
-    </React.Fragment>
+    <div className={baseClass}>
+      <h3>Preview</h3>
+      <Table columns={columns} data={dataToRender} />
+    </div>
   )
 }
