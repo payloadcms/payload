d4a5b669fbd302abe5f7405137b9414ae086e36c
Alessio Gravili <alessio@gravili.de>
2025-02-11 13:26:55 -0700

    fix eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ export const defaultESLintIgnores = [
   '**/build/',
   '**/node_modules/',
   '**/temp/',
-  '**/packages/*.spec.ts',
+  'packages/**/*.spec.ts',
   'next-env.d.ts',
   '**/app',
 ]
