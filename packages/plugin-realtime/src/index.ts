82663a265aa32d98fabadaf1e47796c8229a8728
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2025-02-17 15:14:44 -0300

    improvements

--- packages/plugin-realtime/src/index.ts
+++ packages/plugin-realtime/src/index.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type { CollectionAfterChangeHook, CollectionAfterDeleteHook, Config } from 'payload'
+import {
+  type CollectionAfterChangeHook,
+  type CollectionAfterDeleteHook,
+  type Config,
+  type Payload,
+} from 'payload'
 
 // export type MyPluginConfig = {
 //   /**
@@ -11,11 +16,17 @@ export const realtimePlugin =
   (/**pluginOptions: MyPluginConfig */) =>
   (config: Config): Config => {
     const myAfterChangeHook: CollectionAfterChangeHook = (args) => {
+      // If the collection that changed has a registered reactive count, we have to trigger all its count listeners
+      // If the collection that changed has a registered find and satisfies the where of the options, we have to trigger all its find listeners
+      // If the document that changed has findById registered, we have to trigger all its find listeners
       console.log('myAfterChangeHook', args)
       return args
     }
 
     const myAfterDeleteHook: CollectionAfterDeleteHook = (args) => {
+      // If the collection that changed has a registered reactive count, we have to trigger all its count listeners
+      // If the collection that changed has a registered find and satisfies the where of the options, we have to trigger all its find listeners
+      // If the document that changed has findById registered, we have to trigger all its find listeners (set to null)
       console.log('myAfterDeleteHook', args)
       return args
     }
@@ -37,35 +48,30 @@ export const realtimePlugin =
 
 type OnChange<T> = ({ onChange }: { onChange: (value: T) => void }) => T
 
-// type PayloadReactiveType = {
-//   count: (
-//     ...args: [...Parameters<Payload['count']>, OnChange<ReturnType<Payload['count']>>]
-//   ) => ReturnType<Payload['count']>
-//   find: (
-//     ...args: [...Parameters<Payload['find']>, OnChange<ReturnType<Payload['find']>>]
-//   ) => ReturnType<Payload['find']>
-//   findByID: (
-//     ...args: [...Parameters<Payload['findByID']>, OnChange<ReturnType<Payload['findByID']>>]
-//   ) => ReturnType<Payload['findByID']>
-// }
+type PayloadReactiveType = {
+  count: (
+    ...args: [Payload, ...Parameters<Payload['count']>, OnChange<ReturnType<Payload['count']>>]
+  ) => void
+  find: (
+    ...args: [Payload, ...Parameters<Payload['find']>, OnChange<ReturnType<Payload['find']>>]
+  ) => void
+  findByID: (
+    ...args: [
+      Payload,
+      ...Parameters<Payload['findByID']>,
+      OnChange<ReturnType<Payload['findByID']>>,
+    ]
+  ) => void
+}
 
-// export const payloadReactive: PayloadReactiveType = {
-//   count: (options, onChange) => {
-//     return payload.count(options, onChange)
-//   },
-//   find: (...args) => {
-//     // Extract or handle the extra parameter, then use the original payload.find as needed.
-//     const onChange = args.pop() as OnChange<ReturnType<Payload['find']>>
-//     const originalArgs = args as Parameters<Payload['find']>
-//     // return payload.find(...originalArgs)
-//     return {} as ReturnType<Payload['find']>
-//   },
-//   findByID: (...args) => {
-//     const onChange = args.pop() as OnChange<ReturnType<Payload['findByID']>>
-//     const originalArgs = args as Parameters<Payload['findByID']>
-//     // return payload.findByID(...originalArgs)
-//     return {} as ReturnType<Payload['findByID']>
-//   },
-// }
-
-// // usePayloadQuery
+export const payloadReactive: PayloadReactiveType = {
+  count: (payload, options, onChange) => {
+    // payload.count(options)
+  },
+  find: (payload, options, onChange) => {
+    // return payload.find(...originalArgs)
+  },
+  findByID: (payload, options, onChange) => {
+    // return payload.findByID(...originalArgs)
+  },
+}
