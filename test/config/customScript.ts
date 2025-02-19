d64de5f7f665f4332b4324b19e10e3a9bf886a05
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-02-20 01:39:29 +0200

    test: reuse variable

--- test/config/customScript.ts
+++ test/config/customScript.ts
@@ -1,12 +1,13 @@
 import type { SanitizedConfig } from 'payload'
 
 import { writeFileSync } from 'fs'
-import path from 'path'
 import payload from 'payload'
+
+import { testFilePath } from './testFilePath.js'
 
 export const script = async (config: SanitizedConfig) => {
   await payload.init({ config })
   const data = await payload.find({ collection: 'users' })
-  writeFileSync(path.resolve(import.meta.dirname, '_data.json'), JSON.stringify(data), 'utf-8')
+  writeFileSync(testFilePath, JSON.stringify(data), 'utf-8')
   process.exit(0)
 }
