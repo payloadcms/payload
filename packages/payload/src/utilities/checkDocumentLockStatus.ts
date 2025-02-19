7626ff46350fb9476716489e5d8f48f28e50810d
Alessio Gravili <alessio@gravili.de>
2025-02-19 01:26:55 -0700

    test

--- packages/payload/src/utilities/checkDocumentLockStatus.ts
+++ packages/payload/src/utilities/checkDocumentLockStatus.ts
@@ -93,6 +93,7 @@ export const checkDocumentLockStatus = async ({
   // Perform the delete operation regardless of overrideLock status
   await payload.db.deleteMany({
     collection: 'payload-locked-documents',
+    req, //
     where: lockedDocumentQuery,
   })
 }
