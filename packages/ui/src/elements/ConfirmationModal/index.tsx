d3ed239c1795517695987f9c907e58d5a688b6ea
Jacob Fletcher <jacobsfletch@gmail.com>
2025-02-19 08:51:02 -0500

    uses useCallback, adds className, adds ellipsis to default confirming label

--- packages/ui/src/elements/ConfirmationModal/index.tsx
+++ packages/ui/src/elements/ConfirmationModal/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Modal, useModal } from '@faceless-ui/modal'
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import { useEditDepth } from '../../providers/EditDepth/index.js'
 import { useTranslation } from '../../providers/Translation/index.js'
@@ -15,6 +15,7 @@ export type OnCancel = () => void
 export type ConfirmationModalProps = {
   body: React.ReactNode
   cancelLabel?: string
+  className?: string
   confirmingLabel?: string
   confirmLabel?: string
   heading: React.ReactNode
@@ -27,12 +28,13 @@ export function ConfirmationModal(props: ConfirmationModalProps) {
   const {
     body,
     cancelLabel,
+    className,
     confirmingLabel,
     confirmLabel,
     heading,
     modalSlug,
-    onCancel,
-    onConfirm,
+    onCancel: onCancelFromProps,
+    onConfirm: onConfirmFromProps,
   } = props
 
   const editDepth = useEditDepth()
@@ -42,9 +44,32 @@ export function ConfirmationModal(props: ConfirmationModalProps) {
   const { closeModal } = useModal()
   const { t } = useTranslation()
 
+  const onConfirm = useCallback(async () => {
+    if (!confirming) {
+      setConfirming(true)
+
+      if (typeof onConfirmFromProps === 'function') {
+        await onConfirmFromProps()
+      }
+
+      setConfirming(false)
+      closeModal(modalSlug)
+    }
+  }, [confirming, onConfirmFromProps, closeModal, modalSlug])
+
+  const onCancel = useCallback(() => {
+    if (!confirming) {
+      closeModal(modalSlug)
+
+      if (typeof onCancelFromProps === 'function') {
+        onCancelFromProps()
+      }
+    }
+  }, [confirming, onCancelFromProps, closeModal, modalSlug])
+
   return (
     <Modal
-      className={baseClass}
+      className={[baseClass, className].filter(Boolean).join(' ')}
       slug={modalSlug}
       style={{
         zIndex: drawerZBase + editDepth,
@@ -60,35 +85,15 @@ export function ConfirmationModal(props: ConfirmationModalProps) {
             buttonStyle="secondary"
             disabled={confirming}
             id="confirm-cancel"
-            onClick={
-              confirming
-                ? undefined
-                : () => {
-                    closeModal(modalSlug)
-                    if (typeof onCancel === 'function') {
-                      onCancel()
-                    }
-                  }
-            }
+            onClick={onCancel}
             size="large"
             type="button"
           >
             {cancelLabel || t('general:cancel')}
           </Button>
-          <Button
-            id="confirm-action"
-            onClick={async () => {
-              if (!confirming) {
-                setConfirming(true)
-                await onConfirm()
-                setConfirming(false)
-                closeModal(modalSlug)
-              }
-            }}
-            size="large"
-          >
+          <Button id="confirm-action" onClick={onConfirm} size="large">
             {confirming
-              ? confirmingLabel || t('general:loading')
+              ? confirmingLabel || `${t('general:loading')}...`
               : confirmLabel || t('general:confirm')}
           </Button>
         </div>
