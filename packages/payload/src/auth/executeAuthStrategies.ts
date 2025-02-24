ebdf4a3afb54f51f1d0dbf72d1b8095dd3495c94
Alessio Gravili <alessio@gravili.de>
2025-02-24 12:00:10 -0700

    refactor: simplify collection, global and auth operations

--- packages/payload/src/auth/executeAuthStrategies.ts
+++ packages/payload/src/auth/executeAuthStrategies.ts
@@ -1,20 +1,19 @@
-// @ts-strict-ignore
 import type { AuthStrategyFunctionArgs, AuthStrategyResult } from './index.js'
-
 export const executeAuthStrategies = async (
   args: AuthStrategyFunctionArgs,
 ): Promise<AuthStrategyResult> => {
-  return args.payload.authStrategies.reduce(
-    async (accumulatorPromise, strategy) => {
-      const result: AuthStrategyResult = await accumulatorPromise
-      if (!result.user) {
-        // add the configured AuthStrategy `name` to the strategy function args
-        args.strategyName = strategy.name
+  if (!args.payload.authStrategies?.length) {
+    return { user: null }
+  }
 
-        return strategy.authenticate(args)
-      }
+  for (const strategy of args.payload.authStrategies) {
+    // add the configured AuthStrategy `name` to the strategy function args
+    args.strategyName = strategy.name
+
+    const result = await strategy.authenticate(args)
+    if (result.user) {
       return result
-    },
-    Promise.resolve({ user: null }),
-  )
+    }
+  }
+  return { user: null }
 }
