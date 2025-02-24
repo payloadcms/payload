375a030ee2f6339633953a4a02da3ed74d722a3f
Dan Ribbens <dan.ribbens@gmail.com>
2025-02-24 16:25:54 -0500

    chore: fix unsaved changes warning

--- packages/plugin-import-export/src/components/CollectionField/index.tsx
+++ packages/plugin-import-export/src/components/CollectionField/index.tsx
@@ -1,18 +1,22 @@
 'use client'
 import type React from 'react'
 
-import { useField } from '@payloadcms/ui'
+import { useDocumentInfo, useField } from '@payloadcms/ui'
 import { useEffect } from 'react'
 
 import { useImportExport } from '../ImportExportProvider/index.js'
 
 export const CollectionField: React.FC = () => {
+  const { id } = useDocumentInfo()
   const { setValue } = useField({ path: 'collectionSlug' })
   const { collection } = useImportExport()
 
   useEffect(() => {
+    if (id) {
+      return
+    }
     setValue(collection)
-  }, [collection, setValue])
+  }, [id, collection, setValue])
 
   return null
 }
