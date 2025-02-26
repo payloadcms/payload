b9712128d3a8b3c685de17b93c4d4bee5ee69ee2
Jacob Fletcher <jacobsfletch@gmail.com>
2025-02-26 16:07:16 -0500

    removes more unrelated changes

--- test/joins/collections/Categories.ts
+++ test/joins/collections/Categories.ts
@@ -62,133 +62,133 @@ export const Categories: CollectionConfig = {
       on: 'category',
       maxDepth: 1,
     },
-    // {
-    //   name: 'hasManyPosts',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   admin: {
-    //     description: 'Static Description',
-    //   },
-    //   on: 'categories',
-    // },
-    // {
-    //   name: 'hasManyPostsLocalized',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   on: 'categoriesLocalized',
-    // },
-    // {
-    //   name: 'hiddenPosts',
-    //   type: 'join',
-    //   collection: hiddenPostsSlug,
-    //   on: 'category',
-    // },
-    // {
-    //   name: 'group',
-    //   type: 'group',
-    //   fields: [
-    //     {
-    //       name: 'relatedPosts',
-    //       label: 'Related Posts (Group)',
-    //       type: 'join',
-    //       collection: postsSlug,
-    //       on: 'group.category',
-    //       admin: {
-    //         defaultColumns: ['id', 'createdAt', 'title'],
-    //       },
-    //     },
-    //     {
-    //       name: 'camelCasePosts',
-    //       type: 'join',
-    //       collection: postsSlug,
-    //       on: 'group.camelCaseCategory',
-    //     },
-    //   ],
-    // },
-    // {
-    //   name: 'arrayPosts',
-    //   type: 'join',
-    //   collection: 'posts',
-    //   on: 'array.category',
-    // },
-    // {
-    //   name: 'localizedArrayPosts',
-    //   type: 'join',
-    //   collection: 'posts',
-    //   on: 'localizedArray.category',
-    // },
-    // {
-    //   name: 'blocksPosts',
-    //   type: 'join',
-    //   collection: 'posts',
-    //   on: 'blocks.category',
-    // },
-    // {
-    //   name: 'polymorphic',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   on: 'polymorphic',
-    // },
-    // {
-    //   name: 'polymorphics',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   on: 'polymorphics',
-    // },
-    // {
-    //   name: 'localizedPolymorphic',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   on: 'localizedPolymorphic',
-    // },
-    // {
-    //   name: 'localizedPolymorphics',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   on: 'localizedPolymorphics',
-    // },
-    // {
-    //   name: 'singulars',
-    //   type: 'join',
-    //   collection: singularSlug,
-    //   on: 'category',
-    // },
-    // {
-    //   name: 'filtered',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   on: 'category',
-    //   where: {
-    //     isFiltered: { not_equals: true },
-    //   },
-    // },
-    // {
-    //   name: 'joinWithError',
-    //   type: 'join',
-    //   collection: postsSlug,
-    //   on: 'category',
-    //   hooks: {
-    //     beforeValidate: [
-    //       ({ data }) => {
-    //         if (data?.enableErrorOnJoin) {
-    //           throw new ValidationError({
-    //             collection: 'categories',
-    //             errors: [
-    //               {
-    //                 message: 'enableErrorOnJoin is true',
-    //                 path: 'joinWithError',
-    //               },
-    //             ],
-    //           })
-    //         }
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   name: 'enableErrorOnJoin',
-    //   type: 'checkbox',
-    //   defaultValue: false,
-    // },
+    {
+      name: 'hasManyPosts',
+      type: 'join',
+      collection: postsSlug,
+      admin: {
+        description: 'Static Description',
+      },
+      on: 'categories',
+    },
+    {
+      name: 'hasManyPostsLocalized',
+      type: 'join',
+      collection: postsSlug,
+      on: 'categoriesLocalized',
+    },
+    {
+      name: 'hiddenPosts',
+      type: 'join',
+      collection: hiddenPostsSlug,
+      on: 'category',
+    },
+    {
+      name: 'group',
+      type: 'group',
+      fields: [
+        {
+          name: 'relatedPosts',
+          label: 'Related Posts (Group)',
+          type: 'join',
+          collection: postsSlug,
+          on: 'group.category',
+          admin: {
+            defaultColumns: ['id', 'createdAt', 'title'],
+          },
+        },
+        {
+          name: 'camelCasePosts',
+          type: 'join',
+          collection: postsSlug,
+          on: 'group.camelCaseCategory',
+        },
+      ],
+    },
+    {
+      name: 'arrayPosts',
+      type: 'join',
+      collection: 'posts',
+      on: 'array.category',
+    },
+    {
+      name: 'localizedArrayPosts',
+      type: 'join',
+      collection: 'posts',
+      on: 'localizedArray.category',
+    },
+    {
+      name: 'blocksPosts',
+      type: 'join',
+      collection: 'posts',
+      on: 'blocks.category',
+    },
+    {
+      name: 'polymorphic',
+      type: 'join',
+      collection: postsSlug,
+      on: 'polymorphic',
+    },
+    {
+      name: 'polymorphics',
+      type: 'join',
+      collection: postsSlug,
+      on: 'polymorphics',
+    },
+    {
+      name: 'localizedPolymorphic',
+      type: 'join',
+      collection: postsSlug,
+      on: 'localizedPolymorphic',
+    },
+    {
+      name: 'localizedPolymorphics',
+      type: 'join',
+      collection: postsSlug,
+      on: 'localizedPolymorphics',
+    },
+    {
+      name: 'singulars',
+      type: 'join',
+      collection: singularSlug,
+      on: 'category',
+    },
+    {
+      name: 'filtered',
+      type: 'join',
+      collection: postsSlug,
+      on: 'category',
+      where: {
+        isFiltered: { not_equals: true },
+      },
+    },
+    {
+      name: 'joinWithError',
+      type: 'join',
+      collection: postsSlug,
+      on: 'category',
+      hooks: {
+        beforeValidate: [
+          ({ data }) => {
+            if (data?.enableErrorOnJoin) {
+              throw new ValidationError({
+                collection: 'categories',
+                errors: [
+                  {
+                    message: 'enableErrorOnJoin is true',
+                    path: 'joinWithError',
+                  },
+                ],
+              })
+            }
+          },
+        ],
+      },
+    },
+    {
+      name: 'enableErrorOnJoin',
+      type: 'checkbox',
+      defaultValue: false,
+    },
   ],
 }
