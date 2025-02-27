3e4a7f8f702ad8b362d2ce1fa165eda628e21580
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2025-02-27 08:38:51 +0100

    fix: support node subpath imports in importmap

--- packages/payload/src/bin/generateImportMap/utilities/parsePayloadComponent.ts
+++ packages/payload/src/bin/generateImportMap/utilities/parsePayloadComponent.ts
@@ -11,13 +11,14 @@ export function parsePayloadComponent(PayloadComponent: PayloadComponent): {
   const pathAndMaybeExport =
     typeof PayloadComponent === 'string' ? PayloadComponent : PayloadComponent.path
 
-  let path: string | undefined = ''
+  let path: string | undefined = pathAndMaybeExport
   let exportName: string | undefined = 'default'
 
-  if (pathAndMaybeExport?.includes('#')) {
-    ;[path, exportName] = pathAndMaybeExport.split('#')
-  } else {
-    path = pathAndMaybeExport
+  const lastHashIndex = pathAndMaybeExport?.lastIndexOf('#') ?? -1
+
+  if (lastHashIndex > 0) {
+    exportName = pathAndMaybeExport.substring(lastHashIndex + 1)
+    path = pathAndMaybeExport.substring(0, lastHashIndex)
   }
 
   if (typeof PayloadComponent === 'object' && PayloadComponent.exportName) {
