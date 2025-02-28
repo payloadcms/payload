45a7c8b76413662ba173c278572a488dd764d5cb
Michael Bykovski <bykof@me.com>
2025-02-28 22:38:32 +0100

    fix: response headers from authstrategies are now merged together, even if no user was returned (#10883)
    ### What?
    Merges response headers returned from auth strategies
    
    ---------
    
    Co-authored-by: Jarrod Flesch <jarrodmflesch@gmail.com>

--- packages/payload/src/auth/executeAuthStrategies.ts
+++ packages/payload/src/auth/executeAuthStrategies.ts
@@ -1,19 +1,31 @@
 import type { AuthStrategyFunctionArgs, AuthStrategyResult } from './index.js'
+
+import { mergeHeaders } from '../utilities/mergeHeaders.js'
 export const executeAuthStrategies = async (
   args: AuthStrategyFunctionArgs,
 ): Promise<AuthStrategyResult> => {
+  let result: AuthStrategyResult = { user: null }
+
   if (!args.payload.authStrategies?.length) {
-    return { user: null }
+    return result
   }
 
   for (const strategy of args.payload.authStrategies) {
     // add the configured AuthStrategy `name` to the strategy function args
     args.strategyName = strategy.name
 
-    const result = await strategy.authenticate(args)
+    const authResult = await strategy.authenticate(args)
+    if (authResult.responseHeaders) {
+      authResult.responseHeaders = mergeHeaders(
+        result.responseHeaders || new Headers(),
+        authResult.responseHeaders || new Headers(),
+      )
+    }
+    result = authResult
+
     if (result.user) {
       return result
     }
   }
-  return { user: null }
+  return result
 }
