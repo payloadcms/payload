432c63147d5b61c6c9947b21c1f2675d0a3bddfa
Patrik Kozak <patrik@payloadcms.com>
2025-02-27 20:41:15 -0500

    fix: adds e2e tests

--- test/uploads/payload-types.ts
+++ test/uploads/payload-types.ts
@@ -1002,6 +1002,8 @@ export interface Uploads1 {
   id: string;
   hasManyUpload?: (string | Uploads2)[] | null;
   singleUpload?: (string | null) | Uploads2;
+  hasManyThumbnailUpload?: (string | AdminThumbnailSize)[] | null;
+  singleThumbnailUpload?: (string | null) | AdminThumbnailSize;
   richText?: {
     root: {
       type: string;
@@ -1051,42 +1053,6 @@ export interface Uploads2 {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "admin-thumbnail-function".
- */
-export interface AdminThumbnailFunction {
-  id: string;
-  updatedAt: string;
-  createdAt: string;
-  url?: string | null;
-  thumbnailURL?: string | null;
-  filename?: string | null;
-  mimeType?: string | null;
-  filesize?: number | null;
-  width?: number | null;
-  height?: number | null;
-  focalX?: number | null;
-  focalY?: number | null;
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
- * via the `definition` "admin-thumbnail-with-search-queries".
- */
-export interface AdminThumbnailWithSearchQuery {
-  id: string;
-  updatedAt: string;
-  createdAt: string;
-  url?: string | null;
-  thumbnailURL?: string | null;
-  filename?: string | null;
-  mimeType?: string | null;
-  filesize?: number | null;
-  width?: number | null;
-  height?: number | null;
-  focalX?: number | null;
-  focalY?: number | null;
-}
-/**
- * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "admin-thumbnail-size".
  */
 export interface AdminThumbnailSize {
@@ -1120,6 +1086,42 @@ export interface AdminThumbnailSize {
       filename?: string | null;
     };
   };
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "admin-thumbnail-function".
+ */
+export interface AdminThumbnailFunction {
+  id: string;
+  updatedAt: string;
+  createdAt: string;
+  url?: string | null;
+  thumbnailURL?: string | null;
+  filename?: string | null;
+  mimeType?: string | null;
+  filesize?: number | null;
+  width?: number | null;
+  height?: number | null;
+  focalX?: number | null;
+  focalY?: number | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "admin-thumbnail-with-search-queries".
+ */
+export interface AdminThumbnailWithSearchQuery {
+  id: string;
+  updatedAt: string;
+  createdAt: string;
+  url?: string | null;
+  thumbnailURL?: string | null;
+  filename?: string | null;
+  mimeType?: string | null;
+  filesize?: number | null;
+  width?: number | null;
+  height?: number | null;
+  focalX?: number | null;
+  focalY?: number | null;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -2344,6 +2346,8 @@ export interface ExternallyServedMediaSelect<T extends boolean = true> {
 export interface Uploads1Select<T extends boolean = true> {
   hasManyUpload?: T;
   singleUpload?: T;
+  hasManyThumbnailUpload?: T;
+  singleThumbnailUpload?: T;
   richText?: T;
   updatedAt?: T;
   createdAt?: T;
