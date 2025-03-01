fb29c24f38ec1698b1f119976ead7599fada0a72
Vincent Vu <vincent@rubixstudios.com.au>
2025-03-02 04:48:50 +1100

    Update page.tsx

--- examples/tailwind-shadcn-ui/src/app/(app)/page.tsx
+++ examples/tailwind-shadcn-ui/src/app/(app)/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Page: NextPage = () => {
   return (
-    <div className="container">
+    <div className="w-full max-w-7xl mx-auto px-4">
       <h1 className="text-4xl font-bold">Hello, Next.js 14!</h1>
     </div>
   )
