0052653ca31402325d9c18e0693ba3da9f5ab10d
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2025-03-01 19:39:30 +0100

    Extract extractor to utilities

--- packages/payload/src/bin/generateImportMap/utilities/parsePayloadComponent.ts
+++ packages/payload/src/bin/generateImportMap/utilities/parsePayloadComponent.ts
@@ -1,5 +1,7 @@
 import type { PayloadComponent } from '../../../config/types.js'
 
+import { extractPathAndExportName } from '../../../utilities/extractPathAndExportName.js'
+
 export function parsePayloadComponent(PayloadComponent: PayloadComponent): {
   exportName: string
   path: string
@@ -11,19 +13,15 @@ export function parsePayloadComponent(PayloadComponent: PayloadComponent): {
   const pathAndMaybeExport =
     typeof PayloadComponent === 'string' ? PayloadComponent : PayloadComponent.path
 
-  let path: string | undefined = pathAndMaybeExport
-  let exportName: string | undefined = 'default'
+  const { exportName: possibleExport, path } = extractPathAndExportName(
+    pathAndMaybeExport,
+    'default',
+  )
 
-  const lastHashIndex = pathAndMaybeExport?.lastIndexOf('#') ?? -1
-
-  if (lastHashIndex > 0) {
-    exportName = pathAndMaybeExport.substring(lastHashIndex + 1)
-    path = pathAndMaybeExport.substring(0, lastHashIndex)
-  }
-
+  let exportName = possibleExport
   if (typeof PayloadComponent === 'object' && PayloadComponent.exportName) {
     exportName = PayloadComponent.exportName
   }
 
-  return { exportName: exportName!, path: path! }
+  return { exportName: exportName!, path }
 }
