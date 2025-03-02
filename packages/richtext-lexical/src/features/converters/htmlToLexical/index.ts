fea5e3bdb6ebd1e0fbb7037d0f2ad792d599d930
Alessio Gravili <alessio@gravili.de>
2025-03-01 20:19:45 -0700

    docs

--- packages/richtext-lexical/src/features/converters/htmlToLexical/index.ts
+++ packages/richtext-lexical/src/features/converters/htmlToLexical/index.ts
@@ -15,7 +15,6 @@ export const convertHTMLToLexical = <TNodeTypes extends SerializedLexicalNode = 
 }: {
   editorConfig: SanitizedServerEditorConfig
   html: string
-  // jsdom package constructor that accepts a string. type this so that it acceots constructor
   JSDOM: new (html: string) => {
     window: {
       document: Document
@@ -30,16 +29,12 @@ export const convertHTMLToLexical = <TNodeTypes extends SerializedLexicalNode = 
 
   headlessEditor.update(
     () => {
-      // In a headless environment you can use a package such as JSDom to parse the HTML string.
       const dom = new JSDOM(html)
 
-      // Once you have the DOM instance it's easy to generate LexicalNodes.
       const nodes = $generateNodesFromDOM(headlessEditor, dom.window.document)
 
-      // Select the root
       $getRoot().select()
 
-      // Insert them at a selection.
       const selection = $getSelection()
       if (selection === null) {
         throw new Error('Selection is null')
@@ -49,7 +44,6 @@ export const convertHTMLToLexical = <TNodeTypes extends SerializedLexicalNode = 
     { discrete: true },
   )
 
-  // Do this if you then want to get the editor JSON
   const editorJSON = headlessEditor.getEditorState().toJSON()
 
   return editorJSON as TypedEditorState<TNodeTypes>
