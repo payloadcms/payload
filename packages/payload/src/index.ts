3d38ae47a9d51378027a5df1e457d703db0e6c9d
Alessio Gravili <alessio@gravili.de>
2025-03-03 08:47:28 -0700

    export jobAfterRead

--- packages/payload/src/index.ts
+++ packages/payload/src/index.ts
@@ -1386,7 +1386,9 @@ export type {
   PreferenceUpdateRequest,
   TabsPreferences,
 } from './preferences/types.js'
+export { jobAfterRead } from './queues/config/jobsCollection.js'
 export type { JobsConfig, RunJobAccess, RunJobAccessArgs } from './queues/config/types/index.js'
+
 export type {
   RunInlineTaskFunction,
   RunTaskFunction,
