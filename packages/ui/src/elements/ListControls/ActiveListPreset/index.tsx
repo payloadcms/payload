20b54ee945155de9ab21a30f53e5767dc4e639c8
Jacob Fletcher <jacobsfletch@gmail.com>
2025-03-03 15:19:28 -0500

    refine ui

--- packages/ui/src/elements/ListControls/ActiveListPreset/index.tsx
+++ packages/ui/src/elements/ListControls/ActiveListPreset/index.tsx
@@ -18,7 +18,7 @@ export function ActiveListPreset({
 }) {
   return (
     <Pill
-      className={baseClass}
+      className={[baseClass, activePreset && `${baseClass}--active`].filter(Boolean).join(' ')}
       onClick={() => {
         openPresetListDrawer()
       }}
