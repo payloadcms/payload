0977a9c6172054361be50189d16a7d1581e51ddb
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-03-06 15:28:48 +0200

    Revert "chore: change `Map` to `WeakMap` for caching `flattenAllFields`"
    This reverts commit 218d781092d207c0c3ce91d172fbcbe82f25acfc.

--- packages/payload/src/utilities/flattenAllFields.ts
+++ packages/payload/src/utilities/flattenAllFields.ts
@@ -16,7 +16,7 @@ export const flattenBlock = ({ block }: { block: Block }): FlattenedBlock => {
   }
 }
 
-const flattenedFieldsCache = new WeakMap<Field[], FlattenedField[]>()
+const flattenedFieldsCache = new Map<Field[], FlattenedField[]>()
 
 export const flattenAllFields = ({
   cache,
