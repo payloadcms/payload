48115311e725b293be49a0a381fbedcfb01d42c6
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2025-03-06 14:02:10 -0500

    fix(ui): incorrect error states (#11574)
    Fixes https://github.com/payloadcms/payload/issues/11568
    
    ### What? Out of sync errors states
    - Collaspibles & Tabs were not reporting accurate child error counts
    - Arrays could get into a state where they would not update their error
    states
    - Slight issue with toasts 
    
    ### Tabs & Collapsibles
    The logic for determining matching field paths was not functioning as
    intended. Fields were attempting to match with paths such as `_index-0`
    which will not work.
    
    ### Arrays
    The form state was not updating when the server sent back errorPaths.
    This PR adds `errorPaths` to `serverPropsToAccept`.
    
    ### Toasts
    Some toasts could report errors in the form of `my > > error`. This
    ensures they will be `my > error`
    
    ### Misc
    Removes 2 files that were not in use:
    - `getFieldStateFromPaths.ts`
    - `getNestedFieldState.ts`

--- packages/ui/src/forms/Form/mergeServerFormState.ts
+++ packages/ui/src/forms/Form/mergeServerFormState.ts
@@ -31,6 +31,7 @@ export const mergeServerFormState = ({
       'passesCondition',
       'valid',
       'errorMessage',
+      'errorPaths',
       'rows',
       'customComponents',
       'requiresRender',
