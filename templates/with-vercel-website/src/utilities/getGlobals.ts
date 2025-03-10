4de1fa96b810acf027350cf3b6236fd543ef32df
pierrecabriere <hello@pierrecabriere.fr>
2025-03-10 17:27:14 -0400

    add type detection on getGlobal utility

--- templates/with-vercel-website/src/utilities/getGlobals.ts
+++ templates/with-vercel-website/src/utilities/getGlobals.ts
@@ -1,12 +1,13 @@
 import type { Config } from 'src/payload-types'
 
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
+import configPromise from '@payload.config'
 import { unstable_cache } from 'next/cache'
+import { type DataFromGlobalSlug, getPayload } from 'payload'
+
 
 type Global = keyof Config['globals']
 
-async function getGlobal(slug: Global, depth = 0) {
+async function getGlobal<T extends Global>(slug: T, depth = 0): Promise<DataFromGlobalSlug<T>> {
   const payload = await getPayload({ config: configPromise })
 
   const global = await payload.findGlobal({
@@ -20,7 +21,7 @@ async function getGlobal(slug: Global, depth = 0) {
 /**
  * Returns a unstable_cache function mapped with the cache tag for the slug
  */
-export const getCachedGlobal = (slug: Global, depth = 0) =>
-  unstable_cache(async () => getGlobal(slug, depth), [slug], {
+export const getCachedGlobal = <T extends Global>(slug: T, depth = 0) =>
+  unstable_cache(async () => getGlobal<T>(slug, depth), [slug], {
     tags: [`global_${slug}`],
   })
