72efc843cc11cd128ebfd0108b7c4db4861ace04
Paul <paul@payloadcms.com>
2025-03-10 17:42:30 +0000

    templates: fix issue with populateAuthors hook breaking live-preview on website template (#11608)
    Fixes https://github.com/payloadcms/payload/issues/11468
    
    The populateAuthors hook could break live preview if it returned a
    notFound error as we didn't catch these properly

--- templates/website/src/collections/Posts/hooks/populateAuthors.ts
+++ templates/website/src/collections/Posts/hooks/populateAuthors.ts
@@ -6,26 +6,31 @@ import { User } from 'src/payload-types'
 // GraphQL will not return mutated user data that differs from the underlying schema
 // So we use an alternative `populatedAuthors` field to populate the user data, hidden from the admin UI
 export const populateAuthors: CollectionAfterReadHook = async ({ doc, req, req: { payload } }) => {
-  if (doc?.authors) {
+  if (doc?.authors && doc?.authors?.length > 0) {
     const authorDocs: User[] = []
 
     for (const author of doc.authors) {
-      const authorDoc = await payload.findByID({
-        id: typeof author === 'object' ? author?.id : author,
-        collection: 'users',
-        depth: 0,
-        req,
-      })
+      try {
+        const authorDoc = await payload.findByID({
+          id: typeof author === 'object' ? author?.id : author,
+          collection: 'users',
+          depth: 0,
+        })
 
-      if (authorDoc) {
-        authorDocs.push(authorDoc)
+        if (authorDoc) {
+          authorDocs.push(authorDoc)
+        }
+
+        if (authorDocs.length > 0) {
+          doc.populatedAuthors = authorDocs.map((authorDoc) => ({
+            id: authorDoc.id,
+            name: authorDoc.name,
+          }))
+        }
+      } catch {
+        // swallow error
       }
     }
-
-    doc.populatedAuthors = authorDocs.map((authorDoc) => ({
-      id: authorDoc.id,
-      name: authorDoc.name,
-    }))
   }
 
   return doc
