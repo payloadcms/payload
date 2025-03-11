4f4fd69b68880d8bba28bbc9d3f34cf9b15daf7c
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2025-03-11 15:21:43 -0300

    feat(richtext-lexical): allow disabling indentation for specific nodes

--- packages/richtext-lexical/src/features/indent/server/index.ts
+++ packages/richtext-lexical/src/features/indent/server/index.ts
@@ -1,10 +1,31 @@
 import { createServerFeature } from '../../../utilities/createServerFeature.js'
 import { i18n } from './i18n.js'
 
-export const IndentFeature = createServerFeature({
-  feature: {
-    ClientFeature: '@payloadcms/richtext-lexical/client#IndentFeatureClient',
-    i18n,
+export type IndentFeatureProps = {
+  /**
+   * The nodes that should not be indented. "type" property of the nodes you don't want to be indented.
+   * These can be: "paragraph", "heading", "listitem", "quote" or other indentable nodes if they exist.
+   */
+  disabledNodes?: string[]
+}
+
+/**
+ * This feature is used to indent and outdent the text in the editor.
+ *
+ * @param props - The props for the feature.
+ * @param props.disabledNodes - The nodes that should not be indented. "type" property of the nodes you don't want to be indented. These can be: "paragraph", "heading", "listitem", "quote" or other indentable nodes if they exist.
+ */
+export const IndentFeature = createServerFeature<
+  IndentFeatureProps,
+  IndentFeatureProps,
+  IndentFeatureProps
+>({
+  feature: ({ props }) => {
+    return {
+      ClientFeature: '@payloadcms/richtext-lexical/client#IndentFeatureClient',
+      clientFeatureProps: props,
+      i18n,
+    }
   },
   key: 'indent',
 })
