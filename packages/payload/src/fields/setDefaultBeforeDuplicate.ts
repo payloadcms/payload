ee2a502199ad681012176583391d4630b173fa55
Jessica Chowdhury <jessica@trbl.design>
2025-03-11 17:18:11 +0000

    fix: handles unique fields during duplication

--- packages/payload/src/fields/setDefaultBeforeDuplicate.ts
+++ packages/payload/src/fields/setDefaultBeforeDuplicate.ts
@@ -1,15 +1,14 @@
 // @ts-strict-ignore
 // default beforeDuplicate hook for required and unique fields
 import { type FieldAffectingData, type FieldHook, fieldShouldBeLocalized } from './config/types.js'
+import { handleUniqueFields } from './utilities/handleUniqueFields.ts.js'
 
 const hasValue = (value) => typeof value === 'string' && value.trim() !== ''
-
 const unique: FieldHook = ({ value }) => (hasValue(value) ? `${value} - Copy` : undefined)
 const localizedUnique: FieldHook = ({ req, value }) =>
   hasValue(value) ? `${value} - ${req?.t('general:copy') ?? 'Copy'}` : undefined
-const uniqueRequired: FieldHook = ({ value }) => (hasValue(value) ? `${value} - Copy` : undefined)
-const localizedUniqueRequired: FieldHook = ({ req, value }) =>
-  hasValue(value) ? `${value} - ${req?.t('general:copy') ?? 'Copy'}` : undefined
+const numberUnique: FieldHook = ({ value }) => (value ? `${value + 1}` : undefined)
+const resetUnique: FieldHook = () => undefined
 
 export const setDefaultBeforeDuplicate = (
   field: FieldAffectingData,
@@ -20,16 +19,18 @@ export const setDefaultBeforeDuplicate = (
     (!field.hooks?.beforeDuplicate ||
       (Array.isArray(field.hooks.beforeDuplicate) && field.hooks.beforeDuplicate.length === 0))
   ) {
-    if ((field.type === 'text' || field.type === 'textarea') && field.required && field.unique) {
-      field.hooks.beforeDuplicate = [
-        fieldShouldBeLocalized({ field, parentIsLocalized })
-          ? localizedUniqueRequired
-          : uniqueRequired,
-      ]
-    } else if (field.unique) {
-      field.hooks.beforeDuplicate = [
-        fieldShouldBeLocalized({ field, parentIsLocalized }) ? localizedUnique : unique,
-      ]
+    if (field.unique) {
+      const updateStrategy = handleUniqueFields(field.type)
+
+      const strategyHooks = {
+        append: [fieldShouldBeLocalized({ field, parentIsLocalized }) ? localizedUnique : unique],
+        numericallyAppend: [numberUnique],
+        undefined: [resetUnique],
+      }
+
+      if (strategyHooks[updateStrategy]) {
+        field.hooks.beforeDuplicate = strategyHooks[updateStrategy]
+      }
     }
   }
 }
