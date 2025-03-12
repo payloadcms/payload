f9570793f858cfecdfee705155f1002af3dbf9f3
Slava Nossar <slava.nossar@gmail.com>
2025-03-12 17:19:58 +1100

    use closeDrawer for explicit closing

--- packages/ui/src/elements/AddNewRelation/index.tsx
+++ packages/ui/src/elements/AddNewRelation/index.tsx
@@ -46,11 +46,10 @@ export const AddNewRelation: React.FC<Props> = ({
   const { i18n, t } = useTranslation()
   const [showTooltip, setShowTooltip] = useState(false)
 
-  const [DocumentDrawer, DocumentDrawerToggler, { isDrawerOpen, toggleDrawer }] = useDocumentDrawer(
-    {
+  const [DocumentDrawer, DocumentDrawerToggler, { closeDrawer, isDrawerOpen, toggleDrawer }] =
+    useDocumentDrawer({
       collectionSlug: collectionConfig?.slug,
-    },
-  )
+    })
 
   const onSave: DocumentDrawerContextType['onSave'] = useCallback(
     ({ doc, operation }) => {
@@ -91,10 +90,10 @@ export const AddNewRelation: React.FC<Props> = ({
       }
 
       if (closeOnSave) {
-        toggleDrawer()
+        closeDrawer()
       }
     },
-    [relationTo, collectionConfig, closeOnSave, hasMany, setValue, value, toggleDrawer],
+    [relationTo, collectionConfig, closeOnSave, hasMany, setValue, value, closeDrawer],
   )
 
   const onPopupToggle = useCallback((state) => {
