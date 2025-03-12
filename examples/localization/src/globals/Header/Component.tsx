985bd198a166d8eb54d29ca0b1f545e747d48f21
pierrecabriere <hello@pierrecabriere.fr>
2025-03-12 09:19:32 -0400

    clean getCachedGlobal types

--- examples/localization/src/globals/Header/Component.tsx
+++ examples/localization/src/globals/Header/Component.tsx
@@ -2,11 +2,10 @@ import { HeaderClient } from './Component.client'
 import { getCachedGlobal } from '@/utilities/getGlobals'
 import React from 'react'
 
-import type { Header } from '@/payload-types'
 import { TypedLocale } from 'payload'
 
 export async function Header({ locale }: { locale: TypedLocale }) {
-  const header: Header = await getCachedGlobal('header', 1, locale)()
+  const header = await getCachedGlobal('header', 1, locale)()
 
   return <HeaderClient header={header} />
 }
