7663566fa2d67faf5e2b93a2d5f5907fcb8eabba
Jacob Fletcher <jacobsfletch@gmail.com>
2025-03-14 12:22:39 -0400

    applies select to default value traversal

--- packages/ui/src/forms/fieldSchemasToFormState/calculateDefaultValues/promise.ts
+++ packages/ui/src/forms/fieldSchemasToFormState/calculateDefaultValues/promise.ts
@@ -9,7 +9,7 @@ import type {
   User,
 } from 'payload'
 
-import { getDefaultValue, stripUnselectedFields } from 'payload'
+import { getBlockSelect, getDefaultValue, stripUnselectedFields } from 'payload'
 import { fieldAffectsData, tabHasName } from 'payload/shared'
 
 import { iterateFields } from './iterateFields.js'
@@ -78,6 +78,7 @@ export const defaultValuePromise = async <T>({
 
       if (Array.isArray(rows)) {
         const promises = []
+        const arraySelect = select?.[field.name]
 
         rows.forEach((row) => {
           promises.push(
@@ -87,7 +88,7 @@ export const defaultValuePromise = async <T>({
               fields: field.fields,
               locale,
               req,
-              select,
+              select: typeof arraySelect === 'object' ? arraySelect : undefined,
               selectMode,
               siblingData: row,
               user,
@@ -105,13 +106,21 @@ export const defaultValuePromise = async <T>({
 
       if (Array.isArray(rows)) {
         const promises = []
+
         rows.forEach((row) => {
           const blockTypeToMatch: string = row.blockType
+
           const block =
             req.payload.blocks[blockTypeToMatch] ??
             ((field.blockReferences ?? field.blocks).find(
               (blockType) => typeof blockType !== 'string' && blockType.slug === blockTypeToMatch,
             ) as FlattenedBlock | undefined)
+
+          const { blockSelect, blockSelectMode } = getBlockSelect({
+            block,
+            select: select?.[field.name],
+            selectMode,
+          })
 
           if (block) {
             row.blockType = blockTypeToMatch
@@ -123,8 +132,8 @@ export const defaultValuePromise = async <T>({
                 fields: block.fields,
                 locale,
                 req,
-                select,
-                selectMode,
+                select: typeof blockSelect === 'object' ? blockSelect : undefined,
+                selectMode: blockSelectMode,
                 siblingData: row,
                 user,
               }),
@@ -160,13 +169,15 @@ export const defaultValuePromise = async <T>({
 
       const groupData = siblingData[field.name] as Record<string, unknown>
 
+      const groupSelect = select?.[field.name]
+
       await iterateFields({
         id,
         data,
         fields: field.fields,
         locale,
         req,
-        select,
+        select: typeof groupSelect === 'object' ? groupSelect : undefined,
         selectMode,
         siblingData: groupData,
         user,
@@ -177,14 +188,24 @@ export const defaultValuePromise = async <T>({
 
     case 'tab': {
       let tabSiblingData
-      if (tabHasName(field)) {
+
+      const isNamedTab = tabHasName(field)
+
+      let tabSelect: SelectType | undefined
+
+      if (isNamedTab) {
         if (typeof siblingData[field.name] !== 'object') {
           siblingData[field.name] = {}
         }
 
         tabSiblingData = siblingData[field.name] as Record<string, unknown>
+
+        if (typeof select?.[field.name] === 'object') {
+          tabSelect = select?.[field.name] as SelectType
+        }
       } else {
         tabSiblingData = siblingData
+        tabSelect = select
       }
 
       await iterateFields({
@@ -193,7 +214,7 @@ export const defaultValuePromise = async <T>({
         fields: field.fields,
         locale,
         req,
-        select,
+        select: tabSelect,
         selectMode,
         siblingData: tabSiblingData,
         user,
