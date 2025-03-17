65b18706eac1655a355066d9c7b4bb5fd9199e6b
Ivica Batinić <ivicabatinic123@gmail.com>
2025-03-17 12:52:26 +0100

    feat: add support for additional HTML properties in RootLayout

--- packages/next/src/layouts/Root/index.tsx
+++ packages/next/src/layouts/Root/index.tsx
@@ -25,11 +25,13 @@ export const RootLayout = async ({
   config: configPromise,
   importMap,
   serverFunction,
+  htmlProps = {},
 }: {
   readonly children: React.ReactNode
   readonly config: Promise<SanitizedConfig>
   readonly importMap: ImportMap
   readonly serverFunction: ServerFunctionClient
+  readonly htmlProps?: Omit<React.DetailedHTMLProps<React.HtmlHTMLAttributes<HTMLHtmlElement>, HTMLHtmlElement>, 'suppressHydrationWarning' | 'lang' | 'dir'>
 }) => {
   checkDependencies()
 
@@ -105,6 +107,7 @@ export const RootLayout = async ({
       dir={dir}
       lang={languageCode}
       suppressHydrationWarning={config?.admin?.suppressHydrationWarning ?? false}
+      {...htmlProps}
     >
       <head>
         <style>{`@layer payload-default, payload;`}</style>
