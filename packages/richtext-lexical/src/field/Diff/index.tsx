bb9951451ad23a1d52e578983411c865e50c66df
Alessio Gravili <alessio@gravili.de>
2025-03-17 13:00:09 -0600

    styling

--- packages/richtext-lexical/src/field/Diff/index.tsx
+++ packages/richtext-lexical/src/field/Diff/index.tsx
@@ -1,12 +1,30 @@
+import type { SerializedEditorState } from 'lexical'
 import type { RichTextFieldDiffServerComponent } from 'payload'
 
 import { getTranslation } from '@payloadcms/translations'
 import { FieldDiffLabel } from '@payloadcms/ui/shared'
 import React from 'react'
+
+import './htmlDiff/index.scss'
+import './index.scss'
+import { convertLexicalToHTML } from '../../features/converters/html/sync/index.js'
+import { HtmlDiff } from './htmlDiff/index.js'
 const baseClass = 'lexical-diff'
 
 export const LexicalDiffComponent: RichTextFieldDiffServerComponent = (args) => {
-  const { field, i18n, locale } = args
+  const { comparisonValue, field, i18n, locale, versionValue } = args
+
+  const comparisonHTML = convertLexicalToHTML({
+    data: comparisonValue as SerializedEditorState,
+  })
+
+  const versionHTML = convertLexicalToHTML({
+    data: versionValue as SerializedEditorState,
+  })
+
+  const diffHTML = new HtmlDiff(comparisonHTML, versionHTML)
+
+  const [oldHTML, newHTML] = diffHTML.getSideBySideContents()
 
   return (
     <div className={baseClass}>
@@ -16,7 +34,14 @@ export const LexicalDiffComponent: RichTextFieldDiffServerComponent = (args) => 
           typeof field.label !== 'function' &&
           getTranslation(field.label || '', i18n)}
       </FieldDiffLabel>
-      <code>Diff goes here</code>
+      <div className={`${baseClass}__diff-container`}>
+        {oldHTML && (
+          <div className={`${baseClass}__diff-old`} dangerouslySetInnerHTML={{ __html: oldHTML }} />
+        )}
+        {newHTML && (
+          <div className={`${baseClass}__diff-new`} dangerouslySetInnerHTML={{ __html: newHTML }} />
+        )}
+      </div>
     </div>
   )
 }
