01de4efe33a3628fb69d8bbdcde85c025156f567
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2025-03-17 09:19:36 -0300

    Improve the toggle sort button and extract logic to a custom hook

--- packages/ui/src/elements/SortHeader/index.tsx
+++ packages/ui/src/elements/SortHeader/index.tsx
@@ -1,7 +1,6 @@
 'use client'
-import type { StaticLabel } from 'payload'
 
-import React from 'react'
+import React, { useRef } from 'react'
 
 import { SortDownIcon, SortUpIcon } from '../../icons/Sort/index.js'
 import { useListQuery } from '../../providers/ListQuery/index.js'
@@ -15,25 +14,29 @@ export type SortHeaderProps = {
 
 const baseClass = 'sort-header'
 
+function useSort() {
+  const { handleSortChange, query } = useListQuery()
+  const sort = useRef<'asc' | 'desc'>(query.sort === '-_order' ? 'desc' : 'asc')
+  const isActive = query.sort === '-_order' || query.sort === '_order'
+
+  const handleSortPress = () => {
+    // If it's already sorted by the "_order" field, toggle between "asc" and "desc"
+    if (isActive) {
+      void handleSortChange(sort.current === 'asc' ? '-_order' : '_order')
+      sort.current = sort.current === 'asc' ? 'desc' : 'asc'
+      return
+    }
+    // If NOT sorted by the "_order" field, sort by that field but do not toggle the current value of "asc" or "desc".
+    void handleSortChange(sort.current === 'asc' ? '_order' : '-_order')
+  }
+
+  return { handleSortPress, isActive, sort }
+}
+
 export const SortHeader: React.FC<SortHeaderProps> = (props) => {
   const { appearance } = props
-  const { handleSortChange, query } = useListQuery()
+  const { handleSortPress, isActive, sort } = useSort()
   const { t } = useTranslation()
-
-  const { sort } = query
-
-  const desc = `-_order`
-  const asc = '_order'
-
-  const ascClasses = [`${baseClass}__asc`]
-  if (sort === asc) {
-    ascClasses.push(`${baseClass}--active`)
-  }
-
-  const descClasses = [`${baseClass}__desc`]
-  if (sort === desc) {
-    descClasses.push(`${baseClass}--active`)
-  }
 
   return (
     <div
@@ -42,14 +45,14 @@ export const SortHeader: React.FC<SortHeaderProps> = (props) => {
         .join(' ')}
     >
       <div className={`${baseClass}__buttons`}>
-        {sort === desc ? (
+        {sort.current === 'desc' ? (
           <button
             aria-label={t('general:sortByLabelDirection', {
               direction: t('general:ascending'),
               label: 'Order',
             })}
-            className={[...descClasses, `${baseClass}__button`].filter(Boolean).join(' ')}
-            onClick={() => void handleSortChange(asc)}
+            className={`${baseClass}__button ${baseClass}__${sort.current} ${isActive ? `${baseClass}--active` : ''}`}
+            onClick={handleSortPress}
             type="button"
           >
             <SortDownIcon />
@@ -60,8 +63,8 @@ export const SortHeader: React.FC<SortHeaderProps> = (props) => {
               direction: t('general:descending'),
               label: 'Order',
             })}
-            className={[...ascClasses, `${baseClass}__button`].filter(Boolean).join(' ')}
-            onClick={() => void handleSortChange(desc)}
+            className={`${baseClass}__button ${baseClass}__${sort.current} ${isActive ? `${baseClass}--active` : ''}`}
+            onClick={handleSortPress}
             type="button"
           >
             <SortUpIcon />
