d8bfb227b7f3bae6766c79ce8a843803e463b503
Jacob Fletcher <jacobsfletch@gmail.com>
2025-03-17 23:06:58 -0400

    perf(ui): implements select in bulk edit (#11708)
    Bulk edit can now request a partial form state thanks to #11689. This
    means that we only need to build form state (and send it through the
    network) for the currently selected fields, as opposed to the entire
    field schema.
    
    Not only this, but there is no longer a need to filter out unselected
    fields before submitting the form, as the form state will only ever
    include the currently selected fields. This is unnecessary processing
    and causes an excessive amount of rendering, especially since we were
    dispatching actions within a `for` loop to remove each field. React may
    have batched these updates, but is bad practice regardless.
    
    Related: stripping unselected fields was also error prone. This is
    because the `overrides` function we were using to do this receives
    `FormState` (shallow) as an argument, but was being treated as `Data`
    (not shallow, what the create and update operations expect).
    
    E.g. `{ myGroup.myTitle: { value: 'myValue' }}` → `{ myGroup: { myTitle:
    'myValue' }}`.
     
    This led to the `sanitizeUnselectedFields` function improperly
    formatting data sent to the server and would throw an API error upon
    submission. This is only evident when sanitizing nested fields. Instead
    of converting this data _again_, the select API takes care of this by
    ensuring only selected fields exist in form state.
    
    Related: bulk upload was not hitting form state on change. This means
    that no field-level validation was occurring on type.

--- packages/ui/src/elements/FieldSelect/index.tsx
+++ packages/ui/src/elements/FieldSelect/index.tsx
@@ -4,19 +4,32 @@ import type { ClientField, FieldWithPathClient, FormState } from 'payload'
 import { fieldAffectsData, fieldHasSubFields, fieldIsHiddenOrDisabled } from 'payload/shared'
 import React, { Fragment, useState } from 'react'
 
+import type { FieldAction } from '../../forms/Form/types.js'
+
 import { RenderCustomComponent } from '../../elements/RenderCustomComponent/index.js'
 import { FieldLabel } from '../../fields/FieldLabel/index.js'
 import { useForm } from '../../forms/Form/context.js'
 import { createNestedClientFieldPath } from '../../forms/Form/createNestedClientFieldPath.js'
 import { useTranslation } from '../../providers/Translation/index.js'
+import { filterOutUploadFields } from '../../utilities/filterOutUploadFields.js'
 import { ReactSelect } from '../ReactSelect/index.js'
 import './index.scss'
 
 const baseClass = 'field-select'
 
+export type OnFieldSelect = ({
+  dispatchFields,
+  formState,
+  selected,
+}: {
+  dispatchFields: React.Dispatch<FieldAction>
+  formState: FormState
+  selected: FieldOption[]
+}) => void
+
 export type FieldSelectProps = {
   readonly fields: ClientField[]
-  readonly setSelected: (fields: FieldWithPathClient[]) => void
+  readonly onChange: OnFieldSelect
 }
 
 export const combineLabel = ({
@@ -46,6 +59,8 @@ export const combineLabel = ({
   )
 }
 
+export type FieldOption = { Label: React.ReactNode; value: FieldWithPathClient }
+
 const reduceFields = ({
   fields,
   formState,
@@ -56,7 +71,7 @@ const reduceFields = ({
   formState?: FormState
   labelPrefix?: React.ReactNode
   path?: string
-}): { Label: React.ReactNode; value: FieldWithPathClient }[] => {
+}): FieldOption[] => {
   if (!fields) {
     return []
   }
@@ -117,38 +132,13 @@ const reduceFields = ({
   }, [])
 }
 
-export const FieldSelect: React.FC<FieldSelectProps> = ({ fields, setSelected }) => {
+export const FieldSelect: React.FC<FieldSelectProps> = ({ fields, onChange }) => {
   const { t } = useTranslation()
   const { dispatchFields, getFields } = useForm()
 
-  const [options] = useState(() => reduceFields({ fields, formState: getFields() }))
-
-  const handleChange = (selected) => {
-    const activeFields = getFields()
-
-    if (selected === null) {
-      setSelected([])
-    } else {
-      setSelected(selected.map(({ value }) => value))
-    }
-
-    // remove deselected values from form state
-    if (selected === null || Object.keys(activeFields || []).length > selected.length) {
-      Object.keys(activeFields).forEach((path) => {
-        if (
-          selected === null ||
-          !selected.find((field) => {
-            return field.value.path === path
-          })
-        ) {
-          dispatchFields({
-            type: 'REMOVE',
-            path,
-          })
-        }
-      })
-    }
-  }
+  const [options] = useState<FieldOption[]>(() =>
+    reduceFields({ fields: filterOutUploadFields(fields), formState: getFields() }),
+  )
 
   return (
     <div className={baseClass}>
@@ -161,7 +151,9 @@ export const FieldSelect: React.FC<FieldSelectProps> = ({ fields, setSelected })
           return String(option.value)
         }}
         isMulti
-        onChange={handleChange}
+        onChange={(selected: FieldOption[]) =>
+          onChange({ dispatchFields, formState: getFields(), selected })
+        }
         options={options}
       />
     </div>
