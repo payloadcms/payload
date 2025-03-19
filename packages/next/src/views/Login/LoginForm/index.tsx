2ff38a19871c405f75b3f72ee01bec377e547967
Patrik Kozak <patrik@payloadcms.com>
2025-03-19 14:14:34 -0400

    fix: conditionally encode only query params

--- packages/next/src/views/Login/LoginForm/index.tsx
+++ packages/next/src/views/Login/LoginForm/index.tsx
@@ -91,9 +91,13 @@ export const LoginForm: React.FC<{
       initialState={initialState}
       method="POST"
       onSuccess={handleLogin}
-      redirect={encodeURIComponent(
-        typeof searchParams?.redirect === 'string' ? searchParams.redirect : adminRoute,
-      )}
+      redirect={
+        typeof searchParams?.redirect === 'string'
+          ? searchParams.redirect.startsWith('/')
+            ? searchParams.redirect
+            : encodeURIComponent(searchParams.redirect)
+          : adminRoute
+      }
       waitForAutocomplete
     >
       <div className={`${baseClass}__inputWrap`}>
