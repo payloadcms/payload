9bd25737088d9fff2e06d8b57bbc81ed7d7d5416
Jacob Fletcher <jacobsfletch@gmail.com>
2025-03-21 00:05:22 -0400

    simplifies access, passing tests, and updates docs

--- packages/payload/src/query-presets/access.ts
+++ packages/payload/src/query-presets/access.ts
@@ -1,5 +1,5 @@
 import type { Access, Config } from '../config/types.js'
-import type { Operation, Where } from '../types/index.js'
+import type { Operation } from '../types/index.js'
 
 const operations: Operation[] = ['delete', 'read', 'update', 'create'] as const
 
@@ -9,25 +9,20 @@ export const getAccess = (config: Config): Record<Operation, Access> =>
       acc[operation] = async (args) => {
         const { req } = args
 
-        const accessFromConfig = config?.queryPresets?.access?.[operation]
-          ? await config.queryPresets.access[operation](args)
-          : undefined
-
-        // If root access control is a boolean, but there are constraints, ignore the root access control
-        // This ensures that the constraint-level access is processed, instead of ignored
-        // This is because the config sets defaults of `() => true`
-        if (
-          typeof accessFromConfig === 'boolean' &&
-          !config?.queryPresets?.constraints?.[operation]
-        ) {
-          return accessFromConfig
-        }
-
         if (!req.user) {
           return false
         }
 
-        const where: Where = {
+        const customRootAccess = config?.queryPresets?.access?.[operation]
+          ? await config.queryPresets.access[operation](args)
+          : undefined
+
+        // If root access control is false, no need to continue to constraint-level access
+        if (customRootAccess === false) {
+          return false
+        }
+
+        return {
           and: [
             {
               or: [
@@ -50,57 +45,31 @@ export const getAccess = (config: Config): Record<Operation, Access> =>
                     equals: 'everyone',
                   },
                 },
-                // push operation-level constraints into here
+                ...(await Promise.all(
+                  (config?.queryPresets?.constraints?.[operation] || []).map(async (constraint) => {
+                    const customConstraintAccess = constraint.access
+                      ? await constraint.access(args)
+                      : undefined
+
+                    return {
+                      and: [
+                        ...(typeof customConstraintAccess === 'object'
+                          ? [customConstraintAccess]
+                          : []),
+                        {
+                          [`access.${operation}.constraint`]: {
+                            equals: constraint.value,
+                          },
+                        },
+                      ],
+                    }
+                  }),
+                )),
               ],
             },
-            // push root-level constraints into here
+            ...(typeof customRootAccess === 'object' ? [customRootAccess] : []),
           ],
         }
-
-        if (typeof accessFromConfig === 'object') {
-          if (!where.and) {
-            where.and = []
-          }
-
-          where.and.push(accessFromConfig)
-        }
-
-        if (config?.queryPresets?.constraints?.[operation]) {
-          await Promise.all(
-            config.queryPresets.constraints[operation].map(async (constraint) => {
-              const accessFromConstraint = constraint?.access
-                ? await constraint.access(args)
-                : undefined
-
-              if (typeof accessFromConstraint === 'boolean') {
-                return accessFromConstraint
-              }
-
-              if (typeof accessFromConstraint === 'object') {
-                if (!where.and) {
-                  where.and = []
-                }
-
-                if (!where.and[0].or) {
-                  where.and[0].or = []
-                }
-
-                where.and[0].or.push({
-                  and: [
-                    accessFromConstraint,
-                    {
-                      [`access.${operation}.constraint`]: {
-                        equals: constraint.value,
-                      },
-                    },
-                  ],
-                })
-              }
-            }),
-          )
-        }
-
-        return where
       }
 
       return acc
