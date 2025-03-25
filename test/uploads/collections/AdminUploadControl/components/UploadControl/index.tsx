ca462d4606c83642fccd1a3dc61d6266156d73a3
Elliot DeNolf <kendelljoseph@gmail.com>
2025-03-25 16:55:40 -0400

    chore: removes server rendered button

--- test/uploads/collections/AdminUploadControl/components/UploadControl/index.tsx
+++ test/uploads/collections/AdminUploadControl/components/UploadControl/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@payloadcms/ui'
 import React from 'react'
 
 import { UploadControl } from './index.client.js'
@@ -6,7 +5,6 @@ import { UploadControl } from './index.client.js'
 export const UploadControlRSC: React.FC = () => {
   return (
     <div>
-      <Button id="server-rendered-upload-button">Server Rendered Button</Button>
       <UploadControl />
     </div>
   )
