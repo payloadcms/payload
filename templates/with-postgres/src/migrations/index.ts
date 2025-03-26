48553b538581de064eb9b025b5f2e6b324494fe3
Alessio Gravili <alessio@gravili.de>
2025-03-26 12:16:18 -0600

    run gen script

--- templates/with-postgres/src/migrations/index.ts
+++ templates/with-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250324_203114_initial from './20250324_203114_initial'
+import * as migration_20250326_181533_initial from './20250326_181533_initial'
 
 export const migrations = [
   {
-    up: migration_20250324_203114_initial.up,
-    down: migration_20250324_203114_initial.down,
-    name: '20250324_203114_initial',
+    up: migration_20250326_181533_initial.up,
+    down: migration_20250326_181533_initial.down,
+    name: '20250326_181533_initial',
   },
 ]
--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250324_203108_initial from './20250324_203108_initial'
+import * as migration_20250326_181527_initial from './20250326_181527_initial'
 
 export const migrations = [
   {
-    up: migration_20250324_203108_initial.up,
-    down: migration_20250324_203108_initial.down,
-    name: '20250324_203108_initial',
+    up: migration_20250326_181527_initial.up,
+    down: migration_20250326_181527_initial.down,
+    name: '20250326_181527_initial',
   },
 ]
