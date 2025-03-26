59c9feeb456cd4d06ebc8706a64e75c0ddf72782
Alessio Gravili <alessio@gravili.de>
2025-03-26 14:52:53 -0600

    templates: pin all payload packages, improve gen-templates script (#11841)
    This PR comes with a bunch of improvements to our template generation
    script that makes it safer and more reliable
    
    - bumps all our templates
    - Using `latest` as payload version in our templates has proven to be
    unreliable. This updates the gen-templates script to pin all payload
    packages to the latest version
    - adds the missing `website` entry for our template variations, thus
    ensuring its lockfile gets updated
    - adds importmap generation to the gen-templates script
    - adds new `script:gen-templates:build` script to verify that all
    templates still build correctly

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250311_214549_initial from './20250311_214549_initial'
+import * as migration_20250326_181520_initial from './20250326_181520_initial'
 
 export const migrations = [
   {
-    up: migration_20250311_214549_initial.up,
-    down: migration_20250311_214549_initial.down,
-    name: '20250311_214549_initial',
+    up: migration_20250326_181520_initial.up,
+    down: migration_20250326_181520_initial.down,
+    name: '20250326_181520_initial',
   },
 ]
--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250311_214555_initial from './20250311_214555_initial'
+import * as migration_20250326_181527_initial from './20250326_181527_initial'
 
 export const migrations = [
   {
-    up: migration_20250311_214555_initial.up,
-    down: migration_20250311_214555_initial.down,
-    name: '20250311_214555_initial',
+    up: migration_20250326_181527_initial.up,
+    down: migration_20250326_181527_initial.down,
+    name: '20250326_181527_initial',
   },
 ]
