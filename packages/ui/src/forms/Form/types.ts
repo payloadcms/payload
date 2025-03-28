9f46ea6ca2667689b0080c237be6fe51612fe6d6
Jacob Fletcher <jacobsfletch@gmail.com>
2025-03-28 17:31:05 -0400

    fix build

--- packages/ui/src/forms/Form/types.ts
+++ packages/ui/src/forms/Form/types.ts
@@ -80,7 +80,9 @@ export type Submit = (
   options?: SubmitOptions,
   e?: React.FormEvent<HTMLFormElement>,
 ) => Promise<void>
+
 export type ValidateForm = () => Promise<boolean>
+
 export type CreateFormData = (
   overrides?: Record<string, unknown>,
   /**
@@ -89,6 +91,7 @@ export type CreateFormData = (
    */
   options?: { mergeOverrideData?: boolean },
 ) => FormData | Promise<FormData>
+
 export type GetFields = () => FormState
 export type GetField = (path: string) => FormField
 export type GetData = () => Data
