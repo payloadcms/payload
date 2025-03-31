a6f7ef837aafda92d0be85dd1ffa2d26cd105f12
Alessio Gravili <alessio@gravili.de>
2025-03-31 09:45:02 -0600

    feat(db-*): export types from main export (#11914)
    In 3.0, we made the decision to export all types from the main package
    export (e.g. `payload/types` => `payload`). This improves type
    discoverability by IDEs and simplifies importing types.
    
    This PR does the same for our db adapters, which still have a separate
    `/types` subpath export. While those are kept for
    backwards-compatibility, we can remove them in 4.0.

--- packages/db-postgres/src/index.ts
+++ packages/db-postgres/src/index.ts
@@ -214,6 +214,11 @@ export function postgresAdapter(args: Args): DatabaseAdapterObj<PostgresAdapter>
   }
 }
 
+export type {
+  Args as PostgresAdapterArgs,
+  GeneratedDatabaseSchema,
+  PostgresAdapter,
+} from './types.js'
 export type { MigrateDownArgs, MigrateUpArgs } from '@payloadcms/drizzle/postgres'
 export { geometryColumn } from '@payloadcms/drizzle/postgres'
 export { sql } from 'drizzle-orm'
