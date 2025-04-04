5eb63bea4634dc78dc8d9e47d1918089095dd8f3
Elliot DeNolf <denolfe@gmail.com>
2025-04-04 15:45:17 -0400

    templates: bump for v3.33.0

--- templates/with-postgres/src/migrations/index.ts
+++ templates/with-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250326_181533_initial from './20250326_181533_initial'
+import * as migration_20250404_194252_initial from './20250404_194252_initial'
 
 export const migrations = [
   {
-    up: migration_20250326_181533_initial.up,
-    down: migration_20250326_181533_initial.down,
-    name: '20250326_181533_initial',
+    up: migration_20250404_194252_initial.up,
+    down: migration_20250404_194252_initial.down,
+    name: '20250404_194252_initial',
   },
 ]
