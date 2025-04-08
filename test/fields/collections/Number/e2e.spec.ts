7a99946521965133796986108a3d7815b1f3a328
Patrik Kozak <patrik@payloadcms.com>
2025-04-08 15:54:18 -0400

    fix(ui): adds multi select inputs for text field in where builder

--- test/fields/collections/Number/e2e.spec.ts
+++ test/fields/collections/Number/e2e.spec.ts
@@ -2,7 +2,6 @@ import type { Page } from '@playwright/test'
 
 import { expect, test } from '@playwright/test'
 import { addListFilter } from 'helpers/e2e/addListFilter.js'
-import { openListFilters } from 'helpers/e2e/openListFilters.js'
 import path from 'path'
 import { wait } from 'payload/shared'
 import { fileURLToPath } from 'url'
