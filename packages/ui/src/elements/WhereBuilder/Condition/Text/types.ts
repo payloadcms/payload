18ff9cbdb16005bb5bf7ea0c8019555dcb77ea94
Patrik <patrik@payloadcms.com>
2025-04-10 08:54:50 -0400

    fix(ui): adds multi select inputs for text fields in where builder (#12054)
    ### What?
    
    The `in` & `not_in` operators were not properly working for `text`
    fields as this operator requires an array of values for it's input.
    
    ### How?
    
    Conditionally renders a multi select input for `text` fields when
    filtering by `in` & `not_in` operators.

--- packages/ui/src/elements/WhereBuilder/Condition/Text/types.ts
+++ packages/ui/src/elements/WhereBuilder/Condition/Text/types.ts
@@ -5,5 +5,5 @@ import type { DefaultFilterProps } from '../types.js'
 export type TextFilterProps = {
   readonly field: TextFieldClient
   readonly onChange: (val: string) => void
-  readonly value: string
+  readonly value: string | string[]
 } & DefaultFilterProps
