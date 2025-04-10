15f4c5f439f61d3a0ebd49c2f19867a5860ee338
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2025-04-10 16:59:38 -0300

    remove unused lexical utils

--- test/fields/config.blockreferences.ts
+++ test/fields/config.blockreferences.ts
@@ -3,14 +3,8 @@
 import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
 import { autoDedupeBlocksPlugin } from '../helpers/autoDedupeBlocksPlugin/index.js'
 import { baseConfig } from './baseConfig.js'
-import { lexicalBlocks, lexicalInlineBlocks } from './collections/Blocks/lexicalUtils.js'
 
 export default buildConfigWithDefaults({
   ...baseConfig,
-  blocks: [
-    ...(baseConfig.blocks ?? []),
-    ...lexicalBlocks.filter((block) => typeof block !== 'string'),
-    ...lexicalInlineBlocks.filter((block) => typeof block !== 'string'),
-  ],
   plugins: [autoDedupeBlocksPlugin({ silent: true })],
 })
