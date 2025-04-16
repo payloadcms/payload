59077aa7fc7d30ba520ca2110bb66a83e39f2fd2
Eduardo Würch <ewurch@gmail.com>
2025-04-16 11:28:39 -0300

    chore(templates): remove redundant condition check

--- templates/website/src/components/Card/index.tsx
+++ templates/website/src/components/Card/index.tsx
@@ -44,28 +44,24 @@ export const Card: React.FC<{
       <div className="p-4">
         {showCategories && hasCategories && (
           <div className="uppercase text-sm mb-4">
-            {showCategories && hasCategories && (
-              <div>
-                {categories?.map((category, index) => {
-                  if (typeof category === 'object') {
-                    const { title: titleFromCategory } = category
-
-                    const categoryTitle = titleFromCategory || 'Untitled category'
-
-                    const isLast = index === categories.length - 1
-
-                    return (
-                      <Fragment key={index}>
-                        {categoryTitle}
-                        {!isLast && <Fragment>, &nbsp;</Fragment>}
-                      </Fragment>
-                    )
-                  }
-
-                  return null
-                })}
-              </div>
-            )}
+            {categories?.map((category, index) => {
+              if (typeof category === 'object') {
+                const { title: titleFromCategory } = category
+  
+                const categoryTitle = titleFromCategory || 'Untitled category'
+  
+                const isLast = index === categories.length - 1
+  
+                return (
+                  <Fragment key={index}>
+                    {categoryTitle}
+                    {!isLast && <Fragment>, &nbsp;</Fragment>}
+                  </Fragment>
+                )
+              }
+  
+              return null
+            })}
           </div>
         )}
         {titleToUse && (
