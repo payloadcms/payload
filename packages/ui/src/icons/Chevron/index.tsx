b0a011536ee8ae96aa7ba1726c3f2599ac75b9e2
Tylan Davis <hello@tylandavis.com>
2025-04-16 12:50:27 -0400

    match ChevronIcon sizing with other icons

--- packages/ui/src/icons/Chevron/index.tsx
+++ packages/ui/src/icons/Chevron/index.tsx
@@ -24,10 +24,10 @@ export const ChevronIcon: React.FC<{
               ? 'rotate(180deg)'
               : undefined,
     }}
-    viewBox="0 0 22 12"
+    viewBox="0 0 20 20"
     width="100%"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path className="stroke" d="M1 1.12109L11 11.1211L21 1.12109" strokeLinecap="square" />
+    <path className="stroke" d="M14 8L10 12L6 8" strokeLinecap="square" />
   </svg>
 )
