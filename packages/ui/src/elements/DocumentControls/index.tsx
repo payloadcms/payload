34ea6ec14f2ccea1ba6cdec9e910dee037972d37
Patrik <patrik@payloadcms.com>
2025-04-17 14:45:10 -0400

    feat: adds `showSaveDraftButton` option to show draft button with autosave enabled (#12150)
    This adds a new `showSaveDraftButton` option to the
    `versions.drafts.autosave` config for collections and globals.
    
    By default, the "Save as draft" button is hidden when autosave is
    enabled. This new option allows the button to remain visible for manual
    saves while autosave is active.
    
    Also updates the admin UI logic to conditionally render the button when
    this flag is set, and updates the documentation with an example usage.

--- packages/ui/src/elements/DocumentControls/index.tsx
+++ packages/ui/src/elements/DocumentControls/index.tsx
@@ -133,9 +133,23 @@ export const DocumentControls: React.FC<{
   const unsavedDraftWithValidations =
     !id && collectionConfig?.versions?.drafts && collectionConfig.versions?.drafts.validate
 
+  const collectionConfigDrafts = collectionConfig?.versions?.drafts
+  const globalConfigDrafts = globalConfig?.versions?.drafts
+
   const autosaveEnabled =
-    (collectionConfig?.versions?.drafts && collectionConfig?.versions?.drafts?.autosave) ||
-    (globalConfig?.versions?.drafts && globalConfig?.versions?.drafts?.autosave)
+    (collectionConfigDrafts && collectionConfigDrafts?.autosave) ||
+    (globalConfigDrafts && globalConfigDrafts?.autosave)
+
+  const collectionAutosaveEnabled = collectionConfigDrafts && collectionConfigDrafts?.autosave
+  const globalAutosaveEnabled = globalConfigDrafts && globalConfigDrafts?.autosave
+
+  const showSaveDraftButton =
+    (collectionAutosaveEnabled &&
+      collectionConfigDrafts.autosave !== false &&
+      collectionConfigDrafts.autosave.showSaveDraftButton === true) ||
+    (globalAutosaveEnabled &&
+      globalConfigDrafts.autosave !== false &&
+      globalConfigDrafts.autosave.showSaveDraftButton === true)
 
   const showCopyToLocale = localization && !collectionConfig?.admin?.disableCopyToLocale
 
@@ -218,7 +232,9 @@ export const DocumentControls: React.FC<{
               <Fragment>
                 {collectionConfig?.versions?.drafts || globalConfig?.versions?.drafts ? (
                   <Fragment>
-                    {(unsavedDraftWithValidations || !autosaveEnabled) && (
+                    {(unsavedDraftWithValidations ||
+                      !autosaveEnabled ||
+                      (autosaveEnabled && showSaveDraftButton)) && (
                       <RenderCustomComponent
                         CustomComponent={CustomSaveDraftButton}
                         Fallback={<SaveDraftButton />}
