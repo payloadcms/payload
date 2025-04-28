e10341ea70be9882b90f7bbbeb00d5168ffd2519
Samuel Gabriel <144083942+sam-gab@users.noreply.github.com>
2025-04-28 22:17:49 +0800

    Bring back fallback
    It seems `relatedTo` can't be undefined in a relationship field type - hence the lint errors. 
    I could be wrong, and the lint error is coming from somewhere else. I don't have this set up locally - updating directly on github.

--- packages/payload/src/query-presets/constraints.ts
+++ packages/payload/src/query-presets/constraints.ts
@@ -74,7 +74,7 @@ export const getConstraints = (config: Config): Field => ({
                 },
               ],
             },
-            relationTo: config.admin?.user
+            relationTo: config.admin?.user ?? 'users'
           },
           ...(config?.queryPresets?.constraints?.[operation]?.reduce(
             (acc: Field[], option: QueryPresetConstraint) => {
