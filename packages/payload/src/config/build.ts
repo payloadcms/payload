00f27bba1613b1fad46f1c1867261214b893fdf1
Alessio Gravili <alessio@gravili.de>
2025-05-10 22:01:06 -0600

    fix it

--- packages/payload/src/config/build.ts
+++ packages/payload/src/config/build.ts
@@ -1,3 +1,5 @@
+import { createDraft, finishDraft, setAutoFreeze } from 'immer'
+
 import type { Config, SanitizedConfig } from './types.js'
 
 import { sanitizeConfig } from './sanitize.js'
@@ -9,10 +11,18 @@ import { sanitizeConfig } from './sanitize.js'
  */
 export async function buildConfig(config: Config): Promise<SanitizedConfig> {
   if (Array.isArray(config.plugins)) {
-    let configAfterPlugins = config
+    // Not freezing objects is faster
+    setAutoFreeze(false)
+
+    // Use Immer to ensure config modifications by plugins are immutable and do not affect the original config / shared references
+    let mutableConfig: Config = createDraft(config) as Config
+
     for (const plugin of config.plugins) {
-      configAfterPlugins = await plugin(configAfterPlugins)
+      mutableConfig = await plugin(mutableConfig)
     }
+
+    const configAfterPlugins = finishDraft(mutableConfig)
+
     return await sanitizeConfig(configAfterPlugins)
   }
 
