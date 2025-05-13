127484aeee31584a276a7b0635b69fa32ca51e35
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2025-05-13 17:31:20 -0300

    remove redundant eslint config files and fix allowDefaultProject

--- packages/richtext-lexical/eslint.config.js
+++ packages/richtext-lexical/eslint.config.js
@@ -4,21 +4,6 @@ import { rootEslintConfig, rootParserOptions } from '../../eslint.config.js'
 /** @typedef {import('eslint').Linter.Config} Config */
 
 /** @type {Config[]} */
-export const index = [
-  ...rootEslintConfig,
-  {
-    ignores: ['bundle.js'],
-    languageOptions: {
-      parserOptions: {
-        ...rootParserOptions,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      '@lexical': lexical,
-    },
-    rules: lexical.configs.recommended.rules,
-  },
-]
+export const index = [...rootEslintConfig]
 
 export default index
