64204f2ff23e00da4ee1b61c6deac5deb8454377
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2025-05-16 11:33:12 -0300

    fix(templates): make images visible in live preview if it is not running on port 3000

--- templates/with-vercel-website/next.config.js
+++ templates/with-vercel-website/next.config.js
@@ -4,7 +4,7 @@ import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
