d94a8e5f4c7b90342e11d2df713d0396c444c653
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-05-19 10:58:35 +0300

    fix(plugin-multi-tenant): multi-locale tenant select label

--- test/plugin-multi-tenant/config.ts
+++ test/plugin-multi-tenant/config.ts
@@ -41,7 +41,7 @@ export default buildConfigWithDefaults({
           isGlobal: true,
         },
       },
-      tenantSelectorLabel: 'Site',
+      tenantSelectorLabel: { en: 'Site', es: 'Site in es' },
     }),
   ],
   typescript: {
