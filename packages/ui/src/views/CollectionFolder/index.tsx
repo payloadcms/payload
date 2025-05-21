5f2e8463509fa0f2ad3f560aea54749015eb53cd
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-05-21 12:47:00 -0400

    fixes cell not updating fromFolderID and missing onCreateSuccess fn

--- packages/ui/src/views/CollectionFolder/index.tsx
+++ packages/ui/src/views/CollectionFolder/index.tsx
@@ -322,6 +322,7 @@ export function DefaultCollectionFolderView(props: FolderListViewClientProps) {
                   buttonLabel={`${t('general:create')} ${t('general:document').toLowerCase()}`}
                   collectionSlugs={[collectionSlug]}
                   key="create-document"
+                  onCreateSuccess={onCreateSuccess}
                 />,
               ].filter(Boolean)}
               Message={
