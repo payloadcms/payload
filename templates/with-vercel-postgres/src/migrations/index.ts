d8e62c3519f22017595ab80fa2be4abf9cfccf97
Elliot DeNolf <denolfe@gmail.com>
2025-05-22 11:11:00 -0400

    templates: include ui package

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250522_142529_initial from './20250522_142529_initial'
+import * as migration_20250522_150833_initial from './20250522_150833_initial'
 
 export const migrations = [
   {
-    up: migration_20250522_142529_initial.up,
-    down: migration_20250522_142529_initial.down,
-    name: '20250522_142529_initial',
+    up: migration_20250522_150833_initial.up,
+    down: migration_20250522_150833_initial.down,
+    name: '20250522_150833_initial',
   },
 ]
--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250522_142545_initial from './20250522_142545_initial'
+import * as migration_20250522_150837_initial from './20250522_150837_initial'
 
 export const migrations = [
   {
-    up: migration_20250522_142545_initial.up,
-    down: migration_20250522_142545_initial.down,
-    name: '20250522_142545_initial',
+    up: migration_20250522_150837_initial.up,
+    down: migration_20250522_150837_initial.down,
+    name: '20250522_150837_initial',
   },
 ]
