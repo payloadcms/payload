3a73a67ef4bcb591f4e3913a63897e4765801985
Elliot DeNolf <denolfe@users.noreply.github.com>
2025-05-22 11:19:49 -0400

    templates: include ui package (#12499)
    Folder view needed to have `@payloadcms/ui` explicitly installed.
    Including this in the templates.

--- templates/with-postgres/src/migrations/index.ts
+++ templates/with-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250522_142559_initial from './20250522_142559_initial'
+import * as migration_20250522_150852_initial from './20250522_150852_initial'
 
 export const migrations = [
   {
-    up: migration_20250522_142559_initial.up,
-    down: migration_20250522_142559_initial.down,
-    name: '20250522_142559_initial',
+    up: migration_20250522_150852_initial.up,
+    down: migration_20250522_150852_initial.down,
+    name: '20250522_150852_initial',
   },
 ]
