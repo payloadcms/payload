0829cfb7125686eaa2e7e959aa79dd25d88ffefe
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-05-25 00:58:32 +0300

    fix imports

--- packages/drizzle/src/exports/sqlite.ts
+++ packages/drizzle/src/exports/sqlite.ts
@@ -1,5 +1,7 @@
 export { columnToCodeConverter } from '../sqlite/columnToCodeConverter.js'
 export { countDistinct } from '../sqlite/countDistinct.js'
+export { convertPathToJSONTraversal } from '../sqlite/createJSONQuery/convertPathToJSONTraversal.js'
+export { createJSONQuery } from '../sqlite/createJSONQuery/index.js'
 export { defaultDrizzleSnapshot } from '../sqlite/defaultSnapshot.js'
 export { deleteWhere } from '../sqlite/deleteWhere.js'
 export { dropDatabase } from '../sqlite/dropDatabase.js'
