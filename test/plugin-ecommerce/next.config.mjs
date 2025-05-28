1415ff63eaddbcf5319e03367860f012aa8e0eeb
Paul Popus <paul@payloadcms.com>
2025-05-28 13:38:30 +0100

    remove console log

--- test/plugin-ecommerce/next.config.mjs
+++ test/plugin-ecommerce/next.config.mjs
@@ -6,7 +6,6 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(__filename)
 
-console.log({ root_dir: dirname })
 export default {
   ...nextConfig,
   env: {
