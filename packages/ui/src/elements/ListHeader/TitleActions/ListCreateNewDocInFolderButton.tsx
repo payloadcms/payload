a1df379bada7584e8b9637f98002ebb00eb85b90
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-06-04 13:04:47 -0400

    moves create/edit folder actions into doc drawers

--- packages/ui/src/elements/ListHeader/TitleActions/ListCreateNewDocInFolderButton.tsx
+++ packages/ui/src/elements/ListHeader/TitleActions/ListCreateNewDocInFolderButton.tsx
@@ -143,6 +143,7 @@ export function ListCreateNewDocInFolderButton({
             }
             closeFolderDrawer()
           }}
+          redirectAfterCreate={false}
         />
       )}
     </React.Fragment>
