9c90f99bd1b71c771ac9eed3a59eac2e33377754
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2025-06-05 21:41:25 +0600

    chore: add Bangla translations

--- packages/translations/src/exports/all.ts
+++ packages/translations/src/exports/all.ts
@@ -3,6 +3,8 @@ import type { SupportedLanguages } from '../types.js'
 import { ar } from '../languages/ar.js'
 import { az } from '../languages/az.js'
 import { bg } from '../languages/bg.js'
+import { bnBD } from '../languages/bn-BD.js'
+import { bnIN } from '../languages/bn-IN.js'
 import { ca } from '../languages/ca.js'
 import { cs } from '../languages/cs.js'
 import { da } from '../languages/da.js'
@@ -44,6 +46,8 @@ export const translations = {
   ar,
   az,
   bg,
+  bnBD,
+  bnIN,
   ca,
   cs,
   da,
