8e96a1a5f1757f2e5e32493ccdfc44ff8427dea4
Alessio Gravili <alessio@gravili.de>
2025-06-09 20:37:26 -0700

    fix undefined htmlFor gen

--- packages/ui/src/utilities/generateFieldID.ts
+++ packages/ui/src/utilities/generateFieldID.ts
@@ -1,3 +1,6 @@
 export const generateFieldID = (path: string, editDepth: number, uuid: string) => {
+  if (!path) {
+    return undefined
+  }
   return `field-${path?.replace(/\./g, '__')}${editDepth > 1 ? `-${editDepth}` : ''}${uuid ? `-${uuid}` : ''}`
 }
