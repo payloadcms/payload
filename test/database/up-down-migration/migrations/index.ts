df8be92d47c93886a428eff54a78263d0862e183
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-06-12 19:37:07 +0300

    fix(db-postgres): x3 and more nested blocks regression (#12770)

--- test/database/up-down-migration/migrations/index.ts
+++ test/database/up-down-migration/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250528_153134 from './20250528_153134.js'
+import * as migration_20250611_163948 from './20250611_163948.js'
 
 export const migrations = [
   {
-    up: migration_20250528_153134.up,
-    down: migration_20250528_153134.down,
-    name: '20250528_153134',
+    up: migration_20250611_163948.up,
+    down: migration_20250611_163948.down,
+    name: '20250611_163948',
   },
 ]
