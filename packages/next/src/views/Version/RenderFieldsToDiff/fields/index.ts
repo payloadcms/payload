4e2e4d2aed8ff1f23fdcce69d4c1a04ef7e78ed9
Alessio Gravili <alessio@gravili.de>
2025-06-16 04:58:03 -0700

    feat(next): version view overhaul (#12027)
    #11769 improved the lexical version view diff component. This PR
    improves the rest of the version view.
    
    ## What changed
    
    - Column layout when selecting a version:
    	- Previously: Selected version on the left, latest version on the left
    - Now: Previous version on the left, previous version on the right
    (mimics behavior of GitHub)
    - Locale selector now displayed in pill selector, rather than
    react-select
    - Smoother, more reliable locale, modifiedOnly and version selection.
    Now uses clean event callbacks rather than useEffects
    - React-diff-viewer-continued has been replaced with the html differ we
    use in lexical
    - Updated Design for all field diffs
    - Version columns now have a clearly defined separator line
    - Fixed collapsibles showing in version view despite having no modified
    fields if modifiedOnly is true
    - New, redesigned header
    	
    
    ## Screenshots
    
    ### Before
    
    ![CleanShot 2025-04-11 at 20 10
    03@2x](https://github.com/user-attachments/assets/a93a500a-3cdd-4cf0-84dd-cf5481aac2b3)
    
    ![CleanShot 2025-04-11 at 20 10
    28@2x](https://github.com/user-attachments/assets/59bc5885-cbaf-49ea-8d1d-8d145463fd80)
    
    ### After
    
    ![Screenshot 2025-06-09 at 17 43
    49@2x](https://github.com/user-attachments/assets/f6ff0369-76c9-4c1c-9aa7-cbd88806ddc1)
    
    ![Screenshot 2025-06-09 at 17 44
    50@2x](https://github.com/user-attachments/assets/db93a3db-48d6-4e5d-b080-86a34fff5d22)
    
    ![Screenshot 2025-06-09 at 17 45
    19@2x](https://github.com/user-attachments/assets/27b6c720-05fe-4957-85af-1305d6b65cfd)
    
    ![Screenshot 2025-06-09 at 17 45
    34@2x](https://github.com/user-attachments/assets/6d42f458-515a-4611-b27a-f4d6bafbf555)

--- packages/next/src/views/Version/RenderFieldsToDiff/fields/index.ts
+++ packages/next/src/views/Version/RenderFieldsToDiff/fields/index.ts
@@ -1,6 +1,7 @@
-import type { FieldDiffClientProps, FieldTypes } from 'payload'
+import type { FieldDiffClientProps, FieldDiffServerProps, FieldTypes } from 'payload'
 
 import { Collapsible } from './Collapsible/index.js'
+import { DateDiffComponent } from './Date/index.js'
 import { Group } from './Group/index.js'
 import { Iterable } from './Iterable/index.js'
 import { Relationship } from './Relationship/index.js'
@@ -8,14 +9,18 @@ import { Row } from './Row/index.js'
 import { Select } from './Select/index.js'
 import { Tabs } from './Tabs/index.js'
 import { Text } from './Text/index.js'
+import { Upload } from './Upload/index.js'
 
-export const diffComponents: Record<FieldTypes, React.ComponentType<FieldDiffClientProps>> = {
+export const diffComponents: Record<
+  FieldTypes,
+  React.ComponentType<FieldDiffClientProps | FieldDiffServerProps>
+> = {
   array: Iterable,
   blocks: Iterable,
   checkbox: Text,
   code: Text,
   collapsible: Collapsible,
-  date: Text,
+  date: DateDiffComponent,
   email: Text,
   group: Group,
   join: null,
@@ -31,5 +36,5 @@ export const diffComponents: Record<FieldTypes, React.ComponentType<FieldDiffCli
   text: Text,
   textarea: Text,
   ui: null,
-  upload: Relationship,
+  upload: Upload,
 }
