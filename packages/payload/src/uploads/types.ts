7c10b8a41bc15e60b18147a4ec9ad658122370a8
Wes DiMiceli <wesley.dimiceli@bethesda.net>
2025-07-07 11:37:30 -0400

    docs: updates default value for focalPoint

--- packages/payload/src/uploads/types.ts
+++ packages/payload/src/uploads/types.ts
@@ -177,7 +177,7 @@ export type UploadConfig = {
   filesRequiredOnCreate?: boolean
   /**
    * Enables focal point positioning for image manipulation.
-   * @default false
+   * @default true
    */
   focalPoint?: boolean
   /**
