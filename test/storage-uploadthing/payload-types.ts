cb6a73e1b44d448e96ac6a23016aa00dac89c710
Paul <paul@payloadcms.com>
2025-07-10 16:00:26 +0100

    feat(storage-*): include modified headers into the response headers of files when using adapters (#12096)
    This PR makes it so that `modifyResponseHeaders` is supported in our
    adapters when set on the collection config. Previously it would be
    ignored.
    
    This means that users can now modify or append new headers to what's
    returned by each service.
    
    ```ts
    import type { CollectionConfig } from 'payload'
    
    export const Media: CollectionConfig = {
      slug: 'media',
      upload: {
        modifyResponseHeaders: ({ headers }) => {
          const newHeaders = new Headers(headers) // Copy existing headers
          newHeaders.set('X-Frame-Options', 'DENY') // Set new header
    
          return newHeaders
        },
      },
    }
    ```
    
    Also adds support for `void` return on the `modifyResponseHeaders`
    function in the case where the user just wants to use existing headers
    and doesn't need more control.
    
    eg:
    
    ```ts
    import type { CollectionConfig } from 'payload'
    
    export const Media: CollectionConfig = {
      slug: 'media',
      upload: {
        modifyResponseHeaders: ({ headers }) => {
          headers.set('X-Frame-Options', 'DENY') // You can directly set headers without returning
        },
      },
    }
    ```
    
    Manual testing checklist (no CI e2es setup for these envs yet):
    - [x] GCS
    - [x] S3
    - [x] Azure
    - [x] UploadThing
    - [x] Vercel Blob
    
    ---------
    
    Co-authored-by: James <james@trbl.design>

--- test/storage-uploadthing/payload-types.ts
+++ test/storage-uploadthing/payload-types.ts
@@ -84,7 +84,7 @@ export interface Config {
     'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
   };
   db: {
-    defaultIDType: string;
+    defaultIDType: number;
   };
   globals: {};
   globalsSelect: {};
@@ -120,7 +120,7 @@ export interface UserAuthOperations {
  * via the `definition` "media".
  */
 export interface Media {
-  id: string;
+  id: number;
   alt?: string | null;
   _key?: string | null;
   updatedAt: string;
@@ -160,7 +160,7 @@ export interface Media {
  * via the `definition` "media-with-prefix".
  */
 export interface MediaWithPrefix {
-  id: string;
+  id: number;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -178,7 +178,7 @@ export interface MediaWithPrefix {
  * via the `definition` "users".
  */
 export interface User {
-  id: string;
+  id: number;
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -188,6 +188,13 @@ export interface User {
   hash?: string | null;
   loginAttempts?: number | null;
   lockUntil?: string | null;
+  sessions?:
+    | {
+        id: string;
+        createdAt?: string | null;
+        expiresAt: string;
+      }[]
+    | null;
   password?: string | null;
 }
 /**
@@ -195,24 +202,24 @@ export interface User {
  * via the `definition` "payload-locked-documents".
  */
 export interface PayloadLockedDocument {
-  id: string;
+  id: number;
   document?:
     | ({
         relationTo: 'media';
-        value: string | Media;
+        value: number | Media;
       } | null)
     | ({
         relationTo: 'media-with-prefix';
-        value: string | MediaWithPrefix;
+        value: number | MediaWithPrefix;
       } | null)
     | ({
         relationTo: 'users';
-        value: string | User;
+        value: number | User;
       } | null);
   globalSlug?: string | null;
   user: {
     relationTo: 'users';
-    value: string | User;
+    value: number | User;
   };
   updatedAt: string;
   createdAt: string;
@@ -222,10 +229,10 @@ export interface PayloadLockedDocument {
  * via the `definition` "payload-preferences".
  */
 export interface PayloadPreference {
-  id: string;
+  id: number;
   user: {
     relationTo: 'users';
-    value: string | User;
+    value: number | User;
   };
   key?: string | null;
   value?:
@@ -245,7 +252,7 @@ export interface PayloadPreference {
  * via the `definition` "payload-migrations".
  */
 export interface PayloadMigration {
-  id: string;
+  id: number;
   name?: string | null;
   batch?: number | null;
   updatedAt: string;
@@ -327,6 +334,13 @@ export interface UsersSelect<T extends boolean = true> {
   hash?: T;
   loginAttempts?: T;
   lockUntil?: T;
+  sessions?:
+    | T
+    | {
+        id?: T;
+        createdAt?: T;
+        expiresAt?: T;
+      };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
