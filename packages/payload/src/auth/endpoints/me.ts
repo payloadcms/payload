4bd8b06787a747cf037919a916ef0ed1d620dbf3
Paul Popus <paul@payloadcms.com>
2025-07-10 13:08:11 +0100

    fix: allow select populate depth and draft to /me operation

--- packages/payload/src/auth/endpoints/me.ts
+++ packages/payload/src/auth/endpoints/me.ts
@@ -4,17 +4,26 @@ import type { PayloadHandler } from '../../config/types.js'
 
 import { getRequestCollection } from '../../utilities/getRequestEntity.js'
 import { headersWithCors } from '../../utilities/headersWithCors.js'
+import { isNumber } from '../../utilities/isNumber.js'
+import { sanitizePopulateParam } from '../../utilities/sanitizePopulateParam.js'
+import { sanitizeSelectParam } from '../../utilities/sanitizeSelectParam.js'
 import { extractJWT } from '../extractJWT.js'
 import { meOperation } from '../operations/me.js'
 
 export const meHandler: PayloadHandler = async (req) => {
+  const { searchParams } = req
   const collection = getRequestCollection(req)
   const currentToken = extractJWT(req)
+  const depth = searchParams.get('depth')
 
   const result = await meOperation({
     collection,
     currentToken: currentToken!,
+    depth: isNumber(depth) ? Number(depth) : undefined,
+    draft: searchParams.get('draft') === 'true',
+    populate: sanitizePopulateParam(req.query.populate),
     req,
+    select: sanitizeSelectParam(req.query.select),
   })
 
   if (collection.config.auth.removeTokenFromResponses) {
