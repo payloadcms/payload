2b4198b84806938f175d5de2a27ad2fd312dbb29
Jacob Fletcher <jacobsfletch@gmail.com>
2025-07-14 09:14:53 -0400

    ensures db snapshot during seed and before each

--- test/group-by/config.ts
+++ test/group-by/config.ts
@@ -3,10 +3,10 @@ import { fileURLToPath } from 'node:url'
 import path from 'path'
 
 import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
-import { devUser } from '../credentials.js'
-import { CategoriesCollection, categoriesSlug } from './collections/Categories/index.js'
+import { CategoriesCollection } from './collections/Categories/index.js'
 import { MediaCollection } from './collections/Media/index.js'
-import { PostsCollection, postsSlug } from './collections/Posts/index.js'
+import { PostsCollection } from './collections/Posts/index.js'
+import { seed } from './seed.js'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -20,40 +20,9 @@ export default buildConfigWithDefaults({
   },
   editor: lexicalEditor({}),
   onInit: async (payload) => {
-    await payload.create({
-      collection: 'users',
-      data: {
-        email: devUser.email,
-        password: devUser.password,
-      },
-    })
-
-    const [category1, category2] = await Promise.all([
-      payload.create({
-        collection: categoriesSlug,
-        data: {
-          title: 'Category 1',
-        },
-      }),
-      payload.create({
-        collection: categoriesSlug,
-        data: {
-          title: 'Category 2',
-        },
-      }),
-    ])
-
-    await Promise.all(
-      Array.from({ length: 30 }).map(async (_, index) =>
-        payload.create({
-          collection: postsSlug,
-          data: {
-            title: `Post ${index + 1}`,
-            category: index < 15 ? category1.id : category2.id,
-          },
-        }),
-      ),
-    )
+    if (process.env.SEED_IN_CONFIG_ONINIT !== 'false') {
+      await seed(payload)
+    }
   },
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
--- test/group-by/e2e.spec.ts
+++ test/group-by/e2e.spec.ts
@@ -1,14 +1,20 @@
 import type { Page } from '@playwright/test'
+import type { PayloadTestSDK } from 'helpers/sdk/index.js'
 
 import { expect, test } from '@playwright/test'
 import { openGroupBy } from 'helpers/e2e/toggleGroupBy.js'
+import { reInitializeDB } from 'helpers/reInitializeDB.js'
 import * as path from 'path'
 import { fileURLToPath } from 'url'
+
+import type { Config } from './payload-types.js'
 
 import { ensureCompilationIsDone, initPageConsoleErrorCatch } from '../helpers.js'
 import { AdminUrlUtil } from '../helpers/adminUrlUtil.js'
 import { initPayloadE2ENoConfig } from '../helpers/initPayloadE2ENoConfig.js'
 import { TEST_TIMEOUT_LONG } from '../playwright.config.js'
+
+const { beforeEach } = test
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -16,16 +22,32 @@ const dirname = path.dirname(filename)
 test.describe('Group By', () => {
   let page: Page
   let url: AdminUrlUtil
+  let serverURL: string
+  let payload: PayloadTestSDK<Config>
 
   test.beforeAll(async ({ browser }, testInfo) => {
     testInfo.setTimeout(TEST_TIMEOUT_LONG)
-
-    const { payload, serverURL } = await initPayloadE2ENoConfig({ dirname })
+    ;({ payload, serverURL } = await initPayloadE2ENoConfig<Config>({ dirname }))
     url = new AdminUrlUtil(serverURL, 'posts')
 
     const context = await browser.newContext()
     page = await context.newPage()
     initPageConsoleErrorCatch(page)
+    await ensureCompilationIsDone({ page, serverURL })
+  })
+
+  beforeEach(async () => {
+    // await throttleTest({
+    //   page,
+    //   context,
+    //   delay: 'Fast 4G',
+    // })
+
+    await reInitializeDB({
+      serverURL,
+      snapshotKey: 'groupByTests',
+    })
+
     await ensureCompilationIsDone({ page, serverURL })
   })
 
