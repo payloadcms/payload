674d7ecaf98be5b6a727338f8e504738552ef300
Alessio Gravili <alessio@gravili.de>
2025-07-14 17:25:12 -0700

    bump templates

--- templates/with-postgres/src/migrations/index.ts
+++ templates/with-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250714_175444_initial from './20250714_175444_initial'
+import * as migration_20250715_002336_initial from './20250715_002336_initial'
 
 export const migrations = [
   {
-    up: migration_20250714_175444_initial.up,
-    down: migration_20250714_175444_initial.down,
-    name: '20250714_175444_initial',
+    up: migration_20250715_002336_initial.up,
+    down: migration_20250715_002336_initial.down,
+    name: '20250715_002336_initial',
   },
 ]
