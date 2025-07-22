dab26d18bf4c1fb85c64e36c55241b9139b3aa48
Jacob Fletcher <jacobsfletch@gmail.com>
2025-07-22 13:57:08 -0400

    cleanup

--- packages/ui/src/providers/Selection/ConditionalProvider.tsx
+++ packages/ui/src/providers/Selection/ConditionalProvider.tsx
@@ -4,6 +4,15 @@ import { useAuth } from '../../providers/Auth/index.js'
 import { useListQuery } from '../../providers/ListQuery/index.js'
 import { SelectionProvider } from './index.js'
 
+/**
+ * @experimental Subject to change. Use at your own risk.
+ * Right now, when using `groupBy`, bulk edit is table-by-table.
+ * This is done by wrapping each table with its own `SelectionProvider`, instead of using the top-level `SelectionProvider`.
+ * In the future, we may want to change this to allow bulk edit across all tables through a single provider.
+ * This may require breaking changes, however, to re-shape the state to accommodate groups.
+ * Or, we can keep this nested provider pattern and lift state up to the parent component.
+ * To be determined.
+ */
 export const ConditionalSelectionProvider: React.FC<{
   children: React.ReactNode
   docs?: any[]
