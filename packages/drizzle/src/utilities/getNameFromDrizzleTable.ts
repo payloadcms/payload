01a916b93f6cab73f68fc7492f4624dcddbdd0cc
Alessio Gravili <alessio@gravili.de>
2025-07-23 16:46:47 -0700

    use gettablename

--- packages/drizzle/src/utilities/getNameFromDrizzleTable.ts
+++ packages/drizzle/src/utilities/getNameFromDrizzleTable.ts
@@ -3,13 +3,5 @@ import type { Table } from 'drizzle-orm'
 import { getTableName } from 'drizzle-orm'
 
 export const getNameFromDrizzleTable = (table: Table): string => {
-  const symbol = Object.getOwnPropertySymbols(table).find((symb) =>
-    symb.description.includes('drizzle:BaseName'),
-  )
-  // Drizzle:Name or drizzle:BaseName sometimes returns a UUID. Example:
-  // drizzle:BaseName: payload_locked_documents_rels
-  // drizzle:Name: 131e2d64_3237_426b_8466_f28e0d2be653
-  // getTableName(table): payload_locked_documents_rels
-
-  return table[symbol] || getTableName(table)
+  return getTableName(table)
 }
