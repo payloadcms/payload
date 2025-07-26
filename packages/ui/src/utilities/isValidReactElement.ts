e6ca66cc995f2c3c0862fb87c233089f064efde8
Alessio Gravili <alessio@gravili.de>
2025-07-26 13:52:41 +0200

    typo

--- packages/ui/src/utilities/isValidReactElement.ts
+++ packages/ui/src/utilities/isValidReactElement.ts
@@ -4,7 +4,7 @@ const LazyReactComponentSymbol = Symbol.for('react.lazy')
 
 /**
  * Since Next.js 15.4, `React.isValidElement()` returns `false` for components that cross the server-client boundary.
- * This utility expands on that check so that it returns true for valid React elements,
+ * This utility expands on that check so that it returns true for valid React elements.
  */
 export function isValidReactElement<P>(object: {} | null | undefined): object is ReactElement<P> {
   return React.isValidElement(object) || object?.['$$typeof'] === LazyReactComponentSymbol
