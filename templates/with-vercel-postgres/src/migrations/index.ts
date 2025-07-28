5c94d2dc7136b3c02c991bd05ad947a2eda699e5
Alessio Gravili <alessio@gravili.de>
2025-07-28 09:23:43 -0700

    feat: support next.js 15.4.4 (#13280)
    - bumps next.js from 15.3.2 to 15.4.4 in monorepo and templates. It's
    important to run our tests against the latest Next.js version to
    guarantee full compatibility.
    - bumps playwright because of peer dependency conflict with next 15.4.4
    - bumps react types because why not
    
    https://nextjs.org/blog/next-15-4
    
    As part of this upgrade, the functionality added by
    https://github.com/payloadcms/payload/pull/11658 broke. This PR fixes it
    by creating a wrapper around `React.isValidElemen`t that works for
    Next.js 15.4.
    
    ---
    - To see the specific tasks where the Asana app for GitHub is being
    used, see below:
      - https://app.asana.com/0/0/1210803039809808

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250725_134227_initial from './20250725_134227_initial'
+import * as migration_20250726_113907_initial from './20250726_113907_initial'
 
 export const migrations = [
   {
-    up: migration_20250725_134227_initial.up,
-    down: migration_20250725_134227_initial.down,
-    name: '20250725_134227_initial',
+    up: migration_20250726_113907_initial.up,
+    down: migration_20250726_113907_initial.down,
+    name: '20250726_113907_initial',
   },
 ]
