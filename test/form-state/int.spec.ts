0ae6ac44c9420a03ca41c53a89ac28de39828c92
Jacob Fletcher <jacobsfletch@gmail.com>
2025-08-12 11:48:55 -0400

    add more tests

--- test/form-state/int.spec.ts
+++ test/form-state/int.spec.ts
@@ -566,7 +566,58 @@ describe('Form State', () => {
     expect(newState === currentState).toBe(true)
   })
 
-  it('should not accept values from the server if they have been modified locally since the request was made', () => {
+  it('should accept all values from the server regardless of local modifications, e.g. on submit', () => {
+    const currentState = {
+      title: {
+        value: 'Test Post (modified on the client)',
+        initialValue: 'Test Post',
+        valid: true,
+        passesCondition: true,
+      },
+      computedTitle: {
+        value: 'Test Post (computed on the client)',
+        initialValue: 'Test Post',
+        valid: true,
+        passesCondition: true,
+      },
+    }
+
+    const formStateAtTimeOfRequest = {
+      ...currentState,
+      title: {
+        value: 'Test Post (modified on the client 2)',
+        initialValue: 'Test Post',
+        valid: true,
+        passesCondition: true,
+      },
+    }
+
+    const incomingStateFromServer = {
+      title: {
+        value: 'Test Post (modified on the server)',
+        initialValue: 'Test Post',
+        valid: true,
+        passesCondition: true,
+      },
+      computedTitle: {
+        value: 'Test Post (computed on the server)',
+        initialValue: 'Test Post',
+        valid: true,
+        passesCondition: true,
+      },
+    }
+
+    const newState = mergeServerFormState({
+      acceptValues: true,
+      currentState,
+      formStateAtTimeOfRequest,
+      incomingState: incomingStateFromServer,
+    })
+
+    expect(newState).toStrictEqual(incomingStateFromServer)
+  })
+
+  it('should not accept values from the server if they have been modified locally since the request was made, e.g. on autosave', () => {
     const currentState = {
       title: {
         value: 'Test Post (modified on the client 1)',
