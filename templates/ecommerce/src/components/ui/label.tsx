f99bc602fef6cc01a7a63ce804e3c9250872b87e
Paul Popus <paul@payloadcms.com>
2025-08-12 13:29:31 +0100

    fix labels

--- templates/ecommerce/src/components/ui/label.tsx
+++ templates/ecommerce/src/components/ui/label.tsx
@@ -10,7 +10,7 @@ function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimiti
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        'flex items-center gap-2 text-sm text-primary/50 font-mono uppercase leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        'flex items-center gap-2 text-sm text-primary/50 font-mono leading-none select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
         className,
       )}
       {...props}
