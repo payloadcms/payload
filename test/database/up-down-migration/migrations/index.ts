047519f47f62ac9bdf256e6e3b1b14ac9af7e449
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-08-14 02:44:56 +0300

    fix(db-postgres): ensure index names are not too long (#13428)
    Fixes https://github.com/payloadcms/payload/issues/13196

--- test/database/up-down-migration/migrations/index.ts
+++ test/database/up-down-migration/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250714_201659 from './20250714_201659.js'
+import * as migration_20250811_134524 from './20250811_134524.js'
 
 export const migrations = [
   {
-    up: migration_20250714_201659.up,
-    down: migration_20250714_201659.down,
-    name: '20250714_201659',
+    up: migration_20250811_134524.up,
+    down: migration_20250811_134524.down,
+    name: '20250811_134524',
   },
 ]
