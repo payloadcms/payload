6549e6dd69d00178faddaa84a9e7aedf588d972a
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-08-15 14:43:13 +0300

    fix(ui): cannot replace the file if the user does not have delete access

--- test/uploads/config.ts
+++ test/uploads/config.ts
@@ -31,6 +31,7 @@ import {
   listViewPreviewSlug,
   mediaSlug,
   mediaWithoutCacheTagsSlug,
+  mediaWithoutDeleteAccessSlug,
   mediaWithoutRelationPreviewSlug,
   mediaWithRelationPreviewSlug,
   noRestrictFileMimeTypesSlug,
@@ -931,6 +932,12 @@ export default buildConfigWithDefaults({
         staticDir: path.resolve(dirname, './svg-only'),
       },
     },
+    {
+      slug: mediaWithoutDeleteAccessSlug,
+      fields: [],
+      upload: true,
+      access: { delete: () => false },
+    },
   ],
   onInit: async (payload) => {
     const uploadsDir = path.resolve(dirname, './media')
@@ -953,6 +960,8 @@ export default buildConfigWithDefaults({
       data: {},
       file: imageFile,
     })
+
+    await payload.create({ collection: mediaWithoutDeleteAccessSlug, data: {}, file: imageFile })
 
     const { id: versionedImage } = await payload.create({
       collection: versionSlug,
