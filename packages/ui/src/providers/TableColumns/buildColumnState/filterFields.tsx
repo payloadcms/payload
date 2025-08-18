760d7828e2e276258239b52ed49a7bbd3e6ff008
Patrik Kozak <35232443+PatrikKozak@users.noreply.github.com>
2025-08-18 13:49:08 -0400

    fix(ui): make filterFields self-recursive for subfields and remove the extra helper

--- packages/ui/src/providers/TableColumns/buildColumnState/filterFields.tsx
+++ packages/ui/src/providers/TableColumns/buildColumnState/filterFields.tsx
@@ -11,40 +11,36 @@ export const filterFields = <T extends ClientField | Field>(incomingFields: T[])
     (field.type !== 'ui' && fieldIsHiddenOrDisabled(field) && !fieldIsID(field)) ||
     field?.admin?.disableListColumn === true
 
-  const recurse = (fields: T[] | undefined): T[] =>
-    (fields ?? []).reduce<T[]>((acc, field) => {
-      if (shouldSkipField(field)) {
-        return acc
-      }
-
-      // handle tabs
-      if (field.type === 'tabs' && 'tabs' in field) {
-        const formattedField: T = {
-          ...field,
-          tabs: field.tabs.map((tab) => ({
-            ...tab,
-            fields: recurse(tab.fields as T[]),
-          })),
-        }
-        acc.push(formattedField)
-        return acc
-      }
-
-      // handle fields with sub fields (row, group, collapsible, etc.)
-      if ('fields' in field && Array.isArray(field.fields)) {
-        const formattedField: T = {
-          ...field,
-          fields: recurse(field.fields as T[]),
-        }
-        acc.push(formattedField)
-        return acc
-      }
-
-      // leaf
-      acc.push(field)
+  return (incomingFields ?? []).reduce<T[]>((acc, field) => {
+    if (shouldSkipField(field)) {
       return acc
-    }, [])
+    }
 
-  const fields: T[] = recurse(incomingFields)
-  return fields
+    // handle tabs
+    if (field.type === 'tabs' && 'tabs' in field) {
+      const formattedField: T = {
+        ...field,
+        tabs: field.tabs.map((tab) => ({
+          ...tab,
+          fields: filterFields(tab.fields as T[]),
+        })),
+      }
+      acc.push(formattedField)
+      return acc
+    }
+
+    // handle fields with subfields (row, group, collapsible, etc.)
+    if ('fields' in field && Array.isArray(field.fields)) {
+      const formattedField: T = {
+        ...field,
+        fields: filterFields(field.fields as T[]),
+      }
+      acc.push(formattedField)
+      return acc
+    }
+
+    // leaf
+    acc.push(field)
+    return acc
+  }, [])
 }
