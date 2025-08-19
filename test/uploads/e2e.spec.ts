368cd901f8b509a10c9c1b505e436297535a31f1
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-08-19 22:39:31 +0300

    fix(ui): replace deprecated `document.execCommand('copy')` API with `navigator` (#13431)
    Replaces the deprecated API which might cause issues with the copy
    button.
    
    Also adds an E2E test for the copy button

--- test/uploads/e2e.spec.ts
+++ test/uploads/e2e.spec.ts
@@ -135,6 +135,7 @@ describe('Uploads', () => {
     mediaWithoutDeleteAccessURL = new AdminUrlUtil(serverURL, mediaWithoutDeleteAccessSlug)
 
     const context = await browser.newContext()
+    await context.grantPermissions(['clipboard-read', 'clipboard-write'])
     page = await context.newPage()
 
     const { consoleErrors, collectErrors, stopCollectingErrors } = initPageConsoleErrorCatch(page, {
@@ -216,6 +217,22 @@ describe('Uploads', () => {
     await page.locator('.doc-drawer__header-close').click()
 
     await expect(filename).toContainText('test-image.png')
+  })
+
+  test('should copy the file url field to the clipboard', async () => {
+    const mediaDoc = (
+      await payload.find({
+        collection: mediaSlug,
+        depth: 0,
+        limit: 1,
+        pagination: false,
+      })
+    ).docs[0]
+
+    await page.goto(mediaURL.edit(mediaDoc!.id))
+    await page.locator('.copy-to-clipboard').click()
+    const clipbaordContent = await page.evaluate(() => navigator.clipboard.readText())
+    expect(clipbaordContent).toBe(mediaDoc?.url)
   })
 
   test('should create file upload', async () => {
