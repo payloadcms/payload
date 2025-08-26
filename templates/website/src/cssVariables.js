94961f6c4887407a273d475e703b2540e395e0e6
Vincent Vu <vincent@rubixstudios.com.au>
2025-08-26 23:57:57 +1000

    fix(theme): reorder breakpoints so smaller max-widths are evaluated first

--- templates/website/src/cssVariables.js
+++ templates/website/src/cssVariables.js
@@ -2,11 +2,11 @@
 
 export const cssVariables = {
   breakpoints: {
-    '3xl': 1920,
-    '2xl': 1536,
-    xl: 1280,
-    lg: 1024,
-    md: 768,
     sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    '2xl': 1536,
+    '3xl': 1920,
   },
 }
