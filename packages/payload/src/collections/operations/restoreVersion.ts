27a461b2cff3ca5e03cb448270383eaf4a8cc58f
Jessica Chowdhury <jessica@trbl.design>
2025-08-27 12:43:38 +0100

    chore: if restoreAsDraft unset use incoming status

--- packages/payload/src/collections/operations/restoreVersion.ts
+++ packages/payload/src/collections/operations/restoreVersion.ts
@@ -260,8 +260,8 @@ export const restoreVersionOperation = async <TData extends TypeWithID = any>(
 
     // Ensure updatedAt date is always updated
     result.updatedAt = new Date().toISOString()
-    // Ensure status respects draft arg
-    result._status = draftArg ? 'draft' : 'published'
+    // Ensure status respects restoreAsDraft arg
+    result._status = draftArg ? 'draft' : result._status
     result = await req.payload.db.updateOne({
       id: parentDocID,
       collection: collectionConfig.slug,
