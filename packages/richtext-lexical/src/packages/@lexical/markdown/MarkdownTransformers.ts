831b8fa362f6b089efb18e90b730c9a4621bf38f
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-09-04 17:18:11 +0100

    re-export ElementTransformer instead of copying them

--- packages/richtext-lexical/src/packages/@lexical/markdown/MarkdownTransformers.ts
+++ packages/richtext-lexical/src/packages/@lexical/markdown/MarkdownTransformers.ts
@@ -1,34 +1,4 @@
 /* eslint-disable regexp/no-unused-capturing-group */
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
-import type { ListType } from '@lexical/list'
-import type { ElementTransformer } from '@lexical/markdown'
-import type { HeadingTagType } from '@lexical/rich-text'
-import type { ElementNode } from 'lexical'
-
-import {
-  $createListItemNode,
-  $createListNode,
-  $isListItemNode,
-  $isListNode,
-  ListItemNode,
-  ListNode,
-} from '@lexical/list'
-import {
-  $createHeadingNode,
-  $createQuoteNode,
-  $isHeadingNode,
-  $isQuoteNode,
-  HeadingNode,
-  QuoteNode,
-} from '@lexical/rich-text'
-import { $createLineBreakNode } from 'lexical'
 
 export type {
   ElementTransformer,
@@ -52,196 +22,21 @@ const TABLE_ROW_DIVIDER_REG_EXP = /^(\| ?:?-*:? ?)+\|\s?$/
 const TAG_START_REGEX = /^[ \t]*<[a-z_][\w-]*(?:\s[^<>]*)?\/?>/i
 const TAG_END_REGEX = /^[ \t]*<\/[a-z_][\w-]*\s*>/i
 
-const createBlockNode = (
-  createNode: (match: Array<string>) => ElementNode,
-): ElementTransformer['replace'] => {
-  return (parentNode, children, match) => {
-    const node = createNode(match)
-    node.append(...children)
-    parentNode.replace(node)
-    node.select(0, 0)
-  }
-}
-
-// Amount of spaces that define indentation level
-// TODO: should be an option
-const LIST_INDENT_SIZE = 4
-
-function getIndent(whitespaces: string): number {
-  const tabs = whitespaces.match(/\t/g)
-  const spaces = whitespaces.match(/ /g)
-
-  let indent = 0
-
-  if (tabs) {
-    indent += tabs.length
-  }
-
-  if (spaces) {
-    indent += Math.floor(spaces.length / LIST_INDENT_SIZE)
-  }
-
-  return indent
-}
-
-const listReplace = (listType: ListType): ElementTransformer['replace'] => {
-  return (parentNode, children, match) => {
-    const previousNode = parentNode.getPreviousSibling()
-    const nextNode = parentNode.getNextSibling()
-    const listItem = $createListItemNode(listType === 'check' ? match[3] === 'x' : undefined)
-    if ($isListNode(nextNode) && nextNode.getListType() === listType) {
-      const firstChild = nextNode.getFirstChild()
-      if (firstChild !== null) {
-        firstChild.insertBefore(listItem)
-      } else {
-        // should never happen, but let's handle gracefully, just in case.
-        nextNode.append(listItem)
-      }
-      parentNode.remove()
-    } else if ($isListNode(previousNode) && previousNode.getListType() === listType) {
-      previousNode.append(listItem)
-      parentNode.remove()
-    } else {
-      const list = $createListNode(listType, listType === 'number' ? Number(match[2]) : undefined)
-      list.append(listItem)
-      parentNode.replace(list)
-    }
-    listItem.append(...children)
-    listItem.select(0, 0)
-    const indent = getIndent(match[1])
-    if (indent) {
-      listItem.setIndent(indent)
-    }
-  }
-}
-
-const listExport = (
-  listNode: ListNode,
-  exportChildren: (node: ElementNode) => string,
-  depth: number,
-): string => {
-  const output: string[] = []
-  const children = listNode.getChildren()
-  let index = 0
-  for (const listItemNode of children) {
-    if ($isListItemNode(listItemNode)) {
-      if (listItemNode.getChildrenSize() === 1) {
-        const firstChild = listItemNode.getFirstChild()
-        if ($isListNode(firstChild)) {
-          output.push(listExport(firstChild, exportChildren, depth + 1))
-          continue
-        }
-      }
-      const indent = ' '.repeat(depth * LIST_INDENT_SIZE)
-      const listType = listNode.getListType()
-      const prefix =
-        listType === 'number'
-          ? `${listNode.getStart() + index}. `
-          : listType === 'check'
-            ? `- [${listItemNode.getChecked() ? 'x' : ' '}] `
-            : '- '
-      output.push(indent + prefix + exportChildren(listItemNode))
-      index++
-    }
-  }
-
-  return output.join('\n')
-}
-
-export const HEADING: ElementTransformer = {
-  type: 'element',
-  dependencies: [HeadingNode],
-  export: (node, exportChildren) => {
-    if (!$isHeadingNode(node)) {
-      return null
-    }
-    const level = Number(node.getTag().slice(1))
-    return '#'.repeat(level) + ' ' + exportChildren(node)
-  },
-  regExp: HEADING_REGEX,
-  replace: createBlockNode((match) => {
-    const tag = ('h' + match[1]!.length) as HeadingTagType
-    return $createHeadingNode(tag)
-  }),
-}
-
-export const QUOTE: ElementTransformer = {
-  type: 'element',
-  dependencies: [QuoteNode],
-  export: (node, exportChildren) => {
-    if (!$isQuoteNode(node)) {
-      return null
-    }
-
-    const lines = exportChildren(node).split('\n')
-    const output: string[] = []
-    for (const line of lines) {
-      output.push('> ' + line)
-    }
-    return output.join('\n')
-  },
-  regExp: QUOTE_REGEX,
-  replace: (parentNode, children, _match, isImport) => {
-    if (isImport) {
-      const previousNode = parentNode.getPreviousSibling()
-      if ($isQuoteNode(previousNode)) {
-        previousNode.splice(previousNode.getChildrenSize(), 0, [
-          $createLineBreakNode(),
-          ...children,
-        ])
-        previousNode.select(0, 0)
-        parentNode.remove()
-        return
-      }
-    }
-
-    const node = $createQuoteNode()
-    node.append(...children)
-    parentNode.replace(node)
-    node.select(0, 0)
-  },
-}
-
-export const UNORDERED_LIST: ElementTransformer = {
-  type: 'element',
-  dependencies: [ListNode, ListItemNode],
-  export: (node, exportChildren) => {
-    return $isListNode(node) ? listExport(node, exportChildren, 0) : null
-  },
-  regExp: UNORDERED_LIST_REGEX,
-  replace: listReplace('bullet'),
-}
-
-export const CHECK_LIST: ElementTransformer = {
-  type: 'element',
-  dependencies: [ListNode, ListItemNode],
-  export: (node, exportChildren) => {
-    return $isListNode(node) ? listExport(node, exportChildren, 0) : null
-  },
-  regExp: CHECK_LIST_REGEX,
-  replace: listReplace('check'),
-}
-
-export const ORDERED_LIST: ElementTransformer = {
-  type: 'element',
-  dependencies: [ListNode, ListItemNode],
-  export: (node, exportChildren) => {
-    return $isListNode(node) ? listExport(node, exportChildren, 0) : null
-  },
-  regExp: ORDERED_LIST_REGEX,
-  replace: listReplace('number'),
-}
-
 export {
   BOLD_ITALIC_STAR,
   BOLD_ITALIC_UNDERSCORE,
   BOLD_STAR,
   BOLD_UNDERSCORE,
+  CHECK_LIST,
+  HEADING,
   HIGHLIGHT,
   INLINE_CODE,
   ITALIC_STAR,
   ITALIC_UNDERSCORE,
+  ORDERED_LIST,
+  QUOTE,
   STRIKETHROUGH,
+  UNORDERED_LIST,
 } from '@lexical/markdown'
 
 export function normalizeMarkdown(input: string, shouldMergeAdjacentLines: boolean): string {
