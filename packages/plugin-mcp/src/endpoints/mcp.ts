b7d2bd394cc22a21df3f7733ca9dc4b1062b3c3e
Kendell Joseph <kendelljoseph@gmail.com>
2025-09-04 15:29:16 -0400

    chore: removes endpoint global, updates capitalization

--- packages/plugin-mcp/src/endpoints/mcp.ts
+++ packages/plugin-mcp/src/endpoints/mcp.ts
@@ -1,27 +1,27 @@
 import type { PayloadHandler } from 'payload'
 
-import type { GlobalSettings, PluginMcpServerConfig } from '../types.js'
+import type { GlobalSettings, PluginMCPServerConfig } from '../types.js'
 
 import { createRequestFromPayloadRequest } from '../mcp/createRequest.js'
-import { getMcpHandler } from '../mcp/getMcpHandler.js'
+import { getMCPHandler } from '../mcp/getMcpHandler.js'
 
-export const initializeMcpHandler = (pluginOptions: PluginMcpServerConfig) => {
+export const initializeMCPHandler = (pluginOptions: PluginMCPServerConfig) => {
+  let toolsSettings: GlobalSettings['tools'] | undefined
+
   const mcpHandler: PayloadHandler = async (req) => {
-    const endpointSettings = await req.payload.findGlobal({
-      slug: 'payload-mcp-endpoint',
-      user: req.user,
-    })
-    const toolsSettings = await req.payload.findGlobal({
-      slug: 'payload-mcp-tools',
-      user: req.user,
-    })
+    if (!toolsSettings) {
+      toolsSettings = await req.payload.findGlobal({
+        slug: 'payload-mcp-tools',
+        user: req.user,
+      })
+    }
+
     const globalSettings = {
-      endpoint: endpointSettings,
       tools: toolsSettings,
     } as GlobalSettings
 
     const request = createRequestFromPayloadRequest(req)
-    const handler = getMcpHandler(pluginOptions, globalSettings, req)
+    const handler = getMCPHandler(pluginOptions, globalSettings, req)
     return await handler(request)
   }
   return mcpHandler
