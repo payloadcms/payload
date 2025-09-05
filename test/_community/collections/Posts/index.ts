38c346b4c72fee53674e293b263ca826925fec90
Jacob Fletcher <jacobsfletch@gmail.com>
2025-09-05 09:44:59 -0400

    reset _community and base tsconfig

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -1,15 +1,13 @@
 import type { CollectionConfig } from 'payload'
 
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+
 export const postsSlug = 'posts'
 
 export const PostsCollection: CollectionConfig = {
   slug: postsSlug,
   admin: {
     useAsTitle: 'title',
-    enableListViewSelectAPI: true,
-  },
-  forceSelect: {
-    title: true,
   },
   fields: [
     {
@@ -17,14 +15,11 @@ export const PostsCollection: CollectionConfig = {
       type: 'text',
     },
     {
-      name: 'array',
-      type: 'array',
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-      ],
+      name: 'content',
+      type: 'richText',
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [...defaultFeatures],
+      }),
     },
   ],
 }
