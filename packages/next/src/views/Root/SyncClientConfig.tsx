d1e198f8a3b8aee2e4ce21229e2006f5c0356dea
Jacob Fletcher <jacobsfletch@gmail.com>
2025-09-08 22:50:10 -0400

    do not sync on first render

--- packages/next/src/views/Root/SyncClientConfig.tsx
+++ packages/next/src/views/Root/SyncClientConfig.tsx
@@ -2,19 +2,25 @@
 import type { ClientConfig } from 'payload'
 
 import { useConfig } from '@payloadcms/ui'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 /**
- * This component is required n order for the _page_ to be able to refresh the client config,
- * which may have been cached on the _layout_ level, where the ConfigProvider lives.
- * Since the layout does not re-render on page navigation, we need to manually update the config
- * when the page changes, as it may have gone from unauthenticated to authenticated, which affects
- * the client config.
+ * This component is required in order for the _page_ to be able to refresh the client config,
+ * which may have been cached on the _layout_ level, where the ConfigProvider is managed.
+ * Since the layout does not re-render on page navigation / authentication, we need to manually
+ * update the config, as the user may have been authenticated in the process, which affects the client config.
  */
 export const SyncClientConfig = ({ clientConfig }: { clientConfig: ClientConfig }) => {
   const { setConfig } = useConfig()
+  const initialRender = useRef(true)
 
   useEffect(() => {
+    // Can skip the initial render, as the config will already be set by the layout
+    if (initialRender.current) {
+      initialRender.current = false
+      return
+    }
+
     setConfig(clientConfig)
   }, [clientConfig, setConfig])
 
