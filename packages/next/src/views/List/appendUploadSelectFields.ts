6c48e34802ff2ba8d82f0adde2d69c3775efed9a
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-09-18 11:48:57 -0400

    moves logic into external function

---
+++ packages/next/src/views/List/appendUploadSelectFields.ts
@@ -0,0 +1,49 @@
+import type { SanitizedCollectionConfig } from 'payload'
+
+/**
+ * Mutates the incoming select object to append fields required for upload thumbnails
+ * @param collectionConfig
+ * @param select
+ */
+export const appendUploadSelectFields = ({
+  collectionConfig,
+  select,
+}: {
+  collectionConfig: SanitizedCollectionConfig
+  select: Record<string, unknown>
+}) => {
+  if (!collectionConfig.upload) {
+    return
+  }
+
+  select.mimeType = true
+  select.thumbnailURL = true
+
+  if (collectionConfig.upload.imageSizes && collectionConfig.upload.imageSizes.length > 0) {
+    if (
+      collectionConfig.upload.adminThumbnail &&
+      typeof collectionConfig.upload.adminThumbnail === 'string'
+    ) {
+      /** Only return image size properties that are required to generate the adminThumbnailURL */
+      select.sizes = {
+        [collectionConfig.upload.adminThumbnail]: {
+          filename: true,
+        },
+      }
+    } else {
+      /** Only return image size properties that are required for thumbnails */
+      select.sizes = collectionConfig.upload.imageSizes.reduce((acc, imageSizeConfig) => {
+        return {
+          ...acc,
+          [imageSizeConfig.name]: {
+            filename: true,
+            url: true,
+            width: true,
+          },
+        }
+      }, {})
+    }
+  } else {
+    select.url = true
+  }
+}
--- packages/next/src/views/List/index.tsx
+++ packages/next/src/views/List/index.tsx
@@ -25,6 +25,7 @@ import {
 import React, { Fragment } from 'react'
 
 import { getDocumentPermissions } from '../Document/getDocumentPermissions.js'
+import { appendUploadSelectFields } from './appendUploadSelectFields.js'
 import { handleGroupBy } from './handleGroupBy.js'
 import { renderListViewSlots } from './renderListViewSlots.js'
 import { resolveAllFilterOptions } from './resolveAllFilterOptions.js'
@@ -224,41 +225,10 @@ export const renderListView = async (
       : undefined
 
     /** Force select image fields for list view thumbnails */
-    if (collectionConfig.upload && select) {
-      select.mimeType = true
-
-      if (collectionConfig.upload.adminThumbnail) {
-        select.thumbnailURL = true
-      }
-
-      if (collectionConfig.upload.imageSizes && collectionConfig.upload.imageSizes.length > 0) {
-        if (
-          collectionConfig.upload.adminThumbnail &&
-          typeof collectionConfig.upload.adminThumbnail === 'string'
-        ) {
-          /** Only return image size properties that are required to generate the adminThumbnailURL */
-          select.sizes = {
-            [collectionConfig.upload.adminThumbnail]: {
-              filename: true,
-            },
-          }
-        } else {
-          /** Only return image size properties that are required for thumbnails */
-          select.sizes = collectionConfig.upload.imageSizes.reduce((acc, imageSizeConfig) => {
-            return {
-              ...acc,
-              [imageSizeConfig.name]: {
-                filename: true,
-                url: true,
-                width: true,
-              },
-            }
-          }, {})
-        }
-      } else {
-        select.url = true
-      }
-    }
+    appendUploadSelectFields({
+      collectionConfig,
+      select,
+    })
 
     try {
       if (collectionConfig.admin.groupBy && query.groupBy) {
