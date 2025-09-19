966fed98fe63eb03c7097480dc8f54061dd06aba
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-09-19 16:41:38 +0100

    fix importMap

---
+++ test/dashboard/components/Revenue.tsx
@@ -0,0 +1,11 @@
+export function Revenue() {
+  return (
+    <div>
+      Revenue
+      <h1>Revenue</h1>
+      <p>This is the revenue component</p>
+      <p>This is the revenue component</p>
+      <p>This is the revenue component</p>
+    </div>
+  )
+}
--- test/dashboard/config.ts
+++ test/dashboard/config.ts
@@ -1,8 +1,21 @@
+import { fileURLToPath } from 'node:url'
+import path from 'path'
+
 import { buildConfigWithDefaults } from '../buildConfigWithDefaults.js'
 import { devUser } from '../credentials.js'
 
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
 export default buildConfigWithDefaults({
-  admin: {},
+  admin: {
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+    components: {
+      afterNavLinks: ['/components/Revenue.js#Revenue'],
+    },
+  },
   onInit: async (payload) => {
     await payload.create({
       collection: 'users',
