78d9d44f99c91f199963eb4b528b28f4f87d648e
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-09-22 16:39:54 +0100

    make responsive

--- packages/next/src/views/Dashboard/Modular/client.tsx
+++ packages/next/src/views/Dashboard/Modular/client.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { type DashboardConfig } from 'payload'
-import GridLayout from 'react-grid-layout'
+import { Responsive, WidthProvider } from 'react-grid-layout'
 
 interface ModularDashboardClientProps {
   dashboardConfig?: DashboardConfig
 }
+
+const ResponsiveGridLayout = WidthProvider(Responsive)
 
 export function ModularDashboardClient({ dashboardConfig }: ModularDashboardClientProps) {
   const layout = [
@@ -13,8 +15,18 @@ export function ModularDashboardClient({ dashboardConfig }: ModularDashboardClie
     { h: 2, i: 'b', maxW: 4, minW: 2, w: 3, x: 1, y: 0 },
     { h: 2, i: 'c', w: 1, x: 4, y: 0 },
   ]
+
   return (
-    <GridLayout className="grid-layout" cols={12} layout={layout} rowHeight={30} width={1200}>
+    <ResponsiveGridLayout
+      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      className="grid-layout"
+      cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+      //   layout={layout}
+      rowHeight={100}
+      //   width={1200} not needed / doesn't have effect when using WidthProvider
+      //   resizeHandle
+      //   onLayoutChange={onLayoutChange}
+    >
       <div className="widget" key="a">
         a
       </div>
@@ -24,6 +36,6 @@ export function ModularDashboardClient({ dashboardConfig }: ModularDashboardClie
       <div className="widget" key="c">
         c
       </div>
-    </GridLayout>
+    </ResponsiveGridLayout>
   )
 }
