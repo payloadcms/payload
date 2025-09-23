9872d24357cd166874621488ff5e2e143cdf15b1
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-09-23 13:47:55 -0400

    simplify react code

--- packages/ui/src/elements/Status/AllLocaleStatusCell/index.server.tsx
+++ packages/ui/src/elements/Status/AllLocaleStatusCell/index.server.tsx
@@ -1,29 +1,47 @@
-import { createLocalReq, type DefaultServerCellComponentProps } from 'payload'
+import type { DefaultServerCellComponentProps, SanitizedLocalizationConfig } from 'payload'
+
+import { createLocalReq } from 'payload'
 import React from 'react'
 
-import { AllLocaleStatusCellClient } from './index.client.js'
+import { Pill } from '../../Pill/index.js'
+import './index.scss'
 
-export const AllLocaleStatusCell = async ({ cellData, payload }: DefaultServerCellComponentProps) => {
-  const { localization } = payload.config
+const baseClass = 'locale-status-cell'
+
+export const AllLocaleStatusCell = async ({
+  cellData,
+  payload,
+  rowData,
+}: DefaultServerCellComponentProps) => {
+  const localization = payload.config.localization as SanitizedLocalizationConfig
   const req = await createLocalReq({}, payload)
 
-  let availableLocales: string[] = []
+  const availableLocale =
+    (await localization.filterAvailableLocales?.({
+      locales: localization.locales,
+      req,
+    })) ?? localization.locales
 
-
-  if (localization) {
-    const filtered =
-      (await localization.filterAvailableLocales?.({
-        locales: localization.locales,
-        req,
-      })) ?? localization.locales
-
-    availableLocales = filtered.map((locale) => locale.code)
+  const fallbackStatus = rowData?._status
+  if (!cellData && !fallbackStatus) {
+    return null
   }
 
   return (
-    <AllLocaleStatusCellClient
-      availableLocales={availableLocales}
-      data={cellData}
-    />
+    <div className={baseClass}>
+      {availableLocale.map((locale) => {
+        const status = cellData[locale.code] || fallbackStatus
+
+        return (
+          <Pill
+            key={locale.code}
+            pillStyle={status === 'published' ? 'success' : 'light-gray'}
+            size="small"
+          >
+            {locale.code}
+          </Pill>
+        )
+      })}
+    </div>
   )
 }
