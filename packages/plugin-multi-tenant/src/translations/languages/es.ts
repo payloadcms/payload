fcb8b5a066f8ea2f5be550423519f54cc6d13216
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2025-09-24 13:19:33 -0400

    feat(plugin-multi-tenant): improves tenant assignment flow (#13881)
    ### Improved tenant assignment flow
    This PR improves the tenant assignment flow. I know a lot of users liked
    the previous flow where the field was not injected into the document.
    But the original flow, confused many of users because the tenant filter
    (top left) was being used to set the tenant on the document _and_ filter
    the list view.
    
    This change shown below is aiming to solve both of those groups with a
    slightly different approach. As always, feedback is welcome while we try
    to really make this plugin work for everyone.
    
    
    https://github.com/user-attachments/assets/ceee8b3a-c5f5-40e9-8648-f583e2412199
    
    Added 2 new localization strings:
    
    ```
    // shown in the 3 dot menu
    'assign-tenant-button-label': 'Assign Tenant',
    
    // shown when needing to assign a tenant to a NEW document
    'assign-tenant-modal-title': 'Assign "{{title}}"',
    ```
    
    Removed 2 localization strings:
    ```
    'confirm-modal-tenant-switch--body',
    'confirm-modal-tenant-switch--heading'
    ```

--- packages/plugin-multi-tenant/src/translations/languages/es.ts
+++ packages/plugin-multi-tenant/src/translations/languages/es.ts
@@ -2,9 +2,8 @@ import type { PluginDefaultTranslationsObject, PluginLanguage } from '../types.j
 
 export const esTranslations: PluginDefaultTranslationsObject = {
   'plugin-multi-tenant': {
-    'confirm-modal-tenant-switch--body':
-      'Está a punto de cambiar la propiedad de <0>{{fromTenant}}</0> a <0>{{toTenant}}</0>',
-    'confirm-modal-tenant-switch--heading': 'Confirme el cambio de {{tenantLabel}}',
+    'assign-tenant-button-label': 'Asignar Inquilino',
+    'assign-tenant-modal-title': 'Asignar "{{title}}"',
     'field-assignedTenant-label': 'Inquilino Asignado',
     'nav-tenantSelector-label': 'Inquilino',
   },
--- packages/plugin-multi-tenant/src/translations/languages/vi.ts
+++ packages/plugin-multi-tenant/src/translations/languages/vi.ts
@@ -2,9 +2,8 @@ import type { PluginDefaultTranslationsObject, PluginLanguage } from '../types.j
 
 export const viTranslations: PluginDefaultTranslationsObject = {
   'plugin-multi-tenant': {
-    'confirm-modal-tenant-switch--body':
-      'Bạn sắp chuyển quyền sở hữu từ <0>{{fromTenant}}</0> đến <0>{{toTenant}}</0>',
-    'confirm-modal-tenant-switch--heading': 'Xác nhận thay đổi {{tenantLabel}}',
+    'assign-tenant-button-label': 'Giao Tenant',
+    'assign-tenant-modal-title': 'Gán "{{title}}"',
     'field-assignedTenant-label': 'Người thuê đã được chỉ định',
     'nav-tenantSelector-label': 'Người thuê',
   },
