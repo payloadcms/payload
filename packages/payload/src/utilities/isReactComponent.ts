4cf710a99655c8dad53a66947752b2dc0ab76369
Luke Sandberg <lukeisandberg@gmail.com>
2025-09-25 13:08:44 -0700

    drop the length check

--- packages/payload/src/utilities/isReactComponent.ts
+++ packages/payload/src/utilities/isReactComponent.ts
@@ -5,21 +5,17 @@ const clientRefSymbol = Symbol.for('react.client.reference')
 export function isReactServerComponentOrFunction<T extends any>(
   component: any | React.ComponentType,
 ): component is T {
-  return isSingleArgFunction(component) && component.$$typeof !== clientRefSymbol
+  return typeof component === 'function' && component.$$typeof !== clientRefSymbol
 }
 
 export function isReactClientComponent<T extends any>(
   component: any | React.ComponentType,
 ): component is T {
-  return isSingleArgFunction(component) && component.$$typeof === clientRefSymbol
+  return typeof component === 'function' && component.$$typeof === clientRefSymbol
 }
 
 export function isReactComponentOrFunction<T extends any>(
   component: any | React.ComponentType,
 ): component is T {
-  return isSingleArgFunction(component)
-}
-
-function isSingleArgFunction(component: unknown): component is ((props: unknown) => unknown)& { $$typeof?: unknown } {
-  return typeof component === 'function' && component.length === 1
+  return typeof component === 'function'
 }
