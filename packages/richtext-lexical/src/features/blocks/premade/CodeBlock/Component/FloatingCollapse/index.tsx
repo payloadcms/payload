56a1b3b25daa9c55cccd805fe098ab367d0d0a87
Alessio Gravili <alessio@gravili.de>
2025-10-01 10:14:27 -0700

    label

--- packages/richtext-lexical/src/features/blocks/premade/CodeBlock/Component/FloatingCollapse/index.tsx
+++ packages/richtext-lexical/src/features/blocks/premade/CodeBlock/Component/FloatingCollapse/index.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 import './index.scss'
 
 const baseClass = 'code-block-floating-collapse-button'
-import { useCollapsible } from '@payloadcms/ui'
+import { useCollapsible, useTranslation } from '@payloadcms/ui'
 
 import { CollapseIcon } from '../../../../../../lexical/ui/icons/Collapse/index.js'
 
 export const FloatingCollapse: React.FC = () => {
   const { isCollapsed, toggle } = useCollapsible()
+  const { t } = useTranslation()
 
   if (!isCollapsed) {
     return null
@@ -16,6 +17,7 @@ export const FloatingCollapse: React.FC = () => {
 
   return (
     <button className={baseClass} onClick={toggle} type="button">
+      <span>{t('general:collapse')}</span>
       <CollapseIcon />
     </button>
   )
