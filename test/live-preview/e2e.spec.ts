d80e055905cbd8caa6b839551fda77f9ea0d3343
Paul Popus <paul@payloadcms.com>
2025-10-02 14:30:49 +0100

    add tests

--- test/live-preview/e2e.spec.ts
+++ test/live-preview/e2e.spec.ts
@@ -31,6 +31,7 @@ import {
 } from './helpers.js'
 import {
   collectionLevelConfigSlug,
+  customLivePreviewSlug,
   desktopBreakpoint,
   mobileBreakpoint,
   pagesSlug,
@@ -53,6 +54,7 @@ describe('Live Preview', () => {
   let postsURLUtil: AdminUrlUtil
   let ssrPagesURLUtil: AdminUrlUtil
   let ssrAutosavePagesURLUtil: AdminUrlUtil
+  let customLivePreviewURLUtil: AdminUrlUtil
   let payload: PayloadTestSDK<Config>
   let user: any
 
@@ -63,6 +65,7 @@ describe('Live Preview', () => {
     pagesURLUtil = new AdminUrlUtil(serverURL, pagesSlug)
     postsURLUtil = new AdminUrlUtil(serverURL, postsSlug)
     ssrPagesURLUtil = new AdminUrlUtil(serverURL, ssrPagesSlug)
+    customLivePreviewURLUtil = new AdminUrlUtil(serverURL, customLivePreviewSlug)
     ssrAutosavePagesURLUtil = new AdminUrlUtil(serverURL, ssrAutosavePagesSlug)
 
     const context = await browser.newContext()
@@ -689,5 +692,13 @@ describe('Live Preview', () => {
     await selectLivePreviewZoom(page, '200%')
     await ensureDeviceIsLeftAligned(page)
     expect(true).toBeTruthy()
+  })
+
+  test('can open a custom live-preview', async () => {
+    await goToCollectionLivePreview(page, customLivePreviewURLUtil)
+
+    const customLivePreview = page.locator('.custom-live-preview')
+
+    await expect(customLivePreview).toContainText('Custom live preview being rendered')
   })
 })
