6175937354c9bc7fa7d7781e2706f920c848d544
Patrik Kozak <35232443+PatrikKozak@users.noreply.github.com>
2025-10-06 14:45:51 -0400

    test: adds test

--- packages/next/src/exports/views.ts
+++ packages/next/src/exports/views.ts
@@ -1,3 +1,4 @@
+export { CreateFirstUserView } from '../views/CreateFirstUser/index.js'
 export { NotFoundPage } from '../views/NotFound/index.js'
 export { type GenerateViewMetadata, RootPage } from '../views/Root/index.js'
 export { generatePageMetadata } from '../views/Root/metadata.js'
