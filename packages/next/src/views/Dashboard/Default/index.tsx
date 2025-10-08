8c732093378b245590524c10f6056100b14895fb
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-10-08 08:31:00 +0100

    fix css

--- packages/next/src/views/Dashboard/Default/index.tsx
+++ packages/next/src/views/Dashboard/Default/index.tsx
@@ -4,7 +4,6 @@ import { Gutter } from '@payloadcms/ui'
 import { RenderServerComponent } from '@payloadcms/ui/elements/RenderServerComponent'
 import React from 'react'
 
-import './index.scss'
 import { GridLayoutDashboard } from './GridLayout/server.js'
 
 const baseClass = 'dashboard'
@@ -14,38 +13,36 @@ export function DefaultDashboard(props: DashboardViewServerProps) {
   const { afterDashboard, beforeDashboard } = payload.config.admin.components
 
   return (
-    <div className={baseClass}>
-      <Gutter className={`${baseClass}__wrap`}>
-        {beforeDashboard &&
-          RenderServerComponent({
-            Component: beforeDashboard,
-            importMap: payload.importMap,
-            serverProps: {
-              i18n,
-              locale,
-              params,
-              payload,
-              permissions,
-              searchParams,
-              user,
-            } satisfies ServerProps,
-          })}
-        <GridLayoutDashboard {...props} />
-        {afterDashboard &&
-          RenderServerComponent({
-            Component: afterDashboard,
-            importMap: payload.importMap,
-            serverProps: {
-              i18n,
-              locale,
-              params,
-              payload,
-              permissions,
-              searchParams,
-              user,
-            } satisfies ServerProps,
-          })}
-      </Gutter>
-    </div>
+    <Gutter className={baseClass}>
+      {beforeDashboard &&
+        RenderServerComponent({
+          Component: beforeDashboard,
+          importMap: payload.importMap,
+          serverProps: {
+            i18n,
+            locale,
+            params,
+            payload,
+            permissions,
+            searchParams,
+            user,
+          } satisfies ServerProps,
+        })}
+      <GridLayoutDashboard {...props} />
+      {afterDashboard &&
+        RenderServerComponent({
+          Component: afterDashboard,
+          importMap: payload.importMap,
+          serverProps: {
+            i18n,
+            locale,
+            params,
+            payload,
+            permissions,
+            searchParams,
+            user,
+          } satisfies ServerProps,
+        })}
+    </Gutter>
   )
 }
--- packages/ui/src/widgets/CollectionCards/index.tsx
+++ packages/ui/src/widgets/CollectionCards/index.tsx
@@ -3,15 +3,15 @@ import type { WidgetServerProps } from 'payload'
 import { getTranslation } from '@payloadcms/translations'
 import { EntityType, getAccessResults } from 'payload'
 import { formatAdminURL } from 'payload/shared'
-import React, { Fragment } from 'react'
+import React from 'react'
 
 import { Button } from '../../elements/Button/index.js'
 import { Card } from '../../elements/Card/index.js'
 import { Locked } from '../../elements/Locked/index.js'
 import { getGlobalData } from '../../utilities/getGlobalData.js'
 import { getNavGroups } from '../../utilities/getNavGroups.js'
-import { getVisibleEntities } from '../../utilities/getVisibleEntities.js'
 import './index.scss'
+import { getVisibleEntities } from '../../utilities/getVisibleEntities.js'
 
 const baseClass = 'collection-cards'
 
@@ -26,108 +26,110 @@ export async function CollectionCards(props: WidgetServerProps) {
   const navGroups = getNavGroups(permissions, visibleEntities, payload.config, i18n)
 
   return (
-    <Fragment>
-      {!navGroups || navGroups?.length === 0 ? (
-        <p>no nav groups....</p>
-      ) : (
-        navGroups.map(({ entities, label }, groupIndex) => {
-          return (
-            <div className={`${baseClass}__group`} key={groupIndex}>
-              <h2 className={`${baseClass}__label`}>{label}</h2>
-              <ul className={`${baseClass}__card-list`}>
-                {entities.map(({ slug, type, label }, entityIndex) => {
-                  let title: string
-                  let buttonAriaLabel: string
-                  let createHREF: string
-                  let href: string
-                  let hasCreatePermission: boolean
-                  let isLocked = null
-                  let userEditing = null
+    <div className={baseClass}>
+      <div className={`${baseClass}__wrap`}>
+        {!navGroups || navGroups?.length === 0 ? (
+          <p>no nav groups....</p>
+        ) : (
+          navGroups.map(({ entities, label }, groupIndex) => {
+            return (
+              <div className={`${baseClass}__group`} key={groupIndex}>
+                <h2 className={`${baseClass}__label`}>{label}</h2>
+                <ul className={`${baseClass}__card-list`}>
+                  {entities.map(({ slug, type, label }, entityIndex) => {
+                    let title: string
+                    let buttonAriaLabel: string
+                    let createHREF: string
+                    let href: string
+                    let hasCreatePermission: boolean
+                    let isLocked = null
+                    let userEditing = null
 
-                  // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
-                  if (type === EntityType.collection) {
-                    title = getTranslation(label, i18n)
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+                    if (type === EntityType.collection) {
+                      title = getTranslation(label, i18n)
 
-                    buttonAriaLabel = t('general:showAllLabel', { label: title })
+                      buttonAriaLabel = t('general:showAllLabel', { label: title })
 
-                    href = formatAdminURL({ adminRoute, path: `/collections/${slug}` })
+                      href = formatAdminURL({ adminRoute, path: `/collections/${slug}` })
 
-                    createHREF = formatAdminURL({
-                      adminRoute,
-                      path: `/collections/${slug}/create`,
-                    })
+                      createHREF = formatAdminURL({
+                        adminRoute,
+                        path: `/collections/${slug}/create`,
+                      })
 
-                    hasCreatePermission = permissions?.collections?.[slug]?.create
-                  }
+                      hasCreatePermission = permissions?.collections?.[slug]?.create
+                    }
 
-                  // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
-                  if (type === EntityType.global) {
-                    title = getTranslation(label, i18n)
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+                    if (type === EntityType.global) {
+                      title = getTranslation(label, i18n)
 
-                    buttonAriaLabel = t('general:editLabel', {
-                      label: getTranslation(label, i18n),
-                    })
+                      buttonAriaLabel = t('general:editLabel', {
+                        label: getTranslation(label, i18n),
+                      })
 
-                    href = formatAdminURL({
-                      adminRoute,
-                      path: `/globals/${slug}`,
-                    })
+                      href = formatAdminURL({
+                        adminRoute,
+                        path: `/globals/${slug}`,
+                      })
 
-                    // Find the lock status for the global
-                    const globalLockData = globalData.find((global) => global.slug === slug)
-                    if (globalLockData) {
-                      isLocked = globalLockData.data._isLocked
-                      userEditing = globalLockData.data._userEditing
+                      // Find the lock status for the global
+                      const globalLockData = globalData.find((global) => global.slug === slug)
+                      if (globalLockData) {
+                        isLocked = globalLockData.data._isLocked
+                        userEditing = globalLockData.data._userEditing
 
-                      // Check if the lock is expired
-                      const lockDuration = globalLockData?.lockDuration
-                      const lastEditedAt = new Date(globalLockData.data?._lastEditedAt).getTime()
+                        // Check if the lock is expired
+                        const lockDuration = globalLockData?.lockDuration
+                        const lastEditedAt = new Date(globalLockData.data?._lastEditedAt).getTime()
 
-                      const lockDurationInMilliseconds = lockDuration * 1000
-                      const lockExpirationTime = lastEditedAt + lockDurationInMilliseconds
+                        const lockDurationInMilliseconds = lockDuration * 1000
+                        const lockExpirationTime = lastEditedAt + lockDurationInMilliseconds
 
-                      if (new Date().getTime() > lockExpirationTime) {
-                        isLocked = false
-                        userEditing = null
+                        if (new Date().getTime() > lockExpirationTime) {
+                          isLocked = false
+                          userEditing = null
+                        }
                       }
                     }
-                  }
 
-                  return (
-                    <li key={entityIndex}>
-                      <Card
-                        actions={
-                          isLocked && user?.id !== userEditing?.id ? (
-                            <Locked className={`${baseClass}__locked`} user={userEditing} />
-                          ) : // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
-                          hasCreatePermission && type === EntityType.collection ? (
-                            <Button
-                              aria-label={t('general:createNewLabel', {
-                                label,
-                              })}
-                              buttonStyle="icon-label"
-                              el="link"
-                              icon="plus"
-                              iconStyle="with-border"
-                              round
-                              to={createHREF}
-                            />
-                          ) : undefined
-                        }
-                        buttonAriaLabel={buttonAriaLabel}
-                        href={href}
-                        id={`card-${slug}`}
-                        title={getTranslation(label, i18n)}
-                        titleAs="h3"
-                      />
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-          )
-        })
-      )}
-    </Fragment>
+                    return (
+                      <li key={entityIndex}>
+                        <Card
+                          actions={
+                            isLocked && user?.id !== userEditing?.id ? (
+                              <Locked className={`${baseClass}__locked`} user={userEditing} />
+                            ) : // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+                            hasCreatePermission && type === EntityType.collection ? (
+                              <Button
+                                aria-label={t('general:createNewLabel', {
+                                  label,
+                                })}
+                                buttonStyle="icon-label"
+                                el="link"
+                                icon="plus"
+                                iconStyle="with-border"
+                                round
+                                to={createHREF}
+                              />
+                            ) : undefined
+                          }
+                          buttonAriaLabel={buttonAriaLabel}
+                          href={href}
+                          id={`card-${slug}`}
+                          title={getTranslation(label, i18n)}
+                          titleAs="h3"
+                        />
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })
+        )}
+      </div>
+    </div>
   )
 }
