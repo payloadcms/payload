d56956aeeef4af8f06b5dbe272abebf37ed9143f
Alessio Gravili <alessio@gravili.de>
2025-10-16 15:45:19 -0700

    refactor: simplify parsePayloadComponent

--- packages/payload/src/bin/generateImportMap/utilities/parsePayloadComponent.ts
+++ packages/payload/src/bin/generateImportMap/utilities/parsePayloadComponent.ts
@@ -11,18 +11,19 @@ export function parsePayloadComponent(PayloadComponent: PayloadComponent): {
   const pathAndMaybeExport =
     typeof PayloadComponent === 'string' ? PayloadComponent : PayloadComponent.path
 
-  let path: string | undefined = ''
-  let exportName: string | undefined = 'default'
+  let path: string
+  let exportName: string
 
-  if (pathAndMaybeExport?.includes('#')) {
+  if (pathAndMaybeExport.includes('#')) {
     ;[path, exportName] = pathAndMaybeExport.split('#')
   } else {
     path = pathAndMaybeExport
+    exportName = 'default'
   }
 
   if (typeof PayloadComponent === 'object' && PayloadComponent.exportName) {
     exportName = PayloadComponent.exportName
   }
 
-  return { exportName: exportName!, path: path! }
+  return { exportName, path }
 }
