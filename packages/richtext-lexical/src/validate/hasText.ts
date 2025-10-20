31d68e44608a76f0fcb5b72017fdafb3d18a409d
Alessio Gravili <alessio@gravili.de>
2025-10-20 16:41:12 -0700

    jsdocs

--- packages/richtext-lexical/src/validate/hasText.ts
+++ packages/richtext-lexical/src/validate/hasText.ts
@@ -5,6 +5,11 @@ import type {
   SerializedTextNode,
 } from 'lexical'
 
+/**
+ * This function checks if the editor state is empty (has any text). If the editor state has no nodes,
+ * or only an empty paragraph node (no TextNode with length > 0), it returns false.
+ * Otherwise, it returns true.
+ */
 export function hasText(
   value: null | SerializedEditorState<SerializedLexicalNode> | undefined,
 ): boolean {
