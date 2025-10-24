77ca3670cace1d3c6039fe0c42b69a646fb5c627
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-10-24 16:17:59 +0300

    remove `jest.config.js`

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'node',
-    testTimeout: 90000,
+    testTimeout: 5000,
     projects: ['packages/*'],
   },
 })
