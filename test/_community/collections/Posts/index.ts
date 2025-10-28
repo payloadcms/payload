f7ba5f0276c164d4521ef8d8ebf8664e4ffc81d1
Jessica Chowdhury <jessica@trbl.design>
2025-10-28 11:37:12 +0000

    chore: add repro

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -16,11 +16,48 @@ export const PostsCollection: CollectionConfig = {
       type: 'text',
     },
     {
-      name: 'content',
-      type: 'richText',
-      editor: lexicalEditor({
-        features: ({ defaultFeatures }) => [...defaultFeatures],
-      }),
+      name: 'array',
+      type: 'array',
+      fields: [
+        {
+          name: 'text',
+          type: 'text',
+        },
+        {
+          name: 'testSelect',
+          type: 'select',
+          options: [
+            {
+              label: 'Option 1',
+              value: 'option1',
+            },
+            {
+              label: 'Option 2',
+              value: 'option2',
+            },
+            {
+              label: 'Option 3',
+              value: 'option3',
+            },
+            {
+              label: 'Option 4',
+              value: 'option4',
+            },
+            {
+              label: 'Option 5',
+              value: 'option5',
+            },
+            {
+              label: 'Option 6',
+              value: 'option6',
+            },
+            {
+              label: 'Option 7',
+              value: 'option7',
+            },
+          ],
+        },
+      ],
     },
   ],
 }
