ceb2515f215381d4e8f48bb33ce8c017b6adc069
Alessio Gravili <alessio@gravili.de>
2025-11-04 18:06:32 -0800

    fix lint

--- packages/richtext-lexical/src/packages/@lexical/markdown/MarkdownImport.ts
+++ packages/richtext-lexical/src/packages/@lexical/markdown/MarkdownImport.ts
@@ -272,7 +272,6 @@ function createTextFormatTransformersIndex(
     fullMatchRegExpByTag,
 
     // Regexp to locate *any* potential opening tag (longest first).
-    // eslint-disable-next-line regexp/no-useless-character-class, regexp/no-empty-capturing-group, regexp/no-empty-group
     openTagsRegExp: new RegExp(`${escapeRegExp}(${openTagsRegExp.join('|')})`, 'g'),
     transformersByTag,
   }
