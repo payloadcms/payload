6819b763f87445727122ba8c035afc3b0a6f65e7
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-11-12 15:59:20 -0500

    passing test

--- packages/ui/src/elements/Autosave/index.tsx
+++ packages/ui/src/elements/Autosave/index.tsx
@@ -134,13 +134,13 @@ export const Autosave: React.FC<Props> = ({ id, collection, global: globalDoc })
 
           if (collection && id) {
             entitySlug = collection.slug
-            url = `${serverURL}${api}/${entitySlug}/${id}?depth=0&draft=true&autosave=true&locale=${localeRef.current}`
+            url = `${serverURL}${api}/${entitySlug}/${id}?depth=0&draft=true&autosave=true&fallback-locale=null&locale=${localeRef.current}`
             method = 'PATCH'
           }
 
           if (globalDoc) {
             entitySlug = globalDoc.slug
-            url = `${serverURL}${api}/globals/${entitySlug}?depth=0&draft=true&autosave=true&locale=${localeRef.current}`
+            url = `${serverURL}${api}/globals/${entitySlug}?depth=0&draft=true&autosave=true&fallback-locale=null&locale=${localeRef.current}`
             method = 'POST'
           }
 
