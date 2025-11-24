40065eade563c56aa15dfb8c88e795a2bd5d5e08
Jarrod Flesch <jarrodmflesch@gmail.com>
2025-11-24 09:49:52 -0500

    close modal before navigating

--- packages/ui/src/elements/IDLabel/index.tsx
+++ packages/ui/src/elements/IDLabel/index.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useModal } from '@faceless-ui/modal'
 import React from 'react'
 
 import './index.scss'
@@ -7,6 +8,7 @@ import { useConfig } from '../../providers/Config/index.js'
 import { useDocumentInfo } from '../../providers/DocumentInfo/index.js'
 import { formatAdminURL } from '../../utilities/formatAdminURL.js'
 import { sanitizeID } from '../../utilities/sanitizeID.js'
+import { useDocumentDrawerContext } from '../DocumentDrawer/Provider.js'
 import { useDrawerDepth } from '../Drawer/index.js'
 
 const baseClass = 'id-label'
@@ -24,17 +26,31 @@ export const IDLabel: React.FC<{ className?: string; id: string; prefix?: string
 
   const { collectionSlug, globalSlug } = useDocumentInfo()
   const drawerDepth = useDrawerDepth()
+  const { drawerSlug } = useDocumentDrawerContext()
+  const { closeModal } = useModal()
 
   const docPath = formatAdminURL({
     adminRoute,
     path: `/${collectionSlug ? `collections/${collectionSlug}` : `globals/${globalSlug}`}/${id}`,
   })
 
+  const onClick = React.useCallback(() => {
+    if (drawerSlug) {
+      closeModal(drawerSlug)
+    }
+  }, [drawerSlug, closeModal])
+
   return (
     <div className={[baseClass, className].filter(Boolean).join(' ')} title={id}>
       {prefix}
       &nbsp;
-      {drawerDepth > 1 ? <Link href={docPath}>{sanitizeID(id)}</Link> : sanitizeID(id)}
+      {drawerDepth > 1 ? (
+        <Link href={docPath} onClick={onClick}>
+          {sanitizeID(id)}
+        </Link>
+      ) : (
+        sanitizeID(id)
+      )}
     </div>
   )
 }
