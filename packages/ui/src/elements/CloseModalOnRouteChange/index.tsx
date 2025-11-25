42b430aa1ca0ee670648bef6097a2a08332e056a
Jessica Chowdhury <jessica@trbl.design>
2025-11-25 13:46:25 +0000

    chore: add imports

--- packages/ui/src/elements/CloseModalOnRouteChange/index.tsx
+++ packages/ui/src/elements/CloseModalOnRouteChange/index.tsx
@@ -2,7 +2,7 @@
 
 import { useModal } from '@faceless-ui/modal'
 import { usePathname } from 'next/navigation.js'
-import { useEffect } from 'react'
+import { useEffectEvent, useRef } from 'react'
 
 export function CloseModalOnRouteChange() {
   const { closeAllModals } = useModal()
