edf2c4a1ae0f25fcb72db19113283748de4e1d87
Jacob Fletcher <jacobsfletch@gmail.com>
2025-11-25 21:02:33 -0500

    lookup config after authorization and cleanup jsdocs

--- packages/next/src/utilities/slugify.ts
+++ packages/next/src/utilities/slugify.ts
@@ -8,11 +8,14 @@ import {
 import { slugify as defaultSlugify, type Slugify } from 'payload/shared'
 
 /**
+ * This server function is directly related to the {@link https://payloadcms.com/docs/fields/text#slug-field | Slug Field}.
  * This is a server function that is used to invoke the user's custom slugify function from the client.
- * It is required as there is no other way for us to pass this function across the client-server boundary.
- * Not through props, or even a locally defined server function within a server component.
- * It _must_ be defined entirely outside of the component tree, which also means we lose access to the custom function.
- * Fortunately, a globally scoped server function has access to the full Payload config, but _only_ within `@payloadcms/next`.
+ * This pattern is required, as there is no other way for us to pass their function across the client-server boundary.
+ *   - Not through props
+ *   - Not from a server function defined within a server component (see below)
+ * When a server function contains non-serializable data within its closure, it gets passed through the boundary (and breaks).
+ * The only way to pass server functions to the client (that contain non-serializable data) is if it is globally defined.
+ * But we also cannot define this function alongside the server component, as we will not have access to their custom slugify function.
  * See `ServerFunctionsProvider` for more details.
  */
 export const slugifyHandler: ServerFunction<
@@ -21,15 +24,15 @@ export const slugifyHandler: ServerFunction<
 > = async (args) => {
   const { collectionSlug, globalSlug, path, req, val } = args
 
+  if (!req.user) {
+    throw new UnauthorizedError()
+  }
+
   const docConfig = collectionSlug
     ? req.payload.collections[collectionSlug]?.config
     : globalSlug
       ? req.payload.config.globals.find((g) => g.slug === globalSlug)
       : null
-
-  if (!req.user) {
-    throw new UnauthorizedError()
-  }
 
   const { field } = getFieldByPath({
     config: req.payload.config,
