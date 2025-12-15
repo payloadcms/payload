38c2f899b9b57b337d9323c858f29674972d3e29
Jake <jacobsfletch@gmail.com>
2025-12-15 10:15:07 -0500

    fix(next): omit server url from route matching (#14919)
    Follow up to https://github.com/payloadcms/payload/pull/14907. Fixes
    https://github.com/payloadcms/payload/issues/14900.
    
    Since https://github.com/payloadcms/payload/pull/14869, setting a
    `serverURL` causes some additional admin routing to break, including
    `/admin/logout`.
    
    We need to ensure that all routes are relative before matching them.
    
    ---------
    
    Co-authored-by: Jarrod Flesch <jarrodmflesch@gmail.com>

--- test/server-url/e2e.spec.ts
+++ test/server-url/e2e.spec.ts
@@ -1,6 +1,9 @@
 import type { Page } from '@playwright/test'
 
 import { expect, test } from '@playwright/test'
+import { login } from 'helpers/e2e/auth/login.js'
+import { logoutViaNav } from 'helpers/e2e/auth/logout.js'
+import { openNav } from 'helpers/e2e/toggleNav.js'
 import * as path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -28,8 +31,15 @@ test.describe('serverURL', () => {
     await ensureCompilationIsDone({ page, serverURL })
   })
 
-  test('can load admin panel when serverURL is set', async () => {
+  test('can load admin panel', async () => {
+    await login({ page, serverURL: url.serverURL })
     await page.goto(url.admin)
-    await expect(page.getByText('Dashboard')).toBeVisible()
+    await expect(page.locator('.dashboard')).toBeVisible()
+  })
+
+  test('can log out', async () => {
+    await page.goto(url.admin)
+    await logoutViaNav(page)
+    await expect(page.locator('.login')).toBeVisible()
   })
 })
