8e7cf9a71cc3b2b618cb7597270bfc581ae55835
Jake <jacobsfletch@gmail.com>
2025-12-15 10:17:05 -0500

    refactor: add relative flag to formatAdminURL util

--- packages/payload/src/utilities/formatAdminURL.ts
+++ packages/payload/src/utilities/formatAdminURL.ts
@@ -3,22 +3,37 @@ import type { Config } from '../config/types.js'
 /** Will read the `routes.admin` config and appropriately handle `"/"` admin paths */
 export const formatAdminURL = (args: {
   adminRoute: NonNullable<Config['routes']>['admin']
+  /**
+   * The subpath of your application, if specified.
+   * @see https://nextjs.org/docs/app/api-reference/config/next-config-js/basePath
+   * @example '/docs'
+   */
   basePath?: string
-  path: '' | `/${string}` | null | undefined
+  path?: '' | `/${string}` | null
+  /**
+   * Return a relative URL, e.g. ignore `serverURL`.
+   * Useful for route-matching, etc.
+   */
+  relative?: boolean
   serverURL?: Config['serverURL']
 }): string => {
-  const { adminRoute, basePath = '', path: pathFromArgs, serverURL } = args
-  const path = pathFromArgs || ''
+  const { adminRoute, basePath = '', path = '', relative = false, serverURL } = args
 
-  if (adminRoute) {
-    if (adminRoute === '/') {
-      if (!path) {
-        return `${serverURL || ''}${basePath}${adminRoute}`
-      }
-    } else {
-      return `${serverURL || ''}${basePath}${adminRoute}${path}`
-    }
+  const pathSegments = [basePath]
+
+  if (adminRoute && adminRoute !== '/') {
+    pathSegments.push(adminRoute)
   }
 
-  return `${serverURL || ''}${basePath}${path}`
+  if (path && !(adminRoute === '/' && !path)) {
+    pathSegments.push(path)
+  }
+
+  const pathname = pathSegments.join('') || '/'
+
+  if (relative || !serverURL) {
+    return pathname
+  }
+
+  return new URL(pathname, serverURL).toString()
 }
