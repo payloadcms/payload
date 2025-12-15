16f35200033fe761ac8d584b1208829c6e94396f
Jake <jacobsfletch@gmail.com>
2025-12-15 10:52:11 -0500

    preserve serverURL jsdocs from config

--- packages/payload/src/utilities/formatAdminURL.ts
+++ packages/payload/src/utilities/formatAdminURL.ts
@@ -1,22 +1,29 @@
 import type { Config } from '../config/types.js'
 
-/** Will read the `routes.admin` config and appropriately handle `"/"` admin paths */
-export const formatAdminURL = (args: {
-  adminRoute: NonNullable<Config['routes']>['admin']
-  /**
-   * The subpath of your application, if specified.
-   * @see https://nextjs.org/docs/app/api-reference/config/next-config-js/basePath
-   * @example '/docs'
-   */
-  basePath?: string
-  path?: '' | `/${string}` | null
-  /**
-   * Return a relative URL, e.g. ignore `serverURL`.
-   * Useful for route-matching, etc.
-   */
-  relative?: boolean
-  serverURL?: Config['serverURL']
-}): string => {
+/**
+ * This function builds correct URLs for admin panel routing.
+ * Its primary responsibilities are:
+ * 1. Read from your `routes.admin` config and appropriately handle `"/"` admin paths
+ * 2. Prepend the `basePath` from your Next.js config, if specified
+ * 3. Return relative or absolute URLs, as needed
+ */
+export const formatAdminURL = (
+  args: {
+    adminRoute: NonNullable<Config['routes']>['admin']
+    /**
+     * The subpath of your application, if specified.
+     * @see https://nextjs.org/docs/app/api-reference/config/next-config-js/basePath
+     * @example '/docs'
+     */
+    basePath?: string
+    path?: '' | `/${string}` | null
+    /**
+     * Return a relative URL, e.g. ignore `serverURL`.
+     * Useful for route-matching, etc.
+     */
+    relative?: boolean
+  } & Pick<Config, 'serverURL'>,
+): string => {
   const { adminRoute, basePath = '', path = '', relative = false, serverURL } = args
 
   const pathSegments = [basePath]
