46e1f09fab160eabe7f48036a81512095482dfbe
Kendell Joseph <kjoseph@figma.com>
2025-12-16 14:47:28 -0500

    chore: adds agents and cursor rules to templates

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251107_183841_initial from './20251107_183841_initial'
+import * as migration_20251216_194325_initial from './20251216_194325_initial'
 
 export const migrations = [
   {
-    up: migration_20251107_183841_initial.up,
-    down: migration_20251107_183841_initial.down,
-    name: '20251107_183841_initial',
+    up: migration_20251216_194325_initial.up,
+    down: migration_20251216_194325_initial.down,
+    name: '20251216_194325_initial',
   },
 ]
