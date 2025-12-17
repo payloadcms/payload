996c9e72c09c6562b51ca8fc3370f960c3001d6b
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-12-17 14:09:34 +0000

    try to fix CI

--- packages/ui/src/widgets/CollectionCards/index.tsx
+++ packages/ui/src/widgets/CollectionCards/index.tsx
@@ -22,7 +22,7 @@ export async function CollectionCards(props: WidgetServerProps) {
   const permissions = await getAccessResults({ req: props.req })
   const visibleEntities = getVisibleEntities({ req: props.req })
   const globalData = await getGlobalData(props.req)
-  const { serverURL } = payload.config
+  // const { serverURL } = payload.config
 
   const navGroups = getNavGroups(permissions, visibleEntities, payload.config, i18n)
 
@@ -55,7 +55,7 @@ export async function CollectionCards(props: WidgetServerProps) {
                       href = formatAdminURL({
                         adminRoute,
                         path: `/collections/${slug}`,
-                        serverURL,
+                        // serverURL,
                       })
 
                       createHREF = formatAdminURL({
