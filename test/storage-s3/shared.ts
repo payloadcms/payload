ffd5ed75be4672b8f1d0a046bdaf7ab33990a593
Paul Popus <paul@payloadcms.com>
2025-12-17 16:41:14 -0600

    add tests

--- test/storage-s3/shared.ts
+++ test/storage-s3/shared.ts
@@ -3,3 +3,4 @@ export const mediaWithPrefixSlug = 'media-with-prefix'
 export const prefix = 'test-prefix'
 
 export const mediaWithSignedDownloadsSlug = 'media-with-signed-downloads'
+export const mediaWithDirectAccessSlug = 'media-with-direct-access'
