5cf17cb2ac1f0c7359eac3c496958f437fa02b9e
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-12-19 20:22:30 +0200

    fix ensureIndexes

--- test/collections-graphql/int.spec.ts
+++ test/collections-graphql/int.spec.ts
@@ -30,12 +30,7 @@ describe('collections-graphql', () => {
     // Wait for indexes to be created,
     // as we need them to query by point
     if (payload.db.name === 'mongoose') {
-      await new Promise((resolve, reject) => {
-        payload.db?.collections?.point?.ensureIndexes(function (err) {
-          if (err) reject(err)
-          resolve(true)
-        })
-      })
+      await payload.db.collections[pointSlug].ensureIndexes()
     }
   })
 
