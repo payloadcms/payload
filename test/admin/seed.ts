ccc77de230747da3b1ccffcd5b50f1e74727e438
Sasha <64744993+r1tsuu@users.noreply.github.com>
2025-12-19 23:22:09 +0200

    change test seed to sequential

--- test/admin/seed.ts
+++ test/admin/seed.ts
@@ -4,14 +4,14 @@ import { devUser } from '../credentials'
 import { seedDB } from '../helpers/seed'
 import {
   collectionSlugs,
+  customIdCollectionId,
+  customIdCollectionSlug,
   customViews1CollectionSlug,
   customViews2CollectionSlug,
   geoCollectionSlug,
   noApiViewCollectionSlug,
   postsCollectionSlug,
   usersCollectionSlug,
-  customIdCollectionSlug,
-  customIdCollectionId,
 } from './slugs'
 
 export async function clearAndSeedEverything(_payload: Payload) {
@@ -21,66 +21,72 @@ export async function clearAndSeedEverything(_payload: Payload) {
     collectionSlugs,
     _payload,
     seedFunction: async (_payload) => {
-      await Promise.all([
+      _payload.create({
+        collection: usersCollectionSlug,
+        data: {
+          email: devUser.email,
+          password: devUser.password,
+        },
+      })
+
+      for (let i = 0; i < 11; i++) {
         _payload.create({
-          collection: usersCollectionSlug,
+          collection: postsCollectionSlug,
           data: {
-            email: devUser.email,
-            password: devUser.password,
+            title: 'Title',
+            description: 'Description',
           },
-        }),
-        ...[...Array(11)].map(() => {
-          _payload.create({
-            collection: postsCollectionSlug,
-            data: {
-              title: 'Title',
-              description: 'Description',
-            },
-          })
-        }),
-        _payload.create({
-          collection: customViews1CollectionSlug,
-          data: {
-            title: 'Custom View',
-          },
-        }),
-        _payload.create({
-          collection: customViews2CollectionSlug,
-          data: {
-            title: 'Custom View',
-          },
-        }),
-        _payload.create({
-          collection: geoCollectionSlug,
-          data: {
-            point: [7, -7],
-          },
-        }),
-        _payload.create({
-          collection: geoCollectionSlug,
-          data: {
-            point: [5, -5],
-          },
-        }),
-        _payload.create({
-          collection: noApiViewCollectionSlug,
-          data: {},
-        }),
-        _payload.create({
-          collection: 'customIdTab',
-          data: {
-            id: customIdCollectionId,
-            title: 'Hello world title',
-          },
-        }),
-        _payload.create({
-          collection: 'customIdRow',
-          data: {
-            id: customIdCollectionId,
-            title: 'Hello world title',
-          },
-        }),
-      ])
+        })
+      }
+
+      _payload.create({
+        collection: customViews1CollectionSlug,
+        data: {
+          title: 'Custom View',
+        },
+      })
+
+      _payload.create({
+        collection: customViews2CollectionSlug,
+        data: {
+          title: 'Custom View',
+        },
+      })
+
+      _payload.create({
+        collection: geoCollectionSlug,
+        data: {
+          point: [7, -7],
+        },
+      })
+
+      _payload.create({
+        collection: geoCollectionSlug,
+        data: {
+          point: [5, -5],
+        },
+      })
+
+      _payload.create({
+        collection: noApiViewCollectionSlug,
+        data: {},
+      })
+
+      _payload.create({
+        collection: 'customIdTab',
+        data: {
+          id: customIdCollectionId,
+          title: 'Hello world title',
+        },
+      })
+
+      _payload.create({
+        collection: 'customIdRow',
+        data: {
+          id: customIdCollectionId,
+          title: 'Hello world title',
+        },
+      })
     },
   })
 }
