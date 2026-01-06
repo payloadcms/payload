908cd15e25822db3a1a443ab983e804f88b42f5b
Oliver Abbey <oliver.abbey@madetech.com>
2026-01-06 17:29:28 +0000

    chore: add context to duplicate field name error

--- packages/payload/src/errors/DuplicateFieldName.ts
+++ packages/payload/src/errors/DuplicateFieldName.ts
@@ -1,9 +1,12 @@
 import { APIError } from './APIError.js'
 
 export class DuplicateFieldName extends APIError {
-  constructor(fieldName: string) {
+  constructor(fieldName: string, collectionName?: string, blockFieldName?: string) {
+    const collectionString = collectionName ? ` in collection '${collectionName}'` : ''
+    const blockFieldString = blockFieldName ? ` in field '${blockFieldName}'` : ''
+    const contextString = collectionString + blockFieldString
     super(
-      `A field with the name '${fieldName}' was found multiple times on the same level. Field names must be unique.`,
+      `A field with the name '${fieldName}' was found multiple times on the same level${contextString}. Field names must be unique.`,
     )
   }
 }
