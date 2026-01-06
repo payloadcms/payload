e0ad3d174b7ca81ccfbadff3f996a2cbcf6fa0e3
Alessio Gravili <github@gravili.net>
2026-01-05 18:49:26 -0800

    templates

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-import * as migration_20251218_211052_initial from './20251218_211052_initial'
+import * as migration_20260106_024646_initial from './20260106_024646_initial'
 
 export const migrations = [
   {
-    up: migration_20251218_211052_initial.up,
-    down: migration_20251218_211052_initial.down,
-    name: '20251218_211052_initial',
-=======
-import * as migration_20260105_201440_initial from './20260105_201440_initial'
-
-export const migrations = [
-  {
-    up: migration_20260105_201440_initial.up,
-    down: migration_20260105_201440_initial.down,
-    name: '20260105_201440_initial',
->>>>>>> origin/main
+    up: migration_20260106_024646_initial.up,
+    down: migration_20260106_024646_initial.down,
+    name: '20260106_024646_initial',
   },
 ]
--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-import * as migration_20251218_211104_initial from './20251218_211104_initial'
+import * as migration_20260106_024703_initial from './20260106_024703_initial'
 
 export const migrations = [
   {
-    up: migration_20251218_211104_initial.up,
-    down: migration_20251218_211104_initial.down,
-    name: '20251218_211104_initial',
-=======
-import * as migration_20260105_201500_initial from './20260105_201500_initial'
-
-export const migrations = [
-  {
-    up: migration_20260105_201500_initial.up,
-    down: migration_20260105_201500_initial.down,
-    name: '20260105_201500_initial',
->>>>>>> origin/main
+    up: migration_20260106_024703_initial.up,
+    down: migration_20260106_024703_initial.down,
+    name: '20260106_024703_initial',
   },
 ]
