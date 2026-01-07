3294dec918d36841c013fbcf6ee1bb523ee8b39e
Sasha <64744993+r1tsuu@users.noreply.github.com>
2026-01-07 12:44:11 +0200

    try use normal db

---
+++ test/helpers/db/mongodb/run-test-connection.ts
@@ -0,0 +1,15 @@
+/**
+ * Test script to verify MongoDB connection, replica set, and transaction functionality
+ *
+ * Usage:
+ *   pnpm tsx test/helpers/db/mongodb/run-test-connection.ts
+ */
+
+import { testConnection } from './test-connection.js'
+
+void testConnection(
+  process.env.MONGODB_URL ||
+    'mongodb://payload:payload@localhost:27018/payload?authSource=admin&directConnection=true&replicaSet=rs0',
+).then(() => {
+  console.log('Connected')
+})
