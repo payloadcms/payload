250f29dd9de8186fd94fcb99b370691a98fa2c8d
Alessio Gravili <github@gravili.net>
2026-01-10 17:14:43 -0800

    remove unnecessary DefaultGeneratedTypes type

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -11,56 +11,9 @@ import type {
   TypedCollectionSelect,
   TypedGlobal,
   TypedGlobalSelect,
-  TypeWithID,
   Where,
 } from 'payload'
 import type { MarkOptional } from 'ts-essentials'
-
-export interface DefaultGeneratedTypes extends BaseGeneratedTypes {
-  auth: {
-    [slug: string]: {
-      forgotPassword: {
-        email: string
-      }
-      login: {
-        email: string
-        password: string
-      }
-      registerFirstUser: {
-        email: string
-        password: string
-      }
-      unlock: {
-        email: string
-      }
-    }
-  }
-
-  collections: {
-    [slug: string]: JsonObject & TypeWithID
-  }
-  collectionsJoins: {
-    [slug: string]: {
-      [schemaPath: string]: string
-    }
-  }
-
-  collectionsSelect: {
-    [slug: string]: any
-  }
-  db: {
-    defaultIDType: number | string
-  }
-  globals: {
-    [slug: string]: JsonObject
-  }
-
-  globalsSelect: {
-    [slug: string]: any
-  }
-
-  locale: null | string
-}
 
 // TODO: Investigate reusing the types from payload
 export type DataFromCollectionSlug<
