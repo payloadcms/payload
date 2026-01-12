e0fdf79a2b08c91a359f4a50109faea395e8fa67
Kendell Joseph <kjoseph@figma.com>
2026-01-12 11:37:28 -0500

    chore: adds error message

--- packages/ui/src/elements/Table/DefaultCell/fields/JSON/index.tsx
+++ packages/ui/src/elements/Table/DefaultCell/fields/JSON/index.tsx
@@ -3,16 +3,26 @@ import type { DefaultCellComponentProps, JSONFieldClient } from 'payload'
 
 import React from 'react'
 
+import { useTranslation } from '../../../../../providers/Translation/index.js'
 import './index.scss'
 
 export const JSONCell: React.FC<DefaultCellComponentProps<JSONFieldClient>> = ({ cellData }) => {
-  const cellDataString = JSON.stringify(cellData)
-  const textToShow =
-    cellDataString.length > 100 ? `${cellDataString.substring(0, 100)}\u2026` : cellDataString
+  const { t } = useTranslation()
+  try {
+    const cellDataString = JSON.stringify(cellData)
+    const textToShow =
+      cellDataString.length > 100 ? `${cellDataString.substring(0, 100)}\u2026` : cellDataString
 
-  return (
-    <code className="json-cell">
-      <span>{textToShow}</span>
-    </code>
-  )
+    return (
+      <code className="json-cell">
+        <span>{textToShow}</span>
+      </code>
+    )
+  } catch (_ignore) {
+    return (
+      <code className="json-cell">
+        <span>{t('general:error')}</span>
+      </code>
+    )
+  }
 }
