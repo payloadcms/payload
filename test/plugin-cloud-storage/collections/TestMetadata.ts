0d9a23efbfdb1b20cb1a71089e69c167aa38bc66
Jessica Chowdhury <jessica@trbl.design>
2026-01-13 16:10:56 +0000

    chore: simplify test collection

--- test/plugin-cloud-storage/collections/TestMetadata.ts
+++ test/plugin-cloud-storage/collections/TestMetadata.ts
@@ -18,63 +18,6 @@ export const TestMetadata: CollectionConfig = {
         description: 'Test note to identify this upload',
       },
     },
-    // Metadata fields that will be automatically populated by the custom adapter
-    {
-      name: 'customStorageId',
-      type: 'text',
-      admin: {
-        description: 'Custom storage identifier from adapter',
-        readOnly: true,
-      },
-    },
-    {
-      name: 'uploadTimestamp',
-      type: 'text',
-      admin: {
-        description: 'Upload timestamp from adapter',
-        readOnly: true,
-      },
-    },
-    {
-      name: 'storageProvider',
-      type: 'text',
-      admin: {
-        description: 'Storage provider name from adapter',
-        readOnly: true,
-      },
-    },
-    {
-      name: 'bucketName',
-      type: 'text',
-      admin: {
-        description: 'Storage bucket name from adapter',
-        readOnly: true,
-      },
-    },
-    {
-      name: 'objectKey',
-      type: 'text',
-      admin: {
-        description: 'Storage object key from adapter',
-        readOnly: true,
-      },
-    },
-    {
-      name: 'processingStatus',
-      type: 'text',
-      admin: {
-        description: 'Processing status from adapter',
-        readOnly: true,
-      },
-    },
-    {
-      name: 'uploadVersion',
-      type: 'text',
-      admin: {
-        description: 'Upload version from adapter',
-        readOnly: true,
-      },
-    },
   ],
   upload: {
     adminThumbnail: 'thumbnail',
