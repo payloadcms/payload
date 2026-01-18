644dd5faefffd925dce8386f8711de256f686037
Emad Fussi <43019044+omd0@users.noreply.github.com>
2026-01-18 10:25:15 +0300

    chore: commit built dist files for GitHub installation
    - Add built dist folders for all packages
    - Update package.json exports to use dist files
    - Enable direct GitHub installation without local builds
    - Compatible with Next.js 16 and React 19

---
+++ packages/next/dist/esbuildEntry.js
@@ -0,0 +1,5 @@
+export { RootLayout } from './layouts/Root/index.js';
+export { DashboardView } from './views/Dashboard/index.js';
+export { LoginView } from './views/Login/index.js';
+export { RootPage } from './views/Root/index.js';
+//# sourceMappingURL=esbuildEntry.js.map
