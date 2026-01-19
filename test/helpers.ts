de03674f3ae037b1d0e0de7d784896b740cde6b7
Alessio Gravili <github@gravili.net>
2026-01-19 10:30:18 -0800

    fix more flakiness

--- test/helpers.ts
+++ test/helpers.ts
@@ -177,6 +177,7 @@ export async function saveDocHotkeyAndAssert(page: Page): Promise<void> {
     await page.keyboard.up('Control')
   }
   await expect(page.locator('.payload-toast-container')).toContainText('successfully')
+  await closeAllToasts(page)
 }
 
 export async function saveDocAndAssert(
@@ -212,7 +213,7 @@ export async function saveDocAndAssert(
   await closeAllToasts(page)
 }
 
-export async function closeAllToasts(page: Page): Promise<void> {
+export async function closeAllToasts(page: Locator | Page): Promise<void> {
   const toastCloseSelector = '.payload-toast-container button.payload-toast-close-button'
   let count = await page.locator(toastCloseSelector).count()
 
