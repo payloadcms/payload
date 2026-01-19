3c8d5901031e3c1953e6bd4dc3b13ce14418a82c
Alessio Gravili <github@gravili.net>
2026-01-19 12:19:39 -0800

    templates

--- templates/ecommerce/next.config.js
+++ templates/ecommerce/next.config.js
@@ -12,6 +12,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://loc
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    qualities: [90, 100],
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
--- templates/website/next.config.js
+++ templates/website/next.config.js
@@ -14,6 +14,7 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    qualities: [100],
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
