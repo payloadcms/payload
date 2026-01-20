7e98c4417ee6a6bffb47f2d3d0d48a265fffa414
Alessio Gravili <github@gravili.net>
2026-01-20 11:01:41 -0800

    Revert "reset templates"
    This reverts commit 8f1689448f9ea5c7e15af3badde6fa6a3941f231.

--- templates/with-vercel-website/src/collections/Pages/hooks/revalidatePage.ts
+++ templates/with-vercel-website/src/collections/Pages/hooks/revalidatePage.ts
@@ -16,7 +16,7 @@ export const revalidatePage: CollectionAfterChangeHook<Page> = ({
       payload.logger.info(`Revalidating page at path: ${path}`)
 
       revalidatePath(path)
-      revalidateTag('pages-sitemap')
+      revalidateTag('pages-sitemap', 'max')
     }
 
     // If the page was previously published, we need to revalidate the old path
@@ -26,7 +26,7 @@ export const revalidatePage: CollectionAfterChangeHook<Page> = ({
       payload.logger.info(`Revalidating old page at path: ${oldPath}`)
 
       revalidatePath(oldPath)
-      revalidateTag('pages-sitemap')
+      revalidateTag('pages-sitemap', 'max')
     }
   }
   return doc
@@ -36,7 +36,7 @@ export const revalidateDelete: CollectionAfterDeleteHook<Page> = ({ doc, req: { 
   if (!context.disableRevalidate) {
     const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
     revalidatePath(path)
-    revalidateTag('pages-sitemap')
+    revalidateTag('pages-sitemap', 'max')
   }
 
   return doc
