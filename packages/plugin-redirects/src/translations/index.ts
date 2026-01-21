3b1c560e84a83f2930b7a5a7fb4398671f5465e8
José Eduardo Dias <josedu.dias@gmail.com>
2026-01-21 07:03:06 -0300

    Reorder translations in alphabetical order

--- packages/plugin-redirects/src/translations/index.ts
+++ packages/plugin-redirects/src/translations/index.ts
@@ -4,16 +4,16 @@ import { en } from './languages/en.js'
 import { es } from './languages/es.js'
 import { fr } from './languages/fr.js'
 import { ja } from './languages/ja.js'
-import { sv } from './languages/sv.js'
 import { pt } from './languages/pt.js'
+import { sv } from './languages/sv.js'
 
 export const translations = {
   en,
   es,
   fr,
   ja,
-  sv,
-  pt
+  pt,
+  sv
 }
 
 export type RedirectsTranslations = GenericTranslationsObject
