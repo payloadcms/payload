e14c7a003a5e4f68308d397ec1df53d252e6daeb
Alessio Gravili <github@gravili.net>
2026-01-26 20:13:11 -0800

    further reduce timeout, introduce navigationTimeout

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -12,7 +12,7 @@ let multiplier = process.env.CI ? 4 : 1
 let smallMultiplier = process.env.CI ? 3 : 1
 
 export const TEST_TIMEOUT_LONG = 320000 * multiplier // 4*8 minutes - used as timeOut for the beforeAll
-export const TEST_TIMEOUT = 25000 * smallMultiplier
+export const TEST_TIMEOUT = 20000 * smallMultiplier
 export const EXPECT_TIMEOUT = 6000 * smallMultiplier
 export const POLL_TOPASS_TIMEOUT = EXPECT_TIMEOUT * 4 // That way expect.poll() or expect().toPass can retry 4 times. 4x higher than default expect timeout => can retry 4 times if retryable expects are used inside
 
@@ -25,6 +25,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
+    navigationTimeout: TEST_TIMEOUT/2
   },
   expect: {
     timeout: EXPECT_TIMEOUT,
