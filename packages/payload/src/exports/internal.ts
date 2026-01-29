a29f2a0ecc9dd59bdfe4527504e83b6b9e78fd6b
Paul Popus <paul@payloadcms.com>
2026-01-29 17:37:53 -0600

    export getExternalFile

--- packages/payload/src/exports/internal.ts
+++ packages/payload/src/exports/internal.ts
@@ -2,6 +2,7 @@
  * Modules exported here are not part of the public API and are subject to change without notice and without a major version bump.
  */
 
+export { getExternalFile } from '../uploads/getExternalFile.js'
 export { getRangeRequestInfo } from '../uploads/getRangeRequestInfo.js'
 export { parseRangeHeader } from '../uploads/parseRangeHeader.js'
 export { getEntityPermissions } from '../utilities/getEntityPermissions/getEntityPermissions.js'
