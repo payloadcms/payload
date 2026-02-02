e79931c5f7b8fc40a391f41ba66b4ddb46bde0db
Jake <jacobsfletch@gmail.com>
2026-02-02 09:58:45 -0500

    add additional admin e2e to templates

--- templates/website/tests/e2e/frontend.e2e.spec.ts
+++ templates/website/tests/e2e/frontend.e2e.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, Page } from '@playwright/test'
+import { describe } from 'node:test'
 
 test.describe('Frontend', () => {
   let page: Page
@@ -10,11 +11,25 @@ test.describe('Frontend', () => {
 
   test('can go on homepage', async ({ page }) => {
     await page.goto('http://localhost:3000')
-
     await expect(page).toHaveTitle(/Payload Website Template/)
-
     const heading = page.locator('h1').first()
-
     await expect(heading).toHaveText('Payload Website Template')
+  })
+
+  describe('Admin Panel', () => {
+    test('can navigate to dashboard', async ({ page }) => {
+      await page.goto('http://localhost:3000/admin')
+    })
+
+    test('can navigate to list view', async ({ page }) => {
+      await page.goto('http://localhost:3000/admin/collections/pages')
+    })
+
+    test('can navigate to edit view', async ({ page }) => {
+      await page.goto('http://localhost:3000/admin/collections/pages/create')
+      const titleField = page.locator('input[name="title"]')
+      await titleField.fill('Test Page')
+      expect(await titleField.inputValue()).toBe('Test Page')
+    })
   })
 })
