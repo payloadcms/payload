9eade24bf7a6c0190e4af47b759bdc092cf18749
Jake <jacobsfletch@gmail.com>
2026-02-02 13:04:37 -0500

    improve tests

--- templates/website/tests/e2e/frontend.e2e.spec.ts
+++ templates/website/tests/e2e/frontend.e2e.spec.ts
@@ -19,17 +19,23 @@ test.describe('Frontend', () => {
   describe('Admin Panel', () => {
     test('can navigate to dashboard', async ({ page }) => {
       await page.goto('http://localhost:3000/admin')
+      await expect(page).toHaveURL('http://localhost:3000/admin')
+      const dashboardArtifact = page.locator('span[title="Dashboard"]').first()
+      await expect(dashboardArtifact).toBeVisible()
     })
 
     test('can navigate to list view', async ({ page }) => {
       await page.goto('http://localhost:3000/admin/collections/pages')
+      await expect(page).toHaveURL('http://localhost:3000/admin/collections/pages')
+      const listViewArtifact = page.locator('h1', { hasText: 'Pages' }).first()
+      await expect(listViewArtifact).toBeVisible()
     })
 
     test('can navigate to edit view', async ({ page }) => {
       await page.goto('http://localhost:3000/admin/collections/pages/create')
-      const titleField = page.locator('input[name="title"]')
-      await titleField.fill('Test Page')
-      expect(await titleField.inputValue()).toBe('Test Page')
+      await expect(page).toHaveURL(/\/admin\/collections\/pages\/[a-zA-Z0-9-_]+/)
+      const editViewArtifact = page.locator('input[name="title"]')
+      await expect(editViewArtifact).toBeVisible()
     })
   })
 })
