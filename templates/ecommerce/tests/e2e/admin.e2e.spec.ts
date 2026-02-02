a2720a4fc5e64617e3c2e4fc395a8fb3cc920617
Jake <jacobsfletch@gmail.com>
2026-02-02 15:01:35 -0500

    passing tests and updates other templates

--- templates/ecommerce/tests/e2e/admin.e2e.spec.ts
+++ templates/ecommerce/tests/e2e/admin.e2e.spec.ts
@@ -1,28 +1,38 @@
 import { test, expect, Page } from '@playwright/test'
+import { login } from '../helpers/login'
+import { seedTestUser, cleanupTestUser, testUser } from '../helpers/seedUser'
 
 test.describe('Admin Panel', () => {
   let page: Page
 
-  test.beforeAll(async ({ browser }, testInfo) => {
+  test.beforeAll(async ({ browser }) => {
+    await seedTestUser()
+
     const context = await browser.newContext()
-    page = await context.newPage() // TODO: authenticate before tests
+    page = await context.newPage()
+
+    await login({ page, user: testUser })
   })
 
-  test('can navigate to dashboard', async ({ page }) => {
+  test.afterAll(async () => {
+    await cleanupTestUser()
+  })
+
+  test('can navigate to dashboard', async () => {
     await page.goto('http://localhost:3000/admin')
     await expect(page).toHaveURL('http://localhost:3000/admin')
     const dashboardArtifact = page.locator('span[title="Dashboard"]').first()
     await expect(dashboardArtifact).toBeVisible()
   })
 
-  test('can navigate to list view', async ({ page }) => {
+  test('can navigate to list view', async () => {
     await page.goto('http://localhost:3000/admin/collections/pages')
     await expect(page).toHaveURL('http://localhost:3000/admin/collections/pages')
     const listViewArtifact = page.locator('h1', { hasText: 'Pages' }).first()
     await expect(listViewArtifact).toBeVisible()
   })
 
-  test('can navigate to edit view', async ({ page }) => {
+  test('can navigate to edit view', async () => {
     await page.goto('http://localhost:3000/admin/collections/pages/create')
     await expect(page).toHaveURL(/\/admin\/collections\/pages\/[a-zA-Z0-9-_]+/)
     const editViewArtifact = page.locator('input[name="title"]')
--- templates/website/tests/e2e/admin.e2e.spec.ts
+++ templates/website/tests/e2e/admin.e2e.spec.ts
@@ -18,21 +18,21 @@ test.describe('Admin Panel', () => {
     await cleanupTestUser()
   })
 
-  test('can navigate to dashboard', async ({ page }) => {
+  test('can navigate to dashboard', async () => {
     await page.goto('http://localhost:3000/admin')
     await expect(page).toHaveURL('http://localhost:3000/admin')
     const dashboardArtifact = page.locator('span[title="Dashboard"]').first()
     await expect(dashboardArtifact).toBeVisible()
   })
 
-  test('can navigate to list view', async ({ page }) => {
+  test('can navigate to list view', async () => {
     await page.goto('http://localhost:3000/admin/collections/pages')
     await expect(page).toHaveURL('http://localhost:3000/admin/collections/pages')
     const listViewArtifact = page.locator('h1', { hasText: 'Pages' }).first()
     await expect(listViewArtifact).toBeVisible()
   })
 
-  test('can navigate to edit view', async ({ page }) => {
+  test('can navigate to edit view', async () => {
     await page.goto('http://localhost:3000/admin/collections/pages/create')
     await expect(page).toHaveURL(/\/admin\/collections\/pages\/[a-zA-Z0-9-_]+/)
     const editViewArtifact = page.locator('input[name="title"]')
