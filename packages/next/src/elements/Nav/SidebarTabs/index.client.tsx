e2f3f4ad9009cc89717eb9a46cb8b9edfd245b9b
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-02-02 22:09:31 -0500

    show loading spinner text

--- packages/next/src/elements/Nav/SidebarTabs/index.client.tsx
+++ packages/next/src/elements/Nav/SidebarTabs/index.client.tsx
@@ -141,7 +141,7 @@ export const SidebarTabsClient: React.FC<SidebarTabsClientProps> = ({
         })}
       </div>
       <div className={`${baseClass}__content`}>
-        {loadingTab === activeTabID ? <Spinner loadingText={null} /> : activeContent}
+        {loadingTab === activeTabID ? <Spinner /> : activeContent}
       </div>
     </div>
   )
