b9012318dc3000a563ad8615913f0dc6b3fb7d88
Alessio Gravili <github@gravili.net>
2026-02-03 09:22:39 -0800

    test: reorganize helpers into shared/e2e/int subdirectories (#15479)
    Clean up `test/helpers/` folder structure in preparation for upcoming
    fixtures support.
    
    Previously, all helper files were in a flat structure making it hard to
    distinguish between shared utilities and <e2e/int>-specific helpers.
    Some were part of the `test/helpers/e2e` folder, some were just floating
    at the top-level of the `test/helpers` _folder_, some were stuffed into
    `test/helpers.ts` _file_.
    
    New structure:
    
    - `helpers/shared/` - Common utilities (adminUrlUtil, initPayloadInt,
    REST clients, etc.)
    - `helpers/e2e/` - Playwright/e2e test utilities
    - `helpers/int/` - Integration test utilities
    
    This PR also removes some previously unused files. We still had mock
    files sitting around from our 2.x webpack bundler magic days.

--- test/fields/collections/Blocks/e2e.spec.ts
+++ test/fields/collections/Blocks/e2e.spec.ts
@@ -1,16 +1,16 @@
 import type { BrowserContext, Page } from '@playwright/test'
 
 import { expect, test } from '@playwright/test'
-import { copyPasteField } from 'helpers/e2e/copyPasteField.js'
+import { copyPasteField } from '__helpers/e2e/copyPasteField.js'
 import {
   addBlock,
   addBlockBelow,
   duplicateBlock,
   openBlocksDrawer,
   reorderBlocks,
-} from 'helpers/e2e/fields/blocks/index.js'
-import { scrollEntirePage } from 'helpers/e2e/scrollEntirePage.js'
-import { toggleBlockOrArrayRow } from 'helpers/e2e/toggleCollapsible.js'
+} from '__helpers/e2e/fields/blocks/index.js'
+import { scrollEntirePage } from '__helpers/e2e/scrollEntirePage.js'
+import { toggleBlockOrArrayRow } from '__helpers/e2e/toggleCollapsible.js'
 import path from 'path'
 import { wait } from 'payload/shared'
 import { fileURLToPath } from 'url'
@@ -20,13 +20,13 @@ import {
   initPageConsoleErrorCatch,
   saveDocAndAssert,
   // throttleTest,
-} from '../../../helpers.js'
-import { AdminUrlUtil } from '../../../helpers/adminUrlUtil.js'
-import { assertToastErrors } from '../../../helpers/assertToastErrors.js'
-import { assertNetworkRequests } from '../../../helpers/e2e/assertNetworkRequests.js'
-import { initPayloadE2ENoConfig } from '../../../helpers/initPayloadE2ENoConfig.js'
-import { reInitializeDB } from '../../../helpers/reInitializeDB.js'
-import { RESTClient } from '../../../helpers/rest.js'
+} from '../../../__helpers/e2e/helpers.js'
+import { AdminUrlUtil } from '../../../__helpers/shared/adminUrlUtil.js'
+import { assertToastErrors } from '../../../__helpers/shared/assertToastErrors.js'
+import { assertNetworkRequests } from '../../../__helpers/e2e/assertNetworkRequests.js'
+import { initPayloadE2ENoConfig } from '../../../__helpers/shared/initPayloadE2ENoConfig.js'
+import { reInitializeDB } from '../../../__helpers/shared/clearAndSeed/reInitializeDB.js'
+import { RESTClient } from '../../../__helpers/shared/rest.js'
 import { POLL_TOPASS_TIMEOUT, TEST_TIMEOUT_LONG } from '../../../playwright.config.js'
 
 const filename = fileURLToPath(import.meta.url)
