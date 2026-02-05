8ea162c5c0350e2e9b73c63deb3998634b1359cd
Alessio Gravili <github@gravili.net>
2026-02-05 10:32:47 -0800

    templates: bump to 3.75.0 (#15533)

--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260126_225015_initial from './20260126_225015_initial'
+import * as migration_20260205_181745_initial from './20260205_181745_initial'
 
 export const migrations = [
   {
-    up: migration_20260126_225015_initial.up,
-    down: migration_20260126_225015_initial.down,
-    name: '20260126_225015_initial',
+    up: migration_20260205_181745_initial.up,
+    down: migration_20260205_181745_initial.down,
+    name: '20260205_181745_initial',
   },
 ]
