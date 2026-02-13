2922c1a7cead90f55949743fa7721406b4a76e41
Alessio Gravili <github@gravili.net>
2026-02-13 09:28:47 -0800

    templates

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260205_211221_initial from './20260205_211221_initial'
+import * as migration_20260213_172605_initial from './20260213_172605_initial'
 
 export const migrations = [
   {
-    up: migration_20260205_211221_initial.up,
-    down: migration_20260205_211221_initial.down,
-    name: '20260205_211221_initial',
+    up: migration_20260213_172605_initial.up,
+    down: migration_20260213_172605_initial.down,
+    name: '20260213_172605_initial',
   },
 ]
