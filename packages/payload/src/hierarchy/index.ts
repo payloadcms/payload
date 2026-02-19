03b77678eb546e873cec148fee472c1f62715a70
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-02-19 16:00:07 -0500

    chore: export hierarchy helpers and constants

--- packages/payload/src/hierarchy/index.ts
+++ packages/payload/src/hierarchy/index.ts
@@ -1,3 +1,20 @@
+export {
+  DEFAULT_ALLOW_HAS_MANY,
+  DEFAULT_HIERARCHY_TREE_LIMIT,
+  getHierarchyFieldName,
+  HIERARCHY_DEFAULT_LOCALE,
+  HIERARCHY_PARENT_FIELD,
+  HIERARCHY_SLUG_PATH_FIELD,
+  HIERARCHY_TITLE_PATH_FIELD,
+} from './constants.js'
+export { createFolderCollection } from './createFolderCollection.js'
+export type { CreateFolderCollectionOptions } from './createFolderCollection.js'
+export { createFolderField } from './createFolderField.js'
+export type { CreateFolderFieldOptions } from './createFolderField.js'
+export { createTagCollection } from './createTagCollection.js'
+export type { CreateTagCollectionOptions } from './createTagCollection.js'
+export { createTagField } from './createTagField.js'
+export type { CreateTagFieldOptions } from './createTagField.js'
 export type {
   HierarchyConfig,
   SanitizedHierarchyConfig,
@@ -8,3 +25,4 @@ export type { Ancestor } from './utils/getAncestors.js'
 export { getAncestors } from './utils/getAncestors.js'
 export { getLocalizedValue } from './utils/getLocalizedValue.js'
 export { buildNestedTree, loadTree } from './utils/loadTree.js'
+export { validateHierarchyFields } from './validateHierarchyFields.js'
