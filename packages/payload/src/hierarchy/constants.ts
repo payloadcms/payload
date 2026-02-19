22056fa9590f0be53111fdef5646cfd06547831c
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-02-19 15:41:38 -0500

    chore: add hierarchy default constants

--- packages/payload/src/hierarchy/constants.ts
+++ packages/payload/src/hierarchy/constants.ts
@@ -6,3 +6,12 @@ export const HIERARCHY_SLUG_PATH_FIELD = '_h_slugPath'
 export const HIERARCHY_TITLE_PATH_FIELD = '_h_titlePath'
 export const HIERARCHY_PARENT_FIELD = '_h_parent'
 export const HIERARCHY_DEFAULT_LOCALE = 'en'
+
+/** Default limit for hierarchy tree queries */
+export const DEFAULT_HIERARCHY_TREE_LIMIT = 100
+
+/** Default value for allowing hasMany on hierarchy fields */
+export const DEFAULT_ALLOW_HAS_MANY = true
+
+/** Generate a hierarchy field name from a hierarchy slug (e.g., 'folders' -> '_h_folders') */
+export const getHierarchyFieldName = (hierarchySlug: string): string => `_h_${hierarchySlug}`
