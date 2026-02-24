c9e9a97bf9bc611cb2b90773b4b187996d9599f1
Kendell Joseph <kjoseph@figma.com>
2026-02-24 16:25:52 -0500

    chore: splits evals into smaller evals

---
+++ test/evals/eval.conventions.spec.ts
@@ -0,0 +1,11 @@
+import { beforeAll } from 'vitest'
+
+import { registerConventionsSuite } from './suites/index.js'
+
+beforeAll(() => {
+  if (!process.env.OPENAI_API_KEY) {
+    throw new Error('OPENAI_API_KEY must be set to run eval tests')
+  }
+})
+
+registerConventionsSuite()
