1b4ed409dd0a733df8557bdc566314357d593601
VeiaG <romapalamar.veiag@gmail.com>
2026-02-26 13:38:00 +0200

    test: remove images from content block test config to fix e2e

--- test/fields/collections/Blocks/index.ts
+++ test/fields/collections/Blocks/index.ts
@@ -17,10 +17,6 @@ export const getBlocksField = (prefix?: string): BlocksField => ({
         components: {
           Label: './collections/Blocks/components/CustomBlockLabel.tsx',
         },
-        images: {
-          icon: { url: '/api/uploads/file/payload20x20.png', alt: 'Block icon' },
-          thumbnail: { url: '/api/uploads/file/payload480x320.jpg', alt: 'Block thumbnail' },
-        },
       },
       fields: [
         {
