9639909338436bcad28d0ba496f8e8fc1bb4a6a4
Elliot DeNolf <denolfe@gmail.com>
2026-03-24 13:09:08 -0400

    chore: ignore v2-v3 migration file from eslint flat config

--- packages/drizzle/.eslintrc.cjs
+++ packages/drizzle/.eslintrc.cjs
@@ -1,6 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  ignorePatterns: ['src/postgres/predefinedMigrations/v2-v3/index.ts'],
   parserOptions: {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
