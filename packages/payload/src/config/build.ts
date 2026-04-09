563c101aae04bfc38278b98dff3bb72e412f121d
Alessio Gravili <github@gravili.net>
2026-04-09 14:52:23 -0700

    remove js comments in favor of jsdocs, simplify

--- packages/payload/src/config/build.ts
+++ packages/payload/src/config/build.ts
@@ -9,19 +9,14 @@ import { sanitizeConfig } from './sanitize.js'
  */
 export async function buildConfig(config: Config): Promise<SanitizedConfig> {
   if (Array.isArray(config.plugins)) {
-    let configAfterPlugins = config
     for (const plugin of config.plugins) {
-      configAfterPlugins = await plugin(configAfterPlugins)
+      config = await plugin(config)
     }
-    config = configAfterPlugins
   }
 
-  // Pass 2: run afterPlugins callbacks for cross-plugin discovery.
-  // Plugins can push callbacks to config.afterPlugins during pass 1 to defer
-  // work that depends on the fully-assembled config from all plugins.
   if (Array.isArray(config.afterPlugins)) {
-    for (const callback of config.afterPlugins) {
-      config = await callback(config)
+    for (const afterPlugin of config.afterPlugins) {
+      config = await afterPlugin(config)
     }
   }
 
