69a7dde364b60a986c0543354fccaccd23661c38
Patrik Kozak <35232443+PatrikKozak@users.noreply.github.com>
2026-04-27 18:25:17 -0400

    fix(ui): fix stale closure in EditUpload fineTuneCrop causing crop to not apply correctly in CI

--- packages/ui/src/elements/EditUpload/index.tsx
+++ packages/ui/src/elements/EditUpload/index.tsx
@@ -17,7 +17,6 @@ import './index.scss'
 const baseClass = 'edit-upload'
 
 type Props = {
-  disabled?: boolean
   name: string
   onChange: (value: string) => void
   ref?: React.RefObject<HTMLInputElement>
@@ -25,13 +24,12 @@ type Props = {
 }
 
 const Input: React.FC<Props> = (props) => {
-  const { name, disabled, onChange, ref, value } = props
+  const { name, onChange, ref, value } = props
 
   return (
     <div className={`${baseClass}__input`}>
       {name}
       <input
-        disabled={disabled}
         name={name}
         onChange={(e) => onChange(e.target.value)}
         ref={ref}
@@ -124,10 +122,10 @@ export const EditUpload: React.FC<EditUploadProps> = ({
       return null
     }
 
-    setCrop({
-      ...crop,
+    setCrop((prev) => ({
+      ...prev,
       [dimension]: percentage,
-    })
+    }))
   }
 
   const fineTuneFocalPosition = ({
@@ -278,14 +276,12 @@ export const EditUpload: React.FC<EditUploadProps> = ({
                 </span>
                 <div className={`${baseClass}__inputsWrap`}>
                   <Input
-                    disabled={!imageLoaded}
                     name={`${t('upload:width')} (px)`}
                     onChange={(value) => fineTuneCrop({ dimension: 'width', value })}
                     ref={widthInputRef}
                     value={((crop.width / 100) * uncroppedPixelWidth).toFixed(0)}
                   />
                   <Input
-                    disabled={!imageLoaded}
                     name={`${t('upload:height')} (px)`}
                     onChange={(value) => fineTuneCrop({ dimension: 'height', value })}
                     ref={heightInputRef}
