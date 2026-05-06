029577999b582be3e7434e263551dc0716f8e162
Elliot DeNolf <denolfe@gmail.com>
2026-05-06 13:28:19 -0400

    feat(ui): use Modal instead of Drawer for PayloadVersionMenuItem

--- packages/ui/src/elements/PayloadVersionMenuItem/PayloadVersionModalTrigger.tsx
+++ packages/ui/src/elements/PayloadVersionMenuItem/PayloadVersionModalTrigger.tsx
@@ -3,34 +3,47 @@
 import React from 'react'
 
 // eslint-disable-next-line payload/no-imports-from-exports-dir -- Client component imports from same package's client bundle
-import { Drawer, PopupList, useModal } from '../../exports/client/index.js'
+import { Modal, PopupList, useModal } from '../../exports/client/index.js'
 import { CopyButton } from './CopyButton.js'
 import { formatForClipboard } from './formatForClipboard.js'
 import { VersionList } from './VersionList.js'
 import './index.scss'
 
-const drawerSlug = 'payload-version-info'
+const baseClass = 'payload-version-menu-item'
+const modalSlug = 'payload-version-info'
 
 export const PayloadVersionModalTrigger: React.FC<{
   versions: Record<string, string>
 }> = ({ versions }) => {
-  const { openModal } = useModal()
+  const { closeModal, isModalOpen, openModal } = useModal()
   const payloadVersion = versions.payload ?? '0.0.0'
 
   return (
     <PopupList.ButtonGroup>
-      <PopupList.Button
-        className="payload-version-menu-item__button"
-        onClick={() => openModal(drawerSlug)}
-      >
+      <PopupList.Button className={`${baseClass}__button`} onClick={() => openModal(modalSlug)}>
         {`Payload v${payloadVersion}`}
       </PopupList.Button>
-      <Drawer slug={drawerSlug} title="Payload Version Info">
-        <div className="payload-version-menu-item__content">
-          <VersionList versions={versions} />
-          <CopyButton text={formatForClipboard(versions)} />
-        </div>
-      </Drawer>
+      {isModalOpen(modalSlug) && (
+        <Modal className={baseClass} closeOnBlur={false} slug={modalSlug}>
+          <div className={`${baseClass}__wrapper`}>
+            <div className={`${baseClass}__header`}>
+              <h2>Payload Version Info</h2>
+              <button
+                aria-label="Close"
+                className={`${baseClass}__close`}
+                onClick={() => closeModal(modalSlug)}
+                type="button"
+              >
+                ×
+              </button>
+            </div>
+            <VersionList versions={versions} />
+            <div className={`${baseClass}__controls`}>
+              <CopyButton text={formatForClipboard(versions)} />
+            </div>
+          </div>
+        </Modal>
+      )}
     </PopupList.ButtonGroup>
   )
 }
