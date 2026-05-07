934a3bf31f8fe3af0b1df657be67b864f018ecd8
nehaaprasaad <neh960112rxs@gmail.com>
2026-05-07 07:50:38 +0530

    fix(plugin-ecommerce): sync inventory  to latest version with drafts

--- packages/db-mongodb/src/updateVersion.ts
+++ packages/db-mongodb/src/updateVersion.ts
@@ -1,4 +1,4 @@
-import type { MongooseUpdateQueryOptions } from 'mongoose'
+import type { MongooseUpdateQueryOptions, UpdateQuery } from 'mongoose'
 
 import { buildVersionCollectionFields, type UpdateVersion } from 'payload'
 
@@ -8,6 +8,7 @@ import { buildQuery } from './queries/buildQuery.js'
 import { buildProjectionFromSelect } from './utilities/buildProjectionFromSelect.js'
 import { getCollection } from './utilities/getEntity.js'
 import { getSession } from './utilities/getSession.js'
+import { handleError } from './utilities/handleError.js'
 import { transform } from './utilities/transform.js'
 
 export const updateVersion: UpdateVersion = async function updateVersion(
@@ -42,7 +43,42 @@ export const updateVersion: UpdateVersion = async function updateVersion(
     where: whereToUse,
   })
 
-  transform({ adapter: this, data: versionData, fields, operation: 'write' })
+  let updateData: UpdateQuery<any> = versionData
+
+  const $inc: Record<string, number> = {}
+  const $push: Record<string, { $each: any[] } | any> = {}
+  const $addToSet: Record<string, { $each: any[] } | any> = {}
+  const $pull: Record<string, { $in: any[] } | any> = {}
+
+  transform({
+    $addToSet,
+    $inc,
+    $pull,
+    $push,
+    adapter: this,
+    data: versionData,
+    fields,
+    operation: 'write',
+  })
+
+  const updateOps: UpdateQuery<any> = {}
+
+  if (Object.keys($inc).length) {
+    updateOps.$inc = $inc
+  }
+  if (Object.keys($push).length) {
+    updateOps.$push = $push
+  }
+  if (Object.keys($addToSet).length) {
+    updateOps.$addToSet = $addToSet
+  }
+  if (Object.keys($pull).length) {
+    updateOps.$pull = $pull
+  }
+  if (Object.keys(updateOps).length) {
+    updateOps.$set = updateData
+    updateData = updateOps
+  }
 
   const options: MongooseUpdateQueryOptions = {
     ...optionsArgs,
@@ -58,18 +94,27 @@ export const updateVersion: UpdateVersion = async function updateVersion(
     timestamps: false,
   }
 
+  let result
+
+  try {
+    if (returning === false) {
+      await Model.updateOne(query, updateData, options)
+    } else {
+      result = await Model.findOneAndUpdate(query, updateData, options)
+    }
+  } catch (error) {
+    handleError({ collection: collectionSlug, error, req })
+  }
+
   if (returning === false) {
-    await Model.updateOne(query, versionData, options)
     return null
   }
 
-  const doc = await Model.findOneAndUpdate(query, versionData, options)
-
-  if (!doc) {
+  if (!result) {
     return null
   }
 
-  transform({ adapter: this, data: doc, fields, operation: 'read' })
+  transform({ adapter: this, data: result, fields, operation: 'read' })
 
-  return doc
+  return result
 }
