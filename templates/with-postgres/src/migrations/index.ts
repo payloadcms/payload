9b3606379edae871744c799966a738ce0f175732
Alessio Gravili <github@gravili.net>
2026-05-07 15:33:31 -0700

    templates: bumps next.js to 16.2.6 (#16538)
    This bumps the Next.js version in our templates and monorepo to 16.2.6.
    It also bumps the minimum supported Next.js 16 version to 16.2.6, to
    discourage people from using a Next.js version with known CVEs.

--- templates/with-postgres/src/migrations/index.ts
+++ templates/with-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260423_135446_initial from './20260423_135446_initial'
+import * as migration_20260507_221522_initial from './20260507_221522_initial'
 
 export const migrations = [
   {
-    up: migration_20260423_135446_initial.up,
-    down: migration_20260423_135446_initial.down,
-    name: '20260423_135446_initial',
+    up: migration_20260507_221522_initial.up,
+    down: migration_20260507_221522_initial.down,
+    name: '20260507_221522_initial',
   },
 ]
--- templates/with-vercel-website/src/app/(payload)/admin/importMap.js
+++ templates/with-vercel-website/src/app/(payload)/admin/importMap.js
@@ -26,6 +26,7 @@ import { RowLabel as RowLabel_ec255a65fa6fa8d1faeb09cf35284224 } from '@/Header/
 import { RowLabel as RowLabel_1f6ff6ff633e3695d348f4f3c58f1466 } from '@/Footer/RowLabel'
 import { default as default_1a7510af427896d367a49dbf838d2de6 } from '@/components/BeforeDashboard'
 import { default as default_8a7ab0eb7ab5c511aba12e68480bfe5e } from '@/components/BeforeLogin'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 /** @type import('payload').ImportMap */
@@ -76,5 +77,7 @@ export const importMap = {
   '@/Footer/RowLabel#RowLabel': RowLabel_1f6ff6ff633e3695d348f4f3c58f1466,
   '@/components/BeforeDashboard#default': default_1a7510af427896d367a49dbf838d2de6,
   '@/components/BeforeLogin#default': default_8a7ab0eb7ab5c511aba12e68480bfe5e,
+  '@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler':
+    VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e,
   '@payloadcms/next/rsc#CollectionCards': CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
 }
