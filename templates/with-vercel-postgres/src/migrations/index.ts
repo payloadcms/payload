79c9c519e77fce0a67dbe966a1b283351cdf11c5
Alessio Gravili <github@gravili.net>
2026-05-07 15:17:27 -0700

    templates

--- templates/with-vercel-postgres/src/migrations/index.ts
+++ templates/with-vercel-postgres/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260423_135419_initial from './20260423_135419_initial'
+import * as migration_20260507_221425_initial from './20260507_221425_initial'
 
 export const migrations = [
   {
-    up: migration_20260423_135419_initial.up,
-    down: migration_20260423_135419_initial.down,
-    name: '20260423_135419_initial',
+    up: migration_20260507_221425_initial.up,
+    down: migration_20260507_221425_initial.down,
+    name: '20260507_221425_initial',
   },
 ]
--- templates/with-vercel-website/src/migrations/index.ts
+++ templates/with-vercel-website/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260423_135435_initial from './20260423_135435_initial'
+import * as migration_20260507_221443_initial from './20260507_221443_initial'
 
 export const migrations = [
   {
-    up: migration_20260423_135435_initial.up,
-    down: migration_20260423_135435_initial.down,
-    name: '20260423_135435_initial',
+    up: migration_20260507_221443_initial.up,
+    down: migration_20260507_221443_initial.down,
+    name: '20260507_221443_initial',
   },
 ]
