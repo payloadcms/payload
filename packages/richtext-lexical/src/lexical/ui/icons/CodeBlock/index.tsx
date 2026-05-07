24cea9007b02338fd4451d58da6730264fd6483b
Jessica Rynkar <67977755+JessRynkar@users.noreply.github.com>
2026-05-07 22:13:57 +0100

    feat(richtext-lexical): update lexical icons to v4 design (#16533)
    Update all icons within the `lexical` package to match v4 designs.
    
    Remaining (awaiting updated design):
    - H3 through H6
    - Blockquote
    
    ---------
    
    Co-authored-by: Patrik Kozak <35232443+PatrikKozak@users.noreply.github.com>

--- packages/richtext-lexical/src/lexical/ui/icons/CodeBlock/index.tsx
+++ packages/richtext-lexical/src/lexical/ui/icons/CodeBlock/index.tsx
@@ -1,12 +1,25 @@
 'use client'
 import React from 'react'
 
-export const CodeBlockIcon: React.FC = () => (
-  <svg fill="none" height="12" viewBox="0 0 15 12" width="15" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M11.3738 8.62598L14.0404 5.95931L11.3738 3.29264M3.37376 3.29264L0.707092 5.95931L3.37376 8.62598M9.04043 0.625977L5.70709 11.2926"
-      stroke="currentColor"
-      strokeLinecap="square"
-    />
+const paths = {
+  // icon-16-code-block
+  16: 'M6.646 6.146a.5.5 0 1 0 .708.708l1.5-1.5a.5.5 0 0 0 0-.708l-1.5-1.5a.5.5 0 1 0-.708.708L7.793 5zm-1.292-3a.5.5 0 0 1 0 .708L4.207 5l1.147 1.146a.5.5 0 1 1-.708.708l-1.5-1.5a.5.5 0 0 1 0-.708l1.5-1.5a.5.5 0 0 1 .708 0M10.5 3h1A1.5 1.5 0 0 1 13 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 11.5v-3a.5.5 0 0 1 1 0v3a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 1 0-1',
+  // icon-24-code-block
+  24: 'M7.354 4.146a.5.5 0 0 1 0 .708L5.207 7l2.147 2.146a.5.5 0 1 1-.708.708l-2.5-2.5a.5.5 0 0 1 0-.708l2.5-2.5a.5.5 0 0 1 .708 0M14 4.5a.5.5 0 0 1 .5-.5H18a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-6.5a.5.5 0 0 1 1 0V18a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1h-3.5a.5.5 0 0 1-.5-.5m-4.354.354a.5.5 0 1 1 .708-.708l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L11.793 7z',
+}
+
+export const CodeBlockIcon: React.FC<{
+  readonly className?: string
+  readonly size?: 16 | 24
+}> = ({ className, size = 24 }) => (
+  <svg
+    className={className}
+    fill="none"
+    height={size}
+    viewBox={`0 0 ${size} ${size}`}
+    width={size}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path clipRule="evenodd" d={paths[size]} fill="currentColor" fillRule="evenodd" />
   </svg>
 )
--- packages/richtext-lexical/src/lexical/ui/icons/Italic/index.tsx
+++ packages/richtext-lexical/src/lexical/ui/icons/Italic/index.tsx
@@ -1,20 +1,22 @@
 'use client'
 import React from 'react'
 
-export const ItalicIcon: React.FC = () => (
+const path =
+  'M10 6.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-2.083L11.6 17h1.9a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1h2.083L12.4 7h-1.9a.5.5 0 0 1-.5-.5'
+
+export const ItalicIcon: React.FC<{
+  readonly className?: string
+}> = ({ className }) => (
   <svg
     aria-hidden="true"
-    className="icon"
-    fill="currentColor"
+    className={['icon', className].filter(Boolean).join(' ')}
+    fill="none"
     focusable="false"
-    height="20"
-    viewBox="0 0 20 20"
-    width="20"
+    height={24}
+    viewBox="0 0 24 24"
+    width={24}
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M11.311 14.2969L11.0327 15H6.18408L6.4624 14.2969C7.54639 14.2969 7.70752 14.209 7.83936 13.8721L10.8423 6.45996C10.8716 6.38672 10.8862 6.32812 10.8862 6.26953C10.8862 6.09375 10.6519 6.03516 9.80225 6.03516L10.0952 5.33203H14.9438L14.6509 6.03516C13.5669 6.03516 13.4204 6.12305 13.2886 6.45996L10.2856 13.8721C10.2563 13.9453 10.2271 14.0039 10.2271 14.0625C10.2271 14.2383 10.4614 14.2969 11.311 14.2969Z"
-      fill="currentColor"
-    />
+    <path clipRule="evenodd" d={path} fill="currentColor" fillRule="evenodd" />
   </svg>
 )
