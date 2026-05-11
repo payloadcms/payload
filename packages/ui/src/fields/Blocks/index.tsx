e1faaceb5b181d6202b0b1fa40084572b9cd3b57
deepshekhardas <deepshekharadas1234@gmail.com>
2026-05-11 08:54:20 +0530

    fix: add Below action respects filterOptions in blocks field
    Issue: #15829

--- packages/ui/src/fields/Blocks/index.tsx
+++ packages/ui/src/fields/Blocks/index.tsx
@@ -475,6 +475,7 @@ const BlocksFieldComponent: BlocksFieldClientComponent = (props) => {
                       block={blockConfig}
                       // Pass all blocks, not just clientBlocksAfterFilter, as existing blocks should still be displayed even if they don't match the new filter
                       blocks={clientBlocks}
+                      blocksFiltered={clientBlocksAfterFilter}
                       copyRow={copyRow}
                       duplicateRow={duplicateRow}
                       errorCount={rowErrorCount}
