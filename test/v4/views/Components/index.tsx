28f0d6e683855441ef82ab0904b52709f7ffea60
Patrik Kozak <35232443+PatrikKozak@users.noreply.github.com>
2026-05-14 13:45:40 -0400

    feat(ui): updates RenderTitle styling to v4 redesign

--- test/v4/views/Components/index.tsx
+++ test/v4/views/Components/index.tsx
@@ -22,6 +22,7 @@ import { ModalSection } from './sections/ModalSection.js'
 import { NoListResultsSection } from './sections/NoListResults.js'
 import { PillSection } from './sections/Pill.js'
 import { PopupSection } from './sections/Popup.js'
+import { RenderTitleSection } from './sections/RenderTitle.js'
 import { SpinnerSection } from './sections/Spinner.js'
 import { StatusCellSection } from './sections/StatusCell.js'
 import { ThumbnailCardSection } from './sections/ThumbnailCard.js'
@@ -75,6 +76,7 @@ type ComponentId =
   | 'popup'
   | 'radio'
   | 'radiogroup-field'
+  | 'render-title'
   | 'select'
   | 'select-field'
   | 'shimmer'
@@ -109,6 +111,7 @@ const componentOptions: ComponentOption[] = [
   { category: 'primitives', label: 'Pill', value: 'pill' },
   { category: 'primitives', label: 'Popup', value: 'popup' },
   { category: 'primitives', label: 'Radio', value: 'radio' },
+  { category: 'primitives', label: 'Render Title', value: 'render-title' },
   { category: 'primitives', label: 'Select', value: 'select' },
   { category: 'primitives', label: 'Spinner', value: 'spinner' },
   { category: 'primitives', label: 'Textarea', value: 'textarea' },
@@ -238,6 +241,9 @@ export const ComponentsView: React.FC = () => {
         {shouldShow('pill', 'primitives') && <PillSection selectedComponent="pill" />}
         {shouldShow('icons', 'primitives') && <IconsSection selectedComponent="icons" />}
         {shouldShow('id-label', 'primitives') && <IDLabelSection selectedComponent="id-label" />}
+        {shouldShow('render-title', 'primitives') && (
+          <RenderTitleSection selectedComponent="render-title" />
+        )}
         {shouldShow('lexical-icons', 'primitives') && (
           <LexicalIconsSection selectedComponent="lexical-icons" />
         )}
