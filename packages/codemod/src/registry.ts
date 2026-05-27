fd83922b2e318fbac6eb2bcadf45b01b183eb160
Jake Fletcher <jacobsfletch@gmail.com>
2026-05-27 18:10:45 -0400

    docs(migration-guide): document removal of @payloadcms/next subpath exports + add codemod
    Adds a v4 migration guide entry covering the removal of
    @payloadcms/next/{client,rsc,templates} and a companion
    @payloadcms/codemod transform (migrate-next-subpath-exports) that
    rewrites both import declarations and string-literal component paths in
    Payload config to point at the canonical @payloadcms/ui entry points.

--- packages/codemod/src/registry.ts
+++ packages/codemod/src/registry.ts
@@ -10,6 +10,7 @@ import { migrateForceSelect } from './transforms/migrate-force-select/index.js'
 import { migrateHideAPIURL } from './transforms/migrate-hide-api-url/index.js'
 import { migrateImportExportHooks } from './transforms/migrate-import-export-hooks/index.js'
 import { migrateListViewSelectAPI } from './transforms/migrate-list-view-select-api/index.js'
+import { migrateNextSubpathExports } from './transforms/migrate-next-subpath-exports/index.js'
 import { migrateStorageAdaptersToConfig } from './transforms/migrate-storage-adapters-to-config/index.js'
 import { renameStorageAdaptersToStorage } from './transforms/rename-storage-adapters-to-storage/index.js'
 
@@ -26,4 +27,5 @@ export const transforms: Transform[] = [
   renameStorageAdaptersToStorage,
   migrateImportExportHooks,
   migrateDbTypesSubpath,
+  migrateNextSubpathExports,
 ]
