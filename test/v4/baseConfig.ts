5b151f387ead4aa29fb56c73ad5bcd57dae73b01
Jessica Rynkar <67977755+JessRynkar@users.noreply.github.com>
2026-06-01 21:18:45 +0100

    feat(ui): update account view to v4 design (#16808)
    Updates account view to match v4 design.
    
    Before:
    <img width="1237" height="340" alt="Screenshot 2026-06-01 at 14 39 10"
    src="https://github.com/user-attachments/assets/af783371-c255-4b7d-8f80-8a2cbc816536"
    />
    
    After:
    <img width="1252" height="308" alt="Screenshot 2026-06-01 at 14 37 55"
    src="https://github.com/user-attachments/assets/690fa134-2e39-465c-9415-303027f39fa3"
    />
    
    ---------
    
    Co-authored-by: Patrik Kozak <35232443+PatrikKozak@users.noreply.github.com>

--- test/v4/baseConfig.ts
+++ test/v4/baseConfig.ts
@@ -92,10 +92,12 @@ export const collections: CollectionConfig[] = [
     admin: {
       useAsTitle: 'email',
     },
-    auth: true,
+    auth: {
+      useAPIKey: true,
+    },
     access: {
       admin: ({ req: { user } }) => {
-        return Boolean(user?.roles?.includes('admin'))
+        return Boolean(user && 'roles' in user && user.roles?.includes('admin'))
       },
     },
     fields: [
