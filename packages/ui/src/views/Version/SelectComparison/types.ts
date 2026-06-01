6e0bc0788f7f6f797893abf1252a8cbe14537fa4
Jake Fletcher <jacobsfletch@gmail.com>
2026-06-01 14:44:13 -0400

    feat(ui): move VersionView + Default + Restore + SelectComparison + SelectLocales to ui package

---
+++ packages/ui/src/views/Version/SelectComparison/types.ts
@@ -0,0 +1,30 @@
+import type { PaginatedDocs, SanitizedCollectionConfig } from 'payload'
+
+import type { CompareOption } from '../Default/types.js'
+
+export type Props = {
+  collectionSlug?: string
+  docID?: number | string
+  globalSlug?: string
+  onChange: (val: CompareOption) => void
+  versionFromID?: string
+  versionFromOptions: CompareOption[]
+}
+
+type CLEAR = {
+  required: boolean
+  type: 'CLEAR'
+}
+
+type ADD = {
+  collection: SanitizedCollectionConfig
+  data: PaginatedDocs<any>
+  type: 'ADD'
+}
+
+export type Action = ADD | CLEAR
+
+export type ValueWithRelation = {
+  relationTo: string
+  value: string
+}
