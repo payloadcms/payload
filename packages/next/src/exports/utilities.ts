33458ce5351383ec27fbe31930e281fcecf6d5fe
Jake Fletcher <jacobsfletch@gmail.com>
2026-06-02 17:03:18 -0400

    refactor(next)!: drop getHierarchyAncestry re-export, consumers import from @payloadcms/ui

--- packages/next/src/exports/utilities.ts
+++ packages/next/src/exports/utilities.ts
@@ -1,10 +1,4 @@
 // NOTICE: Server-only utilities, do not import anything client-side here.
 
-export { getHierarchyAncestry } from '@payloadcms/ui/utilities/getHierarchyAncestry'
-export type {
-  GetHierarchyAncestryArgs,
-  HierarchyAncestryItem,
-  HierarchyAncestryResult,
-} from '@payloadcms/ui/utilities/getHierarchyAncestry'
 export { getNextRequestI18n } from '../utilities/getNextRequestI18n.js'
 export { getPayloadHMR } from '../utilities/getPayloadHMR.js'
