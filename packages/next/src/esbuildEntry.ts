e84344b3efd91ddef653769305e91a567a28be88
Jake Fletcher <jacobsfletch@gmail.com>
2026-06-02 16:38:34 -0400

    fix(next): import ui app.scss in esbuildEntry so prebuilt prod CSS includes the full design-token chain

--- packages/next/src/esbuildEntry.ts
+++ packages/next/src/esbuildEntry.ts
@@ -1,3 +1,5 @@
+import '@payloadcms/ui/scss/app.scss'
+
 export { RootLayout } from './adapters/layout.js'
 export { DashboardView } from './views/Dashboard/index.js'
 export { LoginView } from './views/Login/index.js'
