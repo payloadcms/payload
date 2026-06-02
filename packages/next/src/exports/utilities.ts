73747ada72244166c21d2277d7a2f6b414588185
Jake Fletcher <jacobsfletch@gmail.com>
2026-06-02 16:50:54 -0400

    refactor(ui,next): move getHierarchyAncestry to ui, drop unused timestamp

--- packages/next/src/exports/utilities.ts
+++ packages/next/src/exports/utilities.ts
@@ -1,10 +1,10 @@
 // NOTICE: Server-only utilities, do not import anything client-side here.
 
-export { getHierarchyAncestry } from '../utilities/getHierarchyAncestry.js'
+export { getHierarchyAncestry } from '@payloadcms/ui/utilities/getHierarchyAncestry'
 export type {
   GetHierarchyAncestryArgs,
   HierarchyAncestryItem,
   HierarchyAncestryResult,
-} from '../utilities/getHierarchyAncestry.js'
+} from '@payloadcms/ui/utilities/getHierarchyAncestry'
 export { getNextRequestI18n } from '../utilities/getNextRequestI18n.js'
 export { getPayloadHMR } from '../utilities/getPayloadHMR.js'
