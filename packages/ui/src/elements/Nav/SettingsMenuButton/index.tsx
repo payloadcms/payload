c6461000511720c2f8bbcf434adaa16e645e79b7
Jessica Rynkar <jrynkar@figma.com>
2026-06-04 17:06:05 +0100

    chore(ui): revert settingsMenuButton conversion

--- packages/ui/src/elements/Nav/SettingsMenuButton/index.tsx
+++ packages/ui/src/elements/Nav/SettingsMenuButton/index.tsx
@@ -4,7 +4,7 @@ import React, { Fragment } from 'react'
 import { GearIcon } from '../../../icons/Gear/index.js'
 import { useTranslation } from '../../../providers/Translation/index.js'
 import { Popup } from '../../Popup/index.js'
-import './index.css'
+import './index.scss'
 
 const baseClass = 'settings-menu-button'
 
