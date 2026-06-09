82a61298525254ffae5b41efde5838e541e522e5
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-06-09 14:42:38 -0400

    tweak date timezone styles

--- packages/ui/src/elements/DatePicker/index.tsx
+++ packages/ui/src/elements/DatePicker/index.tsx
@@ -8,7 +8,7 @@ import { ShimmerEffect } from '../ShimmerEffect/index.js'
 const DatePicker = lazy(() => import('./DatePicker.js'))
 
 export const DatePickerField: React.FC<Props> = (props) => (
-  <Suspense fallback={<ShimmerEffect height={50} />}>
+  <Suspense fallback={<ShimmerEffect height="var(--field-min-height)" />}>
     <DatePicker {...props} />
   </Suspense>
 )
