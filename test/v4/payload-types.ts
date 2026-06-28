ed34b5aabfd8f5a5bc22bc6932fa8b1dfeb9bfac
Alessio Gravili <github@gravili.net>
2026-06-28 01:14:10 -0700

    generated types

--- test/v4/payload-types.ts
+++ test/v4/payload-types.ts
@@ -4122,6 +4122,6 @@ export interface SerializedTableCellNode<TChildren> extends SerializedLexicalEle
 
 
 declare module 'payload' {
-  // @ts-ignore
+  // @ts-ignore 
   export interface GeneratedTypes extends Config {}
 }
