3a36ee6efc756b3a015185842e0d9603239520f5
Sasha Rakhmatulin <sasha@ritsuko.dev>
2026-07-02 15:09:41 +0100

    restore envPaths

--- packages/payload/src/utilities/telemetry/conf/envPaths.ts
+++ packages/payload/src/utilities/telemetry/conf/envPaths.ts
@@ -17,6 +17,50 @@ import os from 'node:os'
 import path from 'node:path'
 import process from 'node:process'
 
+const homedir = os.homedir()
+const tmpdir = os.tmpdir()
+const { env } = process
+
+const macos = (name: string) => {
+  const library = path.join(homedir, 'Library')
+
+  return {
+    cache: path.join(library, 'Caches', name),
+    config: path.join(library, 'Preferences', name),
+    data: path.join(library, 'Application Support', name),
+    log: path.join(library, 'Logs', name),
+    temp: path.join(tmpdir, name),
+  }
+}
+
+const windows = (name: string) => {
+  const appData = env.APPDATA || path.join(homedir, 'AppData', 'Roaming')
+  const localAppData = env.LOCALAPPDATA || path.join(homedir, 'AppData', 'Local')
+
+  return {
+    // Data/config/cache/log are invented by me as Windows isn't opinionated about this
+    cache: path.join(localAppData, name, 'Cache'),
+    config: path.join(appData, name, 'Config'),
+    data: path.join(localAppData, name, 'Data'),
+    log: path.join(localAppData, name, 'Log'),
+    temp: path.join(tmpdir, name),
+  }
+}
+
+// https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+const linux = (name: string) => {
+  const username = path.basename(homedir)
+
+  return {
+    cache: path.join(env.XDG_CACHE_HOME || path.join(homedir, '.cache'), name),
+    config: path.join(env.XDG_CONFIG_HOME || path.join(homedir, '.config'), name),
+    data: path.join(env.XDG_DATA_HOME || path.join(homedir, '.local', 'share'), name),
+    // https://wiki.debian.org/XDGBaseDirectorySpecification#state
+    log: path.join(env.XDG_STATE_HOME || path.join(homedir, '.local', 'state'), name),
+    temp: path.join(tmpdir, username, name),
+  }
+}
+
 export function envPaths(name: string, { suffix = 'nodejs' } = {}) {
   if (typeof name !== 'string') {
     throw new TypeError(`Expected a string, got ${typeof name}`)
@@ -27,42 +71,13 @@ export function envPaths(name: string, { suffix = 'nodejs' } = {}) {
     name += `-${suffix}`
   }
 
-  const homedir = os.homedir()
-  const tmpdir = os.tmpdir()
-  const { env } = process
-
   if (process.platform === 'darwin') {
-    const library = path.join(homedir, 'Library')
-    return {
-      cache: path.join(library, 'Caches', name),
-      config: path.join(library, 'Preferences', name),
-      data: path.join(library, 'Application Support', name),
-      log: path.join(library, 'Logs', name),
-      temp: path.join(tmpdir, name),
-    }
+    return macos(name)
   }
 
   if (process.platform === 'win32') {
-    const appData = env.APPDATA || path.join(homedir, 'AppData', 'Roaming')
-    const localAppData = env.LOCALAPPDATA || path.join(homedir, 'AppData', 'Local')
-    return {
-      // Data/config/cache/log are invented by me as Windows isn't opinionated about this
-      cache: path.join(localAppData, name, 'Cache'),
-      config: path.join(appData, name, 'Config'),
-      data: path.join(localAppData, name, 'Data'),
-      log: path.join(localAppData, name, 'Log'),
-      temp: path.join(tmpdir, name),
-    }
+    return windows(name)
   }
 
-  // https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-  const username = path.basename(homedir)
-  return {
-    cache: path.join(env.XDG_CACHE_HOME || path.join(homedir, '.cache'), name),
-    config: path.join(env.XDG_CONFIG_HOME || path.join(homedir, '.config'), name),
-    data: path.join(env.XDG_DATA_HOME || path.join(homedir, '.local', 'share'), name),
-    // https://wiki.debian.org/XDGBaseDirectorySpecification#state
-    log: path.join(env.XDG_STATE_HOME || path.join(homedir, '.local', 'state'), name),
-    temp: path.join(tmpdir, username, name),
-  }
+  return linux(name)
 }
