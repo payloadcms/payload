37eb4fcdcfb4c0cfe13131ea6462c3fbe031a4a7
Alessio Gravili <github@gravili.net>
2026-07-23 00:11:21 -0700

    feat: add jobs processing lease migrations

--- packages/db-postgres/src/exports/migration-utils.ts
+++ packages/db-postgres/src/exports/migration-utils.ts
@@ -1,3 +1,7 @@
 export { migrateLocalizeStatus } from '../predefinedMigrations/migrateLocalizeStatus.js'
 export { getBlocksToJsonMigrator } from '@payloadcms/drizzle'
-export { migratePostgresLocalizeStatus, migratePostgresV2toV3 } from '@payloadcms/drizzle/postgres'
+export {
+  migratePostgresJobsProcessingLease as migrateJobsProcessingLease,
+  migratePostgresLocalizeStatus,
+  migratePostgresV2toV3,
+} from '@payloadcms/drizzle/postgres'
