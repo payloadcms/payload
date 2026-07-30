d1cc1d3b31b6eb47d1082ec562bf3eb5d0bf0965
Alessio Gravili <github@gravili.net>
2026-07-30 16:54:55 -0700

    simplify

--- packages/richtext-lexical/src/utilities/getDefaultSanitizedEditorConfig.ts
+++ packages/richtext-lexical/src/utilities/getDefaultSanitizedEditorConfig.ts
@@ -4,32 +4,17 @@ import { type SanitizedServerEditorConfig } from '../index.js'
 import { defaultEditorConfig } from '../lexical/config/server/default.js'
 import { sanitizeServerEditorConfig } from '../lexical/config/server/sanitize.js'
 
-let cachedDefaultSanitizedServerEditorConfig: null | SanitizedServerEditorConfig = (global as any)
-  ._payload_lexical_defaultSanitizedServerEditorConfig
+const lexicalGlobal = globalThis as {
+  _payload_lexical_defaultSanitizedServerEditorConfig?: SanitizedServerEditorConfig
+} & typeof globalThis
 
-if (!cachedDefaultSanitizedServerEditorConfig) {
-  cachedDefaultSanitizedServerEditorConfig = (
-    global as any
-  )._payload_lexical_defaultSanitizedServerEditorConfig = null
-}
-
-export const getDefaultSanitizedEditorConfig = (args: {
+export const getDefaultSanitizedEditorConfig = ({
+  config,
+  parentIsLocalized,
+}: {
   config: SanitizedConfig
   parentIsLocalized: boolean
 }): SanitizedServerEditorConfig => {
-  const { config, parentIsLocalized } = args
-
-  if (cachedDefaultSanitizedServerEditorConfig) {
-    return cachedDefaultSanitizedServerEditorConfig
-  }
-
-  cachedDefaultSanitizedServerEditorConfig = sanitizeServerEditorConfig(
-    defaultEditorConfig,
-    config,
-    parentIsLocalized,
-  )
-  ;(global as any).payload_lexical_defaultSanitizedServerEditorConfig =
-    cachedDefaultSanitizedServerEditorConfig
-
-  return cachedDefaultSanitizedServerEditorConfig
+  return (lexicalGlobal._payload_lexical_defaultSanitizedServerEditorConfig ??=
+    sanitizeServerEditorConfig(defaultEditorConfig, config, parentIsLocalized))
 }
