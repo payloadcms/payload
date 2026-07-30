b78a527711d8c1ae617419665c1baa0ee1d25079
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-07-30 15:44:48 -0400

    test(richtext-lexical): minimize link feature e2e diff

--- test/lexical/collections/LexicalLinkFeature/e2e.spec.ts
+++ test/lexical/collections/LexicalLinkFeature/e2e.spec.ts
@@ -1,18 +1,18 @@
 import { expect, test } from '@playwright/test'
+import { lexicalLinkFeatureSlug } from 'lexical/slugs.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
-import type { Config } from '../../payload-types.js'
 
 import { ensureCompilationIsDone, waitForFormReady } from '../../../__helpers/e2e/helpers.js'
 import { AdminUrlUtil } from '../../../__helpers/shared/adminUrlUtil.js'
 import { initPayloadE2ENoConfig } from '../../../__helpers/shared/initPayloadE2ENoConfig.js'
 import { TEST_TIMEOUT_LONG } from '../../../playwright.config.js'
-import { lexicalLinkFeatureSlug } from '../../slugs.js'
 import { LexicalHelpers } from '../utils.js'
 const filename = fileURLToPath(import.meta.url)
 const currentFolder = path.dirname(filename)
 const dirname = path.resolve(currentFolder, '../../')
+
+const { beforeAll, beforeEach, describe } = test
 
 // Unlike the other suites, this one runs in parallel, as they run on the `lexical-fully-featured/create` URL and are "pure" tests
 // PLEASE do not reset the database or perform any operations that modify it in this file.
@@ -20,17 +20,18 @@ const dirname = path.resolve(currentFolder, '../../')
 // it runs multiple times in parallel, for each single test, which causes the tests to fail occasionally in CI.
 // test.describe.configure({ mode: 'parallel' })
 
-const initResult = await initPayloadE2ENoConfig<Config>({ dirname })
-const { serverURL } = initResult
+const { serverURL } = await initPayloadE2ENoConfig({
+  dirname,
+})
 
-test.describe('Lexical Link Feature', () => {
-  test.beforeAll(async ({ browser }, testInfo) => {
+describe('Lexical Link Feature', () => {
+  beforeAll(async ({ browser }, testInfo) => {
     testInfo.setTimeout(TEST_TIMEOUT_LONG)
     process.env.SEED_IN_CONFIG_ONINIT = 'false' // Makes it so the payload config onInit seed is not run. Otherwise, the seed would be run unnecessarily twice for the initial test run - once for beforeEach and once for onInit
 
     await ensureCompilationIsDone({ browser, serverURL })
   })
-  test.beforeEach(async ({ page }) => {
+  beforeEach(async ({ page }) => {
     const url = new AdminUrlUtil(serverURL, lexicalLinkFeatureSlug)
     const lexical = new LexicalHelpers(page)
     await page.goto(url.create)
