0a91355301283bbb99f8311bf80c3de87f062d7a
Kurt Tomlinson <kurttomlinson@gmail.com>
2026-08-07 12:20:06 -0400

    revert quotation mark changes

--- packages/create-payload-app/src/lib/generate-secret.ts
+++ packages/create-payload-app/src/lib/generate-secret.ts
@@ -1,5 +1,5 @@
-import { randomBytes } from "crypto"
+import { randomBytes } from 'crypto'
 
 export function generateSecret(): string {
-  return randomBytes(32).toString("hex")
+  return randomBytes(32).toString('hex')
 }
