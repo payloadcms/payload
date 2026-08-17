1237994e0ce3bebd0c8681838344318669f9c81a
Jake Fletcher <jacobsfletch@gmail.com>
2026-08-17 16:38:40 -0400

    refactor(ui): organize server export barrels

--- packages/ui/src/exports/internal-server.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @internal
- * Modules exported here are not part of the public API and may change without notice or a major version bump.
- */
-
-/** @internal */
-export type { CollectionCardsData } from '../widgets/CollectionCards/getCollectionCardsData.js'
-/** @internal */
-export { getCollectionCardsData } from '../widgets/CollectionCards/getCollectionCardsData.js'
---
+++ packages/ui/src/exports/server/internal.ts
@@ -0,0 +1,9 @@
+/**
+ * @internal
+ * Modules exported here are not part of the public API and may change without notice or a major version bump.
+ */
+
+/** @internal */
+export type { CollectionCardsData } from '../../widgets/CollectionCards/getCollectionCardsData.js'
+/** @internal */
+export { getCollectionCardsData } from '../../widgets/CollectionCards/getCollectionCardsData.js'
