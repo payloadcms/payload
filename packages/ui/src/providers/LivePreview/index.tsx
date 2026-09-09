455e5b97f14e6c21fcd99241d2d467e3b92066e1
Jarrod Flesch <30633324+JarrodMFlesch@users.noreply.github.com>
2026-09-09 09:35:18 -0400

    adjusts function names and formatAbsoluteURL structure

--- packages/ui/src/providers/LivePreview/index.tsx
+++ packages/ui/src/providers/LivePreview/index.tsx
@@ -10,7 +10,7 @@ import { usePopupWindow } from '../../hooks/usePopupWindow.js'
 import { useDocumentInfo } from '../../providers/DocumentInfo/index.js'
 import { usePreferences } from '../../providers/Preferences/index.js'
 import { formatAbsoluteURL } from '../../utilities/formatAbsoluteURL.js'
-import { isAllowedURL } from '../../utilities/isAllowedURL.js'
+import { isHttpURL } from '../../utilities/isHttpURL.js'
 import { customCollisionDetection } from './collisionDetection.js'
 import { LivePreviewContext } from './context.js'
 import { sizeReducer } from './sizeReducer.js'
@@ -83,7 +83,7 @@ export const LivePreviewProvider: React.FC<LivePreviewProviderProps> = ({
   const [previewURL, _setPreviewURL] = useState<string>()
 
   const setPreviewURL = useCallback<LivePreviewContextType['setPreviewURL']>((incomingURL) => {
-    _setPreviewURL(isAllowedURL(incomingURL) ? incomingURL : undefined)
+    _setPreviewURL(isHttpURL(incomingURL) ? incomingURL : undefined)
   }, [])
 
   useEffect(() => {
