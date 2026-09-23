529a37933943763b7082932d24b43191080449c1
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-09-23 12:48:53 -0400

    test: expect hidden password control in trash

--- test/trash/e2e.spec.ts
+++ test/trash/e2e.spec.ts
@@ -1147,7 +1147,7 @@ describe('Trash', () => {
       await expect(page).toHaveURL(/\/users\/trash\/[a-f0-9]{24}/)
 
       await expect(page.locator('input[name="email"]')).toBeDisabled()
-      await expect(page.locator('#change-password')).toBeDisabled()
+      await expect(page.locator('#change-password')).toHaveCount(0)
 
       await expect(page.locator('#field-name')).toBeDisabled()
       await expect(page.locator('#field-roles .rs__input')).toBeDisabled()
