62da42690a315260ee6138ed0f309a1ff8a06dbb
Bar M <32390017+galshir@users.noreply.github.com>
2026-09-24 17:57:33 +0300

    fix(plugin-ecommerce): honour inventory.fieldName in inventoryField

--- packages/plugin-ecommerce/src/fields/inventoryField.ts
+++ packages/plugin-ecommerce/src/fields/inventoryField.ts
@@ -1,15 +1,21 @@
 import type { NumberField } from 'payload'
 
+import { defaultInventoryFieldName } from '../utilities/inventory.js'
+
 type Props = {
+  /**
+   * The name of the field used to track inventory levels. Defaults to 'inventory'.
+   */
+  fieldName?: string
   overrides?: Partial<NumberField>
 }
 
 export const inventoryField: (props?: Props) => NumberField = (props) => {
-  const { overrides } = props || {}
+  const { fieldName = defaultInventoryFieldName, overrides } = props || {}
 
   // @ts-expect-error - issue with payload types
   const field: NumberField = {
-    name: 'inventory',
+    name: fieldName,
     type: 'number',
     defaultValue: 0,
     // @ts-expect-error - translations are not typed in plugins yet
