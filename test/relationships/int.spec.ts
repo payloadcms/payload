449f580de7a31ce4be4f8f33fa851939641c2fc8
Helen Li <helen.li@figma.com>
2026-09-24 13:38:16 -0700

    test: cover select fields in relationship sorting

--- test/relationships/int.spec.ts
+++ test/relationships/int.spec.ts
@@ -1198,7 +1198,7 @@ test.suite('Relationships', { config: './config.ts' }, () => {
           const movie_1 = await payload.create({
             collection: 'movies',
             depth: 0,
-            data: { director: director_1.id, name: 'Some Movie 1' },
+            data: { director: director_1.id, name: 'Some Movie 1', select: [] },
             overrideAccess: true,
           })
 
@@ -1208,6 +1208,9 @@ test.suite('Relationships', { config: './config.ts' }, () => {
             data: { director: director_2.id, name: 'Some Movie 2' },
             overrideAccess: true,
           })
+
+          expect(movie_1.select).toStrictEqual([])
+          expect(movie_2.select).toBeNull()
 
           const res_1 = await payload.find({
             collection: 'movies',
