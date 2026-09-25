124b55a8747d9b45db0af30aad337569d37a9b3e
Jake Fletcher <jacobsfletch@gmail.com>
2026-09-25 12:23:58 -0400

    chore: change modification warning in generated app files (#18320)
    Replaces the misleading “DO NOT MODIFY” warning with “MODIFY AT YOUR OWN
    RISK” within autogenerated app files. These files are created during
    initial setup and are never rewritten by Payload.
    
    Removes the outdated docs sections that clarify this discrepancy.

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,5 +1,4 @@
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. MODIFY AT YOUR OWN RISK. */
 // import '@payloadcms/ui/styles.css' // Uncomment this line if `@payloadcms/ui` in `tsconfig.json` points to `/ui/dist` instead of `/ui/src`
 import type { ServerFunctionClient } from 'payload'
 
